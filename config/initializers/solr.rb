ee363c2c32def7e556fb8d6a7cb0fb261dbc818f
Ben Keith <ben.keith@quoininc.com>
2014-04-10 12:34:26 -0400

    making solr port envvar and rails config in sync

--- config/initializers/solr.rb
+++ config/initializers/solr.rb
@@ -1,11 +1,12 @@
 
+default_port = 8983
 path = ::Rails.root.join "config", "solr.yml"
 begin
   config = YAML::load(ERB.new(File.read(path)).result)
 rescue
-  Rails.logger.error 'Could not load solr yaml config!'
-  raise
+  Rails.logger.warn "Could not load solr yaml config! Using default solr port #{default_port}"
 end
 
-ENV['SOLR_PORT'] ||= "#{config['port']}"
-Sunspot.config.solr.url = "http://localhost:#{ config['port'] || "8983" }/solr"
+config['port'] ||= default_port
+ENV['SOLR_PORT'] ||= config['port'].to_s
+Sunspot.config.solr.url = "http://localhost:#{ config['port'] }/solr"
