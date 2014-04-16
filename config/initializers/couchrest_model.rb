6641aba2dd9ecd37ee6bebddef04e3b2bf0d26a4
Chavez, Carlos <carlos.chavez@quoininc.com>
2014-04-16 11:36:37 -0600

    Patch embedded object to track down if they exists or not in the database and not rely on the new? of the parent object.

--- config/initializers/couchrest_model.rb
+++ config/initializers/couchrest_model.rb
@@ -20,6 +20,60 @@ module CouchRest
         set_a_value key, value
       end
       #TODO: what about overriding the delete method?
+
+      #flag as saved CastedArray and CastedHash fields.
+      after_save do
+        flag_saved_embedded_properties
+      end
+
+      # Instantiate a new CouchRest::Model::Base by preparing all properties
+      # using the provided document hash.
+      #
+      # Options supported:
+      #
+      # * :directly_set_attributes, true when data comes directly from database
+      # * :database, provide an alternative database
+      #
+      # If a block is provided the new model will be passed into the
+      # block so that it can be populated.
+      def initialize(attributes = {}, options = {})
+        super()
+        prepare_all_attributes(attributes, options)
+        # set the instance's database, if provided
+        self.database = options[:database] unless options[:database].nil?
+        unless self['_id'] && self['_rev']
+          self[self.model_type_key] = self.class.model_type_value
+        end
+
+        #:directly_set_attributes is set to true when the object is built from the database.
+        #flag as saved CastedArray and CastedHash fields.
+        if options[:directly_set_attributes]
+          flag_saved_embedded_properties
+        end
+
+        yield self if block_given?
+
+        after_initialize if respond_to?(:after_initialize)
+        run_callbacks(:initialize) { self }
+      end
+
+      private
+
+      #Flag saved CastedBy fields (:document_saved to true) in order to be aware
+      #that items were saved or they were loaded from the database.
+      def flag_saved_embedded_properties
+        casted_properties = self.properties_with_values.select { |property, value| value.respond_to?(:casted_by) && value.respond_to?(:casted_by_property) }
+        casted_properties.each do |property, value|
+          if value.instance_of?(CouchRest::Model::CastedArray)
+            value.each do |item|
+              item.document_saved = true if item.respond_to?(:document_saved)
+            end
+          elsif value.instance_of?(CouchRest::Model::CastedHash) && value.respond_to?(:document_saved)
+            value.document_saved = true
+          end
+        end
+      end
+
     end
   end
 end
@@ -27,6 +81,20 @@ end
 module CouchRest
   module Model
     module Embeddable
+      #track down whether the instance is new or not.
+      #CouchRest::Model::Embeddable new? method rely
+      #on the new? of the parent object. This make not
+      #possible to know if the embedded object already
+      #exists or not in the database.
+      #document_saved nil or false consider the field as new.
+      attr_accessor :document_saved
+
+      #Override new? method to not rely on the new? of the parent object.
+      def new?
+        !@document_saved
+      end
+      alias :new_record? :new?
+
       #couchrest 0.34
       #CouchRest::CastedModel did not provide "to_key" method.
       #
