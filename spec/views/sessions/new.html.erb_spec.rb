45ab84cfad24f7177dcbe7c3211d0b81f84a0aef
Ron Elledge <ronald.elledge@quoininc.com>
2014-04-21 14:50:24 -0400

    commented out specs related to removed functionality in PRIMERO-72

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
-describe "/sessions/new.html.erb" do
-  it "should have a 'Request Password Reset' link" do
-    render
-    rendered.should have_tag("a")
-  end
-end
+# PRIMERO-72
+# Removed Request Password Reset link as part of demo cleanup
+# TODO add test back when functionality is added back
+# describe "/sessions/new.html.erb" do
+  # it "should have a 'Request Password Reset' link" do
+    # render
+    # rendered.should have_tag("a")
+  # end
+# end
