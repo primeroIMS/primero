9c00cdef6d307c0e63fef30bce35b57f6b145772
Chavez, Carlos <carlos.chavez@quoininc.com>
2014-04-24 17:20:05 -0600

    Minor change, rename variable.

--- config/initializers/solr.rb
+++ config/initializers/solr.rb
@@ -1,7 +1,7 @@
 path = ::Rails.root.join "config", "solr.yml"
 begin
-  SUNSPOT_CONFIG = YAML::load(ERB.new(File.read(path)).result)[Rails.env]
-  Sunspot.config.solr.url = SUNSPOT_CONFIG['url']
+  SOLR_CONFIG = YAML::load(ERB.new(File.read(path)).result)[Rails.env]
+  Sunspot.config.solr.url = SOLR_CONFIG['url']
 rescue => e
   Rails.logger.error "Could not load solr.yml configuration file.\n#{e}"
 end
