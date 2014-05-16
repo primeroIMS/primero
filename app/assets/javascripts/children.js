ca9cb5aaa6e0ad8d33b72db601fded7e5710c7a7
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2014-05-16 11:35:21 -0400

    Created a remove button for subforms.

--- app/assets/javascripts/children.js
+++ app/assets/javascripts/children.js
@@ -5,21 +5,25 @@ $(document).ready(function() {
 
 
 function addSubform(event){
-
   //grab the correct template
   var template = $(event.srcElement).parent().prev();
-
 
   //figure out the subforms
   var subforms = template.prev();
 
   //figure out the next i
-  var i = subforms.children().size();
+  var i = 0;
+  if (subforms.children().size() > 0){
+    i = parseInt(subforms.children(":last").attr("id").split("_").pop()) + 1;
+  }
 
   //clone the template
   var newSubform = template.clone();
 
   //replace template values, make it visible
+  var subformId = newSubform.attr("id").replace("template",i);
+  newSubform.attr("id", subformId);
+
   var fieldsetId = newSubform.find("fieldset").attr("id").replace("template",i);
   newSubform.find("fieldset").attr("id", fieldsetId);
 
@@ -37,8 +41,13 @@ function addSubform(event){
 
   var newSubformClass = newSubform.attr("class").replace("template", "");
   newSubform.attr("class", newSubformClass);
-  newSubform.attr("style","");
+  newSubform.removeAttr("style");
+  newSubform.find("input").removeAttr("disabled");
 
   newSubform.appendTo(subforms);
+}
 
+function removeSubform(event){
+  subform = $(event.srcElement).parent();
+  subform.remove();
 }
--- spec/controllers/children_controller_spec.rb
+++ spec/controllers/children_controller_spec.rb
@@ -31,6 +31,8 @@ describe ChildrenController do
     response.should be_success
   end
 
+
+
   describe '#authorizations' do
     describe 'collection' do
       it "GET index" do
@@ -739,5 +741,26 @@ describe ChildrenController do
       updated_child.first.name.should == 'new name'
     end
   end
+
+	describe "reindex_params_subforms" do
+
+		it "should correct indexing for nested subforms" do
+			params = {
+				"child"=> {
+					"name"=>"",
+	   		 "top_1"=>"This is a top value",
+	        "nested_form_section" => {
+						"0"=>{"nested_1"=>"Keep", "nested_2"=>"Keep", "nested_3"=>"Keep"},
+	     		 "2"=>{"nested_1"=>"Drop", "nested_2"=>"Drop", "nested_3"=>"Drop"}},
+	        "fathers_name"=>""}}
+
+			controller.reindex_params_subforms params
+			expected_subform = params["child"]["nested_form_section"]["1"]
+
+			expect(expected_subform.present?).to be_true
+			expect(expected_subform).to eq({"nested_1"=>"Drop", "nested_2"=>"Drop", "nested_3"=>"Drop"})
+		end
+
+	end
 
 end
