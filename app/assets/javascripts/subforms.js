33d4d847058038091a9ccbc4d33f9b22b4fa9e6c
Joshua Toliver <jtoliver@quoininc.com>
2014-05-29 10:52:08 -0400

    Most Primero-114 functional work

--- app/assets/javascripts/subforms.js
+++ app/assets/javascripts/subforms.js
@@ -1,53 +1,87 @@
-//Disable all template inputs
-$(document).ready(function() {
-  $('div.template').find('input').attr("disabled","disabled");
+var SubformView = Backbone.View.extend({
+  el: '.side-tab-content',
+
+  events: {
+    'click .subform_remove': 'remove',
+    'click .subform_add': 'add'
+  },
+
+  intialize: function() {
+    this.heading_removed = false
+  },
+
+  remove_first_heading: function() {
+    if (this.heading_removed == false) {
+      console.log('here')
+      $('h5.tool-tip-label:first').remove()
+      this.heading_removed = true
+    }
+  },
+
+  add: function(event) {
+    event.preventDefault();
+
+    // removing very first heading
+    this.remove_first_heading();
+
+    //grab the correct template
+    var template = $(event.srcElement).parent().prev();
+
+    //figure out the subforms
+    var subforms = template.prev();
+
+    //figure out the next i
+    var i = 0;
+    if (subforms.children().size() > 0){
+      i = parseInt(subforms.children(":last").attr("id").split("_").pop()) + 1;
+    }
+
+    //clone the template
+    var newSubform = template.clone();
+
+    //replace template values, make it visible
+    var subformId = newSubform.attr("id").replace("template",i);
+    newSubform.attr("id", subformId);
+
+    var fieldsetId = newSubform.find("fieldset").attr("id").replace("template",i);
+    newSubform.find("fieldset").attr("id", fieldsetId);
+
+    newSubform.find("label.key").each(function(x, el){
+      var for_attr = el.getAttribute("for").replace("template",i);
+      el.setAttribute("for", for_attr);
+    });
+
+    newSubform.find("input").each(function(x, el){
+      var id = el.getAttribute("id").replace("template",i);
+      el.setAttribute("id", id);
+      var name = el.getAttribute("name").replace("template",i);
+      el.setAttribute("name", name);
+    });
+
+    var newSubformClass = newSubform.attr("class").replace("template", "");
+    newSubform.attr("class", newSubformClass);
+//    newSubform.removeAttr("style");
+    newSubform.fadeIn(600);
+    newSubform.find("input").removeAttr("disabled");
+
+    newSubform.appendTo(subforms);
+  },
+
+  remove: function(event) {
+    event.preventDefault();
+    var message = $(event.target).data('message'),
+        confirm_remove = confirm(message);
+
+    if (confirm_remove == true) {
+      var subform = $(event.srcElement).parent();
+      subform.fadeOut(600).remove();
+    }
+  }
 });
 
+$(document).ready(function() {
+  //Disable all template inputs
+  $('div.template').find('input').attr("disabled","disabled");
 
-function addSubform(event){
-  //grab the correct template
-  var template = $(event.srcElement).parent().prev();
-
-  //figure out the subforms
-  var subforms = template.prev();
-
-  //figure out the next i
-  var i = 0;
-  if (subforms.children().size() > 0){
-    i = parseInt(subforms.children(":last").attr("id").split("_").pop()) + 1;
-  }
-
-  //clone the template
-  var newSubform = template.clone();
-
-  //replace template values, make it visible
-  var subformId = newSubform.attr("id").replace("template",i);
-  newSubform.attr("id", subformId);
-
-  var fieldsetId = newSubform.find("fieldset").attr("id").replace("template",i);
-  newSubform.find("fieldset").attr("id", fieldsetId);
-
-  newSubform.find("label.key").each(function(x, el){
-    var for_attr = el.getAttribute("for").replace("template",i);
-    el.setAttribute("for", for_attr);
-  });
-
-  newSubform.find("input").each(function(x, el){
-    var id = el.getAttribute("id").replace("template",i);
-    el.setAttribute("id", id);
-    var name = el.getAttribute("name").replace("template",i);
-    el.setAttribute("name", name);
-  });
-
-  var newSubformClass = newSubform.attr("class").replace("template", "");
-  newSubform.attr("class", newSubformClass);
-  newSubform.removeAttr("style");
-  newSubform.find("input").removeAttr("disabled");
-
-  newSubform.appendTo(subforms);
-}
-
-function removeSubform(event){
-  subform = $(event.srcElement).parent();
-  subform.remove();
-}
+  var subform = new SubformView();
+});
--- db/forms/tracing.rb
+++ db/forms/tracing.rb
@@ -1,3 +1,79 @@
+tracing_sub_form = [
+    Field.new({"name" => "date_tracing",
+               "type" => "date_field",
+               "display_name_all" => "Date of tracing",
+              }),
+    Field.new({"name" => "tracing_type",
+               "type" => "text_field",
+               "display_name_all" => "Type of action taken",
+              }),
+    Field.new({"name" => "address_tracing",
+               "type" => "text_field",
+               "display_name_all" => "Address/Village where the tracing action took place",
+              }),
+    Field.new({"name" => "location_tracing",
+               "type" => "text_field",
+               "display_name_all" => "Place of tracing",
+              }),
+    Field.new({"name" => "tracing_action_description",
+               "type" => "text_field",
+               "display_name_all" => "Action taken and remarks",
+              }),
+    Field.new({"name" => "tracing_outcome",
+               "type" => "select_box",
+               "display_name_all" => "Outcome of tracing action",
+               "option_strings_text_all" => "Pending\nSuccessful\nUnsuccessful\nYes",
+              })
+]
+
+nested_form_section = FormSection.create!(
+    {"visible"=>false,
+     "is_nested"=>true,
+     :order=> 1, :unique_id=>"nested_form_section", "editable"=>true,
+     :fields => tracing_sub_form, :perm_enabled => true,
+     "name_all" => "Nested Fields",
+     "description_all" => "Some test nested fields testing."
+    }
+)
+
+tracing_sub_form_2 = [
+    Field.new({"name" => "date_tracing2",
+               "type" => "date_field",
+               "display_name_all" => "Date of tracing 2",
+              }),
+    Field.new({"name" => "tracing_type2",
+               "type" => "text_field",
+               "display_name_all" => "Type of action taken 2",
+              }),
+    Field.new({"name" => "address_tracing2",
+               "type" => "text_field",
+               "display_name_all" => "Address/Village where the tracing action took place 2",
+              }),
+    Field.new({"name" => "location_tracing2",
+               "type" => "text_field",
+               "display_name_all" => "Place of tracing 2",
+              }),
+    Field.new({"name" => "tracing_action_description2",
+               "type" => "text_field",
+               "display_name_all" => "Action taken and remarks 2",
+              }),
+    Field.new({"name" => "tracing_outcome2",
+               "type" => "select_box",
+               "display_name_all" => "Outcome of tracing action 2",
+               "option_strings_text_all" => "Pending\nSuccessful\nUnsuccessful\nYes",
+              })
+]
+
+nested_form_section_2 = FormSection.create!(
+    {"visible"=>false,
+     "is_nested"=>true,
+     :order=> 1, :unique_id=>"nested_form_section_2", "editable"=>true,
+     :fields => tracing_sub_form_2, :perm_enabled => true,
+     "name_all" => "Nested Fields 2",
+     "description_all" => "Some test nested fields testing."
+    }
+)
+
 tracing_fields = [
   Field.new({"name" => "ftr_status",
              "type" => "select_box",
@@ -84,6 +160,16 @@ tracing_fields = [
              "type" => "date_field",
              "display_name_all" => "Arrival Date",
             }),
+  Field.new({"name" => "nested_form_section",
+             "type" => "subform", "editable" => true,
+             "subform_section_id" => nested_form_section.id,
+             "display_name_all" => "Top 2 Subform"
+            }),
+  Field.new({"name" => "nested_form_section_2",
+             "type" => "subform", "editable" => true,
+             "subform_section_id" => nested_form_section_2.id,
+             "display_name_all" => "Top 2 Subform 2"
+            })
 ]
 
 FormSection.create_or_update_form_section({
