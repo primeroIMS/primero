16528817cb60ca518edbae34d8ecc8893c82f85b
Chavez, Carlos <carlos.chavez@quoininc.com>
2014-06-19 16:12:55 -0600

    Primero-97 Family Details Form (nested): Added FormSection/Subform for Family Details. Added Cucumber Test Cases. Currently Chosen does not work for Subform Some Fields are regular select for now.

---
+++ capybara_features/features_primero/family_details_form.feature
@@ -0,0 +1,121 @@
+#JIRA PRIMERO-97
+
+@javascript @primero
+Feature: Family Details Form
+  As a Social worker, I want to enter the information related to the family details.
+
+  Scenario: As a logged in user, I should access the form section family details subform
+    Given I am logged in as an admin with username "primero" and password "primero"
+    When I access "form section page"
+    And I press the "Nested Family Details" button
+    Then I should see the following fields:
+      |Name|
+      |How are they related to the child?|
+      |Is this person the caregiver?|
+      |Did the child live with this person before separation?|
+      |Is the child in contact with this person?|
+      |Is the child separated from this person?|
+      |List any agency identifiers as a comma separated list|
+      |Nickname|
+      |Are they alive?|
+      |If dead, please provide details|
+      |Age|
+      |Date of Birth|
+      |Language|
+      |Religion|
+      |Ethnicity|
+      |Sub Ethnicity 1|
+      |Sub Ethnicity 2|
+      |Nationality|
+      |Comments|
+      |Occupation|
+      |Current Address|
+      |Is this a permanent location?|
+      |Current Location|
+      |Last Known Address|
+      |Last Known Location|
+      |Telephone|
+      |Other persons well known to the child|
+
+  Scenario: As a logged in user, I should access the form section family details
+    Given I am logged in as an admin with username "primero" and password "primero"
+    When I access "form section page"
+    And I press the "Family Details" button
+    Then I should see the following fields:
+      |Size of Family|
+      |Notes about Family|
+      |What is the child’s intended address?|
+      |Family Details|
+
+  Scenario: I create a case with family details information.
+    Given I am logged in as an admin with username "primero" and password "primero"
+    When I access "cases page"
+    And I press the "Create a New Case" button
+    And I press the "Family Details" button
+    And I fill in the following:
+      | Size of Family                        | 3                  |
+      | Notes about Family                    | Some Family Notes  |
+      | What is the child’s intended address? | Some Child Address |
+    #Added Family Details
+    And I fill in the 1st "Family Details Section" subform with the follow:
+      |Name                                                  | First Name Relation                    |
+      |How are they related to the child?                    | <Select> Mother                        |
+      |Is this person the caregiver?                         | <Select> Yes                           |
+      |Did the child live with this person before separation?| <Select> Yes                           |
+      |Is the child in contact with this person?             | <Select> Yes                           |
+      |Is the child separated from this person?              | <Select> Yes                           |
+      |List any agency identifiers as a comma separated list | Agency1 ,Agency 2                      |
+      |Nickname                                              | First Nickname Relation                |
+      |Are they alive?                                       | <Select> Alive                         |
+      |If dead, please provide details                       | No Dead Notes                          |
+      |Age                                                   | 5                                      |
+      |Date of Birth                                         | 21/May/2000                            |
+      |Language                                              | <Select> Language 1                    |
+      |Religion                                              | <Select> Religion 1                    |
+      |Ethnicity                                             | <Select> Ethnicity 1                   |
+      |Sub Ethnicity 1                                       | <Select> Sub Ethnicity 1               |
+      |Sub Ethnicity 2                                       | <Select> Sub Ethnicity 2               |
+      |Nationality                                           | <Select> Nationality 1                 |
+      |Comments                                              | Some Comments Relation                 |
+      |Occupation                                            | Some Ocupation Relation                |
+      |Current Address                                       | The Current Address Relation           |
+      |Is this a permanent location?                         | <Select> No                            |
+      |Current Location                                      | The Current Location                   |
+      |Last Known Address                                    | The Last Known Address                 |
+      |Last Known Location                                   | The Last Known Location                |
+      |Telephone                                             | The Telephone                          |
+      |Other persons well known to the child                 | Some Other Person Well Known the Child |
+    And I press "Save"
+    Then I should see "Case record successfully created" on the page
+    And I should see a value for "Size of Family" on the show page with the value of "3"
+    And I should see a value for "Notes about Family" on the show page with the value of "Some Family Notes"
+    And I should see a value for "What is the child’s intended address?" on the show page with the value of "Some Child Address"
+    #Verify values from the subform
+    And I should see "Father's Name" on the page
+    And I should see "First Name Relation" on the page
+    And I should see "Mother" on the page
+    And I should see "Yes" on the page
+    And I should see "Yes" on the page
+    And I should see "Yes" on the page
+    And I should see "Yes" on the page
+    And I should see "Agency1 ,Agency 2" on the page
+    And I should see "First Nickname Relation" on the page
+    And I should see "Alive" on the page
+    And I should see "No Dead Notes" on the page
+    And I should see "5" on the page
+    And I should see "21/May/2000" on the page
+    And I should see "Language 1" on the page
+    And I should see "Religion 1" on the page
+    And I should see "Ethnicity 1" on the page
+    And I should see "Sub Ethnicity 1" on the page
+    And I should see "Sub Ethnicity 2" on the page
+    And I should see "Nationality 1" on the page
+    And I should see "Some Comments Relation" on the page
+    And I should see "Some Ocupation Relation" on the page
+    And I should see "The Current Address Relation" on the page
+    And I should see "No" on the page
+    And I should see "The Current Location" on the page
+    And I should see "The Last Known Address" on the page
+    And I should see "The Last Known Location" on the page
+    And I should see "The Telephone" on the page
+    And I should see "Some Other Person Well Known the Child" on the page
---
+++ db/forms/family_details.rb
@@ -0,0 +1,188 @@
+family_details_fields_subform = [
+  Field.new({"name" => "relation_name",
+             "type" => "text_field",
+             "display_name_all" => "Name"
+           }),
+  Field.new({"name" => "relation",
+             "type" => "select_box",
+             "display_name_all" => "How are they related to the child?",
+             "option_strings_text_all" =>
+                                    ["Mother", 
+                                     "Father",
+                                     "Aunt", 
+                                     "Uncle",
+                                     "Grandmother",
+                                     "Grandfather",
+                                     "Brother", 
+                                     "Sister",
+                                     "Husband", 
+                                     "Wife",
+                                     "Partner",
+                                     "Other Family",
+                                     "Other Nonfamily"].join("\n")
+            }),
+  Field.new({"name" => "relation_is_caregiver",
+             "type" => "select_box",
+             "display_name_all" => "Is this person the caregiver?",
+             "option_strings_text_all" => "Yes\nNo"
+            }),
+  Field.new({"name" => "relation_child_lived_with_pre_separation",
+             "type" => "select_box",
+             "display_name_all" => "Did the child live with this person before separation?",
+             "option_strings_text_all" => "Yes\nNo"
+            }),
+  Field.new({"name" => "relation_child_is_in_contact",
+             "type" => "select_box",
+             "display_name_all" => "Is the child in contact with this person?",
+             "option_strings_text_all" => "Yes\nNo"
+            }),
+  Field.new({"name" => "relation_child_is_separated_from",
+             "type" => "select_box",
+             "display_name_all" => "Is the child separated from this person?",
+             "option_strings_text_all" => "Yes\nNo"
+            }),
+  Field.new({"name" => "relation_identifiers",
+             "type" => "text_field",
+             "display_name_all" => "List any agency identifiers as a comma separated list"
+           }),
+  Field.new({"name" => "relation_nickname",
+             "type" => "text_field",
+             "display_name_all" => "Nickname"
+           }),
+  Field.new({"name" => "relation_is_alive",
+             "type" => "select_box",
+             "display_name_all" => "Are they alive?",
+             "option_strings_text_all" => "Unknown\nAlive\nDead"
+            }),
+  Field.new({"name" => "relation_death_details",
+             "type" => "textarea",
+             "display_name_all" => "If dead, please provide details"
+           }),
+  Field.new({"name" => "relation_age",
+             "type" => "numeric_field",
+             "display_name_all" => "Age"
+           }),
+  Field.new({"name" => "relation_date_of_birth",
+             "type" => "date_field",
+             "display_name_all" => "Date of Birth"
+           }),
+  Field.new({"name" => "relation_language",
+             "type" => "select_box",
+             "display_name_all" => "Language",
+             #TODO: uncomment once the Chosen works for SubForms
+             #"multi_select" => true,
+             "option_strings_text_all" => "Language 1\nLanguage 2"
+           }),
+  Field.new({"name" => "relation_religion",
+             "type" => "select_box",
+             "display_name_all" => "Religion",
+             #TODO: uncomment once the Chosen works for SubForms
+             #"multi_select" => true,
+             "option_strings_text_all" => "Religion 1\nReligion 2"
+           }),
+  Field.new({"name" => "relation_ethnicity",
+             "type" => "select_box",
+             "display_name_all" => "Ethnicity",
+             "option_strings_text_all" => "Ethnicity 1\nEthnicity 2"
+           }),
+  Field.new({"name" => "relation_sub_ethnicity1",
+             "type" => "select_box",
+             "display_name_all" => "Sub Ethnicity 1",
+             "option_strings_text_all" => "Sub Ethnicity 1\nSub Ethnicity 2"
+           }),
+  Field.new({"name" => "relation_sub_ethnicity2",
+             "type" => "select_box",
+             "display_name_all" => "Sub Ethnicity 2",
+             "option_strings_text_all" => "Sub Ethnicity 1\nSub Ethnicity 2"
+           }),
+  Field.new({"name" => "relation_nationality",
+             "type" => "select_box",
+             "display_name_all" => "Nationality",
+             #"multi_select" => true,
+             "option_strings_text_all" => "Nationality 1\nNationality 2"
+           }),
+  Field.new({"name" => "relation_comments",
+             "type" => "textarea",
+             "display_name_all" => "Comments"
+           }),
+  Field.new({"name" => "relation_occupation",
+             "type" => "text_field",
+             "display_name_all" => "Occupation"
+           }),
+  Field.new({"name" => "relation_address_current",
+             "type" => "text_field",
+             "display_name_all" => "Current Address"
+           }),
+  Field.new({"name" => "relation_address_is_permanent",
+             "type" => "select_box",
+             "display_name_all" => "Is this a permanent location?",
+             "option_strings_text_all" => "Yes\nNo"
+            }),
+  Field.new({"name" => "relation_location_current",
+             "type" => "text_field",
+             "display_name_all" => "Current Location"
+           }),
+  Field.new({"name" => "relation_address_last",
+             "type" => "text_field",
+             "display_name_all" => "Last Known Address"
+           }),
+  Field.new({"name" => "relation_location_last",
+             "type" => "text_field",
+             "display_name_all" => "Last Known Location"
+           }),
+  Field.new({"name" => "relation_telephone",
+             "type" => "text_field",
+             "display_name_all" => "Telephone"
+           }),
+  Field.new({"name" => "relation_other_family",
+             "type" => "text_field",
+             "display_name_all" => "Other persons well known to the child"
+           })
+]
+
+family_details_section = FormSection.create_or_update_form_section({
+    "visible"=>false,
+    "is_nested"=>true,
+    :order=> 1,
+    :unique_id=>"family_details_section",
+    "editable"=>true,
+    :fields => family_details_fields_subform,
+    :perm_enabled => false,
+    :perm_visible => false,
+    "name_all" => "Nested Family Details",
+    "description_all" => "Family Details Subform"
+})
+
+family_details_fields = [
+  Field.new({"name" => "family_size",
+             "type" => "numeric_field",
+             "display_name_all" => "Size of Family"
+           }),
+  Field.new({"name" => "family_notes",
+             "type" => "textarea",
+             "display_name_all" => "Notes about Family"
+           }),
+  Field.new({"name" => "childs_intended_address",
+             "type" => "text_field",
+             "display_name_all" => "What is the child’s intended address?"
+           }),
+  ##Subform##
+  Field.new({"name" => "family_details_section",
+             "type" => "subform", 
+             "editable" => true,
+             "subform_section_id" => family_details_section.id,
+             "display_name_all" => "Family Details"
+            }),
+  ##Subform##
+]
+
+FormSection.create_or_update_form_section({
+  :unique_id => "family_details",
+  "visible" => true,
+  :order => 3,
+  "editable" => true,
+  :fields => family_details_fields,
+  :perm_enabled => true,
+  "name_all" => "Family Details",
+  "description_all" => "Family Details"
+})
