a8317c9bc269ad867b7cc5fc9347a27ed0d813b8
Ron Elledge <ronald.elledge@quoininc.com>
2014-07-03 11:54:23 -0400

    removed case cleanup script from db:seeds and put in a migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,64 +20,6 @@ def clean_db_table(table)
   myDb.delete! rescue nil
 end
 
-#PRIMERO-269
-# This fixes records that have dates in an invalid format
-# and if they do not have a "record_state", add one
-def fix_case_data
-  children = Child.all
-  children.each do |child|
-    puts "Scrubbing old record data for case #{child.short_id}..."
-    
-    recordModified = false 
-    
-    #Change / to - to fix old date formats    
-    child.each do |key, value|
-      if value.present? && is_a_date_field?(key)
-        unless is_date_valid_format?(value)
-          puts "#{key}: #{value} is an invalid date format... setting to a default 01-Jan-2000"
-          child[key] = "01-Jan-2000"
-          recordModified = true
-        end
-      end
-    end
-    
-    #Add a record_state to old records
-    unless child[:record_state].present?
-      child.merge! record_state: 'Valid record'
-      recordModified = true
-    end   
-    
-    if recordModified
-      if child.valid?
-        puts "Saving changes to case record..."
-        child.save!
-      else
-        puts "Case still not valid... not saving"
-      end
-    end
-  end
-end
-
-#PRIMERO-269
-def is_a_date_field? aField
-  fs = FormSection.get_form_containing_field aField
-  return false unless fs.present?
- 
-  myField = fs.fields.select {|x| x.name == aField}.first
-  myField[:type] == "date_field"  
-end
-
-#PRIMERO-269
-def is_date_valid_format? aValue
-  begin
-    Date.strptime(aValue, '%d-%b-%Y')
-    true
-  rescue
-    false
-  end  
-end
-
-
 
 if should_seed? User
   require File.dirname(__FILE__) + "/users/roles.rb"
@@ -96,6 +38,3 @@ Dir[File.dirname(__FILE__) + '/forms/*/*.rb'].each {|file| require file }
 if should_seed? ContactInformation
   ContactInformation.create(:id=>"administrator")
 end
-
-#PRIMERO-269
-fix_case_data
