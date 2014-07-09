824d4c209b5528e3202aa54726130605d07864e5
Chavez, Carlos <carlos.chavez@quoininc.com>
2014-07-09 16:36:24 -0600

    Primero-270 GBV-006 - Violations-Sexual Violence Form.

---
+++ capybara_features/features_primero/sexual_violence.feature
@@ -0,0 +1,59 @@
+#JIRA PRIMERO-270
+
+@javascript @primero
+Feature: Sexual Violence Form
+  As a Social Worker / Data Entry Person, I want to enter information about the incident type in
+  so that I can report on specific details related to the type of incident.
+
+  Scenario: As a logged in user, I will create a incident for sexual violence
+    Given I am logged in as an admin with username "primero" and password "primero"
+    When I access "incidents page"
+    And I press the "Create a New Incident" button
+    And I press the "Sexual Violence" button
+    And I fill in the following:
+      | Number of victims: boys    | 1 |
+      | Number of victims: girls   | 2 |
+      | Number of victims: unknown | 3 |
+      | Number of total victims    | 6 |
+      | Type of Violence           | <Choose>Forced Marriage<Choose>Forced Sterilization      |
+      | Type of GBV                | <Select> Denial of Resources, Opportunities, or Services |
+      | If Non-GBV, describe       | describe: It is not a GBV |
+      | Was this incident a Harmful Traditional Practice | <Select> Option 1 |
+      | Were money, goods, benefits, and/or services exchanged in relation to the incident? | <Radio> Yes |
+      | Stage of displacement at time of incident            | <Select> During Flight       |
+      | Type of abduction at time of the incident            | <Select> Forced Conscription |
+      | Has the client reported this incident anywhere else? | <Radio> Unknown              |
+    And I fill in the 1st "GBV Details Section" subform with the follow:
+      |Type of service provider where the survivor reported the incident | <Select> Legal Assistance Services |
+      |Name of the service provider                                      | Organization 1                     |
+      |Is this a GBV reporting organization?                             | <Radio> Yes                        |
+    And I fill in the 2nd "GBV Details Section" subform with the follow:
+      |Type of service provider where the survivor reported the incident | <Select> Psychosocial/Counseling Services |
+      |Name of the service provider                                      | Organization 2                            |
+      |Is this a GBV reporting organization?                             | <Radio> No                                |
+    And I fill in the following:
+      | Has the client had any previous incidents of GBV perpetrated against them? | <Radio> No |
+    And I press "Save"
+    Then I should see "Incident record successfully created" on the page
+    #Uncomment when show page is ready.
+    #And I should see a value for "Number of victims: boys" on the show page with the value of "1"
+    #And I should see a value for "Number of victims: girls" on the show page with the value of "2"
+    #And I should see a value for "Number of victims: unknown" on the show page with the value of "3"
+    #And I should see a value for "Number of total victims" on the show page with the value of "6"
+    #And I should see a value for "Type of Violence" on the show page with the value of "Forced Marriage,Forced Sterilization"
+    #And I should see a value for "Type of GBV" on the show page with the value of "Denial of Resources, Opportunities, or Services"
+    #And I should see a value for "If Non-GBV, describe" on the show page with the value of "describe: It is not a GBV"
+    #And I should see a value for "Was this incident a Harmful Traditional Practice" on the show page with the value of "Option 1"
+    #And I should see a value for "Were money, goods, benefits, and/or services exchanged in relation to the incident?" on the show page with the value of "Yes"
+    #And I should see a value for "Stage of displacement at time of incident" on the show page with the value of "During Flight"
+    #And I should see a value for "Type of abduction at time of the incident" on the show page with the value of "Forced Conscription"
+    #And I should see a value for "Has the client reported this incident anywhere else?" on the show page with the value of "Unknown"
+    #And I should see in the 1st "GBV Detail" subform with the follow:
+    #  | Type of service provider where the survivor reported the incident | Legal Assistance Services  |
+    #  | Name of the service provider                                      | Organization 1             |
+    #  | Is this a GBV reporting organization?                             | Yes                        |
+    #And I should see in the 2nd "GBV Detail" subform with the follow:
+    #  | Type of service provider where the survivor reported the incident | Psychosocial/Counseling Services  |
+    #  | Name of the service provider                                      | Organization 2                    |
+    #  | Is this a GBV reporting organization?                             | No                                |
+    #And I should see a value for "Has the client had any previous incidents of GBV perpetrated against them?" on the show page with the value of "No"
---
+++ db/forms/incident/sexual_violence.rb
@@ -0,0 +1,144 @@
+gbv_details_fields_subform = [
+  Field.new({"name" => "gbv_reported_elsewhere_organization_type",
+             "type" => "select_box",
+             "display_name_all" => "Type of service provider where the survivor reported the incident",
+             "option_strings_text_all" =>
+                                    ["Health/Medical Services",
+                                     "Psychosocial/Counseling Services",
+                                     "Police/Other Security Actor",
+                                     "Legal Assistance Services",
+                                     "Livelihoods Program",
+                                     "Safe House/Shelter",
+                                     "Other"].join("\n")
+            }),
+  Field.new({"name" => "gbv_reported_elsewhere_organization_provider",
+             "type" => "text_field", 
+             "display_name_all" => "Name of the service provider"
+            }),
+  Field.new({"name" => "gbv_reported_elsewhere_reporting",
+             "type" => "radio_button",
+             "display_name_all" => "Is this a GBV reporting organization?",
+             "option_strings_text_all" => "Yes\nNo"
+            })
+]
+
+gbv_details_section = FormSection.create_or_update_form_section({
+  "visible"=>false,
+  "is_nested"=>true,
+  :order=> 1,
+  :unique_id=>"gbv_details_section",
+  :parent_form=>"incident",
+  "editable"=>true,
+  :fields => gbv_details_fields_subform,
+  :perm_enabled => false,
+  :perm_visible => false,
+  "name_all" => "Nested GBV Details",
+  "description_all" => "GBV Details Subform"
+})
+
+violations_sexual_violence_fields = [
+  Field.new({"name" => "violation_boys",
+             "type" => "numeric_field", 
+             "display_name_all" => "Number of victims: boys"
+            }),
+  Field.new({"name" => "violation_girls",
+             "type" => "numeric_field", 
+             "display_name_all" => "Number of victims: girls"
+            }),
+  Field.new({"name" => "violation_unknown",
+             "type" => "numeric_field", 
+             "display_name_all" => "Number of victims: unknown"
+            }),
+  Field.new({"name" => "violation_total",
+             "type" => "numeric_field", 
+             "display_name_all" => "Number of total victims"
+            }),
+  Field.new({"name" => "sexual_violence_type",
+             "type" => "select_box",
+             "multi_select" => true,
+             "display_name_all" => "Type of Violence",
+             "option_strings_text_all" =>
+                                    ["Rape",
+                                     "Sexual Assault",
+                                     "Forced Marriage",
+                                     "Mutilation",
+                                     "Forced Sterilization",
+                                     "Other"].join("\n")
+            }),
+  Field.new({"name" => "gbv_type",
+             "type" => "select_box",
+             "display_name_all" => "Type of GBV",
+             "option_strings_text_all" =>
+                                    ["Rape",
+                                     "Sexual Assault",
+                                     "Physical Assault",
+                                     "Forced Marriage",
+                                     "Denial of Resources, Opportunities, or Services",
+                                     "Psychological/Emotional Abuse",
+                                     "Non-GBV"].join("\n")
+            }),
+  Field.new({"name" => "non_gbv_tyoe_notes",
+             "type" => "textarea",
+             "display_name_all" => "If Non-GBV, describe"
+            }),
+  Field.new({"name" => "harmful_traditional_practice",
+             "type" => "select_box",
+             "display_name_all" => "Was this incident a Harmful Traditional Practice",
+             "option_strings_text_all" => "Option 1\nOption 2\nOption 3\nOption 4\nOption 5\nNo"
+            }),
+  Field.new({"name" => "goods_money_exchanged",
+             "type" => "radio_button",
+             "display_name_all" => "Were money, goods, benefits, and/or services exchanged in relation to the incident?",
+             "option_strings_text_all" => "Yes\nNo\nUnknown"
+            }),
+  Field.new({"name" => "displacement_at_time_of_incident",
+             "type" => "select_box",
+             "display_name_all" => "Stage of displacement at time of incident",
+             "option_strings_text_all" =>
+                                    ["Not Displaced/Home Country",
+                                     "Pre-displacement",
+                                     "During Flight",
+                                     "During Refuge",
+                                     "During Return/Transit",
+                                     "Post-Displacement"].join("\n")
+            }),
+  Field.new({"name" => "abduction_status_time_of_incident",
+             "type" => "select_box",
+             "display_name_all" => "Type of abduction at time of the incident",
+             "option_strings_text_all" =>
+                                    ["None",
+                                     "Forced Conscription",
+                                     "Trafficked",
+                                     "Other Abduction/Kidnapping"].join("\n")
+            }),
+  Field.new({"name" => "gbv_reported_elsewhere",
+             "type" => "radio_button",
+             "display_name_all" => "Has the client reported this incident anywhere else?",
+             "option_strings_text_all" => "Yes\nNo\nUnknown"
+            }),
+  ##Subform##
+  Field.new({"name" => "gbv_details_section",
+             "type" => "subform", 
+             "editable" => true,
+             "subform_section_id" => gbv_details_section.id,
+             "display_name_all" => "GBV Details"
+            }),
+  ##Subform##
+  Field.new({"name" => "gbv_previous_incidents",
+             "type" => "radio_button",
+             "display_name_all" => "Has the client had any previous incidents of GBV perpetrated against them?",
+             "option_strings_text_all" => "Yes\nNo"
+            })
+]
+
+FormSection.create_or_update_form_section({
+  :unique_id => "sexual_violence",
+  :parent_form=>"incident",
+  "visible" => true,
+  :order => 60,
+  "editable" => true,
+  :fields => violations_sexual_violence_fields,
+  :perm_enabled => true,
+  "name_all" => "Sexual Violence",
+  "description_all" => "Sexual Violence"
+})
