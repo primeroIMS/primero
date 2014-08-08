979e873d2a71e89068418b810b40fcf58fa912b1
Ron Elledge <ronald.elledge@quoininc.com>
2014-08-08 16:26:34 -0400

    WIP - reload violations

--- app/assets/javascripts/reload_violations.js
+++ app/assets/javascripts/reload_violations.js
@@ -10,6 +10,20 @@ var ViolationListReload = Backbone.View.extend({
 
   //Refresh Stuff....
   reload: function(event) {
+      // temp... for testing
+      // TODO - get real violations list
+      var violation_list = ['Violation1', 'Violation2', 'Violation3'];
+      
+      //Find all violation select boxes and for each, refresh the options list
+      var fooId = 'incident_perpetrator_subform_section_0_perpetrator_violations_';
+      var fooEl = this.$el.find("#" + fooId);
+      fooEl.empty();
+
+       _.each(violation_list, function(i){
+         var newOption = $('<option value="' + i + '">' + i + '</option>');
+         fooEl.append(newOption);
+       });
+       fooEl.trigger("chosen:updated");
   },
 });
 
