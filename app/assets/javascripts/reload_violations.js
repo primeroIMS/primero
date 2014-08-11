79b7a7c344791b22e7624f53591a01fe837fdb7e
Ron Elledge <ronald.elledge@quoininc.com>
2014-08-11 10:44:16 -0400

    loop on violation selects

--- app/assets/javascripts/reload_violations.js
+++ app/assets/javascripts/reload_violations.js
@@ -3,27 +3,29 @@ var ViolationListReload = Backbone.View.extend({
 
   events: {
     'click .incident_reload_violations': 'reload'
-  }, 
+  },
 
   initialize: function() {
   },
 
   //Refresh Stuff....
   reload: function(event) {
-      // temp... for testing
-      // TODO - get real violations list
-      var violation_list = ['Violation1', 'Violation2', 'Violation3'];
-      
-      //Find all violation select boxes and for each, refresh the options list
-      var fooId = 'incident_perpetrator_subform_section_0_perpetrator_violations_';
-      var fooEl = this.$el.find("#" + fooId);
-      fooEl.empty();
+    // temp... for testing
+    // TODO - get real violations list
+    var violation_list = ['Violation1', 'Violation2', 'Violation3'];
 
-       _.each(violation_list, function(i){
-         var newOption = $('<option value="' + i + '">' + i + '</option>');
-         fooEl.append(newOption);
-       });
-       fooEl.trigger("chosen:updated");
+    var context = this.el;
+    $(context).find("select[id$='_violations_']").each(function(x, violationEl){
+      //Clear out existing options
+      violationEl.empty();
+
+      //Add new options
+      _.each(violation_list, function(i){
+        var newOption = $('<option value="' + i + '">' + i + '</option>');
+        violationEl.append(newOption);
+      });
+      violationEl.trigger("chosen:updated");
+    });
   },
 });
 
