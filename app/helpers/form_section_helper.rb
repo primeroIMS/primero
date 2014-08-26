105fbd3308798757373e2fc8f6c31be26d35edc2
Ron Elledge <ronald.elledge@quoininc.com>
2014-08-26 12:33:11 -0400

    Customize form index page

--- app/helpers/form_section_helper.rb
+++ app/helpers/form_section_helper.rb
@@ -38,12 +38,47 @@ module FormSectionHelper
   end
 
   def build_group_tabs(forms)
-    
     group_id = "group_" + forms[0].form_group_name.gsub(" ", "").gsub("/", "")
     content_tag :ul , class: 'sub', id: group_id do
      for form in forms
       concat(content_tag(:li, 
         link_to("#tab_#{form.section_name}") do 
+          concat(t(form.unique_id, :default => form.name))
+        end, class: "#{form.is_first_tab ? 'current': ''}"
+      ))
+     end
+    end
+  end
+
+  #PRIMERO-439  WIP
+  def build_form_custom_list(group, forms)
+    form = forms.first
+    if forms.count > 1
+      content_tag :li, class: 'group' do
+        concat(
+          link_to(edit_form_section_path(form.section_name), class: 'group') do
+            concat(t(group, :default => group))
+          end
+        )
+        concat(build_group_custom_list(forms))
+      end
+    else
+      content_tag :li, class: "#{form.is_first_tab ? 'current': ''}" do
+        concat(
+          link_to(edit_form_section_path(form.section_name), class: 'non-group') do
+            concat(t(form.unique_id, :default => form.name))
+          end
+        )
+      end
+    end
+  end
+
+  def build_group_custom_list(forms)
+    group_id = "group_" + forms[0].form_group_name.gsub(" ", "").gsub("/", "")
+    content_tag :ul , class: 'sub', id: group_id do
+     for form in forms
+      concat(content_tag(:li,
+        link_to(edit_form_section_path(form.section_name)) do
           concat(t(form.unique_id, :default => form.name))
         end, class: "#{form.is_first_tab ? 'current': ''}"
       ))
