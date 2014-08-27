a5ba3ef4e40dd73c38d63219c2fbd359cd83523c
Ron Elledge <ronald.elledge@quoininc.com>
2014-08-27 13:39:36 -0400

    fixed rspec related to form_section route change

--- spec/routing/formsection_routing_spec.rb
+++ spec/routing/formsection_routing_spec.rb
@@ -2,32 +2,32 @@ require 'spec_helper'
 
 describe 'Form Section routing' do
 
-  it 'routes /form_section/foo/fields correctly' do
-    {:get => '/form_section/foo/fields'}.should route_to(
+  it 'routes /forms/foo/fields correctly' do
+    {:get => '/forms/foo/fields'}.should route_to(
       :controller => 'fields', :action => 'index', :form_section_id => 'foo' )
   end
 
   it 'has route to post a new field' do
-    {:post => '/form_section/foo/fields' }.should route_to(:controller => 'fields', :action => 'create', :form_section_id => 'foo' )
-    form_section_fields_path('some_formsection').should == '/form_section/some_formsection/fields'
+    {:post => '/forms/foo/fields' }.should route_to(:controller => 'fields', :action => 'create', :form_section_id => 'foo' )
+    form_section_fields_path('some_formsection').should == '/forms/some_formsection/fields'
   end
 
   it 'has route to save order of fields' do
-    {:post => '/form_section/foo/fields/save_order'}.should route_to(:controller => 'fields', :action=>'save_order', :form_section_id=>'foo')
-    save_order_form_section_fields_path('some_formsection').should == '/form_section/some_formsection/fields/save_order'
+    {:post => '/forms/foo/fields/save_order'}.should route_to(:controller => 'fields', :action=>'save_order', :form_section_id=>'foo')
+    save_order_form_section_fields_path('some_formsection').should == '/forms/some_formsection/fields/save_order'
   end
 
   it 'has route for form sections index page' do
-    {:get => '/form_section'}.should route_to(:controller => 'form_section', :action=>'index')
-    form_sections_path.should == '/form_section'
+    {:get => '/forms'}.should route_to(:controller => 'form_section', :action=>'index')
+    form_sections_path.should == '/forms'
   end
 
   it 'has route for form sections new page' do
-    {:get => '/form_section/new'}.should route_to(:controller => 'form_section', :action=>'new')
-    new_form_section_path.should == '/form_section/new'
+    {:get => '/forms/new'}.should route_to(:controller => 'form_section', :action=>'new')
+    new_form_section_path.should == '/forms/new'
   end
 
   it "has route for update field page" do
-    {:put => '/form_section/form_section_unique_id/fields/field_id'}.should route_to(:controller => 'fields', :action => "update", "form_section_id"=>"form_section_unique_id", "id"=>"field_id")
+    {:put => '/forms/form_section_unique_id/fields/field_id'}.should route_to(:controller => 'fields', :action => "update", "form_section_id"=>"form_section_unique_id", "id"=>"field_id")
   end
 end
