7386264b1f58b51d422bd0d017fb1362f3f15654
Aldo Dávila <adavila@agssa.net>
2014-08-27 11:17:11 -0600

    PRIMERO-476 Incidents -> Services: Change form name to Service Referrals

--- db/forms/incident/services.rb
+++ db/forms/incident/services.rb
@@ -397,16 +397,16 @@ services_fields = [
 ]
 
 FormSection.create_or_update_form_section({
-  :unique_id => "incident_services",
+  :unique_id => "incident_services_referrals",
   :parent_form=>"incident",
   "visible" => true,
   :order_form_group => 100,
   :order => 10,
   :order_subform => 0,
-  :form_group_name => "Services",
+  :form_group_name => "Services Referrals",
   "editable" => true,
   :fields => services_fields,
   :perm_enabled => true,
-  "name_all" => "Services",
-  "description_all" => "Services"
+  "name_all" => "Services Referrals",
+  "description_all" => "Services Referrals"
 })
