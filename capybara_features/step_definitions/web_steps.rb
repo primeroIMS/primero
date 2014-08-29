d3468a2b438487a9c071e5fb8858218225c9554a
Ron Elledge <ronald.elledge@quoininc.com>
2014-08-29 16:23:39 -0400

    fixed typo.. one too many quotes... in web_steps

--- capybara_features/step_definitions/web_steps.rb
+++ capybara_features/step_definitions/web_steps.rb
@@ -353,7 +353,7 @@ Then /^I should see link to "(.*?)"$/ do |text|
 end
 
 Then /^I should( not)? be able to view the tab (.+)$/ do|not_visible,tab_name|
-  tab_element = page.has_xpath?("//div[@class='main_bar']//ul/li/a[text()=\"#{tab_name}"\"]")
+  tab_element = page.has_xpath?("//div[@class='main_bar']//ul/li/a[text()=\"#{tab_name}\"]")
   tab_element.should == !not_visible
 end
 
