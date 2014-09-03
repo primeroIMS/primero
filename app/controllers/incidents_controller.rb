fe68bfce80a2681290973437f68f29f1acd266e1
Chavez, Carlos <carlos.chavez@quoininc.com>
2014-09-03 16:17:11 -0600

    Small refactoring to Copy GBV Survivor Information to GBV Incidents Individual Details. Use i18n to avoid translate issues with the button value. Remove unnecesary checks when create the GBV Incident creation from GBV cases.

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -60,11 +60,9 @@ class IncidentsController < ApplicationController
     @incident['mrm_verification_status'] = "Pending"
     @incident['module_id'] = params['module_id']
 
-    if params['module_id'].present? and params['case_id'].present?
-      primero_module = PrimeroModule.get(params['module_id'])
+    if params['case_id'].present?
       case_record = Child.get(params['case_id'])
-      if primero_module.present? and case_record.present? and
-         primero_module.name == PrimeroModule::GBV and case_record.module.name == PrimeroModule::GBV
+      if case_record.present?
          @incident.copy_survivor_information(case_record)
       end
     end
