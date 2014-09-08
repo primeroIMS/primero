319cf5e3b0d293c3a645c7a455b63237fbd9593b
Aldo Dávila <adavila@agssa.net>
2014-09-08 14:35:11 -0600

    PRIMERO-540 UAT12 GBV Cases -> Move GBV Data Confidentiality form to above Survivor Information

--- db/forms/case/gbv_data_confidentiality.rb
+++ db/forms/case/gbv_data_confidentiality.rb
@@ -87,7 +87,7 @@ FormSection.create_or_update_form_section({
   :unique_id=>"gbv_data_confidentiality",
   :parent_form=>"case",
   "visible" => true,
-  :order_form_group => 40,
+  :order_form_group => 30,
   :order => 10,
   :order_subform => 0,
   :form_group_name => "GBV Data Confidentiality",
