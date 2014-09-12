d3ef2e6a1ff5d2449e1d0a39d47e6cf184c1c513
Ron Elledge <ronald.elledge@quoininc.com>
2014-09-12 08:40:39 -0400

    changed set_violation_verification_default to use self.

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -55,17 +55,17 @@ class Incident < CouchRest::Model::Base
   end
 
   def incident_code
-    (self['unique_identifier'] || "").last 7
+    (self.unique_identifier || "").last 7
   end
 
   def violations_list
     violations_list = []
 
-    if self['violations'].present?
-      self['violations'].to_hash.each do |key, value|
+    if self.violations.present?
+      self.violations.to_hash.each do |key, value|
         value.each_with_index do |v, i|
-          if v['violation_id'].present?
-            violations_list << v['violation_id']
+          if v.violation_id.present?
+            violations_list << v.violation_id
           else
             violations_list << "#{key.titleize} #{i}"
           end
@@ -99,11 +99,11 @@ class Incident < CouchRest::Model::Base
   end
 
   def set_violation_verification_default
-    if self['violations'].present?
-      self['violations'].to_hash.each do |key, value|
+    if self.violations.present?
+      self.violations.to_hash.each do |key, value|
         value.each do |v|
-          unless v['verified'].present?
-            v['verified'] = "Pending"
+          unless v.verified.present?
+            v.verified = "Pending"
           end
         end
       end
