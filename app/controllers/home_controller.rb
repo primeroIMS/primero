49369eef2f5b21c6a72a038780d24ede0a887484
Ron Elledge <ronald.elledge@quoininc.com>
2014-09-21 14:45:16 -0400

    backing out temp changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,6 @@ class HomeController < ApplicationController
     @page_name = t("home.label")
     @user = User.find_by_user_name(current_user_name)
     @notifications = PasswordRecoveryRequest.to_display
-    #TODO - RSE - Temporary.  Commented out to gain some speed
-    # Due to Child loading formsections bug
-    #@suspect_record_count = Child.flagged.count
+    @suspect_record_count = Child.flagged.count
   end
 end
