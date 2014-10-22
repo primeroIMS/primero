60ce7f274a502c08d569eb4e42709ac6067e8d1e
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2014-10-22 09:53:49 -0400

    Added forms and record types and programs to the show view.

--- app/models/primero_module.rb
+++ app/models/primero_module.rb
@@ -16,4 +16,27 @@ class PrimeroModule < CouchRest::Model::Base
   validates_presence_of :associated_form_ids, :message => I18n.t("errors.models.primero_module.associated_form_ids")
   validates_presence_of :associated_record_types, :message => I18n.t("errors.models.primero_module.associated_record_types")
 
+  def program
+    PrimeroProgram.get self.program_id
+  end
+
+  def program_name
+    program.name
+  end
+
+  def associated_forms(include_subforms=false)
+    result = FormSection.by_unique_id(keys: self.associated_form_ids).all
+    unless include_subforms
+      result = result.select{|f| !f.is_nested}
+    end
+    return result
+  end
+
+  def associated_forms_grouped_by_record_type(include_subforms=false)
+    result = {}
+    forms = associated_forms(include_subforms)
+    result = forms.group_by(&:parent_form) if forms.present?
+    return result
+  end
+
 end
