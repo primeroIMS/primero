9f19e40b960204c46ee664aafa96e50fb13d552e
Aldo Dávila <adavila@agssa.net>
2014-11-05 10:38:12 -0600

    PRIMERO-783: Added capybara testcases

--- lib/extensions/date.rb
+++ lib/extensions/date.rb
@@ -1,16 +1,22 @@
 class Date
   def self.parse(value)
-    # valid_formats:
+    # Separator can be "-" or "/". Valid formats:
     #   "%d-%b-%Y" # 05-Sep-2014 | 05-September-2014 | 5-Sep-2014 | 5-September-2014
     #   "%d-%b-%y" # 05-Sep-14   | 05-September-14   | 5-Sep-14   | 05-September-14
     #   "%d-%m-%Y" # 05-09-2014  | 05-9-2014         | 5-09-2014  | 5-9-2014
     #   "%d-%m-%y" # 05-09-14    | 05-9-14           | 5-09-14    | 5-9-14
-    value = value.gsub(/^(.*)[-|\/](.*)[-|\/](.*)$/, "#{$1.strip}-#{$2.strip}-#{$3.strip}") if value.match /^(.*)[-|\/](.*)[-|\/](.*)$/
-    match_data = value.match /^(\d{1,2})[-|\/](.*)[-|\/](\d{2,4})$/
+
+    # Remove empty spaces if found. (12 - Jun - 2014 -> 12-Jun-2014)
+    value = "#{$1.strip}-#{$2.strip}-#{$3.strip}" if value.match /^(.*)[-|\/](.*)[-|\/](.*)$/
+    # If the value to parse has a valid format, get the format and parse the value.
+    # It should be day month year
+    match_data = value.match /^(\d{1,2})-(.*)-(\d{2,4})$/
     if match_data
-      value = "#{match_data[1]}-#{match_data[2]}-#{match_data[3]}"
+      # Determine whether we are given the month number or the month name
       month_format = match_data[2].match(/^(0[1-9]|[1-9]|1[0-2])$/).nil? ? "%b" : "%m"
+      # Determine whether the year has two-digits format or four-digits format
       year_format = match_data[3].match(/^(\d{2})$/).nil? ? "%Y" : "%y"
+      # Try to parse the value with the detected format
       return Date.strptime value, "%d-#{month_format}-#{year_format}"
     end
     raise ArgumentError, "invalid date"
