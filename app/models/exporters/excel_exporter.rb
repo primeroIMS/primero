146c2f773b6be56efb30d0a388b30aa849e62297
Chavez, Carlos <carlos.chavez@quoininc.com>
2014-11-14 09:05:35 -0600

    PRIMERO-688 excel export - forms. For every form section it will be a work sheet. Subforms are no included for now.

--- app/models/exporters/excel_exporter.rb
+++ app/models/exporters/excel_exporter.rb
@@ -13,37 +13,161 @@ module Exporters
 
       # @returns: a String with the Excel file data
       def export(models, properties, *args)
+        @forms = build_forms(models)
+        @sheets = {}
+
         io = StringIO.new
         workbook = WriteExcel.new(io)
-        worksheet = workbook.add_worksheet
-
-        i = 0
-        column_widths = []
-        to_2D_array(models, properties_to_export(properties)) do |row|
-          worksheet.write_row(i,0,row)
-
-          row.each_with_index do |r, i|
-            row_len = r.to_s.length
-            if column_widths[i].nil? || (row_len > column_widths[i])
-              column_widths[i] = row_len
+        models.each do |model|
+          @forms[model.module_id].each do |key, form|
+            sheet = build_sheet(key, form, workbook)
+            #module_id was injected as part of the first 3 fields for any row.
+            values = sheet["fields"].reject{|name| name == "module_id"}.map do |name|
+              get_value(model, name)
+            end.flatten
+            #try to find out if the row has no value, so there is no need to fill.
+            if values.reject(&:blank?).present?
+              #get the value of the 3 fields are always goes in any sheet.
+              others_fields = ["_id", "model_type", "module_id"].map do |name|
+                get_value(model, name)
+              end.flatten
+              col = 0
+              (others_fields + values).each do |value|
+                sheet["work_sheet"].write(sheet["row"], col, value)
+                column_widths(sheet, col, value)
+                col += 1
+              end
+              #the row should be tracked per sheet.
+              sheet["row"] += 1
+              adjust_column_width(sheet)
             end
           end
-          i += 1
         end
-
-        column_widths.each_with_index {|w, i| worksheet.set_column(i, i, w)}
-
         workbook.close
         io.string
       end
 
       private
 
-      # For some reason, bold doesn't seem to work
-      def make_headers_bold(workbook)
-        header_format = workbook.add_format(:bold => 1)
-        workbook.worksheets.each {|ws| ws.set_row(0, 0, header_format) }
+      def adjust_column_width(sheet)
+        sheet["column_widths"].each_with_index {|w, i| sheet["work_sheet"].set_column(i, i, w)}
       end
+
+      def get_value(model, field_name)
+        if field_name == "model_type"
+          value = model.class.name
+        else
+          name = field_name.sub(/\[\]/, "")
+          value = model.send(name)
+          value = value.join(" ||| ") if value.is_a?(Array)
+        end
+        value
+      end
+
+      def column_widths(sheet, col, value)
+        sheet["column_widths"][col] = value.to_s.length if value.to_s.length > sheet["column_widths"][col]
+      end
+
+      def build_forms(models)
+        parent_form = models.first.class.parent_form
+        modules = PrimeroModule.all.all
+        modules.select{|pm| pm.associated_record_types.include?(parent_form)}.map do |pm|
+          [pm.id, build_form_definition(pm, parent_form)]
+        end.to_h.each{|key, value| value["___record_state___"]=other_form_section_fields()}
+      end
+
+      def build_form_definition(primero_module, parent_form)
+        excluded_forms = ["mrm_summary_page", "tracing_request_photos_and_audio",
+                          "incident_other_documents", "photos_and_audio",
+                          "other_documents"]
+        form_section_by_module(primero_module, parent_form)
+          .reject{|form_section| excluded_forms.include?(form_section.unique_id)}
+          .map do |form_section|
+          [
+            form_section.unique_id,
+            {
+              "name" => form_section.name,
+              "fields" => exportable_fields(form_section.fields)
+            }
+          ]
+        end.to_h
+      end
+
+      def other_form_section_fields
+        {"name"=>"___record_state___",
+         "fields"=>[
+            "created_organization",
+            "created_by_full_name",
+            "last_updated_at",
+            "last_updated_by",
+            "last_updated_by_full_name",
+            "posted_at",
+            "unique_identifier",
+            "record_state",
+            "hidden_name",
+            "owned_by_full_name",
+            "previously_owned_by_full_name",
+            "duplicate",
+            "duplicate_of"]
+        }
+      end
+
+      def build_sheet(form_id, form, workbook)
+        return @sheets[form_id] if @sheets[form_id].present?
+        work_sheet = generate_work_sheet(workbook, form["name"])
+        column_widths = generate_column_widths((["_id", "model_type", "module_id"] + form["fields"]).uniq)
+        write_header(work_sheet, (["_id", "model_type", "module_id"] + form["fields"]).uniq)
+        @sheets[form_id] = {"work_sheet" => work_sheet, "fields" => form["fields"], "column_widths" => column_widths, "row" => 1}
+      end
+
+      def generate_work_sheet(workbook, form_name)
+        #Clean up name and truncate to the allowed limit.
+        work_sheet_name = form_name.sub(/[\[\]\:\*\?\/\\]/, " ").truncate(31)
+        workbook.add_worksheet(work_sheet_name)
+      end
+
+      def exportable_fields(form_fields)
+        form_fields.map do |field|
+          if field.type == Field::DATE_RANGE
+            [field.name + "_date_or_date_range",
+             field.name,
+             field.name + "_from",
+             field.name + "_to"]
+          elsif field.type == Field::TALLY_FIELD
+            [field.name + "_boys",
+             field.name + "_girls",
+             field.name + "_unknown",
+             field.name + "_total"]
+          elsif field.type == Field::CHECK_BOXES
+            field.name + "[]"
+          elsif field.type == Field::SELECT_BOX && field.multi_select == true
+            field.name + "[]"
+          #Separator should be ignored, it has no data.
+          #Subforms should ignored for now.
+          elsif field.type != Field::SEPARATOR && field.type != Field::SUBFORM
+            field.name
+          end
+        end.compact.uniq.flatten
+      end
+
+      def write_header(work_sheet, fields)
+        col = 0
+        fields.each do |name|
+          work_sheet.write(0, col, name)
+          col += 1
+        end
+      end
+
+      def generate_column_widths(fields)
+        fields.map { |name| name.length }
+      end
+
+      def form_section_by_module(primero_module, parent_form)
+        forms_by_record_type = primero_module.associated_forms_grouped_by_record_type
+        forms_for_current_record_type = forms_by_record_type[parent_form]
+        forms_for_current_record_type.select(&:visible).sort {|a, b| [a.order_form_group, a.order] <=> [b.order_form_group, b.order] }
+      end
+
     end
   end
 end
--- spec/models/exporters/excel_exporter_spec.rb
+++ spec/models/exporters/excel_exporter_spec.rb
@@ -4,25 +4,25 @@ require 'spreadsheet'
 module Exporters
   describe ExcelExporter do
     it "converts data to Excel format" do
-      BaseExporter.should_receive(:to_2D_array)
-        .and_yield([ 'name', 'age', 'birthday', 'height' ])
-        .and_yield([ 'Joe', 12, Date.today, 120 ])
-        .and_yield([ 'Mo', 14, Date.today, 140 ])
-
-      data = ExcelExporter.export(nil, [])
+      forms = {
+        "primeromodule-cp" => {
+          "test_form_section_1" => {"name"=>"Test Form Section 1", "fields"=>["first_name", "last_name"]},
+          "test_form_section_2" => {"name"=>"Test Form Section 2", "fields"=>["relation", "module_id", "array_field"]}
+        }
+      }
+      records = [Child.new("module_id" => "primeromodule-cp", "first_name" => "John", "last_name" => "Doe", 
+                           "relation"=>"Mother", "array_field"=> ["Option1", "Option2"])]
+      ExcelExporter.should_receive(:build_forms).with(records).and_return(forms)
+      data = ExcelExporter.export(records, [])
 
       book = Spreadsheet.open(StringIO.new(data))
       sheet = book.worksheets[0]
-      sheet.row(1)[2].should == Date.today.to_s
-      sheet.row(0).to_a.should == ['name', 'age', 'birthday', 'height']
-    end
+      sheet.row(0).to_a.should == ["_id", "model_type", "module_id", "first_name", "last_name"]
+      sheet.row(1).to_a.should == [nil, "Child", "primeromodule-cp", "John", "Doe"]
 
-    it "excludes histories from export" do
-      c = Child.create({:name => 'Juan'})
-      data = ExcelExporter.export([c], c.properties)
-      book = Spreadsheet.open(StringIO.new(data))
-      sheet = book.worksheets[0]
-      sheet.row(0).to_a.each {|c| c.should_not include 'histories' }
+      sheet = book.worksheets[1]
+      sheet.row(0).to_a.should == ["_id", "model_type", "module_id", "relation", "array_field"]
+      sheet.row(1).to_a.should == [nil, "Child", "primeromodule-cp", "Mother", "Option1 ||| Option2"]
     end
   end
 end
