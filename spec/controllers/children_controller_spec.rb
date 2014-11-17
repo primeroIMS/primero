1ce28a95314bbf6274f1812de415ac2163384529
Ron Elledge <ronald.elledge@quoininc.com>
2014-11-17 10:54:38 -0500

    resolved issue with stub of child.list_records

--- spec/controllers/children_controller_spec.rb
+++ spec/controllers/children_controller_spec.rb
@@ -134,9 +134,7 @@ describe ChildrenController do
           children = mock_child(@stubs)
           scope = {"child_status"=>"single||open"} if not scope.present?
           children.stub(:paginate).and_return(children)
-          Child.should_receive(:list_records).with(scope, {:created_at=>:desc}, {:page=> page, :per_page=> per_page}, ["fakefieldadmin"], nil, nil).and_return(children)
-          #TODO resolve this!!!
-          #Child.should_receive(:list_records).with({"child_status" => {:type => "single", :value => "open"}}, {:created_at=>:desc}, {:page=> page, :per_page=> per_page}, ["fakefieldadmin"], nil).and_return(children)
+          Child.should_receive(:list_records).with({"child_status" => {:type => "single", :value => "open"}}, {:created_at=>:desc}, {:page=> page, :per_page=> per_page}, ["fakefieldadmin"], nil, nil).and_return(children)
 
           get :index, :scope => scope
           assigns[:children].should == children
@@ -191,9 +189,7 @@ describe ChildrenController do
         search = double(Sunspot::Search::StandardSearch)
         search.should_receive(:results).and_return(collection)
         search.should_receive(:total).and_return(100)
-        Child.should_receive(:list_records).with({"child_status"=>"open"}, {:created_at=>:desc}, {:page=> 1, :per_page=> 100}, ["fakefieldworker"], nil, nil).and_return(search)
-        # TODO resolve this
-        #Child.should_receive(:list_records).with({"child_status" => {:type => "single", :value => "open"}}, {:created_at=>:desc}, {:page=> 1, :per_page=> 100}, ["fakefieldworker"], nil).and_return(search)
+        Child.should_receive(:list_records).with({"child_status" => {:type => "single", :value => "open"}}, {:created_at=>:desc}, {:page=> 1, :per_page=> 100}, ["fakefieldworker"], nil, nil).and_return(search)
         params = {"page" => "all"}
         get :index, params
         assigns[:children].should == collection
@@ -206,9 +202,7 @@ describe ChildrenController do
         search = double(Sunspot::Search::StandardSearch)
         search.should_receive(:results).and_return(collection)
         search.should_receive(:total).and_return(100)
-        Child.should_receive(:list_records).with({"module_id" => "single,primeromodule-cp", "child_status"=>"open"}, {:created_at=>:desc}, {:page=> 1, :per_page=> 100}, ["fakefieldworker"], nil, nil).and_return(search)
-        #TODO resolve this
-        #Child.should_receive(:list_records).with({"module_id" => {:type => "single", :value => "primeromodule-cp"}, "child_status"=>{:type => "single", :value => "open"}}, {:created_at=>:desc}, {:page=> 1, :per_page=> 100}, ["fakefieldworker"], nil).and_return(search)
+        Child.should_receive(:list_records).with({"module_id" => {:type => "single", :value => "primeromodule-cp"}, "child_status"=>{:type => "single", :value => "open"}}, {:created_at=>:desc}, {:page=> 1, :per_page=> 100}, ["fakefieldworker"], nil, nil).and_return(search)
         params = {"page" => "all", "format" => "unhcr_csv"}
         get :index, params
         assigns[:children].should == collection
@@ -227,9 +221,7 @@ describe ChildrenController do
         search = double(Sunspot::Search::StandardSearch)
         search.should_receive(:results).and_return(collection)
         search.should_receive(:total).and_return(2)
-        Child.should_receive(:list_records).with({"child_status"=>"open"}, {:created_at=>:desc}, {:page=> 1, :per_page=> 100}, ["all"], nil, nil).and_return(search)
-        #TODO resolve this
-        #Child.should_receive(:list_records).with({"child_status"=>{:type => "single", :value => "open"}}, {:created_at=>:desc}, {:page=> 1, :per_page=> 100}, ["all"], nil).and_return(search)
+        Child.should_receive(:list_records).with({"child_status"=>{:type => "single", :value => "open"}}, {:created_at=>:desc}, {:page=> 1, :per_page=> 100}, ["all"], nil, nil).and_return(search)
     
         #User
         @session.user.should_receive(:has_module?).with(PrimeroModule::CP).and_return(cp_result)
