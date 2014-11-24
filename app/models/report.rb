63d6ebf4beb0caece7fb9c29ad42ece8a8f5dfe4
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2014-11-24 16:14:58 -0500

    Crude use of Solr for generating aggregate report pivot data. Needs to be refactored.

--- app/models/report.rb
+++ app/models/report.rb
@@ -6,9 +6,10 @@ class Report < CouchRest::Model::Base
   property :description
   property :module_id
   property :record_type #case, incident, etc.
-  property :rows, [String]
-  property :columns, [String]
+  property :aggregate_by, [String] #Y-axis
+  property :disaggregate_by, [String] #X-axis
   property :is_graph, TrueClass
+  #property :data
 
   attr_accessor :data
 
@@ -21,7 +22,44 @@ class Report < CouchRest::Model::Base
   end
 
   def build_report
+    pivots = (aggregate_by + disaggregate_by)
+    if pivots.present?
+      pivots = pivots.map{|p| self.indexed_field_name(p)}.join(',')
+      response = rsolr.get(
+        'select',
+        :params => {
+          :q => '*:*',
+          :rows => 0,
+          :facet => 'on',
+          :'facet.pivot' => pivots,
+          :'facet.pivot.mincount' => -1,
+        }
+      )
+      #TODO: The format needs to change and we should probably store data? Although the report seems pretty fast for 100...
+      self.data = {
+        total: response['response']['numFound'],
+        pivots: response['facet_counts']['facet_pivot'][pivots]
+      }
+    end
 
+
+  end
+
+  #TODO: Any connection tests?
+  def rsolr
+    @rsolr ||= Sunspot.session.session.rsolr_connection
+  end
+
+  #TODO: This doesn't really belong here and should be accessible by other things
+  def sunspot_setup
+    type = self.record_type == 'case' ? 'Child' : self.record_type.camel_case
+    @setup ||= Sunspot::Setup.for(eval(type))
+  end
+
+  #TODO: Neither does this.
+  def indexed_field_name(name)
+    field = sunspot_setup.field(name)
+    field.indexed_name
   end
 
 
