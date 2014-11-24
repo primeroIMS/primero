929a9408ee76e2cb0f5befa397511b7e40691ba0
Sergio Infantes <sergio.infantes@quoininc.com>
2014-11-24 11:59:47 -0500

    searchable do modified

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -11,6 +11,10 @@ module Searchable
       eval(self.name).quicksearch_fields.each {|f| text f}
       searchable_string_fields.each {|f| string f, as: "#{f}_sci".to_sym}
       searchable_multi_fields.each {|f| string f, multiple: true}
+      
+      if self.instance_of?(Child)
+        searchable_arabic_names__fields.each {|f| string f, as: "#{f}_an".to_sym}
+      end
       # TODO: Left date as string. Getting invalid date format error
       searchable_date_fields.each {|f| date f}
       searchable_numeric_fields.each {|f| integer f}
