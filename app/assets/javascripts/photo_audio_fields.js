0b6b0b905e2ae66354529dc851d59d1be8139407
Aldo Dávila <adavila@agssa.net>
2014-12-15 14:24:32 -0600

    PRIMERO-806: Moved javascript for jplayer initialization from _audio_player partial to photo_audio_fields.js

--- app/assets/javascripts/photo_audio_fields.js
+++ app/assets/javascripts/photo_audio_fields.js
@@ -1,12 +1,41 @@
-$(document).ready(function() {
+var PhotoAudioFields = Backbone.View.extend({
+  el: "body",
+  events: {
+    "click .audio_player_section .audio .delete_check_box input[type='checkbox']" : "delete_check_box_click"
+  },
+
+  initialize: function(){
+    var jplayer_input = $("#jquery_jplayer_1");
+    var audio_url = jplayer_input.data("audio-url");
+    jplayer_input.jPlayer({
+      ready: function () {
+        $(this).jPlayer("setMedia", {
+          mp3: audio_url
+        });
+      },
+      swfPath: "/assets",
+      supplied: "mp3",
+      useStateClassSkin: true,
+      autoBlur: false,
+      solution:"flash, html"
+    });
+  },
+
   //Check the current audio for delete will disable the input file and clear it.
-  $(".audio_player_section .audio .delete_check_box input[type='checkbox']" ).on("click", function() {
-    var model = $(this).attr("data-model");
-    if ($(this).attr('checked')) {
-      $("fieldset[id$='photos_and_audio'] .file #" + model + "_audio").attr('disabled', 'disabled');
-      $("fieldset[id$='photos_and_audio'] .file #" + model + "_audio").val("");
+  delete_check_box_click: function(event){
+    var target = $(event.target);
+    var model = target.attr("data-model");
+    var upload_audio_input = $("fieldset[id$='photos_and_audio'] #" + model + "_audio");
+    var audio_path_label = $("fieldset[id$='photos_and_audio'] #" + model + "_audio_file_path");
+    if (target.attr('checked')) {
+      upload_audio_input.attr('disabled', 'disabled').val("");
+      audio_path_label.text(audio_path_label.data('default-text'));
     } else {
-      $("fieldset[id$='photos_and_audio'] .file #" + model + "_audio").removeAttr('disabled');
+      upload_audio_input.removeAttr('disabled');
     }
-  });
+  }
+});
+
+$(document).ready(function() {
+  new PhotoAudioFields();
 });
