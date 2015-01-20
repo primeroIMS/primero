9dbb0ecd0848f2230fa6d0a6651d99c7c699f6fb
Ben Keith <ben.keith@quoininc.com>
2015-01-20 11:37:39 -0500

    starting on script to clean tmp dir daily

--- cookbook/recipes/common.rb
+++ cookbook/recipes/common.rb
@@ -14,3 +14,14 @@ end
 node.force_default[:primero][:couchdb][:config][:ssl][:cacert_file] =
   node.force_default[:primero][:couchdb][:config][:replicator][:ssl_trusted_certificates_file] =
     '/etc/ssl/certs/couch_ca.pem'
+
+file "/etc/cron.daily/clean-tmp" do
+  mode '0755'
+  owner "root"
+  group "root"
+  content <<EOH
+#!/bin/bash
+
+/usr/bin/find /tmp -type f -atime +2 -mtime +2 | xargs /bin/rm -f
+EOH
+end
