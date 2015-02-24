7ee922988048954fed5b5b8025279fd7369072ad
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2015-02-24 11:31:26 -0500

    Closed ruby injection holes through the eval method.

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -8,7 +8,7 @@ module Searchable
 
     # Note that the class will need to be reloaded when the fields change. The current approach is to gently bounce Passenger.
     searchable do
-      eval(self.name).quicksearch_fields.each {|f| text f}
+      quicksearch_fields.each {|f| text f}
       searchable_string_fields.each {|f| string f, as: "#{f}_sci".to_sym}
       searchable_multi_fields.each {|f| string f, multiple: true}
       # TODO: Left date as string. Getting invalid date format error
