a3241762ec95f15c6601d05e0e76743680e9af53
Joshua Toliver <jtoliver@quoininc.com>
2015-03-05 11:09:20 -0500

    Primero-1150: Filtering does not revert back to page 1 of list in pagination

--- app/assets/javascripts/primero.js
+++ app/assets/javascripts/primero.js
@@ -286,21 +286,21 @@ var Primero = Backbone.View.extend({
 
   _primero_clean_page_params: function(q_param) {
     var source = location.href,
-        rtn = source.split("?")[0],
-        param,
-        params_arr = [],
-        query = (source.indexOf("?") !== -1) ? source.split("?")[1] : "";
+      rtn = source.split("?")[0],
+      param,
+      params_arr = [],
+      query = (source.indexOf("?") !== -1) ? source.split("?")[1] : "";
     if (query !== "") {
-        params_arr = query.split("&");
-        for (var i = params_arr.length - 1; i >= 0; i -= 1) {
-            param = params_arr[i].split("=")[0];
-            for(var j = 0; j < q_param.length; j++) {
-              if (param === q_param[j] || param.indexOf(q_param) === 0) {
-                  params_arr.splice(i, 1);
-              }
-            }
+      params_arr = query.split("&");
+      for (var i = params_arr.length - 1; i >= 0; i -= 1) {
+        param = params_arr[i].split("=")[0];
+        for(var j = 0; j < q_param.length; j++) {
+          if (param === q_param[j] || param.indexOf(q_param[j]) === 0) {
+            params_arr.splice(i, 1);
+          }
         }
-        rtn = params_arr.join("&");
+      }
+      rtn = params_arr.join("&");
     } else {
       rtn = "";
     }
