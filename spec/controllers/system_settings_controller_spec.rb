a7f58c7d51286355c4cfbdbfc59efdf64cc82d28
Ron Elledge <ronald.elledge@quoininc.com>
2015-04-14 09:58:38 -0400

    renamed primero_locale_controller_spec to system_settings_controller_spec

--- spec/controllers/system_settings_controller_spec.rb
+++ spec/controllers/system_settings_controller_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-describe PrimeroLocaleController do
+describe SystemSettingsController do
   before :each do
     fake_admin_login
   end
@@ -12,14 +12,14 @@ describe PrimeroLocaleController do
   end
 
   it "should set the given locale as default" do
-    put :update, :locale => "fr", :id => "administrator"
+    put :update_locale, :locale => "fr", :id => "administrator"
     I18n.default_locale.should == :fr
   end
 
   it "should flash a update message when the system language is changed and affected by language changed " do
-    put :update, :locale => "zh", :id => "administrator"
+    put :update_locale, :locale => "zh", :id => "administrator"
     flash[:notice].should =="Default Language was successfully updated."
-    response.should redirect_to(edit_primero_locale_path)
+    response.should redirect_to(edit_locale_system_setting_path)
   end
 end
 
