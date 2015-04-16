a3f225b8e0657d53ca44da55c967deec7f5da325
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2015-04-16 15:53:48 -0400

    Added nil check to locale initializer

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -2,8 +2,10 @@
 docs = SystemSettings.database.documents['rows']
 if docs.present?
   doc_id = docs.map{|d| d['id']}.select{|i| !i.start_with?('_design')}.first
-  saved_settings = SystemSettings.database.get(doc_id)
-  if saved_settings.present? && saved_settings['default_locale'].present?
-    I18n.default_locale = saved_settings['default_locale']
+  if doc_id.present?
+    saved_settings = SystemSettings.database.get(doc_id)
+    if saved_settings.present? && saved_settings['default_locale'].present?
+      I18n.default_locale = saved_settings['default_locale']
+    end
   end
 end
