9cb30c71be1c4c4a4d91f65429a773cf2afda316
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2015-04-20 10:26:41 -0400

    Changed locale labels to system settings lables.

--- app/controllers/system_settings_controller.rb
+++ app/controllers/system_settings_controller.rb
@@ -5,7 +5,7 @@ class SystemSettingsController < ApplicationController
   end
 
   def show
-    @page_name = t("primero_locale.show")
+    @page_name = t("system_settings.show")
     @primero_language = I18n.locale
     @system_settings = SystemSettings.first
     respond_to do |format|
@@ -15,7 +15,7 @@ class SystemSettingsController < ApplicationController
   end
 
   def edit
-    @page_name = t("primero_locale.edit")
+    @page_name = t("system_settings.edit")
     @system_settings = SystemSettings.first
   end
 
@@ -27,7 +27,7 @@ class SystemSettingsController < ApplicationController
       system_settings.default_locale = I18n.default_locale
       system_settings.save!
     end
-    flash[:notice] = I18n.t("primero_locale.updated")
+    flash[:notice] = I18n.t("system_settings.updated")
     redirect_to edit_system_setting_path("administrator")
   end
 
