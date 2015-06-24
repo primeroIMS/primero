842821421d147d16dfc7386d14126093c9bfcf59
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2015-06-24 16:36:17 -0400

    Loading in only West Africa locations

--- db/lookups/locations.rb
+++ db/lookups/locations.rb
@@ -3,6 +3,9 @@ Location.all.all.each{ |l| l.destroy }
 
 
 #TODO: Temporarily load all the other locations code in this directory
-Dir[File.dirname(__FILE__) + '/locations_*.rb'].each {|file| require file}
+#Dir[File.dirname(__FILE__) + '/locations_*.rb'].each {|file| require file}
 
 #TODO: Add default location setting code here, if that make sense.
+require File.dirname(__FILE__) + "/locations_sierra_leone.rb"
+require File.dirname(__FILE__) + "/locations_liberia.rb"
+require File.dirname(__FILE__) + "/locations_guinea.rb"
