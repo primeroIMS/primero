88c0a8a173454d0c2dee7c0afbf72eed0687bd24
Ron Elledge <relledge@quoininc.com>
2015-08-17 14:11:30 -0400

    removed unused methods mark_for_mobile and unmark_for_mobile

--- app/models/concerns/syncable_mobile.rb
+++ app/models/concerns/syncable_mobile.rb
@@ -9,19 +9,4 @@ module SyncableMobile
       FormSection.find_mobile_forms.all.map {|f| f.parent_form}.uniq.include?(parent_form)
     end
   end
-
-  #TODO - decide if this should be here or in a controller
-  def mark_for_mobile
-    if self.marked_for_mobile.present?
-      self.marked_for_mobile = true
-      self.save!
-    end
-  end
-
-  def unmark_for_mobile
-    if self.marked_for_mobile.present?
-      self.marked_for_mobile = false
-      self.save!
-    end
-  end
 end
