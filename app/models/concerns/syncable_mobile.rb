4ba47b398010df36162b5b16324b3344ec42075f
Ron Elledge <relledge@quoininc.com>
2015-08-21 12:10:27 -0400

    added TODO to is_syncable_with_mobile?

--- app/models/concerns/syncable_mobile.rb
+++ app/models/concerns/syncable_mobile.rb
@@ -6,6 +6,8 @@ module SyncableMobile
 
   module ClassMethods
     def is_syncable_with_mobile?
+      #TODO - for now, this is memoized
+      #       if further optimization is needed, may need to move to a class variable
       FormSection.find_mobile_forms_by_parent_form(parent_form).count > 0
     end
   end
