e7b2ab7f072ad982bbc9ee0ad29cf241c689a264
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2015-09-22 17:16:09 -0400

    Moving bundle import logic to the model

--- app/models/configuration_bundle.rb
+++ app/models/configuration_bundle.rb
@@ -7,6 +7,36 @@ class ConfigurationBundle < CouchRest::Model::Base
   property :applied_by
   property :applied_at, DateTime, default: DateTime.now
 
+  def self.import(model_data, applied_by=nil)
+    Rails.logger.info "Starting configuration bundle import..."
+    model_data.map do |model_name, data_arr|
+      begin
+        modelCls = model_name.constantize
+        modelCls.database.recreate!
+        begin
+          modelCls.design_doc.sync!
+        rescue RestClient::ResourceNotFound
+          #TODO: CouchDB transactions are asynchronous.
+          #      That means that sometimes the server will receive a command to update a database
+          #      that has yet to be created. For now this is a hack that should work most of the time.
+          #      The real solution is to synchronize on the database creation, and only trigger
+          #      the design record sync (and any other updates) when we know the database exists.
+          Rails.logger.warn "Problem recreating databse #{modelCls.database.name}. Trying again"
+          modelCls.database.create!
+          modelCls.design_doc.sync!
+        end
+        modelCls.database.bulk_save(data_arr, false, false)
+      rescue NameError => e
+        Rails.logger.error "Invalid model name in bundle import: #{model_name}"
+        flash[:error] = "Invalid model name #{model_name}"
+        redirect_to :action => :index, :controller => :users and return
+      end
+    end
+    ConfigurationBundle.create! applied_by: applied_by
+    Rails.logger.info "Successfully completed configuration bundle import."
+  end
+
+
   #Although nothing is truly memoized on this class, changes to this will trigger a refresh
   #of the memoization cache for all metadata-type classes
   def self.memoized_dependencies
