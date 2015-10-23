74041f28f36aa8efd84e88f26d70428310c4e15f
Ron Elledge <relledge@quoininc.com>
2015-10-23 12:07:10 -0400

    added approve case plan actions concern. Cleaned up approve case plan modal

--- app/assets/javascripts/views/approve_case_plan.js
+++ app/assets/javascripts/views/approve_case_plan.js
@@ -3,63 +3,14 @@ _primero.Views.ApproveCasePlan = Backbone.View.extend({
   el: '#menu',
 
   events: {
-    'click a.approve_case_plan' : 'approve_case_plan',
     'click div#approve-case-plan-modal input[type="submit"]' : 'submit_approval'
   },
 
-
-  approve_case_plan: function(event) {
-
-  },
-
-  //approve_case_plan: function(event) {
-  //  var approval_button = $(event.target),
-  //      request_url = approval_button.data('approve_case_plan_url'),
-  //      id = approval_button.data('id');
-  //
-  //  $.post(request_url,
-  //    {
-  //      'id': id
-  //    },
-  //    function(response){
-  //      if (response.success) {
-  //        location.reload(true);
-  //      } else {
-  //        $('.flash').remove();
-  //        var message = '<div class="flash row"> <p class="error large-12">' + response.message + '</p></div>';
-  //        $('.page_container').prepend(message);
-  //        setTimeout(function(){
-  //          $('.flash').remove();
-  //        },7000);
-  //      }
-  //    }
-  //  );
-  //},
-
   submit_approval: function(e) {
     e.preventDefault();
-    //var password = $('div#referral-modal input#password').val(),
-    //    local_user = $('div#referral-modal select#existing_user').val(),
-    //    remote_user = $('div#referral-modal input#other_user').val(),
-    //    is_remote = $('div#referral-modal input#is_remote').prop('checked'),
-    //    localUserErrorDiv = $("div#referral-modal .local_user_flash"),
-    //    remoteUserErrorDiv = $("div#referral-modal .remote_user_flash"),
-    //    passwordErrorDiv = $("div#referral-modal .password_flash"),
-    //    is_valid = true;
-    var is_valid = true;
-
-
-
-    if(is_valid){
-      //localUserErrorDiv.hide();
-      //remoteUserErrorDiv.hide();
-      //passwordErrorDiv.hide();
-      $(e.target).parents('form').submit();
-      $('#approve-case-plan-modal').foundation('reveal', 'close');
-      $('#approve-case-plan-modal form')[0].reset();
-      window.disable_loading_indicator = true;
-    } else {
-      return false;
-    }
+    $(e.target).parents('form').submit();
+    $('#approve-case-plan-modal').foundation('reveal', 'close');
+    $('#approve-case-plan-modal form')[0].reset();
+    window.disable_loading_indicator = true;
   }
 });
---
+++ app/controllers/concerns/approve_case_plan_actions.rb
@@ -0,0 +1,27 @@
+module ApproveCasePlanActions
+  extend ActiveSupport::Concern
+
+  def approve_case_plan
+    load_record
+    if @record.present?
+      begin
+        set_approval
+        @record.save!
+      rescue => error
+        logger.error "Case #{@record.id} approve_case_plan... failure"
+        logger.error error.message
+        logger.error error.backtrace
+      end
+    end
+    redirect_to :back
+  end
+
+  private
+
+  def set_approval
+    @record.case_plan_approved = ((params[:approval].present?) && params[:approval] == 'true') ? true : false
+    @record.case_plan_approved_date = Date.today
+    @record.case_plan_approved_comments = params[:comments] if params[:comments].present?
+  end
+
+end
