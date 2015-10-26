015ffd9c4521b98ad3f1489ad06d0a0514ca6f2d
Ron Elledge <relledge@quoininc.com>
2015-10-26 12:26:40 -0400

    renamed a couple variables in cloneable concern

--- app/models/concerns/cloneable.rb
+++ app/models/concerns/cloneable.rb
@@ -6,17 +6,17 @@ module Cloneable
   #without conflicting with the original
   #If a name is passed in, that name is used
   #Otherwise, the current name is prepended with 'copy of'
-  def clone(newName = '')
-    newRecord = self.dup
-    newRecord['_id'] = nil
-    newRecord['_rev'] = nil
-    if newName.present?
-      newRecord.name = newName
+  def clone(new_name = '')
+    new_record = self.dup
+    new_record['_id'] = nil
+    new_record['_rev'] = nil
+    if new_name.present?
+      new_record.name = new_name
     else
-      newRecord.name.prepend('copy of ')
+      new_record.name.prepend('copy of ')
     end
-    newRecord.generate_id
-    newRecord
+    new_record.generate_id
+    new_record
   end
 
 end
