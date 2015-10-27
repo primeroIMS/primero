6a9708120199875b1eb71f5b8c62d7d3ccd840ee
Ron Elledge <relledge@quoininc.com>
2015-10-27 17:00:56 -0400

    added copy_failure error message in copy_actions concern

--- app/controllers/concerns/copy_actions.rb
+++ app/controllers/concerns/copy_actions.rb
@@ -6,13 +6,15 @@ module CopyActions extend ActiveSupport::Concern
       new_record = old_record.clone(name_from_params)
       begin
         if new_record.save
-          flash[:notice] = t("#{model_class.to_s.downcase}.successfully_copied")
+          flash[:notice] = t("#{model_class.to_s.downcase}.copy_success")
         else
+          flash[:notice] = t("#{model_class.to_s.downcase}.copy_failure")
           #TODO - Ticket SL-236 says to fail silently.  Is that so?
         end
       rescue => e
         Rails.logger.error "Error [#{e.to_s}] copying #{model_class}: #{old_record.id} to #{new_record.id}"
         e.backtrace.each { |line| Rails.logger.error line }
+        flash[:notice] = t("#{model_class.to_s.downcase}.copy_failure")
       end
     end
     redirect_to :back
