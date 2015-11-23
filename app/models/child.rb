cc210efc8534fd9a63b46545d1d8aacaa0682e7c
Ron Elledge <relledge@quoininc.com>
2015-11-23 14:27:54 -0500

    added case_id_display as searchable field

--- app/models/child.rb
+++ app/models/child.rb
@@ -159,6 +159,10 @@ class Child < CouchRest::Model::Base
       self.mothers_name
     end
 
+    string :case_id_display do
+      self.case_id_display
+    end
+
     boolean :estimated
     boolean :consent_for_services
   end
