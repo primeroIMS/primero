2f89deccdcf331e84999553915bb7c2642a16f83
Joshua Toliver <jtoliver@quoininc.com>
2016-01-12 09:38:52 -0500

    Corrected min reportable fields

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -101,8 +101,9 @@ class Incident < CouchRest::Model::Base
   def self.minimum_reportable_fields
     {
       'boolean' => ['record_state'],
-       'string' => ['status'],
-         'date' => ['incident_date_derived']
+      'string' => ['status', 'owned_by'],
+      'multistring' => ['associated_user_names'],
+      'date' => ['incident_date_derived']
     }
   end
 
