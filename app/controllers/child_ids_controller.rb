b669a8cdb934edda41ecffa7caddf3a54af0fc01
Raymond <jyyan@thoughtworks.com>
2016-07-12 11:31:57 +0800

    now children-ids can return the ids and revs filter by last update and mobile case

--- app/controllers/child_ids_controller.rb
+++ app/controllers/child_ids_controller.rb
@@ -1,6 +1,35 @@
 class ChildIdsController < ApplicationController
   def all
-    child_json = Child.fetch_all_ids_and_revs
-    render :json => child_json
+    @user = current_user.managed_user_names
+    @children = Child.by_ids_and_revs.select { |f| @user.include? f.owned_by }
+    if params["mobile"]
+      @children = mark_for_mobile(@children)
+    end
+    if params["last_update"]
+      @children = new_update_after_last_update(@children, params["last_update"])
+    end
+    @children = show_ids_and_revs(@children)
+    render :json => @children
   end
+
+  def new_update_after_last_update(children, last_update)
+    children = children.select { |f| f.last_updated_at > last_update }
+    children
+  end
+
+  def mark_for_mobile(children)
+    children = children.select { |f| f.marked_for_mobile? }
+    children
+  end
+
+  def show_ids_and_revs(children)
+    children = children.map do |child|
+      child_ids_and_revs = Hash.new
+      child_ids_and_revs["_id"], child_ids_and_revs["_rev"] = child._id, child._rev
+      child_ids_and_revs
+    end
+    children
+  end
+
+
 end
