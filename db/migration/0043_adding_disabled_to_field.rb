3b9f9ff14c1ef07bfafe154fa139c59a4dca8fd3
Dagna Bieda <dbieda@quoininc.com>
2016-07-19 10:29:05 -0400

    Fixes after review to the 0043* migration file

--- db/migration/0043_adding_disabled_to_field.rb
+++ db/migration/0043_adding_disabled_to_field.rb
@@ -11,20 +11,19 @@ parent_forms.each do |parent_form|
       field_needs_update = field.disabled.nil?
 
       if field_needs_update
+        form_section_modified = true
         if !field.editable.nil?
           field.disabled = !field.editable
-          form_section_modified = true
         else # assign default values
           field.editable = true
           field.disabled = false
-          form_section_modified = true
         end
       end
     end
 
     if form_section_modified
-      form_section.save!
       puts "Updating #{form_section.name.capitalize}"
+      form_section.save!
     else
       puts "Skipping #{form_section.name.capitalize}, already updated!"
     end
