4cc884081391b347d448b342d704f9650e971a3e
Dagna Bieda <dbieda@quoininc.com>
2016-07-19 08:29:08 -0400

    Updated 0043* migration file

--- db/migration/0043_adding_disabled_to_field.rb
+++ db/migration/0043_adding_disabled_to_field.rb
@@ -3,12 +3,31 @@ parent_forms = ['case', 'incident', 'tracing_request']
 parent_forms.each do |parent_form|
   form_sections = FormSection.find_by_parent_form(parent_form)
 
-    form_sections.each do |fs|
-    puts "Analyzing #{fs.name.capitalize}"
-      fs.fields.each do |field|
-      if !field.editable.nil?
-        field.disabled = !field.editable
+  form_sections.each do |form_section|
+    form_section_modified = false
+
+    form_section.fields.each do |field|
+
+      field_needs_update = field.disabled.nil?
+
+      if field_needs_update
+        if !field.editable.nil?
+          field.disabled = !field.editable
+          form_section_modified = true
+        else # assign default values
+          field.editable = true
+          field.disabled = false
+          form_section_modified = true
+        end
       end
     end
+
+    if form_section_modified
+      form_section.save!
+      puts "Updating #{form_section.name.capitalize}"
+    else
+      puts "Skipping #{form_section.name.capitalize}, already updated!"
+    end
+
   end
 end
