60858310774d5b58242d39d461994d530207bbb6
Raymond <jyyan@thoughtworks.com>
2016-08-04 09:18:14 +0000

    add user Access control to match result page

--- app/controllers/potential_matches_controller.rb
+++ app/controllers/potential_matches_controller.rb
@@ -5,12 +5,14 @@ class PotentialMatchesController < ApplicationController
   include RecordFilteringPagination
   include RecordActions
   require "will_paginate/array"
+  ALL_FILTER = "all"
 
   def index
     authorize! :index, model_class
     @page_name = t("home.view_records")
     @aside = "shared/sidebar_links"
     @associated_users = current_user.managed_user_names
+    puts @associated_users
     @filters = record_filter(filter)
     #make sure to get all records when querying for ids to sync down to mobile
     params["page"] = "all" if params["mobile"] && params["ids"]
@@ -28,11 +30,11 @@ class PotentialMatchesController < ApplicationController
 
     case @type
       when "case"
-        @match_results = get_all_case
-        @match_results = get_all_match_details_by_case @match_results, @potential_matches
+        @match_results = get_all_case @associated_users
+        @match_results = get_all_match_details_by_case @match_results, @potential_matches, @associated_users
       when "tracing_request"
-        @match_results = get_all_tr_pairs
-        @match_results = get_all_match_details_by_tr @match_results, @potential_matches
+        @match_results = get_all_tr_pairs @associated_users
+        @match_results = get_all_match_details_by_tr @match_results, @potential_matches, @associated_users
       else
         @match_results = []
     end
@@ -80,9 +82,17 @@ class PotentialMatchesController < ApplicationController
     filter
   end
 
-
-  def get_all_tr_pairs
-    @tracing_requests = TracingRequest.all
+  def get_all_tr_pairs associated_users
+    search = TracingRequest.search do
+      if associated_users.present? && associated_users.first != ALL_FILTER
+        any_of do
+          associated_users.each do |user_name|
+            with(:associated_user_names, user_name)
+          end
+        end
+      end
+    end
+    @tracing_requests = search.results
     match_result=[]
     for t in @tracing_requests
       for subform in t.tracing_request_subform_section
@@ -100,8 +110,17 @@ class PotentialMatchesController < ApplicationController
     match_result
   end
 
-  def get_all_case
-    @cases = Child.all
+  def get_all_case associated_users
+    search = Child.search do
+      if associated_users.present? && associated_users.first != ALL_FILTER
+        any_of do
+          associated_users.each do |user_name|
+            with(:associated_user_names, user_name)
+          end
+        end
+      end
+    end
+    @cases = search.results
     match_result=[]
     for c in @cases
       case_details = {}
@@ -110,14 +129,13 @@ class PotentialMatchesController < ApplicationController
       case_details["age"] = c.age
       case_details["sex"] = c.sex
       case_details["registration_date"] = c.registration_date
-      case_details["owned_by"] = c.owned_by
       case_details["match_details"] = []
       match_result << case_details
     end
     match_result
   end
 
-  def get_all_match_details_by_tr(match_results=[], potential_matches=[])
+  def get_all_match_details_by_tr(match_results=[], potential_matches=[], associated_users)
     for match_result in match_results
       for potential_match in potential_matches
         if potential_match["tr_id"] == match_result["tracing_request_id"] && potential_match["tr_subform_id"] == match_result["subform_tracing_request_id"]
@@ -125,9 +143,9 @@ class PotentialMatchesController < ApplicationController
           match_detail["child_id"] = potential_match.child_id
           child = Child.find_by_case_id potential_match.child_id
           match_detail["case_id"] = potential_match.case_id
-          match_detail["age"] = child.age
-          match_detail["sex"] = child.sex
-          match_detail["registration_date"] = child.registration_date
+          match_detail["age"] = is_visible?(child.owned_by, associated_users) ? child.age : "***"
+          match_detail["sex"] = is_visible?(child.owned_by, associated_users) ? child.sex : "***"
+          match_detail["registration_date"] = is_visible?(child.owned_by, associated_users) ? child.registration_date : "***"
           match_detail["owned_by"] = child.owned_by
           match_detail["average_rating"] =potential_match.average_rating
           match_result["match_details"] << match_detail
@@ -138,7 +156,7 @@ class PotentialMatchesController < ApplicationController
     sort_hash match_results
   end
 
-  def get_all_match_details_by_case(match_results=[], potential_matches=[])
+  def get_all_match_details_by_case(match_results=[], potential_matches=[], associated_users)
     for match_result in match_results
       for potential_match in potential_matches
         if potential_match["case_id"] == match_result["case_id"]
@@ -148,14 +166,13 @@ class PotentialMatchesController < ApplicationController
           match_detail["tr_uuid"] = inquiry._id
           for subform in inquiry.tracing_request_subform_section
             if subform.unique_id == potential_match.tr_subform_id
-              # match_detail["age"] = subform.age
-              # match_detail["sex"] = subform.sex
-              match_detail["subform_tracing_request_name"] =subform.name
+              match_detail["subform_tracing_request_name"] = is_visible?(inquiry.owned_by, associated_users) ? subform.name : "***"
             end
           end
-          match_detail["inquiry_date"] = inquiry.inquiry_date
-          match_detail["relation_name"] = inquiry.relation_name
+          match_detail["inquiry_date"] = is_visible?(inquiry.owned_by, associated_users) ? inquiry.inquiry_date : "***"
+          match_detail["relation_name"] = is_visible?(inquiry.owned_by, associated_users) ? inquiry.relation_name : "***"
           match_detail["average_rating"] =potential_match.average_rating
+          match_detail["owned_by"] =inquiry.owned_by
           match_result["match_details"] << match_detail
         end
       end
@@ -181,4 +198,7 @@ class PotentialMatchesController < ApplicationController
     array
   end
 
+  def is_visible? owner, associated_users
+    return (associated_users.first == ALL_FILTER || associated_users.include?(owner))
+  end
 end
