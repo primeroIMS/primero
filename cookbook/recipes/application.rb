08d94b64e70a622db279dc87ecb2c93f2b3faaf0
Raymond <jyyan@thoughtworks.com>
2016-08-10 17:21:08 +0800

    tempararily change the recipe to repair the test env

--- cookbook/recipes/application.rb
+++ cookbook/recipes/application.rb
@@ -267,6 +267,14 @@ supervisor_service 'who-watches-the-couch-watcher' do
   action [:enable, :stop]
 end
 
+execute_bundle 'reset_app' do
+  command "rake app:reset"
+end
+
+execute_bundle 'remove_all_caches' do
+  command "rails runner Sunspot.remove_all"
+end
+
 execute_bundle 'setup-db-migrate-design-views' do
   command "rake db:migrate:design"
 end
