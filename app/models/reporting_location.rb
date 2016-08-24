d6d35d61478912a05e1fd6ec62acbf1c6d2997eb
Ron Elledge <relledge@quoininc.com>
2016-08-24 16:08:44 -0500

    added reporting_location_config to system_settings seed

--- app/models/reporting_location.rb
+++ app/models/reporting_location.rb
@@ -5,7 +5,18 @@ class ReportingLocation
   property :label_key
   property :admin_level, Integer, :default => 0
 
+  validate :validate_label_key
+
   DEFAULT_FIELD_KEY = 'owned_by_location'
   DEFAULT_LABEL_KEY = 'district'
   DEFAULT_ADMIN_LEVEL = 2
+
+  def validate_label_key
+    if Location::BASE_TYPES.include? self.label_key
+      true
+    else
+      errors.add(:label_key, I18n.t("errors.models.reporting_location.label_key"))
+      false
+    end
+  end
 end
