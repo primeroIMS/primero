dc61e15d3da9eb52acc452951691409995cb2c1a
Dagna Bieda <dbieda@quoininc.com>
2016-08-29 16:37:39 -0400

    Refactored export function of CSVExporter

--- app/models/exporters/csv_exporter.rb
+++ app/models/exporters/csv_exporter.rb
@@ -11,17 +11,12 @@ module Exporters
       def supported_models
         [Child, Incident, TracingRequest]
       end
-
-      # @returns: a String with the CSV data and header
-      def export(models, properties, *args)
-        props = properties_to_export(properties)
-        CSV.generate do |rows|
-          to_2D_array(models, props) do |row|
-            rows << row
-          end
-        end
-      end
-
     end
+
+    def export(models, properties, *args)
+      props = CSVExporter.properties_to_export(properties)
+      self.buffer.write( CSV.generate { |rows| CSVExporter.to_2D_array(models, props) { |row| rows << row } } )
+    end
+
   end
 end
