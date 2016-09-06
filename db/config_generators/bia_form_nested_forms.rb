39f85b5e4950f8453a893ddccae285c5029e6578
Dagna Bieda <dbieda@quoininc.com>
2016-09-06 13:51:02 -0400

    Tiny refactor in bia_form_nested_forms

--- db/config_generators/bia_form_nested_forms.rb
+++ db/config_generators/bia_form_nested_forms.rb
@@ -1,9 +1,32 @@
-mother_fields_subform = [
+mother_name = [
   Field.new({"name" => "relation_name",
              "type" => "text_field",
              "display_name_all" => "Mother's Name",
              "disabled" => true
            }),
+]
+father_name = [
+  Field.new({"name" => "relation_name",
+             "type" => "text_field",
+             "display_name_all" => "Father's Name",
+             "disabled" => true
+           })
+]
+male_caregiver_name = [
+  Field.new({"name" => "relation_name",
+             "type" => "text_field",
+             "display_name_all" => "Male Primary Caregiver's Name (if applicable)",
+             "disabled" => true
+           })
+]
+female_caregiver_name = [
+  Field.new({"name" => "relation_name",
+             "type" => "text_field",
+             "display_name_all" => "Female Primary Caregiver's Name (if applicable)",
+             "disabled" => true
+           })
+]
+nickname_ids = [ 
   Field.new({"name" => "relation_nickname",
              "type" => "text_field",
              "display_name_all" => "Nickname / Alias",
@@ -23,10 +46,33 @@ mother_fields_subform = [
              "display_name_all" => "Ration Card / Service ID Number",
              "type" => "text_field",
              "disabled" => true
-           }),
+           })
+]
+mother_citizenship_origin = [
   Field.new({"name" => "relation_nationality",
              "type" => "select_box",
              "display_name_all" => "Citizenship of the mother",
+             "multi_select" => true,
+             "disabled" => true,
+             "option_strings_source" => "lookup Nationality"
+           }),
+  Field.new({"name" => "relation_nationality_other",
+             "display_name_all" => "Other Citizenship",
+             "type" => "text_field",
+             "disabled" => true
+           }),
+  Field.new({"name" => "relation_country_of_origin",
+             "display_name_all" => "Country of Origin",
+             "type" => "select_box",
+             "option_strings_source" => "lookup Country",
+             "multi_select" => false,
+             "disabled" => true
+           })
+]
+father_citizenship_origin = [
+  Field.new({"name" => "relation_nationality",
+             "type" => "select_box",
+             "display_name_all" => "Citizenship of the father",
              "multi_select" => true,
              "disabled" => true,
              "option_strings_source" => "lookup Nationality"
@@ -51,58 +97,11 @@ mother = FormSection.create_or_update_form_section({
     "is_nested" => true,
     :parent_form => "case",
     "editable" => false,
-    :fields => mother_fields_subform,
+    :fields => mother_name + nickname_ids + mother_citizenship_origin,
     :initial_subforms => 1,
     "name_all" => "Mother",
     "description_all" => "Mother"
 })
-
-father_fields_subform = [
-  Field.new({"name" => "relation_name",
-             "type" => "text_field",
-             "display_name_all" => "Father's Name",
-             "disabled" => true
-           }),
-  Field.new({"name" => "relation_nickname",
-             "type" => "text_field",
-             "display_name_all" => "Nickname / Alias",
-             "disabled" => true
-           }),
-  Field.new({"name" => "relation_unhcr_id_no",
-             "type" => "text_field",
-             "display_name_all" => "UN ID Number",
-             "disabled" => true
-           }),
-  Field.new({"name" => "national_id_no_relation",
-             "type" => "text_field",
-             "display_name_all" => "National ID Number",
-             "disabled" => true
-           }),
-  Field.new({"name" => "ration_card_no_relation",
-             "display_name_all" => "Ration Card / Service ID Number",
-             "type" => "text_field",
-             "disabled" => true
-           }),
-  Field.new({"name" => "relation_nationality",
-             "type" => "select_box",
-             "display_name_all" => "Citizenship of the father",
-             "multi_select" => true,
-             "disabled" => true,
-             "option_strings_source" => "lookup Nationality"
-           }),
-  Field.new({"name" => "relation_nationality_other",
-             "display_name_all" => "Other Citizenship",
-             "type" => "text_field",
-             "disabled" => true
-           }),
-  Field.new({"name" => "relation_country_of_origin",
-             "display_name_all" => "Country of Origin",
-             "type" => "select_box",
-             "option_strings_source" => "lookup Country",
-             "multi_select" => false,
-             "disabled" => true
-           })
-]
 
 father = FormSection.create_or_update_form_section({
     :unique_id => "bia_father_family_details",
@@ -110,50 +109,11 @@ father = FormSection.create_or_update_form_section({
     "is_nested" => true,
     :parent_form => "case",
     "editable" => false,
-    :fields => father_fields_subform,
+    :fields => father_name + nickname_ids + father_citizenship_origin,
     :initial_subforms => 1,
     "name_all" => "Father",
     "description_all" => "Father"
 })
-
-male_caregiver_name = [
-  Field.new({"name" => "relation_name",
-             "type" => "text_field",
-             "display_name_all" => "Male Primary Caregiver's Name (if applicable)",
-             "disabled" => true
-           })
-]
-
-female_caregiver_name = [
-  Field.new({"name" => "relation_name",
-             "type" => "text_field",
-             "display_name_all" => "Female Primary Caregiver's Name (if applicable)",
-             "disabled" => true
-           })
-]
-
-nickname_ids = [ 
-  Field.new({"name" => "relation_nickname",
-             "type" => "text_field",
-             "display_name_all" => "Nickname / Alias",
-             "disabled" => true
-           }),
-  Field.new({"name" => "relation_unhcr_id_no",
-             "type" => "text_field",
-             "display_name_all" => "UN ID Number",
-             "disabled" => true
-           }),
-  Field.new({"name" => "national_id_no_relation",
-             "type" => "text_field",
-             "display_name_all" => "National ID Number",
-             "disabled" => true
-           }),
-  Field.new({"name" => "ration_card_no_relation",
-             "display_name_all" => "Ration Card / Service ID Number",
-             "type" => "text_field",
-             "disabled" => true
-           })
-]
 
 male_caregiver = FormSection.create_or_update_form_section({
     :unique_id => "bia_male_caregiver_family_details",
