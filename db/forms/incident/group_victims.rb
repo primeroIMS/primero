7d743d1cad9131dbe40e6e7806b1ba903dc4ffd3
Ron Elledge <relledge@quoininc.com>
2016-12-09 08:57:47 -0600

    renamed group_details form to group_victims

--- db/forms/incident/group_victims.rb
+++ db/forms/incident/group_victims.rb
@@ -1,4 +1,4 @@
-group_details_section_fields_subform = [
+group_victims_section_fields_subform = [
   Field.new({"name" => "group_description",
              "type" => "text_field",
              "display_name_all" => "Description of the Group of Children"
@@ -61,43 +61,53 @@ group_details_section_fields_subform = [
             })
 ]
 
-group_details_section = FormSection.create_or_update_form_section({
+group_victims_section = FormSection.create_or_update_form_section({
   "visible"=>false,
   "is_nested"=>true,
   :order_form_group => 60,
   :order => 10,
   :order_subform => 1,
-  :unique_id=>"group_details_section",
+  :unique_id=>"group_victims_section",
   :parent_form=>"incident",
   "editable"=>true,
-  :fields => group_details_section_fields_subform,
-  "name_all" => "Nested Group Details",
-  "description_all" => "Group Details Subform",
+  :fields => group_victims_section_fields_subform,
+  "name_all" => "Nested Group Victims",
+  "description_all" => "Group Victims Subform",
   "collapsed_fields" => ["group_gender", "group_age_band"],
   :initial_subforms => 1
 })
 
-group_details_fields = [
+group_victims_fields = [
+  #The sole purpose of this field is to have Guiding Questions above the subforms
+  Field.new({"name" => "group_victims_guiding_questions",
+             "type" => "select_box",
+             "display_name_all" => "Definition",
+             "disabled" => true,
+             "option_strings_text_all" => ["Please read guidance text below for the violation definition.",
+                                           "Other"].join("\n"),
+             "selected_value" => "Please read guidance text below for the violation definition.",
+             "guiding_questions" => "Please enter here any information on group(s) of unidentified victims"
+            }),
   ##Subform##
-  Field.new({"name" => "group_details_section",
+  Field.new({"name" => "group_victims_section",
              "type" => "subform",
              "editable" => true,
-             "subform_section_id" => group_details_section.unique_id,
-             "display_name_all" => "Group Details"
+             "subform_section_id" => group_victims_section.unique_id,
+             "display_name_all" => "Group Victims"
             }),
   ##Subform##
 ]
 
 FormSection.create_or_update_form_section({
-  :unique_id => "group_details",
+  :unique_id => "group_victims",
   :parent_form=>"incident",
   "visible" => true,
   :order_form_group => 60,
   :order => 10,
   :order_subform => 0,
-  :form_group_name => "Group Details",
+  :form_group_name => "Group Victims",
   "editable" => true,
-  :fields => group_details_fields,
-  "name_all" => "Group Details",
-  "description_all" => "Group Details"
+  :fields => group_victims_fields,
+  "name_all" => "Group Victims",
+  "description_all" => "Group Victims"
 })
