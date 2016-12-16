17bc98f8c147b2bca7d055ecd009707e15ae9307
Ron Elledge <relledge@quoininc.com>
2016-12-16 17:33:01 -0600

    adding agencies. WIP

--- db/users/default_agencies.rb
+++ db/users/default_agencies.rb
@@ -12,7 +12,69 @@ def create_or_update_agency(agency_hash)
 
 end
 
+#TODO - ADD LOGOS
+
 create_or_update_agency(
-  name: "UNICEF",
-  agency_code: "UN"
+    {
+      name: "DPA",
+      description: "",
+      disabled: "",
+      order: 0,
+      logo_enabled: false,
+      core_resource: false,
+      agency_code: "DPA",
+      logo_key: "DPA_logo",
+    }
+)
+
+create_or_update_agency(
+    {
+      name: "DPKO",
+      description: "",
+      disabled: "",
+      order: 0,
+      logo_enabled: false,
+      core_resource: false,
+      agency_code: "DPKO",
+      logo_key: "dpko_logo",
+    }
+)
+
+create_or_update_agency(
+    {
+      name: "MRM",
+      description: "",
+      disabled: "",
+      order: 0,
+      logo_enabled: false,
+      core_resource: false,
+      agency_code: "MRM",
+      logo_key: "MRMIMS_logo",
+    }
+)
+
+create_or_update_agency(
+    {
+      name: "OSRSG-CAAC",
+      description: "",
+      disabled: "",
+      order: 0,
+      logo_enabled: false,
+      core_resource: false,
+      agency_code: "OSRSG-CAAC",
+      logo_key: "OSRSG_logo",
+    }
+)
+
+create_or_update_agency(
+    {
+      name: "UNICEF",
+      description: "",
+      disabled: "",
+      order: 0,
+      logo_enabled: false,
+      core_resource: false,
+      agency_code: "UN",
+      logo_key: "unicef_logo",
+    }
 )
