24e53953c727e8406f51aa5f820ad9b4bbd6bd9e
Ron Elledge <relledge@quoininc.com>
2016-12-21 15:57:48 -0500

    Renamed Supporting Documents to Supporting Materials

---
+++ db/forms/incident/supporting_materials.rb
@@ -0,0 +1,22 @@
+supporting_materials_fields = [
+    Field.new({"name" => "upload_document",
+              "type" => "document_upload_box",
+              "editable" => false,
+              "display_name_all" => "Supporting Document"
+             })
+]
+
+FormSection.create_or_update_form_section({
+  :unique_id => "supporting_materials",
+  :parent_form=>"incident",
+  "visible" => true,
+  :order_form_group => 100,
+  :order => 11,
+  :order_subform_ => 0,
+  :fields => supporting_materials_fields,
+  "editable" => false,
+  "name_all" => "Supporting Materials",
+  "description_all" => "Supporting Materials",
+  "form_group_name" => "Supporting Materials",
+  "display_help_text_view" => true
+})
