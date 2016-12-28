85b1d5ce293e014ddaf0461e12b5ac3ec115d1b4
Ron Elledge <relledge@quoininc.com>
2016-12-28 17:31:46 -0500

    added new MRM roles

--- db/users/default_users.rb
+++ db/users/default_users.rb
@@ -123,7 +123,7 @@ create_or_update_user(
   "disabled" => "false",
   "organization" => "agency-unicef",
   "role_ids" => [
-    Role.by_name(key: "MRM Worker").first.id
+    Role.by_name(key: "MRM Specialist (editing)").first.id
   ],
   "module_ids" => [PrimeroModule.by_name(key: "MRM").first.id],
   "user_group_ids" => [UserGroup.by_name(key: "Primero MRM").first.id]
@@ -138,7 +138,7 @@ create_or_update_user(
   "disabled" => "false",
   "organization" => "agency-unicef",
   "role_ids" => [
-    Role.by_name(key: "MRM Manager").first.id
+    Role.by_name(key: "MRM Co-chair (editing)").first.id
   ],
   "module_ids" => [PrimeroModule.by_name(key: "MRM").first.id],
   "user_group_ids" => [UserGroup.by_name(key: "Primero MRM").first.id],
