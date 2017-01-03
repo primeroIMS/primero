5d7dbcc3f2f87cbec36ef19dce71f6cd24fb4fbb
Ron Elledge <relledge@quoininc.com>
2017-01-03 09:53:56 -0600

    added guiding text to sexual violence form.  Other small changes per UAT

--- db/forms/incident/sexual_violence.rb
+++ db/forms/incident/sexual_violence.rb
@@ -12,9 +12,14 @@ sexual_violence_subform_fields = [
              "type" => "select_box",
              "multi_select" => true,
              "display_name_all" => "Form(s) of sexual violence",
-             "option_strings_text_all" => ["Rape", "Enforced prostitution", "Enforced sterilization", "Forced pregnancy",
-                                           "Forced abortion", "Mutilation", "Sexual harrassment/assault",
-                                           "Sexual slavery and/or trafficking", "Sexual exploitation and/or abuse"].join("\n")
+             "option_strings_text_all" => ["Rape", "Sexual assault", "Sexual slavery and/or trafficking",
+                                           "Enforced prostitution", "Enforced sterilization", "Forced pregnancy",
+                                           "Forced abortion", "Forced marriage", "Sexual mutilation"].join("\n"),
+             "guiding_questions" => "For MRM purposes, incidents or patterns of rape and/or other sexual violence should "\
+                                    "have a direct or indirect nexus with the armed conflict, i.e. a temporal, geographical "\
+                                    "and/or causal link. The link with conflict may be evident e.g. in the profile and "\
+                                    "motivations of the perpetrator(s) or in the profile of the victim(s). Incidents of "\
+                                    "Sexual Exploitation and Abuse (SEA) should not be recorded in the MRMIMS+."
              }),
   Field.new({"name" => "associated_violation_status",
              "type" => "select_box",
@@ -32,7 +37,16 @@ sexual_violence_subform_fields = [
              "type" => "select_box",
              "multi_select" => true,
              "display_name_all" => "What implications did the sexual violence have?",
-             "option_strings_text_all" => ["Child(ren) born out of rape", "health implications"].join("\n")
+             "option_strings_text_all" => ["Child(ren) born out of rape", "Complications from unsafe abortion",
+                                           "Infanticide", "Death", "Injuries", "Disability/Physical impairment",
+                                           "Acute/chronic illness", "Sexually transmitted diseases (e.g. HIV/AIDS)",
+                                           "Mental health problems (e.g. depression, anxiety, post-traumatic stress disorder)",
+                                           "Suicide", "Family rejection", "Social stigma/Community ostracism",
+                                           "Investigation/prosecution/arrest", "School dropout"].join("\n"),
+             "guiding_quiestions" => "This field is not compulsory.The relevant information should not be actively "\
+                                     "sought/pursued by the MRM monitors, and should only be recorded in the database "\
+                                     "if spontaneously provided by the victim or otherwise available without interviewing "\
+                                     "the victim."
             }),
   Field.new({"name" => "sexual_violence_crossborder",
              "type" => "select_box",
@@ -63,6 +77,20 @@ sexual_violence_subform_section = FormSection.create_or_update_form_section({
 })
 
 sexual_violence_fields = [
+  #The sole purpose of this field is to have Guiding Questions above the subforms
+  Field.new({"name" => "sexual_violence_guiding_questions",
+             "type" => "select_box",
+             "display_name_all" => "Definition",
+             "disabled" => true,
+             "option_strings_text_all" => ["Please read guidance text below for the violation definition.",
+                                           "Other"].join("\n"),
+             "selected_value" => "Please read guidance text below for the violation definition.",
+             "guiding_questions" => "For guidance on the definition of rape and other grave sexual violence for MRM "\
+                                    "purposes see MRM Field Manual, p. 6 and Annex 1: Glossary of Terms and Acronyms, "\
+                                    "p. 64; Annex 2: International Legal Foundations and Standards, pp. 79-70; Annex 4: "\
+                                    "Q & A Guidance on Security Council Resolution 1882, pp. 7-8."
+            }),
+  ##Subform##
   Field.new({"name" => "sexual_violence",
              "type" => "subform", "editable" => true,
              "subform_section_id" => sexual_violence_subform_section.unique_id,
