49f6288121f6398cb6f86f4f4820b8e7504fddbe
Matt Counts <mcounts@quoininc.com>
2017-01-23 11:29:35 -0500

    javascript formatting clean up

--- app/assets/javascripts/views/management/form_fields.js
+++ app/assets/javascripts/views/management/form_fields.js
@@ -1,18 +1,18 @@
 var FormFields = {
-  init : function(options, elem) {
-    this.options = $.extend({},this.options,options);
+  init: function(options, elem) {
+    this.options = $.extend({}, this.options,options);
     this.elem  = elem;
     this.$elem = $(elem);
     this._build();
     return this;
   },
 
-  _build : function() {
+  _build: function() {
     var self = this;
     self.$elem.find(".form").live("click", function() {
       self.selectForm($(this));
     });
-    self.$elem.find(".field:not(.prev-selected)").live("click", function(){
+    self.$elem.find(".field:not(.prev-selected)").live("click", function() {
       self.selectItem($(this));
     });
     self.$elem.find(".close-link").live("click", function() {
@@ -20,44 +20,46 @@ var FormFields = {
     });
   },
 
-  selectForm : function(form){
+  selectForm: function(form) {
     var self = this;
     var $selected_form = self.$elem.find(".form.selected");
     var $form = $(form);
-    if($selected_form){
+    if ($selected_form) {
       $selected_form.removeClass("selected");
-      self.$elem.find("#fields-for-"+$selected_form.attr("id")).removeClass("selected");
+      self.$elem.find("#fields-for-" + $selected_form.attr("id")).removeClass("selected");
     }
     $form.addClass("selected");
-    self.$elem.find("#fields-for-"+$form.attr("id")).addClass("selected");
+    self.$elem.find("#fields-for-" + $form.attr("id")).addClass("selected");
   },
 
-  selectItem: function(select_element){
+  selectItem: function(select_element) {
     var self = this;
     self.hide();
-    var selected_field = { field_name: $(select_element).data("name"),
-                          form_id: $(select_element).parents("ul").data("id") };
+    var selected_field = {
+      field_name: $(select_element).data("name"),
+      form_id: $(select_element).parents("ul").data("id")
+    };
     self.options.onItemSelect(selected_field);
   },
 
-  show : function(args) {
+  show: function(args) {
     var self = this;
     self.reset(args.prevSelectedFields);
-    if(args.actionElement && args.actionElement.position().top && args.actionElement.position().left && args.actionElement.width()){
+    if (args.actionElement && args.actionElement.position().top && args.actionElement.position().left && args.actionElement.width()) {
       self.$elem.css("top", args.actionElement.position().top + "px");
-      self.$elem.css("left", args.actionElement.position().left + args.actionElement.width() +  "px");
+      self.$elem.css("left", args.actionElement.position().left + args.actionElement.width() + "px");
     }
     self.$elem.show();
   },
 
-  hide : function( ) {var self = this; self.$elem.hide(); },
+  hide: function() {var self = this; self.$elem.hide();},
 
-  reset : function(prev_selected_fields) {
+  reset: function(prev_selected_fields) {
     var self = this;
     var first_form = self.$elem.find(".form").first();
     self.selectForm(first_form);
-    $.each(prev_selected_fields, function(index, field_name){
-     self.$elem.find('#field-'+field_name).addClass("prev-selected");
+    $.each(prev_selected_fields, function(index, field_name) {
+     self.$elem.find('#field-' + field_name).addClass("prev-selected");
     });
   }
 };
@@ -65,11 +67,11 @@ var FormFields = {
 $.plugin('formFields', FormFields);
 
 $(function() {
-  $("#locale").change( function(event){
+  $("#locale").change(function(event) {
     var language_field = $(event.target);
     var locale = language_field.val();
     $(".translation_fields").hide();
-    $("div ."+locale).show();
+    $("div ." + locale).show();
   });
 });
 
