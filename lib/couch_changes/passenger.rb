ca90a8e1855ea94aae82cdbef66d785ba2f2b8e5
Joshua Ra'anan <jraanan@quoininc.com>
2017-03-23 13:55:01 -0400

    Rename Phusion functions according to updated syntax

--- lib/couch_changes/passenger.rb
+++ lib/couch_changes/passenger.rb
@@ -8,8 +8,9 @@ module CouchChanges
 
           PhusionPassenger.locate_directories
           PhusionPassenger.require_passenger_lib 'platform_info'
-          PhusionPassenger.require_passenger_lib 'admin_tools/instance'
           PhusionPassenger.require_passenger_lib 'admin_tools/instance_registry'
+          PhusionPassenger.require_passenger_lib 'admin_tools/instance'
+
 
           class << self
             include PhusionPassenger::AdminTools
@@ -19,13 +20,13 @@ module CouchChanges
         end
       end
 
-      def client
-        @_client ||= server_instance.connect(:role => :passenger_status)
-      end
+      # def client
+      #   @_client ||= server_instance.connect(:role => :passenger_status)
+      # end
 
       def server_instance
         initialize_passenger
-        @_server_instance ||= ServerInstance.list.tap do |instances|
+        @_server_instance ||= InstanceRegistry.new().list do |instances|
           if instances.length == 0
             CouchChanges.logger.error "No Passenger servers found!"
             raise PassengerNotRunningError
@@ -37,12 +38,16 @@ module CouchChanges
       end
 
       def http_process_info
-        procs = reset_client_if_necessary { server_instance.processes(client) }
-        procs.map do |p|
+        # procs = reset_client_if_necessary { server_instance.processes(client) }
+        # procs.map do |p|
+        @_server_instance.each do |p|
           OpenStruct.new({
-            :address => "http://#{p.server_sockets[:http].address.gsub('tcp://', '')}",
-            :pid => p.pid,
-            :password => p.connect_password,
+            # :address => "http://#{p.server_sockets[:http].address.gsub('tcp://', '')}",
+            :address => "http://"
+            # :pid => p.pid,
+            :pid => p.web_server_control_process_pid,
+            # :password => p.connect_password,
+            :password => p.full_admin_password,
           })
         end
       end
@@ -54,7 +59,7 @@ module CouchChanges
         begin
           block.call
         rescue Errno::EPIPE
-          @_client = nil
+          # @_client = nil
           block.call
         end
       end
