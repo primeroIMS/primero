fafc22602fd2996c7cb9df8867f2a2ffe2f24525
Joshua Toliver <jtoliver@quoininc.com>
2017-03-30 16:52:08 -0400

    Taking care of more cases in forms migration, cleanup, and adding record migration

--- db/i18n_migration/migration_helper.rb
+++ db/i18n_migration/migration_helper.rb
@@ -15,4 +15,26 @@ module MigrationHelper
       end
     end
   end
+
+  def create_locales
+    Primero::Application::locales.each do |locale|
+      yield(locale)
+    end
+  end
+
+  def get_field_options(prefix)
+    fields = {}
+    prefix = 'case' if prefix == 'child'
+    field_types = ['select_box', 'tick_box', 'radio_button']
+
+    FormSection.find_by_parent_form(prefix).each do |fs|
+      i18n_fields = fs.fields.select{|f| field_types.include?(f.type)}
+
+      i18n_fields.each do |f|
+        fields[f.name] = f.options_list if f.options_list.present?
+      end
+    end
+
+    fields
+  end
 end
--- db/i18n_migration/migrations/lookups.rb
+++ db/i18n_migration/migrations/lookups.rb
@@ -1,14 +1,15 @@
-include MigrationHelper
-
 puts 'Migrating (i81n): Lookups'
 
+# What if repo adds new lookups and user added options
 Lookup.all.rows.map {|r| Lookup.database.get(r["id"]) }.each do |lookup|
   if lookup["lookup_values"].present?
     value = MigrationHelper.generate_keyed_value(lookup['lookup_values'])
 
-    Primero::Application::locales.each do |locale|
+    MigrationHelper.create_locales do |locale|
       lookup["lookup_values_#{locale}"] = value
     end
+
+    lookup.delete("lookup_values")
   end
 
   lookup.save
