00885e939a4e23473b0f7224b8bbb7354a025a21
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2017-03-30 11:24:15 -0400

    Repointing CouchWatcher to use the bundler-installed Passenger library

--- lib/couch_changes/passenger.rb
+++ lib/couch_changes/passenger.rb
@@ -4,8 +4,8 @@ module CouchChanges
     class << self
       def initialize_passenger
         if !@_passenger_loaded
-          ENV["PASSENGER_LOCATION_CONFIGURATION_FILE"] = "/usr/lib/ruby/vendor_ruby/phusion_passenger/locations.ini"
-          require '/usr/lib/ruby/vendor_ruby/phusion_passenger'
+          #ENV["PASSENGER_LOCATION_CONFIGURATION_FILE"] = "/usr/lib/ruby/vendor_ruby/phusion_passenger/locations.ini"
+          require 'phusion_passenger'
 
           PhusionPassenger.locate_directories
           PhusionPassenger.require_passenger_lib 'platform_info'
