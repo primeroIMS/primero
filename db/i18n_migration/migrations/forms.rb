e59b2375929e5af51d3e0cb9a036f8e921c0e5a7
Joshua Toliver <jtoliver@quoininc.com>
2017-03-30 16:52:08 -0400

    Taking care of more cases in forms migration, cleanup, and adding record migration

--- db/i18n_migration/migrations/forms.rb
+++ db/i18n_migration/migrations/forms.rb
@@ -1,24 +1,35 @@
-include MigrationHelper
-
 puts 'Migrating (i81n): FormSections'
 
 FormSection.all.rows.map {|r| FormSection.database.get(r["id"]) }.each do |fs|
   fs['fields'].each do |field|
-    if field['type'] == 'check_boxes'
+    case field['type']
+    when 'check_boxes'
       field['type'] = Field::FIELD_DISPLAY_TYPES['tick_box']
 
       value = MigrationHelper.generate_keyed_value('Yes')
 
-      Primero::Application::locales.each do |locale|
+      MigrationHelper.create_locales do |locale|
         field["tick_box_label_#{locale}"] = value
+        field.delete("option_strings_text_#{locale}")
       end
-    end
+    when 'radio_button'
+      field['options_string_source'] = "lookup lookup-yes-no"
 
-    if field["option_strings_text_#{I18n.default_locale}"].present?
-      value = MigrationHelper.generate_keyed_value(field['option_strings_text_en'])
+      MigrationHelper.create_locales do |locale|
+        field.delete("option_strings_text_#{locale}")
+      end
+    else
+      if field['option_strings_source'] =~ /\Alookup/
+        lookup = field['option_strings_source'].match(/\w+\b(?<!lookup).*/)
+        field['option_strings_source'] = "lookup lookup-#{lookup[0].downcase.parameterize}" if lookup.present?
+      else
+        if field['option_strings_text_en'].present?
+          value = MigrationHelper.generate_keyed_value(field['option_strings_text_en'])
 
-      Primero::Application::locales.each do |locale|
-        field["option_strings_text_#{locale}"] = value
+          MigrationHelper.create_locales do |locale|
+            field["option_strings_text_#{locale}"] = value
+          end
+        end
       end
     end
   end
--- db/i18n_migration/migrations/records.rb
+++ db/i18n_migration/migrations/records.rb
@@ -1,17 +1,17 @@
-Records = [Child, TracingRequest, Incident];
-
 puts 'Migrating (i81n): Records'
 
-form_sections = FormSection.all.rows.map {|r| FormSection.database.get(r["id"]) }
+Records = [Child, TracingRequest, Incident];
 
+# TODO: Do some more testing
 Records.each do |record_type|
+  field_options = MigrationHelper.get_field_options(record_type.locale_prefix)
+
   record_type.all.rows.map {|r| record_type.database.get(r["id"]) }.each do |record|
-    # form_sections.each do |fs|
-    #   fs[fields].each do |field|
-    #     if ['select_box', 'tick_box', 'tally_field'].include?(field[:type])
-    #       binding.pry; x = 0;
-    #     end
-    #   end
-    # end
+    record.keys.each do |key|
+      if field_options[key].present? && field_options[key].is_a?(Object) && record[key].present?
+        value = field_options[key].select{|fo| fo['display_name'] == record[key]}
+        record[key] = value if value.present?
+      end
+    end
   end
 end
