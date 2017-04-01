2691acbe0a8ca296bc2365d2df14c151667e5c8b
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2017-04-01 16:29:40 -0400

    Standalone passenger SSL redirects working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,8 @@ Primero::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.force_ssl = true
+
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
