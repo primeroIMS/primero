d6574115635c8ec2c2fa8826608eca6f3b260438
Ron Elledge <relledge@quoininc.com>
2017-04-12 10:26:33 -0500

    added parens to helper methods in potential_match_helper

--- app/helpers/potential_match_helper.rb
+++ app/helpers/potential_match_helper.rb
@@ -1,13 +1,13 @@
 module PotentialMatchHelper
-  def text_to_identify_potential_match potential_match
+  def text_to_identify_potential_match(potential_match)
     "#{potential_match.short_id}"
   end
 
-  def text_to_identify_potential_match_child potential_match
+  def text_to_identify_potential_match_child(potential_match)
     "#{potential_match.case_id.last 7}"
   end
 
-  def text_to_identify_potential_match_tracing_request potential_match
+  def text_to_identify_potential_match_tracing_request(potential_match)
     "#{potential_match.tracing_request_id.last 7}"
   end
 
