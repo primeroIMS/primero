53c7b086c9a24621d84cdec1c42af9f75b3db897
Joshua Ra'anan <jraanan@quoininc.com>
2017-04-12 15:05:53 -0400

    restore correct passenger.rb file

--- lib/couch_changes/passenger.rb
+++ lib/couch_changes/passenger.rb
@@ -8,9 +8,7 @@ module CouchChanges
 
           PhusionPassenger.locate_directories
           PhusionPassenger.require_passenger_lib 'platform_info'
-          PhusionPassenger.require_passenger_lib 'admin_tools/instance_registry'
-          PhusionPassenger.require_passenger_lib 'admin_tools/instance'
-
+          PhusionPassenger.require_passenger_lib 'admin_tools/server_instance'
 
           class << self
             include PhusionPassenger::AdminTools
@@ -20,13 +18,13 @@ module CouchChanges
         end
       end
 
-      # def client
-      #   @_client ||= server_instance.connect(:role => :passenger_status)
-      # end
+      def client
+        @_client ||= server_instance.connect(:role => :passenger_status)
+      end
 
       def server_instance
         initialize_passenger
-        @_server_instance ||= InstanceRegistry.new().list do |instances|
+        @_server_instance ||= ServerInstance.list.tap do |instances|
           if instances.length == 0
             CouchChanges.logger.error "No Passenger servers found!"
             raise PassengerNotRunningError
@@ -38,16 +36,12 @@ module CouchChanges
       end
 
       def http_process_info
-        # procs = reset_client_if_necessary { server_instance.processes(client) }
-        # procs.map do |p|
-        @_server_instance.each do |p|
+        procs = reset_client_if_necessary { server_instance.processes(client) }
+        procs.map do |p|
           OpenStruct.new({
-            # :address => "http://#{p.server_sockets[:http].address.gsub('tcp://', '')}",
-            :address => "http://"
-            # :pid => p.pid,
-            :pid => p.web_server_control_process_pid,
-            # :password => p.connect_password,
-            :password => p.full_admin_password,
+            :address => "http://#{p.server_sockets[:http].address.gsub('tcp://', '')}",
+            :pid => p.pid,
+            :password => p.connect_password,
           })
         end
       end
@@ -59,7 +53,7 @@ module CouchChanges
         begin
           block.call
         rescue Errno::EPIPE
-          # @_client = nil
+          @_client = nil
           block.call
         end
       end
