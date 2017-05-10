261549253757d6ae8de37e79a24f29ae9dc9a339
Matt Counts <mcounts@quoininc.com>
2017-05-10 11:51:46 -0400

    JOR-592 cp incident fields

---
+++ db/forms/incident/cp_incident_record_owner.rb
@@ -0,0 +1,89 @@
+cp_record_owner_fields = [
+  Field.new({
+    "name" => "current_owner_section",
+    "type" => "separator",
+    "display_name_all" => "Current Owner"
+  }),
+  Field.new({
+    "name" => "caseworker_name",
+    "type" => "text_field",
+    "display_name_all" => "Field/Case/Social Worker",
+    "visible" => false
+  }),
+  Field.new({
+    "name" => "owned_by",
+    "type" => "select_box",
+    "display_name_all" => "Caseworker Code",
+    "option_strings_source" => "User",
+    "editable" => false,
+    "disabled" => true
+  }),
+  Field.new({
+    "name" => "owned_by_agency",
+    "type" => "select_box",
+    "display_name_all" => "Agency",
+    "editable" => false,
+    "disabled" => true,
+    "option_strings_source" => "Agency",
+    "visible" => false
+  }),
+  Field.new({
+    "name" => "assigned_user_names",
+    "type" =>"select_box",
+    "multi_select" => true,
+    "display_name_all" => "Other Assigned Users",
+    "option_strings_source" => "User"
+  }),
+  Field.new({
+    "name" => "record_history_section",
+    "type" => "separator",
+    "display_name_all" => "Record History"
+  }),
+  Field.new({
+    "name" => "created_by",
+    "type" => "text_field",
+    "display_name_all" => "Record created by",
+    "editable" => false,
+    "disabled" => true
+  }),
+  Field.new({
+    "name" => "created_organization",
+    "type" => "text_field",
+    "display_name_all" => "Created by agency",
+    "editable" => false,
+    "disabled" => true
+  }),
+  Field.new({
+    "name" => "previously_owned_by",
+    "type" => "text_field",
+    "display_name_all" => "Previous Owner",
+    "editable" => false,
+    "disabled" => true
+  }),
+  Field.new({
+    "name" => "previous_agency",
+    "type" => "text_field",
+    "display_name_all" => "Previous Agency"
+  }),
+  Field.new({
+    "name" => "module_id",
+    "type" => "text_field",
+    "display_name_all" => "Module",
+    "editable" => false,
+    "disabled" => true
+  }),
+]
+
+FormSection.create_or_update_form_section({
+  :unique_id => "cp_incident_record_owner",
+  :parent_form=>"incident",
+  "visible" => true,
+  :order_form_group => 0,
+  :order => 0,
+  :order_subform => 0,
+  :form_group_name => "Record Owner",
+  "editable" => true,
+  :fields => cp_record_owner_fields,
+  "name_all" => "CP Record Owner",
+  "description_all" => "Record Owner"
+})
---
+++ db/forms/incident/cp_other_reportable_fields.rb
@@ -0,0 +1,54 @@
+fields = [
+  Field.new({
+    "name" => "record_state",
+    "type" => "tick_box",
+    "editable" => false,
+    "disabled" => true,
+    "display_name_all" => "Valid Record?"
+  }),
+  Field.new({
+    "name" => "owned_by_agency",
+    "type" => "select_box",
+    "display_name_all" => "Case Manager's Agency",
+    "editable" => false,
+    "disabled" => true,
+    "option_strings_source" => "Agency"
+  }),
+  Field.new({
+    "name" => "owned_by_location",
+    "type" => "select_box",
+    "display_name_all" => "Case Manager's Location",
+    "searchable_select" => true,
+    "editable" => false,
+    "disabled" => true,
+    "option_strings_source" => "Location"
+  }),
+  Field.new({
+    "name" => "has_referrals",
+    "type" => "tick_box",
+    "display_name_all" => "Does this case have any referrals?",
+    "editable" => false,
+    "disabled" => true,
+    "create_property" => false
+  }),
+  Field.new({
+    "name" => "has_case_plan",
+    "type" => "tick_box",
+    "display_name_all" => "Does this case have a case plan?",
+    "editable" => false,
+    "disabled" => true,
+    "create_property" => false
+  })
+]
+
+FormSection.create_or_update_form_section({
+  :unique_id=>"cp_other_reportable_fields",
+  :parent_form=>"case",
+  "visible" => false,
+  :order => 1000,
+  :order_form_group => 1000,
+  "editable" => true,
+  :fields => fields,
+  "name_all" => "Other Reportable Fields",
+  "description_all" => "Other Reportable Fields"
+})
