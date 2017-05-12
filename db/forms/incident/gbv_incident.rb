9f1c712cfc5ffe2a2e2c1356710c9b1000fa9909
Ron Elledge <relledge@quoininc.com>
2017-05-12 14:44:47 -0500

    added marked_for_mobile to GBV Incident form

--- db/forms/incident/gbv_incident.rb
+++ db/forms/incident/gbv_incident.rb
@@ -13,6 +13,14 @@ gbv_incident_fields = [
              "display_name_all" => "Incident ID",
              "create_property" => false
             }),
+  Field.new({"name" => "marked_for_mobile",
+             "type" => "tick_box",
+             "tick_box_label_all" => "Yes",
+             "display_name_all" => "Marked for mobile?",
+             "editable" => false,
+             "disabled" => true,
+             "create_property" => false
+            }),
   Field.new({"name" => "incident_code",
              "type" => "text_field",
              "editable" => false,
