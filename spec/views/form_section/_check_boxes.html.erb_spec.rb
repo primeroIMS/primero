a70421f55f1a609e0479f4bc48c494e626f8e180
Joshua Ra'anan <jraanan@quoininc.com>
2017-06-12 16:27:19 -0400

    Create attribute 'is_disabled' to be used to enable subform fields taken from template

--- spec/views/form_section/_check_boxes.html.erb_spec.rb
+++ spec/views/form_section/_check_boxes.html.erb_spec.rb
@@ -31,4 +31,40 @@ describe "form_section/_check_boxes.html.erb" do
     rendered.should_not have_tag("p.help")
   end
 
+  it "should have 'is_disabled=true' when is disabled" do
+    check_boxes = Field.new :name => "new field",
+    :display_name => "field name",
+    :type => 'check_boxes',
+    :option_strings => ["FOO", "BAR"]
+    check_boxes.disabled = true
+
+    check_boxes.should_receive(:form).exactly(3).times.and_return(FormSection.new("name" => "form_section"))
+    render :partial => 'form_section/check_boxes', :locals => { :check_boxes => check_boxes, :formObject => @child, :is_subform => true }, :formats => [:html], :handlers => [:erb]
+    expect(rendered).to include('is_disabled="true"')
+  end
+
+  it "should have 'is_disabled=false' when is not disabled" do
+    check_boxes = Field.new :name => "new field",
+    :display_name => "field name",
+    :type => 'check_boxes',
+    :option_strings => ["FOO", "BAR"]
+    check_boxes.disabled = false
+
+    check_boxes.should_receive(:form).exactly(3).times.and_return(FormSection.new("name" => "form_section"))
+    render :partial => 'form_section/check_boxes', :locals => { :check_boxes => check_boxes, :formObject => @child, :is_subform => true }, :formats => [:html], :handlers => [:erb]
+    expect(rendered).to include('is_disabled="false"')
+  end
+
+  it "should have 'disable' attribute when is_subform even if disabled is false" do
+    check_boxes = Field.new :name => "new field",
+    :display_name => "field name",
+    :type => 'check_boxes',
+    :option_strings => ["FOO", "BAR"]
+    check_boxes.disabled = false
+
+    check_boxes.should_receive(:form).exactly(3).times.and_return(FormSection.new("name" => "form_section"))
+    render :partial => 'form_section/check_boxes', :locals => { :check_boxes => check_boxes, :formObject => @child, :is_subform => true }, :formats => [:html], :handlers => [:erb]
+    expect(rendered).to include('disabled="disabled"')
+  end
+
 end
--- spec/views/form_section/_radio_button.erb_spec.rb
+++ spec/views/form_section/_radio_button.erb_spec.rb
@@ -18,9 +18,9 @@ describe "form_section/_radio_button.html.erb" do
     radio_button.should_receive(:form).exactly(3).times.and_return(FormSection.new("name" => "form_section"))
     render :partial => 'form_section/radio_button', :locals => { :radio_button => radio_button, :formObject => @child}, :formats => [:html], :handlers => [:erb]
 
-    rendered.should match(/<input data-field-tags="\[\]" id="formsection_child_new_field_m" name="child\[new field\]" type="radio" value="M" \/>/)
+    rendered.should match(/<input data-field-tags="\[\]" id="formsection_child_new_field_m" is_disabled="false" name="child\[new field\]" type="radio" value="M" \/>/)
     rendered.should match(/<label for="formsection_child_new_field_m">M<\/label>/)
-    rendered.should match(/<input data-field-tags="\[\]" id="formsection_child_new_field_f" name="child\[new field\]" type="radio" value="F" \/>/)
+    rendered.should match(/<input data-field-tags="\[\]" id="formsection_child_new_field_f" is_disabled="false" name="child\[new field\]" type="radio" value="F" \/>/)
     rendered.should match(/<label for="formsection_child_new_field_f">F<\/label>/)
     rendered.should match(/<p class="help">This is my help text<\/p>/)
   end
@@ -48,6 +48,42 @@ describe "form_section/_radio_button.html.erb" do
     radio_button.should_receive(:form).exactly(3).times.and_return(FormSection.new("name" => "form_section"))
     render :partial => 'form_section/radio_button', :locals => { :radio_button => radio_button, :formObject => @child}, :formats => [:html], :handlers => [:erb]
     rendered.should_not have_tag("img.vtip")
+  end
+
+  it "should have 'is_disabled=true' when is disabled" do
+    radio_button = Field.new :name => "new field",
+    :display_name => "field name",
+    :type => 'radio_button',
+    :option_strings => Array['M', 'F']
+    radio_button.disabled = true
+
+    radio_button.should_receive(:form).exactly(3).times.and_return(FormSection.new("name" => "form_section"))
+    render :partial => 'form_section/radio_button', :locals => { :radio_button => radio_button, :formObject => @child, :is_subform => true }, :formats => [:html], :handlers => [:erb]
+    expect(rendered).to include('is_disabled="true"')
+  end
+
+  it "should have 'is_disabled=false' when is not disabled" do
+    radio_button = Field.new :name => "new field",
+    :display_name => "field name",
+    :type => 'radio_button',
+    :option_strings => Array['M', 'F']
+    radio_button.disabled = false
+
+    radio_button.should_receive(:form).exactly(3).times.and_return(FormSection.new("name" => "form_section"))
+    render :partial => 'form_section/radio_button', :locals => { :radio_button => radio_button, :formObject => @child, :is_subform => true }, :formats => [:html], :handlers => [:erb]
+    expect(rendered).to include('is_disabled="false"')
+  end
+
+  it "should have 'disable' attribute when is_subform even if disabled is false" do
+    radio_button = Field.new :name => "new field",
+    :display_name => "field name",
+    :type => 'radio_button',
+    :option_strings => Array['M', 'F']
+    radio_button.disabled = false
+
+    radio_button.should_receive(:form).exactly(3).times.and_return(FormSection.new("name" => "form_section"))
+    render :partial => 'form_section/radio_button', :locals => { :radio_button => radio_button, :formObject => @child, :is_subform => true }, :formats => [:html], :handlers => [:erb]
+    expect(rendered).to include('disabled="disabled"')
   end
 
 end
