a10eb58bb1e03c46caef7bfd090130a28b9d31a8
Ron Elledge <relledge@quoininc.com>
2017-06-20 15:11:34 -0500

    updated i18n migration for user_group permissions

--- db/i18n_migration/migrations/roles.rb
+++ db/i18n_migration/migrations/roles.rb
@@ -13,9 +13,15 @@ Role.all.each do |role|
 
   resources = role.permissions_list.map{|pl| pl.resource}
   if resources.include?('user') && resources.exclude?('agency')
-    agency_resource = role.permissions_list.select{|pl| pl.resource. == 'user'}.first.dup
+    agency_resource = role.permissions_list.select{|pl| pl.resource == 'user'}.first.dup
     agency_resource.resource = 'agency'
     role.permissions_list << agency_resource
+    changed = true
+  end
+  if resources.include?('user') && resources.exclude?('user_group')
+    user_group_resource = role.permissions_list.select{|pl| pl.resource == 'user'}.first.dup
+    user_group_resource.resource = 'user_group'
+    role.permissions_list << user_group_resource
     changed = true
   end
 
