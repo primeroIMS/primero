b56071e970364d48423e7cdd54572c9cfbd79549
Jack Miszencin <jack.miszencin@quoininc.com>
2017-08-11 11:54:03 -0400

    Started using checkmarks, fixed missing rows problem, made it so that visible column only shows on show_hidden exports.

--- app/models/exporters/form_exporter.rb
+++ app/models/exporters/form_exporter.rb
@@ -1,6 +1,8 @@
 module Exporters
   class FormExporter
 
+    VISIBLE_COLUMN_INDEX = 5  # So that we always know where to put the "Visible?" column if show_hidden is selected.
+
     def initialize(export_file=nil)
       @export_file_name = export_file || CleansingTmpDir.temp_file_name
       @io = File.new(@export_file_name, "w")
@@ -19,7 +21,8 @@ module Exporters
 
     # Exports forms to an Excel spreadsheet
     def export_forms_to_spreadsheet(type = 'case', module_id = 'primeromodule-cp', show_hidden = false)
-      header = ['Form Group', 'Form Name', 'Field ID', 'Field Type', 'Field Name', 'Visible?', 'Required', 'On Mobile?', 'On Short Form?', 'Options', 'Help Text', 'Guiding Questions']
+      header = ['Form Group', 'Form Name', 'Field ID', 'Field Type', 'Field Name', 'Required', 'On Mobile?', 'On Short Form?', 'Options', 'Help Text', 'Guiding Questions']
+      header = header.insert(VISIBLE_COLUMN_INDEX, 'Visible?') if show_hidden
 
       primero_module = PrimeroModule.get(module_id)
       forms = primero_module.associated_forms_grouped_by_record_type(false)
@@ -64,10 +67,10 @@ module Exporters
         worksheet = @workbook.add_worksheet(workbook_name)
         worksheet.write(0, 0, form.unique_id)
         worksheet.write(1, 0, header)
-        form.fields.each_with_index do |field, i|
+        i = 0
+        form.fields.each do |field|
           if show_hidden || field.visible?
-            visible = field.visible? ? I18n.t("true") : I18n.t("false")
-            required = field.required ? I18n.t("true") : I18n.t("false")
+            required = field.required ? '✔' : ''
             options = ''
             if ['radio_button', 'select_box'].include?(field.type)
               if field.option_strings_source.present? && field.option_strings_source.start_with?('Location')
@@ -86,9 +89,17 @@ module Exporters
             end
             field_type = field.type
             field_type += " (multi)" if field.type == 'select_box' && field.multi_select
-            mobile_visible = ((form.visible || form.is_nested) && form.mobile_form && field.mobile_visible) ? 'Yes' : 'No'
-            minify_visible = field.show_on_minify_form ? 'Yes' : 'No'
-            worksheet.write((i+2),0,[form.form_group_name, form.name, field.name, field_type, field.display_name, visible, required, mobile_visible, minify_visible, options, field.help_text, field.guiding_questions])
+            mobile_visible = ((form.visible || form.is_nested) && form.mobile_form && field.mobile_visible) ? '✔' : ''
+            minify_visible = field.show_on_minify_form ? '✔' : ''
+            row_array = [form.form_group_name, form.name, field.name, field_type, field.display_name, required, mobile_visible, minify_visible, options, field.help_text, field.guiding_questions]
+            if show_hidden
+              visible = field.visible? ? '✔' : ''
+              row_array = row_array.insert(VISIBLE_COLUMN_INDEX, visible)
+            end
+            worksheet.write((i+2),0,row_array)
+            i+=1
+          # else
+          #   i-=1
           end
         end
       end
