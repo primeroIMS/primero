9461559315fdc5c4813b9a37241445d582f90497
mcounts <mcounts@quoininc.com>
2017-10-05 15:33:13 -0400

    use global for en

--- app/models/field.rb
+++ app/models/field.rb
@@ -507,7 +507,7 @@ class Field
 
       Primero::Application::locales.each do |locale|
         option_strings_locale = self.send("option_strings_text_#{locale}")
-        if locale != "en" && option_strings_locale.present?
+        if locale != Primero::Application::LOCALE_ENGLISH && option_strings_locale.present?
           self.send("option_strings_text_#{locale}").each_with_index do |option, index|
             if option.is_a?(Hash) && option['id'].blank? && option['display_text'].present?
               option['id'] = self.option_strings_text[index]['id']
