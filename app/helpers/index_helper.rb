4a120a107a6cadb07f39f7c14295145f4e1a3ca9
Ron Elledge <relledge@quoininc.com>
2017-10-13 16:18:06 -0500

    added Workflow filter to case list

--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -313,6 +313,7 @@ module IndexHelper
     filters << "Mobile" if @can_sync_mobile
     filters << "Social Worker" if @is_manager
     filters << "My Cases"
+    filters << "Workflow"
     filters << "Approvals" if @can_approvals && (allowed_form_ids.any?{|fs_id| ["cp_case_plan", "closure_form", "cp_bia_form"].include?(fs_id) })
     #Check independently the checkboxes on the view.
     filters << "cp_bia_form" if allowed_form_ids.include?("cp_bia_form") && @can_approval_bia
