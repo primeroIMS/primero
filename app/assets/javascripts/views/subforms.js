0c4d14c5b5c184afe6bcca0cd90fa211650be430
Joshua Toliver <jtoliver@quoininc.com>
2017-11-06 10:09:53 -0500

    Fixing subforms with required fields and adding title for subform on edit

--- app/assets/javascripts/views/subforms.js
+++ app/assets/javascripts/views/subforms.js
@@ -62,6 +62,8 @@ _primero.Views.SubformView = _primero.Views.Base.extend({
     var $add_button;
     self.add_subform($target);
 
+    $target.prev('.placeholder_text').hide();
+
     //Replicate the event on shared subforms
     var $target_subform = $target.parent().parent().find('.subforms');
 
@@ -209,9 +211,12 @@ _primero.Views.SubformView = _primero.Views.Base.extend({
     var $subform = $target.parents('fieldset.subform');
     $subform.fadeOut(600, function() {
       var $subform_group = $target.parents('.subforms');
+      has_required_fields = $(this).find('input[required="required"]').length
       $(this).remove();
       self.count_subforms($subform_group);
       $('body').trigger('violation-removed');
+
+      Foundation.reInit('abide');
     });
   },
 
@@ -237,6 +242,7 @@ _primero.Views.SubformView = _primero.Views.Base.extend({
       name += "[" + focus + "]";
       //don't add the input as a child of the subforms container, this will break the generation of id's.
       $target.parent().append("<input id=\"" + id + "\" type=\"hidden\" name=\"" + name + "\" value=\"\" />");
+      $target.parent().find('.placeholder_text').show();
     }
   }
 });
