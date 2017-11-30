0e81c3eb51aa822df9ad73c59213924cde9458c4
Ron Elledge <relledge@quoininc.com>
2017-11-30 10:16:42 -0600

    fixed specs and deprecation warnings

--- app/jobs/approval_response_job.rb
+++ app/jobs/approval_response_job.rb
@@ -2,6 +2,6 @@ class ApprovalResponseJob < ActiveJob::Base
   queue_as :mailer
 
   def perform(user_id, case_id, approval_type, approval, host_url)
-    NotificationMailer.manager_approval_response(user_id, case_id, approval_type, approval, host_url).deliver
+    NotificationMailer.manager_approval_response(user_id, case_id, approval_type, approval, host_url).deliver_later
   end
 end
