4581d77485917bb07387e7d4898e9366abc403ca
Joshua Toliver <jtoliver@quoininc.com>
2017-12-06 15:44:39 -0500

    Using new rspec params syntax for controller specs

--- spec/controllers/child_ids_controller_spec.rb
+++ spec/controllers/child_ids_controller_spec.rb
@@ -52,7 +52,7 @@ describe ChildIdsController do
 
       context 'when passing mobile true' do
         it 'returns ids and revs for all mobile cases' do
-          get :all, mobile: 'true'
+          get :all, params: {mobile: 'true'}
           expect(response.status).to eq(200)
           child_ids = JSON.parse(response.body)
           expect(child_ids).to match_array([{'_id' => @case_1._id, '_rev' => @case_1._rev},
@@ -76,7 +76,7 @@ describe ChildIdsController do
 
       context 'when passing mobile true' do
         it 'returns ids and revs for all mobile cases' do
-          get :all, mobile: 'true'
+          get :all, params: {mobile: 'true'}
           expect(response.status).to eq(200)
           child_ids = JSON.parse(response.body)
           expect(child_ids).to match_array([{'_id' => @case_3._id, '_rev' => @case_3._rev}])
@@ -98,7 +98,7 @@ describe ChildIdsController do
       context 'who has no mobile cases' do
         context 'when passing mobile true' do
           it 'returns no ids and revs' do
-            get :all, mobile: 'true'
+            get :all, params: {mobile: 'true'}
             expect(response.status).to eq(200)
             child_ids = JSON.parse(response.body)
             expect(child_ids).to be_empty
@@ -121,7 +121,7 @@ describe ChildIdsController do
 
       context 'when passing mobile true' do
         it 'returns no ids and revs' do
-          get :all, mobile: 'true'
+          get :all, params: {mobile: 'true'}
           expect(response.status).to eq(200)
           child_ids = JSON.parse(response.body)
           expect(child_ids).to be_empty
@@ -136,7 +136,7 @@ describe ChildIdsController do
 
       context 'that is less than the oldest record' do
         it 'returns all ids and revs' do
-          get :all, last_update: '2016-09-11 00:00:00UTC'
+          get :all, params: {last_update: '2016-09-11 00:00:00UTC'}
           expect(response.status).to eq(200)
           child_ids = JSON.parse(response.body)
           expect(child_ids).to match_array([{'_id' => @case_1._id, '_rev' => @case_1._rev},
@@ -149,7 +149,7 @@ describe ChildIdsController do
 
       context 'that is greater than the newest record' do
         it 'returns no ids and revs' do
-          get :all, last_update: '2016-11-11 01:00:00UTC'
+          get :all, params: {last_update: '2016-11-11 01:00:00UTC'}
           expect(response.status).to eq(200)
           child_ids = JSON.parse(response.body)
           expect(child_ids).to be_empty
@@ -158,7 +158,7 @@ describe ChildIdsController do
 
       context 'that is greater than the oldest record and less than the newest record' do
         it 'returns some ids and revs' do
-          get :all, last_update: '2016-10-31 00:00:00UTC'
+          get :all, params: {last_update: '2016-10-31 00:00:00UTC'}
           expect(response.status).to eq(200)
           child_ids = JSON.parse(response.body)
           expect(child_ids).to match_array([{'_id' => @case_1._id, '_rev' => @case_1._rev},
