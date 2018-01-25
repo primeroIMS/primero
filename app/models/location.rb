bda529b729782a0cc76ac5c1a6b9bb202cdafe04
Joshua Toliver <jtoliver@quoininc.com>
2018-01-25 16:20:59 -0500

    Using locale pass into find_names_by_admin_level_enabled method

--- app/models/location.rb
+++ app/models/location.rb
@@ -128,9 +128,10 @@ class Location < CouchRest::Model::Base
     end
     memoize_in_prod :find_by_admin_level_enabled
 
-    def find_names_by_admin_level_enabled(admin_level = ReportingLocation::DEFAULT_ADMIN_LEVEL, hierarchy_filter = nil, opts: {})
+    def find_names_by_admin_level_enabled(admin_level = ReportingLocation::DEFAULT_ADMIN_LEVEL, hierarchy_filter = nil, opts={})
+      locale = (opts[:locale].present? ? opts[:locale] : I18n.locale)
       #We need the fully qualified :: separated location name here so the reg_ex filter below will work
-      location_names = Location.find_by_admin_level_enabled(admin_level).map{|r| {id: r.location_code, hierarchy: r.hierarchy, display_text: r.name}.with_indifferent_access}.sort_by!{|l| l['display_text']}
+      location_names = Location.find_by_admin_level_enabled(admin_level).map{|r| {id: r.location_code, hierarchy: r.hierarchy, display_text: r.name(locale)}.with_indifferent_access}.sort_by!{|l| l['display_text']}
       hierarchy_set = hierarchy_filter.to_set if hierarchy_filter.present?
       location_names = location_names.select{|l| l['hierarchy'].present? && (l['hierarchy'].to_set ^ hierarchy_set).length == 0} if hierarchy_filter.present?
       #Now reduce the display text down to just the placename for display
