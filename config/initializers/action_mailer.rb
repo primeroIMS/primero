2724a1abbe0e185b7f6abfe92eb40208e7f07a3a
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2018-01-25 12:40:12 -0500

    Test mailer task and changing how mailer configuration is generated by Chef

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,11 +1,11 @@
 email_settings = YAML::load(File.open("#{Rails.root.to_s}/config/mailers.yml"))[Rails.env.to_s]
 
 Rails.application.config.action_mailer.tap do |action_mailer|
-  action_mailer.delivery_method = email_settings['delivery_method']
+  action_mailer.delivery_method = email_settings[:delivery_method]
   if email_settings[:default_options].present?
-    action_mailer.default_options = email_settings['default_options']
+    action_mailer.default_options = email_settings[:default_options]
   end
-  action_mailer.smtp_settings = email_settings['smtp_conf']
+  action_mailer.smtp_settings = email_settings[:smtp_conf]
   action_mailer.raise_delivery_errors = true
   action_mailer.perform_deliveries = true
 end
