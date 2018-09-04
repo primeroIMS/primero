061e6d73fc0f91d72292d6d74484655cd541fc3f
Ron Elledge <relledge@quoininc.com>
2018-09-04 15:53:17 -0500

    temp commented out note action specs

--- spec/controllers/concerns/note_actions_spec.rb
+++ spec/controllers/concerns/note_actions_spec.rb
@@ -43,77 +43,35 @@ describe NoteActions, type: :controller do
       end
 
       context 'passing no ids' do
-        it 'returns error no selected records' do
+        #TODO
+        xit 'returns error no selected records' do
+          @expected = {
+              :success => false,
+              :message => 'No records selected'
+          }.to_json
+
+          post :add_note
+          expect(response.body).to eq(@expected)
+        end
+      end
+
+      context 'passing ids' do
+        before do
+          User.stub(:find_by_user_name).and_return(@user)
+          @case1 = Child.create(:name => 'Test Case 1', :short_id => 'aaa111', :marked_for_mobile => false, :notes_section => [])
+        end
+
+        #TODO
+        xit 'adds a note' do
           @expected = {
                   :success => false,
                   :message => 'No records selected'
           }.to_json
 
-          post :add_note
-          binding.pry
-          x=0
+          post :add_note, params: {id: @case1.id, subject: 'test subject', notes: 'test notes'}
           expect(response.body).to eq(@expected)
         end
       end
-
-    #   context 'passing ids' do
-    #     before do
-    #       User.stub(:find_by_user_name).and_return(@user)
-    #       @case1 = Child.create(:name => 'Test Case 1', :short_id => 'aaa111', :marked_for_mobile => false)
-    #       @case2 = Child.create(:name => 'Test Case 2', :short_id => 'bbb222', :marked_for_mobile => false)
-    #       @case3 = Child.create(:name => 'Test Case 3', :short_id => 'ccc333')
-    #     end
-    #
-    #     it 'marks as mobile when 1 id is passed' do
-    #       @expected = {
-    #               :success => true,
-    #               :message => 'Case aaa111 successfully marked as mobile'
-    #       }.to_json
-    #
-    #       post :mark_for_mobile, params: {mobile_value: 'true', id: @case1.id}
-    #       expect(response.body).to eq(@expected)
-    #     end
-    #
-    #     it 'marks as mobile when multiple ids are passed' do
-    #       @expected = {
-    #               :success => true,
-    #               :message => '2 Record(s) successfully marked as mobile'
-    #       }.to_json
-    #
-    #       post :mark_for_mobile, params: {mobile_value: 'true', selected_records: [@case1.id, @case2.id].join(',')}
-    #       expect(response.body).to eq(@expected)
-    #     end
-    #
-    #     it 'unmarks as mobile when 1 id is passed' do
-    #       @expected = {
-    #               :success => true,
-    #               :message => 'Case aaa111 successfully unmarked as mobile'
-    #       }.to_json
-    #
-    #       post :mark_for_mobile, params: {mobile_value: 'false', id: @case1.id}
-    #       expect(response.body).to eq(@expected)
-    #     end
-    #
-    #     it 'unmarks as mobile when multiple ids are passed' do
-    #       @expected = {
-    #               :success => true,
-    #               :message => '2 Record(s) successfully unmarked as mobile'
-    #       }.to_json
-    #
-    #       post :mark_for_mobile, params: {mobile_value: 'false', selected_records: [@case1.id, @case2.id].join(',')}
-    #       expect(response.body).to eq(@expected)
-    #     end
-    #
-    #     it 'marks as mobile when mark_for_mobile attribute does not exist' do
-    #       @expected = {
-    #               :success => true,
-    #               :message => '1 Record(s) successfully marked as mobile'
-    #       }.to_json
-    #
-    #       post :mark_for_mobile, params: {mobile_value: 'true', selected_records: [@case3.id].join(',')}
-    #       expect(response.body).to eq(@expected)
-    #     end
-    #   end
     end
   end
 end
