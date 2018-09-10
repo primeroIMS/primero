d2f9f66153ef57fb53c9a158f98fce297775733b
Ron Elledge <relledge@quoininc.com>
2018-09-10 16:31:34 -0500

    adding rspec text for no records selected in note_actions_spec

--- spec/controllers/concerns/note_actions_spec.rb
+++ spec/controllers/concerns/note_actions_spec.rb
@@ -38,38 +38,28 @@ describe NoteActions, type: :controller do
       before do
         @permission_case_add_note = Permission.new(resource: Permission::CASE, actions: [Permission::READ, Permission::ADD_NOTE])
         Role.create(id: 'add_note_permission', name: 'Can Add Note', permissions_list: [@permission_case_add_note], group_permission: Permission::GROUP)
-        @user = User.create(:user_name => 'note_user', :role_ids => ['add_note_permission'])
-        @session = fake_login @user
+        @notes_user = User.create(:user_name => 'note_user', :role_ids => ['add_note_permission'])
+        @session = fake_login @notes_user
       end
 
       context 'passing no ids' do
-        #TODO
-        xit 'returns error no selected records' do
-          @expected = {
-              :success => false,
-              :message => 'No records selected'
-          }.to_json
-
+        it 'returns error no selected records' do
           post :add_note
-          expect(response.body).to eq(@expected)
+          expect(response.status).not_to eq(403)
+          expect(flash[:notice]).to eq('No records selected')
         end
       end
 
       context 'passing ids' do
         before do
-          User.stub(:find_by_user_name).and_return(@user)
-          @case1 = Child.create(:name => 'Test Case 1', :short_id => 'aaa111', :marked_for_mobile => false, :notes_section => [])
+          User.stub(:find_by_user_name).and_return(@notes_user)
+          @case1 = Child.create(:name => 'Test Case 1', :short_id => 'aaa111', :owned_by => @notes_user.user_name, :marked_for_mobile => false, :notes_section => [])
         end
 
-        #TODO
+        #TODO - redirect_to show page is not working in this test... need to investigate
         xit 'adds a note' do
-          @expected = {
-                  :success => false,
-                  :message => 'No records selected'
-          }.to_json
-
           post :add_note, params: {id: @case1.id, subject: 'test subject', notes: 'test notes'}
-          expect(response.body).to eq(@expected)
+          #expect...
         end
       end
     end
