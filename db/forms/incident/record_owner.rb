b71012d47529b027a373ec60339a23cdf2f2b2ef
Ron Elledge <relledge@quoininc.com>
2018-09-28 14:00:36 -0500

    changed _all to _en in incident forms in default seeds.

--- db/forms/incident/record_owner.rb
+++ db/forms/incident/record_owner.rb
@@ -2,19 +2,19 @@ record_owner_fields = [
   Field.new({"name" => "current_owner_section",
              "mobile_visible" => false,
              "type" => "separator",
-             "display_name_all" => "Current Owner"
+             "display_name_en" => "Current Owner"
             }),
   Field.new({"name" => "caseworker_name",
              "show_on_minify_form" => true,
              "mobile_visible" => true,
              "type" => "text_field",
-             "display_name_all" => "Field/Case/Social Worker"
+             "display_name_en" => "Field/Case/Social Worker"
             }),
   Field.new({"name" => "owned_by",
              "show_on_minify_form" => true,
              "mobile_visible" => true,
              "type" => "select_box",
-             "display_name_all" => "Caseworker Code",
+             "display_name_en" => "Caseworker Code",
              "option_strings_source" => "User",
              "editable" => false,
              "disabled" => true
@@ -22,7 +22,7 @@ record_owner_fields = [
   Field.new({"name" => "owned_by_agency",
              "mobile_visible" => false,
              "type" => "select_box",
-             "display_name_all" => "Agency",
+             "display_name_en" => "Agency",
              "editable" => false,
              "disabled" => true,
              "option_strings_source" => "Agency"
@@ -31,44 +31,44 @@ record_owner_fields = [
              "mobile_visible" => false,
              "type" =>"select_box",
              "multi_select" => true,
-             "display_name_all" => "Other Assigned Users",
+             "display_name_en" => "Other Assigned Users",
              "option_strings_source" => "User"
             }),
   Field.new({"name" => "record_history_section",
              "mobile_visible" => false,
              "type" => "separator",
-             "display_name_all" => "Record History"
+             "display_name_en" => "Record History"
             }),
   Field.new({"name" => "created_by",
              "mobile_visible" => false,
              "type" => "text_field",
-             "display_name_all" => "Record created by",
+             "display_name_en" => "Record created by",
              "editable" => false,
              "disabled" => true
             }),
   Field.new({"name" => "created_organization",
              "mobile_visible" => false,
              "type" => "text_field",
-             "display_name_all" => "Created by agency",
+             "display_name_en" => "Created by agency",
              "editable" => false,
              "disabled" => true
             }),
   Field.new({"name" => "previously_owned_by",
              "mobile_visible" => false,
              "type" => "text_field",
-             "display_name_all" => "Previous Owner",
+             "display_name_en" => "Previous Owner",
              "editable" => false,
              "disabled" => true
             }),
   Field.new({"name" => "previous_agency",
              "mobile_visible" => false,
              "type" => "text_field",
-             "display_name_all" => "Previous Agency"
+             "display_name_en" => "Previous Agency"
             }),
   Field.new({"name" => "module_id",
           "mobile_visible" => false,
           "type" => "text_field",
-          "display_name_all" => "Module",
+          "display_name_en" => "Module",
           "editable" => false,
           "disabled" => true
           }),
@@ -85,6 +85,6 @@ FormSection.create_or_update_form_section({
   editable: true,
   fields: record_owner_fields,
   mobile_form: true,
-  name_all: "Record Owner",
-  description_all: "Record Owner"
+  name_en: "Record Owner",
+  description_en: "Record Owner"
 })
