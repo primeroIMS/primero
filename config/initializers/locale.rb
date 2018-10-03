bb0bfa4cd7d6da6623a05b23e8dab7d8179fea1b
Ron Elledge <relledge@quoininc.com>
2018-10-03 11:53:36 -0500

    set up default locales using locales.yml

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,13 +1,9 @@
-#Set default locales first... will be overridden below if SystemSettings exist
-I18n.available_locales = Primero::Application::LOCALES
-docs = SystemSettings.database.documents['rows']
-if docs.present?
-  doc_id = docs.map{|d| d['id']}.select{|i| !i.start_with?('_design')}.first
-  if doc_id.present?
-    saved_settings = SystemSettings.database.get(doc_id)
-    if saved_settings.present?
-      I18n.default_locale = saved_settings['default_locale'].present? ? saved_settings['default_locale'] : Primero::Application::LOCALE_ENGLISH
-      I18n.available_locales = saved_settings['locales'].present? ? saved_settings['locales'] : Primero::Application::LOCALES
-    end
-  end
+yaml_file = "#{Rails.root.to_s}/config/locales.yml"
+locale_settings = YAML::load(File.open(yaml_file))[Rails.env.to_s] if File.exists?(yaml_file)
+if locale_settings.present?
+  I18n.default_locale = locale_settings[:default_locale].present? ? locale_settings[:default_locale] : Primero::Application::LOCALE_ENGLISH
+  I18n.available_locales = locale_settings[:locales].present? ? locale_settings[:locales] : Primero::Application::LOCALES
+else
+  I18n.default_locale = Primero::Application::LOCALE_ENGLISH
+  I18n.available_locales = Primero::Application::LOCALES
 end
