f848b845c1fedc0a9f1997b5096bc86ae1f81af2
Ron Elledge <relledge@quoininc.com>
2018-10-04 11:04:47 -0500

    changed stubs in location_spec to fix text that broke when I changed how default locales are set up

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -4,10 +4,8 @@ describe Location do
 
   before do
     Location.all.each &:destroy
-    SystemSettings.all.each &:destroy
-    SystemSettings.create(default_locale: Primero::Application::LOCALE_ENGLISH,
-                          locales: [Primero::Application::LOCALE_ENGLISH, Primero::Application::LOCALE_FRENCH])
-    I18n.stub :available_locales => [Primero::Application::LOCALE_ENGLISH, Primero::Application::LOCALE_FRENCH]
+    Primero::Application.stub :locales => [ Primero::Application::LOCALE_ENGLISH, Primero::Application::LOCALE_FRENCH]
+    Primero::Application.stub :default_locale => Primero::Application::LOCALE_ENGLISH
 
     @country = create :location, admin_level: 0, placename_all: 'MyCountry', type: 'country', location_code: 'MC01'
     @province1 = create :location, hierarchy: [@country.location_code], placename_all: 'Province 1', type: 'province', location_code: 'PR01'
