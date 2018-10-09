5a62394c22c38afba6ba88958fd37c6d5538af0b
Ron Elledge <relledge@quoininc.com>
2018-10-09 11:58:06 -0500

    fixed rspec tests related to new locales

--- spec/controllers/form_section_controller_spec.rb
+++ spec/controllers/form_section_controller_spec.rb
@@ -92,9 +92,9 @@ describe FormSectionController do
                      "type"=>"subform",
                      "subform"=>
                          {"unique_id"=>"E",
-                          :name=>{"en"=>"E", "fr"=>"", "ar"=>"", "ar-LB"=>"", "so"=>"", "es"=>"", "bn"=>""},
+                          :name=>{"en"=>"E", "fr"=>"", "ar"=>"", "ar-LB"=>"", "so"=>"", "es"=>"", "bn"=>"", "my"=>"", "th"=>"", "ku"=>""},
                           "order"=>0,
-                          :help_text=>{"en"=>"", "fr"=>"", "ar"=>"", "ar-LB"=>"", "so"=>"", "es"=>"", "bn"=>""},
+                          :help_text=>{"en"=>"", "fr"=>"", "ar"=>"", "ar-LB"=>"", "so"=>"", "es"=>"", "bn"=>"", "my"=>"", "th"=>"", "ku"=>""},
                           "base_language"=>"en",
                           "fields"=>
                               [{"name"=>"field1",
@@ -105,16 +105,18 @@ describe FormSectionController do
                                 "option_strings_source"=>nil,
                                 "show_on_minify_form"=>false,
                                 "mobile_visible"=>true,
-                                :display_name=>{"en"=>"field1", "fr"=>"field1", "ar"=>"field1", "ar-LB"=>"field1","so"=>"field1", "es"=>"field1", "bn"=>"field1"},
-                                :help_text=>{"en"=>"", "fr"=>"", "ar"=>"", "ar-LB"=>"", "so"=>"", "es"=>"", "bn"=>""},
-                                :option_strings_text=>{"en"=>[], "fr"=>[], "ar"=>[], "ar-LB"=>[], "so"=>[], "es"=>[], "bn"=>[]}}]},
+                                :display_name=>{"en"=>"field1", "fr"=>"field1", "ar"=>"field1", "ar-LB"=>"field1","so"=>"field1",
+                                                "es"=>"field1", "bn"=>"field1", "my"=>"field1", "th"=>"field1", "ku"=>"field1"},
+                                :help_text=>{"en"=>"", "fr"=>"", "ar"=>"", "ar-LB"=>"", "so"=>"", "es"=>"", "bn"=>"", "my"=>"", "th"=>"", "ku"=>""},
+                                :option_strings_text=>{"en"=>[], "fr"=>[], "ar"=>[], "ar-LB"=>[], "so"=>[], "es"=>[], "bn"=>[], "my"=>[], "th"=>[], "ku"=>[]}}]},
                      "required"=>false,
                      "option_strings_source"=>nil,
                      "show_on_minify_form"=>false,
                      "mobile_visible"=>true,
-                     :display_name=>{"en"=>"nested_e", "fr"=>"nested_e", "ar"=>"nested_e", "ar-LB"=>"nested_e", "so"=>"nested_e", "es"=>"nested_e", "bn"=>"nested_e"},
-                     :help_text=>{"en"=>"", "fr"=>"", "ar"=>"", "ar-LB"=>"", "so"=>"", "es"=>"", "bn"=>""},
-                     :option_strings_text=>{"en"=>[], "fr"=>[], "ar"=>[], "ar-LB"=>[], "so"=>[], "es"=>[], "bn"=>[]}}]
+                     :display_name=>{"en"=>"nested_e", "fr"=>"nested_e", "ar"=>"nested_e", "ar-LB"=>"nested_e", "so"=>"nested_e",
+                                     "es"=>"nested_e", "bn"=>"nested_e", "my"=>"nested_e", "th"=>"nested_e", "ku"=>"nested_e"},
+                     :help_text=>{"en"=>"", "fr"=>"", "ar"=>"", "ar-LB"=>"", "so"=>"", "es"=>"", "bn"=>"", "my"=>"", "th"=>"", "ku"=>""},
+                     :option_strings_text=>{"en"=>[], "fr"=>[], "ar"=>[], "ar-LB"=>[], "so"=>[], "es"=>[], "bn"=>[], "my"=>[], "th"=>[], "ku"=>[]}}]
         get :index, params: {mobile: true, :format => :json}
         expect(assigns[:form_sections]['Children'].select{|f| f['unique_id'] == 'D'}.first['fields']).to eq(expected)
       end
