a078c516274fb7bceda3b9fcb5041b6f5e6cc1ed
Dennis Hernandez <dhernand@quoininc.com>
2018-10-18 14:37:36 -0600

    PRIM-242 IRAQ - Make subform visible in conference details form

--- db/forms/case/conference_details.rb
+++ db/forms/case/conference_details.rb
@@ -89,7 +89,6 @@ conference_details_subform_fields = [
 ]
 
 conference_details_subform_section = FormSection.create_or_update_form_section({
-  :visible => false,
   :is_nested => true,
   :order_form_group => 999,
   :order => 999,
