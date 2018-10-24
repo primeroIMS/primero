d9dddd1fb21e68ac0a36c268f2ee4cd6e6cd77c0
Joshua Toliver <jtoliver@quoininc.com>
2018-10-24 10:04:49 -0400

    Checking for location db before location generation

--- config/initializers/generate_options.rb
+++ config/initializers/generate_options.rb
@@ -1,3 +1,5 @@
-if OptionsQueueStats.options_not_generated?
+db = COUCHDB_SERVER.database!("primero_location_#{Rails.env}")
+
+if OptionsQueueStats.options_not_generated? && db.all_docs['row'].present?
   OptionsJob.perform_now
 end
