19acf9b7ef1c573cab9727fa0e7706f7145b123c
Joshua Toliver <jtoliver@quoininc.com>
2018-10-24 10:42:04 -0400

    Correcting key selection

--- config/initializers/generate_options.rb
+++ config/initializers/generate_options.rb
@@ -1,5 +1,5 @@
 db = COUCHDB_SERVER.database!("primero_location_#{Rails.env}")
 
-if OptionsQueueStats.options_not_generated? && db.all_docs['row'].present?
+if OptionsQueueStats.options_not_generated? && db.all_docs['rows'].present?
   OptionsJob.perform_now
 end
