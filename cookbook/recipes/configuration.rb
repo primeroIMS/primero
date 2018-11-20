5c5d69d8b7f9782646d1d348c66ca10a40a5f936
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2018-11-20 12:06:23 -0500

    Modifying config recipe to call a hard reset

--- cookbook/recipes/configuration.rb
+++ cookbook/recipes/configuration.rb
@@ -5,66 +5,73 @@ if !node[:primero][:no_reseed]
   end
 end
 
+directory node[:primero][:config_dir] do
+  action :create
+  owner node[:primero][:app_user]
+  group node[:primero][:app_group]
+end
+
+ssh_dir = ::File.join(node[:primero][:home_dir], '.ssh')
+directory ssh_dir do
+  owner node[:primero][:app_user]
+  group node[:primero][:app_group]
+  mode '0700'
+end
+
+private_key_path = ::File.join(ssh_dir, 'config_id_rsa')
+file private_key_path do
+  content node[:primero][:seed][:deploy_key]
+  owner node[:primero][:app_user]
+  group node[:primero][:app_group]
+  mode '0400'
+end
+
+known_hosts_path = ::File.join(ssh_dir, 'known_hosts')
+cookbook_file known_hosts_path do
+  source 'ssh/known_hosts'
+  owner node[:primero][:app_user]
+  group node[:primero][:app_group]
+  mode '0400'
+end
+
+git_wrapper_path = ::File.join(ssh_dir, 'git-wrapper-config.sh')
+template git_wrapper_path do
+  source 'ssh_wrapper.sh.erb'
+  mode '0744'
+  owner node[:primero][:app_user]
+  group node[:primero][:app_group]
+  variables({
+                :deploy_private_key_path => private_key_path,
+                :known_hosts_file => known_hosts_path,
+            })
+end
+
+git node[:primero][:config_dir] do
+  repository node[:primero][:seed][:git][:repo]
+  revision node[:primero][:seed][:git][:revision]
+  #Note: The 2 options below are a workaround for a Chef 0.9.0 bug
+  enable_checkout false
+  checkout_branch 'deploy-config'
+  action :sync
+  user node[:primero][:app_user]
+  group node[:primero][:app_group]
+  ssh_wrapper git_wrapper_path
+end
+
 if node[:primero][:seed][:enabled] &&  !::File.exists?(no_reseed_file)
-  directory node[:primero][:config_dir] do
-    action :create
-    owner node[:primero][:app_user]
-    group node[:primero][:app_group]
-  end
-
-  ssh_dir = ::File.join(node[:primero][:home_dir], '.ssh')
-  directory ssh_dir do
-    owner node[:primero][:app_user]
-    group node[:primero][:app_group]
-    mode '0700'
-  end
-
-  private_key_path = ::File.join(ssh_dir, 'config_id_rsa')
-  file private_key_path do
-    content node[:primero][:seed][:deploy_key]
-    owner node[:primero][:app_user]
-    group node[:primero][:app_group]
-    mode '0400'
-  end
-
-  known_hosts_path = ::File.join(ssh_dir, 'known_hosts')
-  cookbook_file known_hosts_path do
-    source 'ssh/known_hosts'
-    owner node[:primero][:app_user]
-    group node[:primero][:app_group]
-    mode '0400'
-  end
-
-  git_wrapper_path = ::File.join(ssh_dir, 'git-wrapper-config.sh')
-  template git_wrapper_path do
-    source 'ssh_wrapper.sh.erb'
-    mode '0744'
-    owner node[:primero][:app_user]
-    group node[:primero][:app_group]
-    variables({
-                  :deploy_private_key_path => private_key_path,
-                  :known_hosts_file => known_hosts_path,
-              })
-  end
-
-  git node[:primero][:config_dir] do
-    repository node[:primero][:seed][:git][:repo]
-    revision node[:primero][:seed][:git][:revision]
-    #Note: The 2 options below are a workaround for a Chef 0.9.0 bug
-    enable_checkout false
-    checkout_branch 'deploy-config'
-    action :sync
-    user node[:primero][:app_user]
-    group node[:primero][:app_group]
-    ssh_wrapper git_wrapper_path
-  end
-
+  reset_script = ::File.join(node[:primero][:home_dir], 'bin', 'reset_config_to')
   config_script = node[:primero][:seed][:script]
   json_file = node[:primero][:seed][:bundle]
   if config_script
     config_script_path = node[:primero][:config_dir] + config_script
-    execute_bundle 'run-config-script' do
-      command "rails r #{config_script_path}"
+    execute 'run-config-script' do
+      command "#{reset_script} #{config_script_path}"
+      cwd node[:primero][:app_dir]
+      user node[:primero][:app_user]
+      environment({
+        'HOME' => node[:primero][:home_dir],
+        'RAILS_ENV' => 'production'
+      })
     end
   elsif json_file
     json_file_path = node[:primero][:config_dir] + json_file
@@ -93,5 +100,8 @@ if node[:primero][:seed][:enabled] &&  !::File.exists?(no_reseed_file)
     command 'supervisorctl restart backburner'
   end
 
+  execute 'Reload Couch Watcher' do
+    command 'supervisorctl restart couch-watcher'
+  end
 
 end
