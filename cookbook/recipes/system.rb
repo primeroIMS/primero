f6c09cd199acc7fbdb99c6311090534fb7d1c150
Joshua Toliver <jtoliver@quoininc.com>
2019-01-23 15:15:58 -0500

    Creating template/steps for sysctl and limitd configuration

--- cookbook/recipes/system.rb
+++ cookbook/recipes/system.rb
@@ -1,24 +1,11 @@
-include_recipe 'sysctl::default'
-
-set_limit 'nginx' do
-  type 'hard'
-  item 'nofile'
-  value node[:system][:file_limit_hard]
+template '/etc/sysctl.d/99-primero-attributes.conf' do
+  source 'primero-attributes.conf.erb'
+  owner "root"
+  group "root"
 end
 
-set_limit 'nginx' do
-  type 'soft'
-  item 'nofile'
-  value node[:system][:file_limit_soft]
-end
-
-sysctl_param 'fs.file-max' do
-  value node[:system][:fs_file_max]
-end
-sysctl_param 'net.core.somaxconn' do
-  value node[:system][:net_core_somaxconn]
-end
-
-sysctl_param 'net.core.netdev_max_backlog' do
-  value node[:system][:net_core_netdev_max_backlog]
+template '/etc/security/limits.d/nginx.conf' do
+  source 'nginx_limits.conf.erb'
+  owner "root"
+  group "root"
 end
