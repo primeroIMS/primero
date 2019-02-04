220f5a02cdfd0d6fe695df88a73e3f92b3144343
Dennis Hernandez <dhernand@quoininc.com>
2019-02-04 11:07:31 -0600

    PRIM-544 Optimize user calls. Use the UserSelectBox in the Service Form

--- app/assets/javascripts/views/service_subform.js
+++ app/assets/javascripts/views/service_subform.js
@@ -10,64 +10,38 @@ _primero.Views.ServiceSubform = _primero.Views.Base.extend({
 
   initialize: function(){
     var self = this;
-    self.collection = new _primero.Collections.UsersCollection();
+
     $('#services_section .subform_container').each(function(index, subform){
       var $subform = $(subform);
-      $subform.find('select[id$=service_implementing_agency_individual]').on('chosen:showing_dropdown', function(){
-        self.load_users($(subform));
+      $subform.find('select[id$="service_implementing_agency_individual"]').on('chosen:ready', function(e) {
+        self.set_user_filters($subform);
       });
     })
   },
 
   on_filter_change: function(e){
-    var self = this;
     var $selected_subform = $(e.target).parents('.subform_container');
+    this.set_user_filters($selected_subform);
     this.clear_user_selection($selected_subform);
+  },
+
+  set_user_filters: function($subform){
+    var $service_select = $subform.find('select[id$="service_type"]');
+    var $agency_select = $subform.find('select[id$="service_implementing_agency"]');
+    var $location_select = $subform.find('select[id$="service_delivery_location"]');
+
+    var $user_select = $subform.find('select[id$="service_implementing_agency_individual"]');
+    $user_select.data('filter-service', $service_select.val());
+    $user_select.data('filter-agency', $agency_select.val());
+    $user_select.data('filter-location', $location_select.val());
   },
 
   init_subform: function(){
     var self = this;
-    var $selected_subform = $('#services_section .subform_container').last(); //Last added service
-    $selected_subform.find('select[id$=service_implementing_agency_individual]').on('chosen:showing_dropdown', function(){
-      self.load_users($selected_subform);
-    });
-  },
-
-  load_users: function($subform){
-    var self = this;
-    var $service_select = $subform.find('select[id$="service_type"]');
-    var $agency_select = $subform.find('select[id$="service_implementing_agency"]');
-    var $user_select = $subform.find('select[id$=service_implementing_agency_individual]');
-    var $location_select = $subform.find('select[id$=service_delivery_location]');
-
-    $user_select.empty();
-    $user_select.html('<option>' + I18n.t("messages.loading") + '</option>');
-    $user_select.trigger("chosen:updated");
-
-    var data = {
-      services: $service_select.val(),
-      agency_id: $agency_select.val(),
-      location: $location_select.val()
-    }
-
-    self.collection
-        .fetch({data: data})
-        .done(function(){
-          $user_select.empty();
-          var select_options = [];
-          if(self.collection.length > 0) {
-            select_options.push('<option value=""></option>');
-          } else {
-            select_options.push('<option value="">'+I18n.t("no_results_found") +'</option>');
-          }
-          self.collection.each(function(user){
-            var user_model = user.attributes;
-            select_options.push('<option value="' + user_model.user_name + '">' + user_model.user_name + '</option>');
-          })
-
-          $user_select.html(select_options.join(''));
-          $user_select.trigger("chosen:updated");
-        });
+    var $new_subform = $('#services_section .subform_container').last(); //Last added service
+    var $new_select = $new_subform.find('select[id$="service_implementing_agency_individual"]');
+    var selectorId = "#" + $new_select.attr('id');
+    new _primero.Views.PopulateUserSelectBoxes({ el: selectorId });
   },
 
   clear_user_selection: function($subform){
