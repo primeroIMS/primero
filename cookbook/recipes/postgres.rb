a4dea2f1d0287b45711aa3a4a949488b8c10f2dc
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2019-02-14 16:40:33 -0500

    Fixing the dev postgres recipe

--- cookbook/recipes/postgres.rb
+++ cookbook/recipes/postgres.rb
@@ -1,8 +1,13 @@
+execute 'Apt Update' do
+  command 'apt-get update'
+end
+
 %w(postgresql postgresql-contrib libpq-dev).each do |package|
   package package
 end
 
-execute 'Crdeate Postgres role' do
-  command "psql -c \"create role #{node[:primero][:postgres][:role]} with login password '#{node[:primero][:postgres][:password]}';\""
+execute 'Create Postgres role' do
+  command "psql -c \"create role #{node[:primero][:postgres][:role]} with superuser login password '#{node[:primero][:postgres][:password]}';\""
+  user 'postgres'
   only_if { `sudo -upostgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='#{node[:primero][:postgres][:role]}'"`.to_i != 1}
 end
