f7ff5f97562168c21efeb9c7968ed61d07728e88
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2019-03-27 22:45:14 -0400

    Cleaning up unused record audit functions functions

--- app/controllers/incident_histories_controller.rb
+++
@@ -1,8 +0,0 @@
-class IncidentHistoriesController < ApplicationController
-  helper :histories
-  def index
-    @incident = Incident.get(params[:id])
-    @page_name = t "incident.history_of", :short_id => @incident.short_id
-    @user = User.find_by_user_name(current_user_name)
-  end
-end
--- app/models/record_history.rb
+++ app/models/record_history.rb
@@ -2,7 +2,8 @@ class RecordHistory < ActiveRecord::Base
   belongs_to :record, polymorphic: true
 
   def user
-    @user || User.get(self.user_name)
+    #TODO: Refactor with User
+    @user || User.find_by_user_name(self.user_name)
   end
 
   #TODO: This is an N+1 performance issue
