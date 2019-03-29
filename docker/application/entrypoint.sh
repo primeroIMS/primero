926aa239ca63f41eb3e97e86d7f8fec96f0a1fe6
Michael Ciociola <michael.ciociola@quoininc.com>
2019-03-29 16:05:57 -0400

    started nginx docker containers
    moved solr configs into docker folder to decrease build context size
    and dramatically improve build performance

--- docker/application/entrypoint.sh
+++ docker/application/entrypoint.sh
@@ -5,6 +5,6 @@ set -ex
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:drop
 bundle exec rake db:create
 bundle exec rake db:migrate
-bundle exec rake db:migrate:design
+# bundle exec rake db:migrate:design
 bundle exec rake db:seed
 bundle exec rails s
