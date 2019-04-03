b2173087ea49b0bab0a4d36ad392935f8cae5817
Ron Elledge <relledge@quoininc.com>
2019-04-03 15:20:52 -0500

    Translate values on Potential Match quick view modal and fixed styling issues

--- spec/helpers/potential_match_helper_spec.rb
+++ spec/helpers/potential_match_helper_spec.rb
@@ -12,17 +12,17 @@ describe PotentialMatchHelper do
   describe 'comparison_value' do
     context 'no value' do
       it 'should show \' - \'' do
-        expect(helper.comparison_value([])).to eq('-')
+        expect(helper.comparison_value([], nil, nil)).to eq('-')
       end
     end
 
     context 'with value' do
       it 'should show comma separated values' do
-        expect(helper.comparison_value(['hi','hello'])).to eq('hi,hello')
+        expect(helper.comparison_value(['hi','hello'],nil, nil)).to eq('hi, hello')
       end
 
       it 'should show exact values' do
-        expect(helper.comparison_value('hi')).to eq('hi')
+        expect(helper.comparison_value('hi', nil, nil)).to eq('hi')
       end
     end
   end
