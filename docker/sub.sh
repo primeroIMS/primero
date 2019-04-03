3205832ad06de14cdc454f14afce53bd7dfad537
Michael Ciociola <michael.ciociola@quoininc.com>
2019-04-03 16:15:24 -0400

    Wrote script that substitutes values into nginx configs
    sub.sh reads variables from the environment and replaces them in
    .template files
    
    this technique has been applied to the self signed nginx

---
+++ docker/sub.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# This script takes a filename as the only input parameter.
+# 
+# It replaces any bash style variables with the correct value
+# from the environment.
+prim_rename_file() {
+  #check if filename has .template extension. 
+  if [ "${PRIM_FILENAME##*.}" == "template" ]; then
+    cp "${PRIM_FILENAME}" "${PRIM_FILENAME%.*}"
+    PRIM_FILENAME="${PRIM_FILENAME%.*}"
+  fi
+
+  echo "$PRIM_FILENAME"
+  return 0
+}
+
+# Checks if there is a matching file with 'filename.template.default'
+# if so we source it to load the default params
+prim_source_defaults() {
+  if [ -x "${PRIM_DEFAULT_FILENAME}" ];
+  then
+    source "${PRIM_DEFAULT_FILENAME}"
+    return 0
+  else 
+    (>&2 echo "Failed to find defaults file")
+    return 1
+  fi
+}
+
+# 
+prim_perform_substitution() {
+  echo "${PRIM_FILENAME}"
+  cat "${PRIM_FILENAME}" | envsubst | tee "${PRIM_FILENAME}"
+}
+PRIM_FILENAME="${1}"
+PRIM_DEFAULT_FILENAME="${1}.default"
+# Check if the filename has .template extension. If so, drop the .template
+# to avoid overwriting the template.
+PRIM_FILENAME=$(prim_rename_file "${PRIM_FILENAME}")
+
+prim_source_defaults "${PRIM_DEFAULT_FILENAME}"
+prim_perform_substitution 
+
