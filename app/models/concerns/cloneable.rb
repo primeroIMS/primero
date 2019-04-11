54efa6942dbd9ef8fd35145b6114351bf3fe5e37
Alberto Espinoza <aespinoz@quoininc.com>
2019-04-11 08:58:45 -0600

    PRIM-826 - Migrate Cloneable concern to Postgres for Roles. Using I18n

--- app/models/concerns/cloneable.rb
+++ app/models/concerns/cloneable.rb
@@ -3,7 +3,7 @@ module Cloneable
 
   def clone(new_name = '')
     new_record = self.dup.tap do |r|
-      r.name = new_name.presence || r.name.prepend('copy of ')
+      r.name = new_name.presence || r.name.prepend("#{I18n.t('copy_of')} ")
       r.unique_id = nil if r.respond_to?('unique_id')
       # Using method reflect_on_all_associations to get all associatons of the origina model
       if r.respond_to?('reflect_on_all_associations')
