0d434e463d9ef0150a0ea07ce563e56a81e0ffc1
Joshua Toliver <jtoliver@quoininc.com>
2019-04-11 17:06:54 -0400

    Revert "Removing references of couchdb in initializers"
    This reverts commit 0d2c062b14585f2a0e90a7ef49ba0d2b45f6ec62.

--- config/initializers/generate_options.rb
+++ config/initializers/generate_options.rb
@@ -1,9 +1,6 @@
-if ActiveRecord::Base.connection.table_exists? :locations
-  count = ActiveRecord::Base.connection.select_all("SELECT COUNT(id) FROM locations")
-                                       .rows
-                                       .flatten
-                                       .first
-  if count.positive?
-    OptionsJob.perform_now
-  end
+db = COUCHDB_SERVER.database!("primero_location_#{Rails.env}")
+system_settings_db = COUCHDB_SERVER.database!("primero_system_settings_#{Rails.env}")
+system_settings_docs = system_settings_db.all_docs['rows']
+if system_settings_docs.present? && system_settings_docs.size >= 2 && db.all_docs['rows'].present? && OptionsQueueStats.options_not_generated?
+  OptionsJob.perform_now
 end
