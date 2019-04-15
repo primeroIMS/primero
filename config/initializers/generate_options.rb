b8a8860e13038329fac138e94d53ad9c961f21d8
Alberto Espinoza <aespinoz@quoininc.com>
2019-04-15 13:18:33 -0600

    Fix initializers

--- config/initializers/generate_options.rb
+++ config/initializers/generate_options.rb
@@ -1,9 +1,13 @@
-if ActiveRecord::Base.connection.table_exists? :locations
-  count = ActiveRecord::Base.connection.select_all("SELECT COUNT(id) FROM locations")
-                                       .rows
-                                       .flatten
-                                       .first
-  if count.positive?
-    OptionsJob.perform_now
+begin
+  if ActiveRecord::Base.connection.table_exists? :locations
+    count = ActiveRecord::Base.connection.select_all("SELECT COUNT(id) FROM locations")
+                                         .rows
+                                         .flatten
+                                         .first
+    if count.positive?
+      OptionsJob.perform_now
+    end
   end
+rescue ActiveRecord::NoDatabaseError => e
+  puts e.message
 end
