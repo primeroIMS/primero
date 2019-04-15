19689d6166a66e0cc25157ebcf1d9a04d99ed7db
Michael Ciociola <michael.ciociola@quoininc.com>
2019-04-15 16:34:32 -0400

    Parameterized config files for application container
    Removed old .sample and .example configs
    Added runtime substitution script to app container
    Configs take runtime env from environment

---
+++ docker/application/root/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+printf "Starting Primero application container\\n"
+printf "Performing configuration substitution"
+
+
+# Search each of these directories for .template files and perform substitution
+# If you want this to be done from environment, redefine TEMPLATE_DIRS not
+APP_TEMPLATE_DIRS_DEFAULT=( "/srv/primero/application/config" )
+APP_TEMPLATE_DIRS=( "${TEMPLATE_DIRS[@]:-"${TEMPLATE_DIRS_DEFAULT[@]}"}" )
+for prim_filename in "${TEMPLATE_DIRS[@]}"/*.template
+do
+  # here we actually do the environment substitution
+  /sub.sh "${prim_filename}"
+done
+
+exec "$@"
