b610ec81ca54ebbf343f6927b0fe164b3cf2d811
Michael Ciociola <michael.ciociola@quoininc.com>
2019-04-22 14:10:06 -0400

    update app container to use new sub.sh

--- docker/application/root/entrypoint.sh
+++ docker/application/root/entrypoint.sh
@@ -6,13 +6,6 @@ printf "Performing configuration substitution"
 
 
 # Search each of these directories for .template files and perform substitution
-# If you want this to be done from environment, redefine TEMPLATE_DIRS not
-APP_TEMPLATE_DIRS_DEFAULT=( "/srv/primero/application/config" )
-APP_TEMPLATE_DIRS=( "${APP_TEMPLATE_DIRS[@]:-"${APP_TEMPLATE_DIRS_DEFAULT[@]}"}" )
-for prim_filename in "${APP_TEMPLATE_DIRS[@]}"/*.template
-do
-  # here we actually do the environment substitution
-  /sub.sh "${prim_filename}"
-done
+/sub.sh "/srv/primero/application/config"
 
 exec "$@"
