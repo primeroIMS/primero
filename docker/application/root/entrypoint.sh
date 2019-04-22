af9683edea25846e8f7f03fb900d431ddbb95488
Michael Ciociola <michael.ciociola@quoininc.com>
2019-04-22 15:17:00 -0400

    removed passwords from docker defaults

--- docker/application/root/entrypoint.sh
+++ docker/application/root/entrypoint.sh
@@ -8,4 +8,11 @@ printf "Performing configuration substitution"
 # Search each of these directories for .template files and perform substitution
 /sub.sh "/srv/primero/application/config"
 
+# Check if the postgres credentials are defined
+if [ -z "$POSTGRES_PASSWORD" ] || [ -z "$POSTGRES_USERNAME" ];
+then
+  printf "Postgres credentials not defined! Please check configuration.\\n"
+  exit 1
+fi
+
 exec "$@"
