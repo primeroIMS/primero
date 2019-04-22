54378d0227a9981071d40f77783028aea3bc0598
Michael Ciociola <michael.ciociola@quoininc.com>
2019-04-22 11:10:26 -0400

    improved build.sh by allowing partial build

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,28 @@
 
 set -ex
 
-docker build -f application/Dockerfile ../ -t application:prim-latest
-docker build -f beanstalkd/Dockerfile . -t beanstalkd:prim-latest
-docker build -f solr/Dockerfile ../ -t solr:prim-latest
-docker build -f nginx/Dockerfile . -t nginx:prim-latest
+BUILD_NGINX="docker build -f nginx/Dockerfile . -t nginx:prim-latest"
+BUILD_BEANSTALK="docker build -f beanstalkd/Dockerfile . -t beanstalkd:prim-latest"
+BUILD_SOLR="docker build -f solr/Dockerfile ../ -t solr:prim-latest"
+BUILD_APP="docker build -f application/Dockerfile ../ -t application:prim-latest"
+
+case $1 in
+  nginx)
+    eval "$BUILD_NGINX"
+    ;;
+  beanstalk)
+    eval "$BUILD_BEANSTALK"
+    ;;
+  solr)
+    eval "$BUILD_SOLR"
+    ;;
+  app)
+    eval "$BUILD_APP"
+    ;;
+  all)
+    eval "$BUILD_APP"
+    eval "$BUILD_SOLR"
+    eval "$BUILD_BEANSTALK"
+    eval "$BUILD_NGINX"
+    ;;
+esac
