2e4b07155e6e2b02b7ade0fc082bf15233d583e3
Michael Ciociola <michael.ciociola@quoininc.com>
2019-04-22 10:52:39 -0400

    removed couchdb from build script

--- docker/build.sh
+++ docker/build.sh
@@ -6,7 +6,3 @@ docker build -f application/Dockerfile ../ -t application:prim-latest
 docker build -f beanstalkd/Dockerfile . -t beanstalkd:prim-latest
 docker build -f solr/Dockerfile ../ -t solr:prim-latest
 docker build -f nginx/Dockerfile . -t nginx:prim-latest
-# docker build -f certbot-crond/Dockerfile . -t certbot-crond:prim-latest
-
-# TODO: not completed
-# docker build -f couchdb/Dockerfile . -t couchdb:prim-latest
