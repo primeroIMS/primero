b017b24af87f3ab5915a26041f534eadeb072f24
Michael Ciociola <michael.ciociola@quoininc.com>
2019-04-24 11:09:43 -0400

    created postgres container

--- docker/build.sh
+++ docker/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-set -ex
+set -euox pipefail
 
 BUILD_NGINX="docker build -f nginx/Dockerfile . -t nginx:prim-latest"
 BUILD_BEANSTALK="docker build -f beanstalkd/Dockerfile . -t beanstalkd:prim-latest"
 BUILD_SOLR="docker build -f solr/Dockerfile ../ -t solr:prim-latest"
 BUILD_APP="docker build -f application/Dockerfile ../ -t application:prim-latest"
+BUILD_POSTGRES="docker build -f postgres/Dockerfile . -t postgres:prim-latest"
 
 # if no params are set then set $1 to all
 if [ $# -eq 0 ];
@@ -28,10 +29,14 @@ case $1 in
   app)
     eval "$BUILD_APP"
     ;;
+  postgres)
+    eval "$BUILD_POSTGRES"
+    ;;
   all)
     eval "$BUILD_APP"
     eval "$BUILD_SOLR"
     eval "$BUILD_BEANSTALK"
     eval "$BUILD_NGINX"
+    eval "$BUILD_POSTGRES"
     ;;
 esac
---
+++ docker/postgres/Dockerfile
@@ -0,0 +1,2 @@
+FROM postgres:alpine
+
