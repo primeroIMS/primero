32518261b4e204a6565b3fda075a685c2bda97e1
Joshua Toliver <jtoliver@quoininc.com>
2019-04-26 15:46:37 -0400

    Correction syntax error

--- app/models/attachment_document.rb
+++ app/models/attachment_document.rb
@@ -2,5 +2,5 @@ class AttachmentDocument < ApplicationRecord
   belongs_to :record, polymorphic: true, optional: true
   has_one_attached :document
 
-  validates :document, file_size: { less_than_or_equal_to: 2.megabyte  }
+  validates :document, file_size: { less_than_or_equal_to: 2.megabytes  }
 end
