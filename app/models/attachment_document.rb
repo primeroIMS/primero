1e68557b87d1def7df0b63bd1020c50af861c457
Joshua Toliver <jtoliver@quoininc.com>
2019-04-26 15:42:59 -0400

    Adding gem to validate active storage, and valiations for document attachments

--- app/models/attachment_document.rb
+++ app/models/attachment_document.rb
@@ -1,4 +1,6 @@
 class AttachmentDocument < ApplicationRecord
   belongs_to :record, polymorphic: true, optional: true
   has_one_attached :document
+
+  validates :document, file_size: { less_than_or_equal_to: 2.megabyte  }
 end
