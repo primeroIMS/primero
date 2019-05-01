e52cc69ea8b8a8a6728afc2f97c1a0ed7ed7a46c
Joshua Toliver <jtoliver@quoininc.com>
2019-05-01 13:52:27 -0400

    Renaming filenames to conform to convention

--- app/javascript/components/index-table/cell-renderers/index.js
+++ app/javascript/components/index-table/cell-renderers/index.js
@@ -1,2 +1,2 @@
-export { default as DateCell } from "./date";
-export { default as ToggleIconCell } from "./toggleIcon";
+export { default as DateCell } from "./DateCell";
+export { default as ToggleIconCell } from "./ToggleIconCell";
