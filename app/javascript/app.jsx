e89710e11dff8f199c10b64cd13b10b360a369df
Joshua Toliver <jtoliver@quoininc.com>
2019-05-08 15:41:02 -0400

    Changing themeprovider to reflect new api, and changing order of css to rid ourselves of !important

--- app/javascript/app.jsx
+++ app/javascript/app.jsx
@@ -1,10 +1,11 @@
-import { MuiThemeProvider } from "@material-ui/core";
-import { createGenerateClassName, jssPreset } from "@material-ui/styles";
+import ThemeProvider from "@material-ui/styles/ThemeProvider";
+import createGenerateClassName from "@material-ui/styles/createGenerateClassName";
+import jssPreset from "@material-ui/styles/jssPreset";
+import StylesProvider from "@material-ui/styles/StylesProvider";
 import { ConnectedRouter } from "connected-react-router/immutable";
 import { create } from "jss";
 import rtl from "jss-rtl";
 import React from "react";
-import JssProvider from "react-jss/lib/JssProvider";
 import { Provider } from "react-redux";
 import { AppLayout } from "components/layouts";
 import { theme } from "./config";
@@ -13,20 +14,26 @@ import configureStore, { history } from "./store";
 
 const store = configureStore();
 
-const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
+const jss = create({
+  plugins: [...jssPreset().plugins, rtl()]
+});
 const generateClassName = createGenerateClassName();
 
 export default () => {
   return (
     <Provider store={store}>
       <I18nProvider>
-        <MuiThemeProvider theme={theme}>
-          <JssProvider jss={jss} generateClassName={generateClassName}>
+        <ThemeProvider theme={theme}>
+          <StylesProvider
+            injectFirst
+            jss={jss}
+            generateClassName={generateClassName}
+          >
             <ConnectedRouter history={history}>
               <AppLayout />
             </ConnectedRouter>
-          </JssProvider>
-        </MuiThemeProvider>
+          </StylesProvider>
+        </ThemeProvider>
       </I18nProvider>
     </Provider>
   );
