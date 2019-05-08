7edc3f2a94164d6f5c4b13968c3e0e93710ae71c
Joshua Toliver <jtoliver@quoininc.com>
2019-05-08 15:35:10 -0400

    Setting application font

--- app/javascript/config/theme.js
+++ app/javascript/config/theme.js
@@ -3,6 +3,21 @@ import { createMuiTheme } from "@material-ui/core/styles";
 export default createMuiTheme({
   direction: "ltr",
   typography: {
-    useNextVariants: true
+    useNextVariants: true,
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      "avenir next",
+      "avenir",
+      "helvetica neue",
+      "helvetica",
+      "ubuntu",
+      "roboto",
+      "noto",
+      "segoe ui",
+      "arial",
+      "sans-serif"
+    ].join(","),
+    fontWeight: 600
   }
 });
