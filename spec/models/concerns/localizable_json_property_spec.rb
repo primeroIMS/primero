9617709e23023141ef795f2bffa6daddaeac1dc9
Gerald Stanley Padgett Espinoza <padgett.gerald93@gmail.com>
2019-05-23 14:14:46 -0600

    PRIM-753 - Fix LocalizableJsonProperty

--- spec/models/concerns/localizable_json_property_spec.rb
+++ spec/models/concerns/localizable_json_property_spec.rb
@@ -46,12 +46,8 @@ describe LocalizableJsonProperty do
 
   it "should provide formatted hash of locale data" do
     @object = @klass.new name_a: "test a", name_b: "test b"
-    @object.formatted_hash.should == { "name_a" => "test a", "name_b" => "test b" }
-  end
-
-  it "should normalize line encoding in formatted hash" do
-    @object = @klass.new name_a: "test a\r\ntest a", name_b: "test b\r\ntest b"
-    @object.formatted_hash.should == { "name_a" => "test a\ntest a", "name_b" => "test b\ntest b" }
+    @object.localized_hash("a").should == { "name" => "test a" }
+    @object.localized_hash("b").should == { "name" => "test b" }
   end
 
 end
