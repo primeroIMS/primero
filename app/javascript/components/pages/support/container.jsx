bfeadc400151ddd255c02bc4524f70d94ba6239b
Gerald Padgett <gpadgett@quoininc.com>
2019-07-16 16:25:35 -0600

    PRIM-1053 - Build Support Page - Changes according to codereview

--- app/javascript/components/pages/support/container.jsx
+++ app/javascript/components/pages/support/container.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { useI18n } from "components/i18n";
-import { Card, CardContent, Grid, Link } from "@material-ui/core";
+import { Card, CardContent, Grid } from "@material-ui/core";
 import makeStyles from "@material-ui/styles/makeStyles";
 import styles from "./styles.css";
 import * as actions from "./action-creators";
@@ -11,11 +11,19 @@ import * as selectors from "./selectors";
 const Support = ({ supportData, fetchSupportData }) => {
   const css = makeStyles(styles)();
   const i18n = useI18n();
-  const data = supportData.toJS();
 
   useEffect(() => {
     fetchSupportData();
   }, []);
+
+  const DisplayData = (title, value) => {
+    return (
+      <p key={title}>
+        <span className={css.Title}> {title}: </span>
+        {value}
+      </p>
+    );
+  };
 
   return (
     <Grid container>
@@ -25,60 +33,15 @@ const Support = ({ supportData, fetchSupportData }) => {
       <Grid container direction="row" justify="center" alignItems="center">
         <Card className={css.CardShadow}>
           <CardContent>
-            <p>
-              <span className={css.Title}>{i18n.t("contact.field.name")}:</span>
-              {data.name}
-            </p>
-            <p>
-              <span className={css.Title}>
-                {i18n.t("contact.field.organization")}:
-              </span>
-              {data.organization}
-            </p>
-            <p>
-              <span className={css.Title}>
-                {i18n.t("contact.field.position")}:
-              </span>
-              {data.position}
-            </p>
-            <p>
-              <span className={css.Title}>
-                {i18n.t("contact.field.phone")}:
-              </span>
-              {data.phone}
-            </p>
-            <p>
-              <span className={css.Title}>
-                {i18n.t("contact.field.email")}:
-              </span>
-              {data.email}
-            </p>
-            <p>
-              <span className={css.Title}>
-                {i18n.t("contact.field.location")}:
-              </span>
-              {data.location}
-            </p>
-            <p>
-              <span className={css.Title}>
-                {i18n.t("contact.field.support_forum")}:
-              </span>
-              <Link target="_blank" href={data.support_forum}>
-                {data.support_forum}
-              </Link>
-            </p>
-            <p>
-              <span className={css.Title}>
-                {i18n.t("contact.field.other_information")}:
-              </span>
-              {data.other_information}
-            </p>
-            <p>
-              <span className={css.Title}>
-                {i18n.t("system_settings.primero_version")}:
-              </span>
-              {data.primero_version}
-            </p>
+            {supportData
+              .keySeq()
+              .toArray()
+              .map(data =>
+                DisplayData(
+                  i18n.t(`contact.field.${data}`),
+                  supportData.get(data)
+                )
+              )}
           </CardContent>
         </Card>
       </Grid>
