b174b67449e42e8c32f960c9d2b269ec502bbf1b
Dennis Hernandez <dhernand@quoininc.com>
2019-07-16 16:55:34 -0600

    PRIM-1006 - API: Form. Return all available locales.

--- app/helpers/api/v2/i18n_field_helper.rb
+++ app/helpers/api/v2/i18n_field_helper.rb
@@ -2,4 +2,12 @@ module Api::V2::I18nFieldHelper
   def strip_i18n_suffix(json)
     json.map{ |k,v| { k.gsub(/_i18n/, '') => v } }.inject(&:merge)
   end
+  
+  def complete_locales_for(properties, json)
+    locales = I18n.available_locales.map{ |locale| { locale.to_s => "" } }.inject(&:merge)
+    properties.each do |prop|
+      json[prop.to_s] = locales.merge(json[prop.to_s]) if json[prop.to_s].present?
+    end
+    json
+  end
 end
