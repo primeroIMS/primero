11143ef4319c0fb541c049076647d1c1ba991b7e
Dennis Hernandez <dhernand@quoininc.com>
2019-08-27 10:32:59 -0600

    PRIM-598 - Dashboard query optimization. Update associated user groups if changed for a user
    Update the associated user groups if user groups were changed for a user. We do
    this after_save because we need to query for users in the child models.

--- app/models/concerns/ownable.rb
+++ app/models/concerns/ownable.rb
@@ -4,7 +4,7 @@ module Ownable
 
   included do
 
-    before_save :update_ownership, :update_associated_user_groups
+    before_save :update_ownership
 
     property :owned_by, String
     property :owned_by_full_name, String
@@ -89,14 +89,16 @@ module Ownable
       self.previously_owned_by_agency = self.changes['owned_by_agency'].try(:fetch, 0) || owned_by_agency
       self.previously_owned_by_location = self.changes['owned_by_location'].try(:fetch, 0) || owned_by_location
     end
+
+    if (self.changes['assigned_user_names'].present? || self.new?)
+      self.update_associated_user_groups
+    end
   end
 
   def update_associated_user_groups
-    if (self.changes['assigned_user_names'].present? || self.new?)
-      self.associated_user_groups = (self.assigned_user_names || []).map do |user_name|
-        User.find_by_user_name(user_name).user_group_ids
-      end.flatten.compact
-    end
+    self.associated_user_groups = (self.assigned_user_names || []).map do |user_name|
+      User.find_by_user_name(user_name).user_group_ids
+    end.flatten.compact
   end
 
   def update_last_updated_by(current_user)
