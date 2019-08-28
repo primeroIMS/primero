6bb95ff80d230702626c644d80f0f85e3423c6b6
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2019-08-28 15:36:55 -0400

    Refactoring AuditLog model, controller, and job

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,13 +1,32 @@
 class AuditLog < ApplicationRecord
 
+  default_scope { order(timestamp: :desc) }
+
+  belongs_to :record, polymorphic: true, optional: true
+  belongs_to :user
+
   after_initialize do
     self.timestamp ||= DateTime.now
   end
 
-  belongs_to :record, polymorphic: true, optional: true
+  def display_id
+    return '' unless record.present?
+    if record.respond_to?(:display_id)
+      record.display_id
+    else
+      record.id
+    end
+  end
 
-  def mobile?
-    self.mobile_data.try(:[], 'mobile_id').present?
+  def user_name
+    metadata['user_name'] || user.user_name
+  end
+
+  def log_message
+    logger_action_prefix = I18n.t("logger.#{action}", locale: :en)
+    logger_action_identifier = "#{record_type} '#{display_id}'"
+    logger_action_suffix = "#{I18n.t("logger.by_user", locale: :en)} '#{user_name}'"
+    "#{logger_action_prefix} #{logger_action_identifier} #{logger_action_suffix}"
   end
 
 end
