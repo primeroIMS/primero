841f98ca76b06ae9b21e29d8de1f1d6ca75eaaab
Joshua Toliver <jtoliver@quoininc.com>
2019-09-03 12:25:38 -0400

    Returning module unqiue id with user endpoint, and selecting by unqiue id on front end

--- app/javascript/components/application/records.js
+++ app/javascript/components/application/records.js
@@ -1,7 +1,6 @@
 import { Record } from "immutable";
 
 export const PrimeroModuleRecord = Record({
-  id: null,
   unique_id: "",
   name: "",
   associated_record_types: [],
--- app/javascript/components/application/selectors.js
+++ app/javascript/components/application/selectors.js
@@ -10,5 +10,5 @@ export const selectLocales = state => state.getIn([NAMESPACE, "locales"], []);
 export const selectUserModules = state =>
   state.getIn([NAMESPACE, "modules"], Map({})).filter(m => {
     const userModules = state.getIn(["user", "modules"], null);
-    return userModules ? userModules.includes(m.id) : false;
+    return userModules ? userModules.includes(m.unique_id) : false;
   });
