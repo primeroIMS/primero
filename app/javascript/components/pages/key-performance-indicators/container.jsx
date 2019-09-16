06bdb494add98a92f3def94552adbffefea8d575
Jasper Lyons <jasper.lyons@gmail.com>
2019-09-16 17:39:57 +0100

    Add base KPI page based on Reports

--- app/javascript/components/pages/key-performance-indicators/container.jsx
+++ app/javascript/components/pages/key-performance-indicators/container.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import { PageContainer } from "components/page-container";
+import { Grid, Box } from "@material-ui/core";
+import { useI18n } from "components/i18n";
+import makeStyles from "@material-ui/styles/makeStyles";
+import styles from "./styles.css";
 
 export default function Reports() {
-  return <div />;
+  const i18n = useI18n();
+  const css = makeStyles(styles)();
+
+  return (
+    <div>
+      <PageContainer>
+        <Grid item xs={12}>
+          <Box alignItems="center" display="flex">
+            <Box flexgrow={1}>
+              <h1 className={css.title}>
+                {i18n.t("key_performance_indicators.label")}
+              </h1>
+            </Box>
+          </Box>
+        </Grid>
+      </PageContainer>
+    </div>
+  );
 }
