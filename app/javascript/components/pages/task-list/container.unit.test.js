0f7abcd549ab294a4d66822ef79a05c81b283931
Alberto Espinoza <aespinoz@quoininc.com>
2019-10-02 16:53:34 -0600

    PRIM-1300 -  R2 - Fix test

--- app/javascript/components/pages/task-list/container.unit.test.js
+++ app/javascript/components/pages/task-list/container.unit.test.js
@@ -5,6 +5,7 @@ import { fromJS, Map, List } from "immutable";
 import MUIDataTable from "mui-datatables";
 import { TableBodyRow } from "mui-datatables";
 import TaskList from "./container";
+import * as userRecord from "../../user/records";
 
 describe("<TaskList />", () => {
   let component;
@@ -15,24 +16,62 @@ describe("<TaskList />", () => {
       {},
       Map({
         records: Map({
-          Tasks: {
-            tasks: fromJS([
-              {
-                id: "123",
+          tasks: Map({
+            data: List([
+              Map({
+                id: "0df32f52-4290-4ce1-b859-74ac14c081bf",
+                record_type: "case",
+                record_id_display: "040e0b7",
                 priority: "high",
                 type: "Shelter temprary house",
                 due_date: "2019-07-01",
-                overdue: "true"
-              },
-              {
-                id: "456",
+              }),
+              Map({
+                id: "0df32f52-4290-4ce1-b859-74ac14c081bf",
+                record_type: "case",
+                record_id_display: "040e0b7",
                 priority: "low",
                 type: "Food service",
                 due_date: "2019-07-01",
-                overdue: "false"
-              }
+              })
             ]),
-          }
+            metadata: Map({
+              total: 2,
+              per: 20,
+              page: 1
+            })
+          })
+        }),
+        user: Map({
+          listHeaders: Map({
+            tasks: List([
+              userRecord.ListHeaderRecord({
+                name: 'id',
+                field_name: 'record_id_display',
+                id_search: false
+              }),
+              userRecord.ListHeaderRecord({
+                name: 'priority',
+                field_name: 'priority',
+                id_search: false
+              }),
+              userRecord.ListHeaderRecord({
+                name: 'type',
+                field_name: 'type',
+                id_search: false
+              }),
+              userRecord.ListHeaderRecord({
+                name: 'due_date',
+                field_name: 'due_date',
+                id_search: false
+              }),
+              userRecord.ListHeaderRecord({
+                name: 'status',
+                field_name: 'status',
+                id_search: false
+              })
+            ])
+          })
         })
       })
     ).component;
