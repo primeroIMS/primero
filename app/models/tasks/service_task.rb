23f37a601217945611538f3b4a4062f99804e027
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2019-10-03 13:07:43 -0400

    Addressed brittleness in task generation originally resolved in PRIM-959 in v1.x

--- app/models/tasks/service_task.rb
+++ app/models/tasks/service_task.rb
@@ -6,18 +6,17 @@ module Tasks
       tasks = []
       if record.services_section.present?
         record.services_section.each do |service|
-          if has_task?(service)
+          if has_task?(record, service)
             tasks << ServiceTask.new(record, service)
           end
         end
       end
-      tasks
+      tasks.select{ |task| task.due_date.present? }
     end
 
-    def self.has_task?(service)
-      #TODO: or should use service['service_implemented'] == Child::SERVICE_NOT_IMPLEMENTED
-      service['service_appointment_date'].present? &&
-      !service['service_implemented_day_time'].present?
+    def self.has_task?(record, service)
+      service['service_implemented_day_time'].blank? &&
+      record&.service_due_date(service).present?
     end
 
     def initialize(record, service)
