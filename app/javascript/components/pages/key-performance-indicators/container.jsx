7ea68aea9db4c151d9d4d5b42e61e7aa85d0a41f
Jasper Lyons <jasper.lyons@gmail.com>
2019-10-20 17:00:32 +0100

    Start building prototype date picker UI
    This should be moved into it's own component w/ test etc as
    it is starting to get fairly complicated.

--- app/javascript/components/pages/key-performance-indicators/container.jsx
+++ app/javascript/components/pages/key-performance-indicators/container.jsx
@@ -1,9 +1,27 @@
+import { fromJS } from "immutable";
 import PropTypes from "prop-types";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
 import { connect, batch } from "react-redux";
 import { PageContainer, PageHeading, PageContent } from "components/page";
-import { Grid, Box } from "@material-ui/core";
+import {
+  Grid,
+  Box,
+  Button,
+  IconButton,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  DialogContentText,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl
+} from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
 import { OptionsBox, DashboardTable } from "components/dashboard";
 import { useI18n } from "components/i18n";
 import makeStyles from "@material-ui/styles/makeStyles";
@@ -21,6 +39,8 @@ function KeyPerformanceIndicators({ fetchNumberOfCases, numberOfCases }) {
     });
   }, []);
 
+  let [numberOfCasesMenuOpen, setNumberOfCasesMenuOpen] = useState(false);
+
   return (
     <div>
       <PageContainer>
@@ -33,6 +53,68 @@ function KeyPerformanceIndicators({ fetchNumberOfCases, numberOfCases }) {
                 <Grid item>
                   <OptionsBox
                     title="Number of Cases"
+                    action={
+                      <div>
+                        <IconButton
+                          aria-label="More"
+                          aria-controls="long-menu"
+                          aria-haspopup="true"
+                          variant="contained"
+                          onClick={() => setNumberOfCasesMenuOpen(true)}
+                        >
+                          <MoreVertIcon />
+                        </IconButton>
+                        <Dialog
+                          open={numberOfCasesMenuOpen}
+                          onClose={() => setNumberOfCasesMenuOpen(false)}
+                        >
+                          <DialogTitle>Date Range</DialogTitle>
+                          <DialogContent>
+                            <DialogContentText>See the number of case over the last 1, 3 or 6 month, or choose a custom time period.</DialogContentText>
+                            <FormControl>
+                              <RadioGroup>
+                                <FormControlLabel
+                                  value="1-month"
+                                  control={<Radio />}
+                                  label="1 Month"
+                                  labelPlacement="end"
+                                />
+                                <FormControlLabel
+                                  value="3-month"
+                                  control={<Radio />}
+                                  label="3 Months"
+                                  labelPlacement="end"
+                                />
+                                <FormControlLabel
+                                  value="6-month"
+                                  control={<Radio />}
+                                  label="6 Months"
+                                  labelPlacement="end"
+                                />
+                              </RadioGroup>
+                              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                <FormControlLabel
+                                  value="from"
+                                  control={<DatePicker />}
+                                  label="From"
+                                  labelPlacement="start"
+                                />
+                                <FormControlLabel
+                                  value="to"
+                                  control={<DatePicker />}
+                                  label="To"
+                                  labelPlacement="start"
+                                />
+                              </MuiPickersUtilsProvider>
+                            </FormControl>
+                          </DialogContent>
+                          <DialogActions>
+                            <Button onClick={() => setNumberOfCasesMenuOpen(false)}>Cancel</Button>
+                            <Button onClick={() => setNumberOfCasesMenuOpen(false)}>Apply</Button>
+                          </DialogActions>
+                        </Dialog>
+                      </div>
+                    }
                     to="/key-performance-indicators"
                   >
                     <DashboardTable
