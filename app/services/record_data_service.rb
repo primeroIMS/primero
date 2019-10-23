fa23a69c571a6c718a9413aa13d0557b7c828139
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2019-10-23 09:47:24 -0400

    Extracting record data transformations for the API into a dedicated service. Adding logic for indicating whether a record is in a user's scope.

---
+++ app/services/record_data_service.rb
@@ -0,0 +1,49 @@
+class RecordDataService
+
+  def self.data(record, user, selected_field_names)
+    data = record.data
+    data = select_fields(data, selected_field_names)
+    data = embed_user_scope(data, record, selected_field_names, user)
+    data = embed_hidden_name(data, record, selected_field_names)
+    data = embed_flag_metadata(data, record, selected_field_names)
+    data = embed_photo_metadata(data, record, selected_field_names)
+    data
+  end
+
+  def self.select_fields(data, selected_field_names)
+    data.compact_deep
+        .select { |k, _| selected_field_names.include?(k) }
+        .to_h
+  end
+
+  def self.embed_user_scope(data, record, selected_field_names, user)
+    return data unless selected_field_names.include?('record_in_scope')
+
+    data['record_in_scope'] = user.can?(:read, record)
+    data
+  end
+
+  def self.embed_hidden_name(data, record, selected_field_names)
+    return data unless record.hidden_name && selected_field_names.include?('name')
+
+    data['name'] = '*******'
+    data
+  end
+
+  def self.embed_flag_metadata(data, record, selected_field_names)
+    return data unless selected_field_names.include?('flag_count')
+
+    data['flag_count'] = record.flag_count
+    data
+  end
+
+  def self.embed_photo_metadata(data, record, selected_field_names)
+    return data unless selected_field_names.include?('photos') && record.photos.count.positive?
+
+    data['photos'] = record.photos.map do |photo|
+      Rails.application.routes.url_helpers.rails_blob_path(photo.image, only_path: true)
+    end
+    data
+  end
+
+end
