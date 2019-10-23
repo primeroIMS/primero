8012e926ab6142b0f801f909c8c733598e1159d5
Alberto Espinoza <aespinoz@quoininc.com>
2019-10-23 16:20:55 -0600

    PRIM-1362 PRIM-1363 R2 Transfers and Assignments Panel
    * Test for components WIP

---
+++ app/javascript/components/transitions/TransitionUser.unit.test.js
@@ -0,0 +1,25 @@
+import "test/test.setup";
+import { expect } from "chai";
+import { setupMountedComponent } from "test";
+import { Box } from "@material-ui/core";
+import TransitionUser from "./TransitionUser";
+
+describe("<TransitionUser />", () => {
+  let component;
+  const props = {
+    label: "Recipient",
+    transitionUser: "primero_cp",
+    classes: {
+      spaceGrid: "testStyle",
+      transtionLabel: "testStyle",
+      transtionValue: "testStyle"
+    }
+  };
+  beforeEach(() => {
+    ({ component } = setupMountedComponent(TransitionUser, props));
+  });
+
+  it("renders a Box component inside of TransitionUser", () => {
+    expect(component.find(TransitionUser).find(Box)).to.have.length(1);
+  });
+})
