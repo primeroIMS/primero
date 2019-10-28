5d976842d52b5c89c22f3e74e0388f2bac010b4c
Dennis Hernandez <dhernand@quoininc.com>
2019-10-28 09:30:04 -0600

    PRIM-1353 - Services Lookups. Handle multiselects.

--- app/assets/javascripts/views/populate_select_boxes.js
+++ app/assets/javascripts/views/populate_select_boxes.js
@@ -114,13 +114,14 @@ _primero.Views.PopulateSelectBoxes = _primero.Views.Base.extend({
       }
 
       if (value) {
-        if(value != "") {
-          // If the selected value does not exists we will render it hidden to avoid losing it on save.
-          var valueNotExists = this_select.find('option[value=' + value + ']').length < 1;
-          if(valueNotExists) {
-            self.addNonActiveOption(this_select, value);
-          }
+
+        if(value !== "") {
+          var values = _.isArray(value) ? value : value.toString().split(",");
+          _.each(values, function(currentValue) {
+            self.addNonActiveOption(this_select, currentValue);
+          });
         }
+
         this_select.val(value);
       }
     });
@@ -132,13 +133,21 @@ _primero.Views.PopulateSelectBoxes = _primero.Views.Base.extend({
     var stringsSource = select_box.attr('data-populate');
     var isAgency = stringsSource.indexOf('Agency') != -1;
     var isService = stringsSource.indexOf('lookup-service-type') != -1;
-    var displayText = value;
-    if(isService) {
-      displayText = I18n.t("messages.non_active_service", { service: value });
-    } else if(isAgency) {
-      displayText = I18n.t("messages.non_active_agency", { agency: value });
+
+    if(isAgency || isService) {
+      // If the selected value does not exists we will render it hidden to avoid losing it on save.
+      var valueNotExists = select_box.find('option[value=' + value + ']').length < 1;
+
+      if(valueNotExists) {
+        var displayText = value;
+        if(isService) {
+          displayText = I18n.t("messages.non_active_service", { service: value });
+        } else if(isAgency) {
+          displayText = I18n.t("messages.non_active_agency", { agency: value });
+        }
+        select_box.append('<option style="display: none" value="' + value + '">' + displayText + '</option>');
+      }
     }
-    select_box.append('<option style="display: none" value="' + value + '">' + displayText + '</option>');
   },
 
 
