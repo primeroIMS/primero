d2b715e822073e91f3bbd40774faf8a8a9dc2e31
Gerald Padgett <gpadgett@quoininc.com>
2019-10-29 15:53:14 -0600

    PRIM-1394 - Consent notification not appearing for users without ability to override consent
    * Adding missing test cases on provided-consent from referrals and transfers.

--- app/javascript/components/record-actions/transitions/parts/referrals/provided-consent.unit.test.js
+++ app/javascript/components/record-actions/transitions/parts/referrals/provided-consent.unit.test.js
@@ -11,7 +11,7 @@ describe("<ProvidedConsent /> - referrals", () => {
       transfer: false
     }
   };
-  it("should not render anything when child has provided consent", () => {
+  it("should not render anything when child has provided consent and user can't consent override", () => {
     const props = {
       canConsentOverride: false,
       providedConsent: true
@@ -26,10 +26,40 @@ describe("<ProvidedConsent /> - referrals", () => {
     expect(component).to.be.empty;
   });
 
-  it("should render <ProvidedForm> when child has not provided consent", () => {
+  it("should not render anything when child has provided consent and user can consent override", () => {
     const props = {
       canConsentOverride: true,
-      providedConsent: false,
+      providedConsent: true
+    };
+    const { component } = setupMountedComponent(
+      ProvidedConsent,
+      props,
+      {},
+      [],
+      formProps
+    );
+    expect(component).to.be.empty;
+  });
+
+  it("should render <ProvidedForm> when child has not provided consent and user can consent override", () => {
+    const props = {
+      canConsentOverride: true,
+      providedConsent: false
+    };
+    const { component } = setupMountedComponent(
+      ProvidedConsent,
+      props,
+      {},
+      [],
+      formProps
+    );
+    expect(component.find(ProvidedForm)).to.have.lengthOf(1);
+  });
+
+  it("should render <ProvidedForm> when child has not provided consent and user can't consent override", () => {
+    const props = {
+      canConsentOverride: false,
+      providedConsent: false
     };
     const { component } = setupMountedComponent(
       ProvidedConsent,
--- app/javascript/components/record-actions/transitions/parts/transfers/provided-consent.unit.test.js
+++ app/javascript/components/record-actions/transitions/parts/transfers/provided-consent.unit.test.js
@@ -10,7 +10,7 @@ describe("<ProvidedConsent /> - transfers", () => {
       transfer: false
     }
   };
-  it("should not render anything when child has provided consent", () => {
+  it("should not render anything when child has provided consent and user can't consent override", () => {
     const props = {
       canConsentOverride: false,
       providedConsent: true
@@ -25,9 +25,39 @@ describe("<ProvidedConsent /> - transfers", () => {
     expect(component).to.be.empty;
   });
 
-  it("should render <ProvidedForm> when child has not provided consent", () => {
+  it("should not render anything when child has provided consent and user can consent override", () => {
     const props = {
       canConsentOverride: true,
+      providedConsent: true
+    };
+    const { component } = setupMountedComponent(
+      ProvidedConsent,
+      props,
+      {},
+      [],
+      formProps
+    );
+    expect(component).to.be.empty;
+  });
+
+  it("should render <ProvidedForm> when child has not provided consent and user can consent override", () => {
+    const props = {
+      canConsentOverride: true,
+      providedConsent: false
+    };
+    const { component } = setupMountedComponent(
+      ProvidedConsent,
+      props,
+      {},
+      [],
+      formProps
+    );
+    expect(component.find(ProvidedForm)).to.have.lengthOf(1);
+  });
+
+  it("should render <ProvidedForm> when child has not provided consent and user can't consent override", () => {
+    const props = {
+      canConsentOverride: false,
       providedConsent: false
     };
     const { component } = setupMountedComponent(
