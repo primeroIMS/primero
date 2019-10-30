e87d5b48909dad942d55077a042601e3596b1375
Ron Elledge <relledge@quoininc.com>
2019-10-30 08:35:53 -0500

    removed SL specific code from 1.6 user migration

--- db/upgrade_migrations/migration_1.5_to_1.6/migrations/users.rb
+++ db/upgrade_migrations/migration_1.5_to_1.6/migrations/users.rb
@@ -4,21 +4,19 @@ include MigrationHelper
 
 locations = MigrationHelper.get_locations
 
-puts "------------------------------------------------------------------"
-puts "Migrating Users..."
-records_to_save = []
-User.all.each do |user|
-  next if user.location.blank?
-  user_location = MigrationHelper.patch_location(user.location)
-  location = locations.select{|l| l[:display_text] == user_location || l[:display_text_2] == user_location}.first
-  
-  if location.present? && user.location.present?
-    user.location = location['id']
-    records_to_save << user
-  end
-end
+    puts "------------------------------------------------------------------"
+    puts "Migrating Users..."
+    records_to_save = []
+    User.all.each do |user|
+      next if user.location.blank?
+      location = locations.select{|l| l[:display_text] == user.location}.first
+      if location.present? && user.location.present?
+        user.location = location['id']
+        records_to_save << user
+      end
+    end
 
-if records_to_save.present?
-  puts "Updating #{records_to_save.count} users"
-  User.save_all!(records_to_save)
-end
+    if records_to_save.present?
+      puts "Updating #{records_to_save.count} users"
+      User.save_all!(records_to_save)
+    end
