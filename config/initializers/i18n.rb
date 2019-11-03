d397d399e910c31065d62e6d66b4f16c7b4c0737
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2019-11-03 14:14:08 -0500

    Moving i18n.js generation and manifest to the docker image build.

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,2 +1,2 @@
-manifest = Rails.root.join('tmp', 'i18n-manifest.txt')
+manifest = Rails.root.join('config', 'i18n-manifest.txt')
 I18N_TRANSLATIONS_FILE = File.exist?(manifest) ? File.open(manifest).read : nil
