e418a660115809056712928630d2bc5d334dc2b2
Joshua Toliver <jtoliver@quoininc.com>
2019-11-04 14:44:22 -0500

    Adding beanstalk command to entrypoint

--- docker/beanstalkd/root/entrypoint.sh
+++ docker/beanstalkd/root/entrypoint.sh
@@ -2,4 +2,8 @@
 
 set -euox pipefail
 
-beanstalkd -V -p $BEANSTALKD_PORT -u beanstalk -b /var/lib/beanstalkd
+if [ $1 == "beanstalkd-start" ]; then
+  beanstalkd -V -p $BEANSTALKD_PORT -u beanstalk -b /var/lib/beanstalkd
+else
+  exec $@
+fi
