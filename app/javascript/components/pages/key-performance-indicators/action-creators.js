24094c36aabb04757f5ea855fbe3ec1ef3830d62
Jasper Lyons <jasper.lyons@gmail.com>
2019-11-06 14:34:49 +0000

    Implement reporting delay KPI
    This encapsulates factoring out the KPI view information into a
    separate component to use for both implemented KPIs. It has
    exposed some issues with regards to how KPI data will be passed in
    to the KeyPerformanceIndicators component. It has also raised an
    issue around displaying different types of table + ations for
    different KPIs.
    
    For example, the "Number of Cases" KPI displays a set of data
    over N months with one column per month. It feels like the date
    ranges that a user will need to view data for should be custom but
    limited to N months where N won't break the layout.
    
    On the other hand the "Reporting Delay" KPI shows traditional
    aggreate data for a specific set of groupings of data. Not only
    do we want to show a custom table cell for percentages but we
    also may want to offer different date range selections. This is
    because the space required to show data is not related to the date
    range we select data over.
    
    I suspect we'll need a separate set of files for each KPI.

--- app/javascript/components/pages/key-performance-indicators/action-creators.js
+++ app/javascript/components/pages/key-performance-indicators/action-creators.js
@@ -12,3 +12,23 @@ export const fetchNumberOfCases = () => async dispatch => {
     }
   });
 };
+
+export const fetchReportingDelay = () => async dispatch => {
+  dispatch({
+    type: Actions.REPORTING_DELAY_SUCCESS,
+    //    api: {
+    //      path: "key_performance_indicators/number_of_cases"
+    //    },
+    payload: {
+      columns: ["Delay", "Total Cases", ""],
+      data: [
+        ["0-3 days", 123, 0.395],
+        ["4-5 days", 56, 0.1],
+        ["6-14 days", 120, 0.395],
+        ["15-30 days", 48, 0.1],
+        ["1-3 months", 4, 0.01],
+        ["3 months +", 0, 0.0],
+      ]
+    }
+  });
+};
--- app/javascript/components/pages/key-performance-indicators/actions.js
+++ app/javascript/components/pages/key-performance-indicators/actions.js
@@ -1,2 +1,4 @@
 export const NUMBER_OF_CASES = "KeyPerformanceIndicators/NUMBER_OF_CASES";
 export const NUMBER_OF_CASES_SUCCESS= "KeyPerformanceIndicators/NUMBER_OF_CASES_SUCCESS";
+export const REPORTING_DELAY = "KeyPerformanceIndicators/REPORTING_DELAY";
+export const REPORTING_DELAY_SUCCESS= "KeyPerformanceIndicators/REPORTING_DELAY_SUCCESS";
