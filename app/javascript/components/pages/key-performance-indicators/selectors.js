c014ce84abf07386c817c10611d441d81678785e
Jasper Lyons <jasper.lyons@gmail.com>
2019-11-06 17:30:05 +0000

    Implement percentage bars in tables
    I've also removed the date picking UI as I suspect a drop down
    be more fitting after consulting the [design document](1) again.
    
    [1]: zpl.io/VQQM65k

--- app/javascript/components/pages/key-performance-indicators/selectors.js
+++ app/javascript/components/pages/key-performance-indicators/selectors.js
@@ -3,14 +3,12 @@ import NAMESPACE from "./namespace";
 
 export const numberOfCases = state => {
   return state.getIn(["records", NAMESPACE, 'numberOfCases'], fromJS({
-    columns: ["REPORTING SITE", "SEP 2019", "AUG 2019", "JUL 2019"],
     data: []
   }));
 };
 
 export const reportingDelay = state => {
   return state.getIn(["records", NAMESPACE, 'reportingDelay'], fromJS({
-    columns: ["Delay", "Total Cases", ""],
     data: []
   }));
 };
