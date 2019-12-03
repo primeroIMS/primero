db54c2c47b3e79875f6028d2455988b391cf9f54
Gerald Padgett <gpadgett@quoininc.com>
2019-12-03 14:14:22 -0600

    PRIM-1463 - Values depending on lookups for I18n
    * Removing not needed selector

--- app/javascript/components/pages/task-list/selectors.js
+++ app/javascript/components/pages/task-list/selectors.js
@@ -2,13 +2,3 @@ import { List } from "immutable";
 
 export const selectListHeaders = (state, namespace) =>
   state.getIn(["user", "listHeaders", namespace], List([]));
-
-export const getServiceTypeLookupValues = state => {
-  const lookup = state
-    .getIn(["forms", "options", "lookups"], List([]))
-    .filter(o => o.get("unique_id") === "lookup-service-type")
-    .first()
-    .get("values");
-
-  return lookup;
-};
