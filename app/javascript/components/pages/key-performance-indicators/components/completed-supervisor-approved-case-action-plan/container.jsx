c514f703c767faf0f27ea277db8d9ff05bbc85e2
Jasper Lyons <jasper.lyons@gmail.com>
2019-12-04 14:22:21 +0000

    Add date range selection to other kpis

--- app/javascript/components/pages/key-performance-indicators/components/completed-supervisor-approved-case-action-plan/container.jsx
+++ app/javascript/components/pages/key-performance-indicators/components/completed-supervisor-approved-case-action-plan/container.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import { OptionsBox } from "components/dashboard";
+import { DateRangeSelect } from "components/key-performance-indicators";
 import { StackedPercentageBar } from "components/key-performance-indicators";
 
 export default function CompletedSupervisorApprovedCaseActionPlan() {
+
+  let threeMonthsAgo = new Date();
+  threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3)
+  let dateRanges = [{
+    value: '3-months',
+    name: 'Last 3 Months',
+    from: threeMonthsAgo,
+    to: new Date()
+  }]
+
   return (
     <OptionsBox
       title="Completed Action Plan Approved by Supervisor"
+      action={
+        <DateRangeSelect
+          ranges={dateRanges}
+          selectedRange={dateRanges[0]}
+          withCustomRange
+        />
+      }
     >
       <StackedPercentageBar
         percentages={[{ percentage: 0.5, label: "Completed action plan approved by supervisor" }]}
