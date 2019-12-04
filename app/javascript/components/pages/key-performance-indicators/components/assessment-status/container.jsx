edfa9dcf1e5a63ab3ca602329d6d413b4d07f5c2
Jasper Lyons <jasper.lyons@gmail.com>
2019-12-04 14:22:21 +0000

    Add date range selection to other kpis

--- app/javascript/components/pages/key-performance-indicators/components/assessment-status/container.jsx
+++ app/javascript/components/pages/key-performance-indicators/components/assessment-status/container.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import { OptionsBox } from "components/dashboard";
+import { DateRangeSelect } from "components/key-performance-indicators";
 import { StackedPercentageBar } from "components/key-performance-indicators";
 
 export default function AssessmentStatus() {
+
+  let threeMonthsAgo = new Date();
+  threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3)
+  let dateRanges = [{
+    value: '3-months',
+    name: 'Last 3 Months',
+    from: threeMonthsAgo,
+    to: new Date()
+  }]
+
   return (
     <OptionsBox
       title="Assessment Status"
+      action={
+        <DateRangeSelect
+          ranges={dateRanges}
+          selectedRange={dateRanges[0]}
+          withCustomRange
+        />
+      }
     >
       <StackedPercentageBar
         percentages={[{ percentage: 0.5, label: "Completed & Supervisor Approved" }, { percentage: 0.26, label: "Completed Only" }]}
--- app/javascript/components/pages/key-performance-indicators/components/completed-case-safety-plan/container.jsx
+++ app/javascript/components/pages/key-performance-indicators/components/completed-case-safety-plan/container.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import { OptionsBox } from "components/dashboard";
+import { DateRangeSelect } from "components/key-performance-indicators";
 import { StackedPercentageBar } from "components/key-performance-indicators";
 
 export default function CompletedCaseSafetyPlan() {
+
+  let threeMonthsAgo = new Date();
+  threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3)
+  let dateRanges = [{
+    value: '3-months',
+    name: 'Last 3 Months',
+    from: threeMonthsAgo,
+    to: new Date()
+  }]
+
   return (
     <OptionsBox
       title="Completed Case Safety Plan"
+      action={
+        <DateRangeSelect
+          ranges={dateRanges}
+          selectedRange={dateRanges[0]}
+          withCustomRange
+        />
+      }
     >
       <StackedPercentageBar
         percentages={[{ percentage: 0.5, label: "Completed Case Safety Plan" }]}
