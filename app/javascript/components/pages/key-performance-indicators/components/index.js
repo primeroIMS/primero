1073e7ea13b4340b38247742d3821d2ac15b923f
Jasper Lyons <jasper.lyons@gmail.com>
2019-12-04 13:58:37 +0000

    Extract AssessmentStatus kpi to component

--- app/javascript/components/pages/key-performance-indicators/components/index.js
+++ app/javascript/components/pages/key-performance-indicators/components/index.js
@@ -2,3 +2,4 @@ export { NumberOfCases } from "./number-of-cases";
 export { NumberOfIncidents } from "./number-of-incidents";
 export { ReportingDelay } from "./reporting-delay";
 export { ServiceAccessDelay } from "./service-access-delay";
+export { AssessmentStatus } from "./assessment-status";
