3d388ca71665b427cdae50f63c818ac4b1a8960a
Jasper Lyons <jasper.lyons@gmail.com>
2019-12-10 12:14:11 +0000

    Extract SingleAggregateMetric component

---
+++ app/javascript/components/key-performance-indicators/single-aggregate-metric/index.js
@@ -0,0 +1,1 @@
+export { default as SingleAggregateMetric } from "./container";
--- app/javascript/components/pages/key-performance-indicators/components/total-follow-up-meetings/container.jsx
+++ app/javascript/components/pages/key-performance-indicators/components/total-follow-up-meetings/container.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import { OptionsBox } from "components/dashboard";
-import { DateRangeSelect } from "components/key-performance-indicators";
-import makeStyles from "@material-ui/styles/makeStyles";
-import styles from "./styles.css";
+import { DateRangeSelect, SingleAggregateMetric } from "components/key-performance-indicators";
 
 export default function TotalFollowUpMeetings() {
-  let css = makeStyles(styles)();
-
   let threeMonthsAgo = new Date();
   threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3)
   let dateRanges = [{
@@ -27,10 +23,10 @@ export default function TotalFollowUpMeetings() {
         />
       }
     >
-      <div className={css.root}>
-        <h1 className={css.value}>45</h1>
-        <span className={css.label}>Total follow-up meetings</span>
-      </div>
+      <SingleAggregateMetric
+        value="45"
+        label="Total follow-up meetings"
+      />
     </OptionsBox>
   );
 }
