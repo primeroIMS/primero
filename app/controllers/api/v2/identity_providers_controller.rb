c0c585f33169cef4e4d09a5f535fb5a25ccd83bd
Matt Counts <mcounts@quoininc.com>
2019-12-11 16:00:11 -0500

    PRIM-1526 update: skip audit log action

--- app/controllers/api/v2/identity_providers_controller.rb
+++ app/controllers/api/v2/identity_providers_controller.rb
@@ -1,6 +1,7 @@
 module Api::V2
   class IdentityProvidersController < ApplicationApiController
     skip_before_action :authenticate_user!, only: [:index]
+    skip_after_action :write_audit_log, only: [:index]
 
     def index
       @identity_providers = IdentityProvider.all
