689251131b2f5e4d8c8489498356450d296acbf5
Matt Counts <mcounts@quoininc.com>
2020-01-08 14:42:20 -0500

    PRIM-1602 update: cleanup

--- app/javascript/components/pages/login/container.jsx
+++ app/javascript/components/pages/login/container.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { withRouter } from "react-router-dom";
 
 import { NAME } from "./config";
 import IdpSelection from "./idp-selection";
 import LoginForm from "./login-form";
 import { selectUseIdentityProvider } from "./selectors";
-import { selectIdentityProviders } from "./idp-selection/selectors";
 
 const Container = () => {
   const useIdentity = useSelector(state => selectUseIdentityProvider(state));
-  let msalApp;
-    return (
-      <>
-        {useIdentity ? <IdpSelection /> : <LoginForm />}
-      </>
-    );
+
+  return (
+    <>
+      {useIdentity ? <IdpSelection /> : <LoginForm />}
+    </>
+  );
 };
 
 Container.displayName = NAME;
 
-export default withRouter(Container);
+export default Container;
