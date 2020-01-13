ee0f7da585bd249c6ab26cb679f634cbde5825f9
Joshua Toliver <jtoliver@quoininc.com>
2020-01-13 13:23:35 -0500

    Adding constants unit test for form component

---
+++ app/javascript/components/form/constants.unit.test.js
@@ -0,0 +1,27 @@
+import { expect } from "../../test/unit-test-helpers";
+
+import * as constants from "./constants";
+
+describe("<Form /> - Constants", () => {
+  it("should have known properties", () => {
+    const clonedConstants = { ...constants };
+
+    expect(clonedConstants).to.be.an("object");
+    [
+      "TEXT_FIELD",
+      "TEXT_AREA",
+      "FORM_MODE_SHOW",
+      "FORM_MODE_EDIT",
+      "FORM_MODE_NEW",
+      "FORM_MODE_DIALOG",
+      "SELECT_FIELD",
+      "TICK_FIELD",
+      "CHECK_BOX_FIELD"
+    ].forEach(property => {
+      expect(clonedConstants).to.have.property(property);
+      delete clonedConstants[property];
+    });
+
+    expect(clonedConstants).to.be.empty;
+  });
+});
