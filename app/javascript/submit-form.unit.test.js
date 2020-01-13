feba70c92ff5e0fd3c5299439ef187cfcf1938f2
Gerald Padgett <gpadgett@quoininc.com>
2020-01-13 08:54:10 -0600

    PRIM-1576 - UX Add Service action
    * Fixing comments

--- app/javascript/submit-form.unit.test.js
+++ app/javascript/submit-form.unit.test.js
@@ -1,10 +1,14 @@
 import { expect } from "chai";
 
-import constants from "./submit-form";
+import * as constants from "./submit-form";
 
 describe("submit-form", () => {
   it("should have known properties", () => {
-    expect(constants).to.be.a("function");
-    expect(constants.name).to.be.equals("submitForm");
+    const clone = { ...constants };
+
+    expect(clone.default).to.be.a("function");
+    expect(clone.default.name).to.be.equals("submitForm");
+    delete clone.default;
+    expect(clone).to.be.empty;
   });
 });
