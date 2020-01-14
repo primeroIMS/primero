d026d8e12a90b00b436986a831464ee71b73807f
Joshua Toliver <jtoliver@quoininc.com>
2020-01-14 12:24:00 -0500

    Using rescue_from instead of being/rescue in login controller

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,11 +1,20 @@
 class LoginController < ApplicationController
 
+  rescue_from ActionView::MissingTemplate do |e|
+    render_404
+  end
+
   def index
-    begin
-      @identity_provider = IdentityProvider.find_by(provider_type: params[:provider])
+    @identity_provider = IdentityProvider.find_by(provider_type: params[:provider])
+    
+    if @identity_provider.present?
       render layout: 'identity', template: "login/#{params[:provider]}"
-    rescue ActionView::MissingTemplate => e
-      render file: 'public/404.html', status: 404, layout: false
+    else
+      render_404
     end
+  end
+
+  def render_404
+    render file: 'public/404.html', status: 404, layout: false
   end
 end
