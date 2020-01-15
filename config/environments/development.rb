a09087a8f37dc22c9130b8fd35f410ad0e4c154e
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2020-01-15 09:13:55 -0500

    Parametrizing Primero with the active storage type

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
-Rails.application.configure do
+# frozen_string_literal: true
 
+Rails.application.configure do
   config.cache_classes = false
   config.action_controller.perform_caching = false
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
@@ -9,10 +10,10 @@ Rails.application.configure do
   # See everything in the log (default is :info)
   config.log_level = :debug
 
-  config.eager_load = ENV['PROFILE'] == 'true' ? true : false
+  config.eager_load = ENV['PROFILE'] == 'true'
 
   config.action_mailer.raise_delivery_errors = false
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  config.active_storage.service = ENV['PRIMERO_STORAGE_TYPE'].to_sym
 end
