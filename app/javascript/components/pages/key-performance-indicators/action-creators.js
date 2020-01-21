09c6eaf3cb640219c1983363cc077f092931122d
Jasper Lyons <jasper.lyons@gmail.com>
2020-01-21 14:08:45 +0000

    Move ServiceAccessDelay to use api

--- app/javascript/components/pages/key-performance-indicators/action-creators.js
+++ app/javascript/components/pages/key-performance-indicators/action-creators.js
@@ -23,35 +23,15 @@ export const fetchReportingDelay = () => async dispatch => {
     type: Actions.REPORTING_DELAY,
     api: {
       path: "key_performance_indicators/reporting_delay"
-    },
-    payload: {
-      data: [
-        { delay: "0-3 days", total_cases: 123, percentage: 0.395 },
-        { delay: "4-5 days", total_cases: 56, percentage: 0.1 },
-        { delay: "6-14 days", total_cases: 120, percentage: 0.395 },
-        { delay: "15-30 days", total_cases: 48, percentage: 0.1 },
-        { delay: "1-3 months", total_cases: 4, percentage: 0.01 },
-        { delay: "3 months +", total_cases: 0, percentage: 0.0 },
-      ]
     }
   });
 };
 
 export const fetchServiceAccessDelay = () => async dispatch => {
   dispatch({
-    type: Actions.SERVICE_ACCESS_DELAY_SUCCESS,
-    //    api: {
-    //      path: "key_performance_indicators/service_access_delay"
-    //    },
-    payload: {
-      data: [
-        { delay: "0-3 days", total_cases: 123, percentage: 0.395 },
-        { delay: "4-5 days", total_cases: 56, percentage: 0.1 },
-        { delay: "6-14 days", total_cases: 120, percentage: 0.395 },
-        { delay: "15-30 days", total_cases: 48, percentage: 0.1 },
-        { delay: "1-3 months", total_cases: 4, percentage: 0.01 },
-        { delay: "3 months +", total_cases: 0, percentage: 0.0 },
-      ]
+    type: Actions.SERVICE_ACCESS_DELAY,
+    api: {
+      path: "key_performance_indicators/service_access_delay"
     }
   });
 };
