1f578f377d4d0cf7508cc35aa92ec6bda89bc731
Jasper Lyons <jasper.lyons@gmail.com>
2020-01-22 13:45:55 +0000

    Update StackedPercentageBar
    It now handles all percentages being 0 or smaller than the label
    by having a minimum width.

--- app/javascript/components/key-performance-indicators/stacked-percentage-bar/container.jsx
+++ app/javascript/components/key-performance-indicators/stacked-percentage-bar/container.jsx
@@ -8,29 +8,35 @@ export default function StackedPercentageBar({ percentages, className }) {
   if (percentages.length > 2)
     throw "StackedPercentageBar components only support a max of 2 percentages";
 
+  // only render percentages above 0.
+  // TODO: Figure out what to do if first percentage is 0 as it will
+  // force all labels after it to overlap. We could use a min-width?
+  let percentagedToRender = percentages
+    .filter(descriptor => descriptor.percentage > 0)
+
   return (
     <div className={css.StackedPercentageBarContainer}>
       <div className={[css.StackedPercentageBar, className].join(" ")}>
+        {
+            percentagedToRender.map((percentageDescriptor, i) => {
+              let percentage = percentageDescriptor.percentage * 100;
+
+              return (
+                <div
+                  className={css["StackedPercentageBar" + (i + 1) + "Complete"]}
+                  style={{ width: percentage + "%" }}
+                ></div>
+              );
+            })
+        }
+      </div>
+      <div className={css.StackedPercentageBarLabels}>
         {
           percentages.map((percentageDescriptor, i) => {
             let percentage = percentageDescriptor.percentage * 100;
 
             return (
-              <div
-                className={css["StackedPercentageBar" + (i + 1) + "Complete"]}
-                style={{ width: percentage + "%" }}
-              ></div>
-            );
-          })
-        }
-      </div>
-      <div className={css.StackedPercentageBarLabels}>
-        {
-          percentages.map((percentageDescriptor) => {
-            let percentage = percentageDescriptor.percentage * 100;
-
-            return (
-              <div className={css.StackedPercentageBarLabelContainer} style={{ width: percentage + "%" }}>
+              <div className={css.StackedPercentageBarLabelContainer} style={{ width: percentage > 0 ? percentage + "%" : 'auto' }}>
                 <div>
                   <h1 className={css.StackedPercentageBarLabelPercentage}>{percentage + '%'}</h1>
                 </div>
