d01c74869059fdfb85b8fb802f07ddeb1124a9a2
Jasper Lyons <jasper.lyons@gmail.com>
2020-01-22 17:17:14 +0000

    Localize NumberofCases KPI

--- app/javascript/components/pages/key-performance-indicators/components/number-of-cases/container.jsx
+++ app/javascript/components/pages/key-performance-indicators/components/number-of-cases/container.jsx
@@ -2,28 +2,33 @@ import * as actions from "../../action-creators";
 import * as selectors from "../../selectors";
 import { DateRangeSelect } from "components/key-performance-indicators";
 import { OptionsBox, DashboardTable } from "components/dashboard";
-import { connect, batch } from "react-redux";
+import { connect } from "react-redux";
 import React, { useEffect } from "react";
+import { useI18n } from "components/i18n";
 
 function NumberOfCases({ fetchNumberOfCases, numberOfCases }) {
+  let i18n = useI18n();
+
   useEffect(() => {
-    batch(() => {
-      fetchNumberOfCases();
-    });
+    fetchNumberOfCases();
   }, []);
+
+  let localizeDate = (date) => i18n.toTime('key_performance_indicators.date_format', date);
 
   let columns = [{
     name: "reporting_site",
-    label: "Reporting Site"
+    label: i18n.t('key_performance_indicators.number_of_cases.reporting_site'),
+    customBodyRender: localizeDate
   }].concat(numberOfCases.get("dates").map(date => {
     return {
       name: date,
-      label: date
+      customHeadRender: localizeDate
     };
   }).toJS());
 
   // data needs to be in an array as object data as rows to mui-datatables
   // is depreciated.
+  // TODO: Some of this data my need translating, if it's a date?
   let rows = numberOfCases.get("data")
     .map(row => columns.map(column => row.get(column.name)))
 
@@ -31,14 +36,14 @@ function NumberOfCases({ fetchNumberOfCases, numberOfCases }) {
   threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3)
   let dateRanges = [{
     value: '3-months',
-    name: 'Last 3 Months',
+    name: i18n.t('key_performance_indicators.time_periods.last_3_months'),
     from: threeMonthsAgo,
     to: new Date()
   }]
 
   return (
     <OptionsBox
-      title="Number of Cases"
+      title={i18n.t('key_performance_indicators.number_of_cases.title')}
       action={
         <DateRangeSelect
           ranges={dateRanges}
