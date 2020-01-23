69a4a9f788979749f0099411671273ad61a80be5
Jasper Lyons <jasper.lyons@gmail.com>
2020-01-23 10:56:41 +0000

    Remove untranslated text

--- app/javascript/components/pages/key-performance-indicators/container.jsx
+++ app/javascript/components/pages/key-performance-indicators/container.jsx
@@ -47,7 +47,7 @@ function KeyPerformanceIndicators({}) {
         <PageContent>
           <Grid>
             <Box>
-              <h2 className={css.subtitle}>{i18n.t('key_performance_indicators.introduction_and_engagement')}INTRODUCTION &amp; ENGAGEMENT</h2>
+              <h2 className={css.subtitle}>{i18n.t('key_performance_indicators.introduction_and_engagement')}</h2>
 
               <Grid container spacing={2}>
                 <Grid item className={css.grow} xs={12} md={6}>
