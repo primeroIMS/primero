a00a04477136d35001a3a794dd59fdde86bc7990
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2020-01-25 23:19:40 -0500

    Failing to log in disabled users with valid JWT tokens

--- spec/auth/idp_token_strategy_spec.rb
+++ spec/auth/idp_token_strategy_spec.rb
@@ -5,7 +5,7 @@ describe IdpTokenStrategy do
   describe '.authenticate!' do
 
     it 'passes the strategy on valid JWT token' do
-      user = double('User')
+      user = instance_double('User', disabled: false)
       token = instance_double('IdpToken', valid?: true, user: user)
       allow(IdpToken).to receive(:build).and_return(token)
 
@@ -25,6 +25,16 @@ describe IdpTokenStrategy do
 
     it 'fails the strategy for a valid JWT token with no corresponding Primero user' do
       token = instance_double('IdpToken', valid?: true, user: nil)
+      allow(IdpToken).to receive(:build).and_return(token)
+
+      strategy = IdpTokenStrategy.new({})
+      strategy.authenticate!
+      expect(strategy.successful?).to be_falsey
+    end
+
+    it 'fails the strategy for a valid JWT token with a corresponding disabled Primero user' do
+      user = instance_double('User', disabled: true)
+      token = instance_double('IdpToken', valid?: true, user: user)
       allow(IdpToken).to receive(:build).and_return(token)
 
       strategy = IdpTokenStrategy.new({})
