733cf50a0dc69b7f067779e6f1d80f4738de5b98
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2020-01-27 23:30:27 -0500

    Adding API queue for external async

--- app/jobs/identity_sync_job.rb
+++ app/jobs/identity_sync_job.rb
@@ -3,7 +3,7 @@
 # Asynchronously handle updates to the User records with external
 # identity management services such as identity providers and MDM
 class IdentitySyncJob < ApplicationJob
-  queue_as :identity_sync
+  queue_as :api
 
   def perform(user_id, admin_user_id)
     user = User.find_by(id: user_id)
