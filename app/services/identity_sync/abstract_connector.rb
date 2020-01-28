d47e89e6567b345382a5463358729e5b0fbc45a9
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2020-01-28 09:51:27 -0500

    Cleaning up style for identity sync service

--- app/services/identity_sync/abstract_connector.rb
+++ app/services/identity_sync/abstract_connector.rb
@@ -1,64 +1,62 @@
 # frozen_string_literal: true
 
-module IdentitySync
-  # Abstract superclass for user identity connectors
-  class AbstractConnector
-    attr_accessor :connection
+# Abstract superclass for user identity connectors
+module IdentitySync::AbstractConnector
+  attr_accessor :connection
 
-    def self.build_from_env
-      prefix = "PRIMERO_IDENTITY_SYNC_#{id.upcase}_"
-      config = ENV.select { |key, _| key.start_with?(prefix) }
-                  .transform_keys { |key| key.delete_prefix(prefix).downcase }
-      new(config)
+  def self.build_from_env
+    prefix = "PRIMERO_IDENTITY_SYNC_#{id.upcase}_"
+    config = ENV.select { |key, _| key.start_with?(prefix) }
+                .transform_keys { |key| key.delete_prefix(prefix).downcase }
+    new(config)
+  end
+
+  def self.id
+    raise NotImplementedError
+  end
+
+  def id
+    self.class.id
+  end
+
+  def initialize(options = {}); end
+
+  def sync(user)
+    return {} unless syncable?(user)
+
+    if new?(user)
+      create(user)
+    elsif relevant_updates?(user)
+      update(user)
+    else
+      {}
     end
+  end
 
-    def self.id
-      raise NotImplementedError
-    end
+  def fetch(_user)
+    raise NotImplementedError
+  end
 
-    def id
-      self.class.id
-    end
+  def create(_user)
+    raise NotImplementedError
+  end
 
-    def initialize(options = {}); end
+  def update(_user)
+    raise NotImplementedError
+  end
 
-    def sync(user)
-      return {} unless syncable?(user)
+  def syncable?(user)
+    # Only if the user's IDP is configured to sync with this connector
+    identity_sync_connector = user&.identity_provider&.configuration&.dig('identity_sync_connector')
+    identity_sync_connector == self.class.name.demodulize
+  end
 
-      if new?(user)
-        create(user)
-      elsif relevant_updates?(user)
-        update(user)
-      else
-        {}
-      end
-    end
+  def new?(user)
+    sync_metadata = user&.identity_provider_sync&.dig(id)
+    !sync_metadata&.dig('synced_on')
+  end
 
-    def fetch(_user)
-      raise NotImplementedError
-    end
-
-    def create(_user)
-      raise NotImplementedError
-    end
-
-    def update(_user)
-      raise NotImplementedError
-    end
-
-    def syncable?(user)
-      # Only if the user's IDP is configured to sync with this connector
-      identity_sync_connector = user&.identity_provider&.configuration&.dig('identity_sync_connector')
-      identity_sync_connector == self.class.name.demodulize
-    end
-
-    def new?(user)
-      sync_metadata = user&.identity_provider_sync&.dig(id)
-      !sync_metadata&.dig('synced_on')
-    end
-
-    def relevant_updates?(_user)
-      true
-    end
+  def relevant_updates?(_user)
+    true
   end
 end
--- app/services/identity_sync/connection.rb
+++ app/services/identity_sync/connection.rb
@@ -1,62 +1,60 @@
 # frozen_string_literal: true
 
-module IdentitySync
-  # Wraps an HTTP connection with an external service.
-  class Connection
-    attr_accessor :options, :driver
+# Wraps an HTTP connection with an external service.
+module IdentitySync::Connection
+  attr_accessor :options, :driver
 
-    def initialize(options = {})
-      self.options = options
+  def initialize(options = {})
+    self.options = options
 
-      self.driver = Faraday.new(url: url(options), headers: options['default_headers'], ssl: ssl(options)) do |faraday|
-        faraday.adapter(:net_http_persistent)
-      end
+    self.driver = Faraday.new(url: url(options), headers: options['default_headers'], ssl: ssl(options)) do |faraday|
+      faraday.adapter(:net_http_persistent)
     end
+  end
 
-    def get(path, params = nil, headers = nil, &block)
-      wrap { driver.get(path, to_query(params), headers, &block) }
-    end
+  def get(path, params = nil, headers = nil, &block)
+    wrap { driver.get(path, to_query(params), headers, &block) }
+  end
 
-    def patch(path, params = nil, headers = nil, &block)
-      wrap { driver.patch(path, to_json(params), headers, &block) }
-    end
+  def patch(path, params = nil, headers = nil, &block)
+    wrap { driver.patch(path, to_json(params), headers, &block) }
+  end
 
-    def post(path, params = nil, headers = nil, &block)
-      wrap { driver.post(path, to_json(params), headers, &block) }
-    end
+  def post(path, params = nil, headers = nil, &block)
+    wrap { driver.post(path, to_json(params), headers, &block) }
+  end
 
-    def url(options = {})
-      "#{options['tls'].present? ? 'https' : 'http'}://#{options['host']}:#{options['port']}"
-    end
+  def url(options = {})
+    "#{options['tls'].present? ? 'https' : 'http'}://#{options['host']}:#{options['port']}"
+  end
 
-    private
+  private
 
-    def ssl(options = {})
-      tls_client_key = options['tls_client_key']
-      tls_client_cert = options['tls_client_cert']
-      if options['tls'] == 'client' && File.exist?(tls_client_key) && File.exist?(tls_client_cert)
-        {
-          cert: OpenSSL::X509::Certificate.new(File.read(tls_client_cert)),
-          key: OpenSSL::PKey::RSA.new(File.read(tls_client_key))
-        }
-      end || {}
-    end
+  def ssl(options = {})
+    tls_client_key = options['tls_client_key']
+    tls_client_cert = options['tls_client_cert']
+    if options['tls'] == 'client' && File.exist?(tls_client_key) && File.exist?(tls_client_cert)
+      {
+        cert: OpenSSL::X509::Certificate.new(File.read(tls_client_cert)),
+        key: OpenSSL::PKey::RSA.new(File.read(tls_client_key))
+      }
+    end || {}
+  end
 
-    def wrap
-      response = yield
-      [response.status, JSON.parse(response.body)]
-    end
+  def wrap
+    response = yield
+    [response.status, JSON.parse(response.body)]
+  end
 
-    def to_query(params)
-      return params unless params.is_a?(Hash)
+  def to_query(params)
+    return params unless params.is_a?(Hash)
 
-      params.map { |k, v| "#{k}=#{v}" }.join('&')
-    end
+    params.map { |k, v| "#{k}=#{v}" }.join('&')
+  end
 
-    def to_json(params)
-      return params unless params.is_a?(Hash)
+  def to_json(params)
+    return params unless params.is_a?(Hash)
 
-      params.to_json
-    end
+    params.to_json
   end
 end
