1eb33f0fc46a5a0411b84e0ffc7f78ad64039693
Dennis Hernandez <dhernand@quoininc.com>
2020-02-07 13:52:27 -0600

    Refactor permitted_fields for user
    * Move the logic for permitted fields to the PermittedFieldService.
    * Permit the associated_user_names field, so that users can use it to filter the results.

--- app/models/user.rb
+++ app/models/user.rb
@@ -431,70 +431,8 @@ class User < ApplicationRecord
     permitted_fields(record_type, visible_forms_only).distinct.pluck(:name)
   end
 
-  def permitted_field_names(model_class, action_name = nil)
-    return @permitted_field_names if @permitted_field_names.present?
-
-    return permitted_fields_from_action_name(model_class, action_name) if action_name.present?
-
-    @permitted_field_names = []
-    @permitted_field_names += %w[id record_in_scope]
-    @permitted_field_names += permitted_field_names_from_forms(model_class.parent_form)
-    @permitted_field_names << 'or'
-    @permitted_field_names << 'cases_by_date'
-    @permitted_field_names << 'alert_count'
-    if model_class == Child
-      @permitted_field_names += %w[workflow status case_status_reopened]
-    end
-    @permitted_field_names << 'record_state' if can?(:enable_disable_record, model_class)
-    @permitted_field_names << 'hidden_name' if can?(:update, model_class)
-    @permitted_field_names << 'flag_count' if can?(:flag, model_class)
-    @permitted_field_names << 'flagged' if can?(:flag, model_class)
-    @permitted_field_names += permitted_approval_field_names(model_class)
-    @permitted_field_names += permitted_overdue_task_fields
-    @permitted_field_names
-  end
-
-  def permitted_approval_field_names(model_class)
-    approval_field_names = []
-    [Approval::BIA, Approval::CASE_PLAN, Approval::CLOSURE].each do |approval_id|
-      if can?(:"request_approval_#{approval_id}", model_class) ||
-         can?(:"approve_#{approval_id}", model_class)
-        approval_field_names << 'approval_subforms'
-        approval_field_names << "#{approval_id}_approved"
-        approval_field_names << "approval_status_#{approval_id}"
-        approval_field_names << "#{approval_id}_approved_date"
-        approval_field_names << "#{approval_id}_approved_comments"
-        approval_field_names << "#{approval_id}_approval_type" if approval_id == Approval::CASE_PLAN
-      else
-        next
-      end
-    end
-    approval_field_names
-  end
-
-  def permitted_fields_from_action_name(model_class, action_name)
-    case action_name
-    when Permission::ADD_NOTE then %w[notes_section] if can?(:add_note, model_class)
-    when Permission::INCIDENT_DETAILS_FROM_CASE then %w[incident_details] if can?(:incident_details_from_case, model_class)
-    when Permission::SERVICES_SECTION_FROM_CASE then %w[services_section] if can?(:services_section_from_case, model_class)
-    when Permission::CLOSE then %w[status]  if can?(:close, model_class)
-    when Permission::REOPEN then %w[status] if can?(:reopen, model_class)
-    when Permission::ENABLE_DISABLE_RECORD then %w[record_state] if can?(:enable_disable_record, model_class)
-    else raise Errors::InvalidPrimeroEntityType, 'case.invalid_action_name'
-    end
-  end
-
   def permitted_roles_to_manage
     role.permitted_role_unique_ids.present? ? role.permitted_roles : Role.all
-  end
-
-  def permitted_overdue_task_fields
-    overdue_task_fields = []
-    overdue_task_fields << 'assessment_due_dates' if can?(:cases_by_task_overdue_assessment, Dashboard)
-    overdue_task_fields << 'case_plan_due_dates' if can?(:cases_by_task_overdue_case_plan, Dashboard)
-    overdue_task_fields << 'service_due_dates' if can?(:cases_by_task_overdue_services, Dashboard)
-    overdue_task_fields << 'followup_due_dates' if can?(:cases_by_task_overdue_followups, Dashboard)
-    overdue_task_fields
   end
 
   def ability
