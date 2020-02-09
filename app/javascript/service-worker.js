5d2f32c941f91764288acbc1d0ae1b48e7bbe2ac
Joshua Toliver <jtoliver@quoininc.com>
2020-02-09 04:01:15 -0500

    Scoping service worker to v2

--- app/javascript/service-worker.js
+++ app/javascript/service-worker.js
@@ -1,3 +1,8 @@
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/worker.js");
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register('/worker.js', {scope: "/v2/"})
+  .then(function(registration) {
+    console.log('Registration successful, scope is:', registration.scope);
+  }).catch(function(err) {
+    console.log('Service worker registration failed, error:', error);
+  });
 }
