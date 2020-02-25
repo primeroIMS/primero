eab1504bdbbe4e7f1f8ad4ac15c08d2f481ce555
Jasper Lyons <jasper.lyons@gmail.com>
2020-02-25 13:59:40 +0000

    Add date ranges to NumberOfIncidents

--- app/javascript/components/pages/key-performance-indicators/components/number-of-incidents/container.jsx
+++ app/javascript/components/pages/key-performance-indicators/components/number-of-incidents/container.jsx
@@ -1,15 +1,30 @@
 import * as actions from "../../action-creators";
 import * as selectors from "../../selectors";
 import { withRouter } from "react-router-dom";
-import { DateRangeSelect } from "components/key-performance-indicators";
+import { DateRangeSelect, DateRange } from "components/key-performance-indicators/date-range-select";
 import { OptionsBox, DashboardTable } from "components/dashboard";
-import { connect  } from "react-redux";
-import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { useI18n } from "components/i18n";
+import { subMonths, addMonths, startOfMonth } from "date-fns";
+import React, { useEffect, useState } from "react";
 
 function NumberOfIncidents({ fetchNumberOfIncidents, numberOfIncidents }) {
+  let i18n = useI18n();
+
+  let today = new Date();
+  let dateRanges = [
+    new DateRange(
+      '3-months',
+      i18n.t('key_performance_indicators.time_periods.last_3_months'),
+      startOfMonth(subMonths(today, 2)),
+      startOfMonth(addMonths(today, 1)))
+  ]
+
+  let [currentDateRange, setCurrentDateRange] = useState(dateRanges[0]);
+
   useEffect(() => {
-    fetchNumberOfIncidents();
-  }, []);
+    fetchNumberOfIncidents(currentDateRange);
+  }, [currentDateRange]);
 
   let columns = [{
     name: "reporting_site",
@@ -26,23 +41,15 @@ function NumberOfIncidents({ fetchNumberOfIncidents, numberOfIncidents }) {
   let rows = numberOfIncidents.get("data")
     .map(row => columns.map(column => row.get(column.name)))
 
-  let threeMonthsAgo = new Date();
-  threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3)
-  let dateRanges = [{
-    value: '3-months',
-    name: 'Last 3 Months',
-    from: threeMonthsAgo,
-    to: new Date()
-  }]
-
   return (
     <OptionsBox
       title="Number of Incidents"
       action={
         <DateRangeSelect
           ranges={dateRanges}
-          selectedRange={dateRanges[0]}
-          withCustomRange
+          selectedRange={currentDateRange}
+          setSelectedRange={setCurrentDateRange}
+          disabled
         />
       }
     >
