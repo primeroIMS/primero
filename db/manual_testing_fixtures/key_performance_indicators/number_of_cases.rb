15bc8277d95c9bd1d9daea9344398bf6381533d1
Jasper Lyons <jasper.lyons@gmail.com>
2020-02-25 14:47:31 +0000

    Add testing fixtures for NumberOfIncidents

--- db/manual_testing_fixtures/key_performance_indicators/number_of_cases.rb
+++ db/manual_testing_fixtures/key_performance_indicators/number_of_cases.rb
@@ -6,55 +6,10 @@
 # * Users with locations
 # * Cases created by users with location
 
-
-def find_or_create_location(location_code, attributes)
-  location = Location.find_or_initialize_by(location_code: location_code)
-  location.update_attributes(attributes)
-  location.save!
-  location
-end
-
-# Check if any county level (admin level 2) locations exist
-county_level_location =
-  if !Location.exists?(admin_level: 2)
-    puts 'Creating locations'
-    find_or_create_location('GBR', name: 'United Kingdom', placename: 'United Kingdom', type: 'Country', hierarchy_path: 'GBR', admin_level: 0)
-    find_or_create_location('01', name: 'England', placename: 'England', type: 'Region', hierarchy_path: 'GBR.01', admin_level: 1)
-    find_or_create_location('41', name: 'London', placename: 'London', type: 'County', hierarchy_path: 'GBR.01.41', admin_level: 2)
-  else
-    Location.where(admin_level: 2).first
-  end
-
-puts "Using location #{county_level_location}"
-
-# Find Agency
-unicef = Agency.find_by(agency_code: 'UNICEF')
-puts "Using agency #{unicef}"
-
-# Create testing user with location
-primero_kpi = User.find_or_initialize_by('user_name' => 'primero_kpi')
-primero_kpi.update_attributes(
-    'password' => 'primer0!',
-    'password_confirmation' => 'primer0!',
-    'full_name' => 'Key Performance Indicator Manual Testing Account',
-    'email' => 'primero_kpi@primero.com',
-    'disabled' => 'false',
-    'agency_id' => unicef.id,
-    'role_id' => Role.find_by_name('GBV Manager').id,
-    'user_groups' => [UserGroup.find_by(name: 'Primero GBV')],
-    'locale' => Primero::Application::LOCALE_ENGLISH,
-    'location' => county_level_location.location_code
-)
-primero_kpi.save!
-puts "Using user #{primero_kpi} in location #{county_level_location}"
-puts <<-LOGIN
-  Log in with:
-  username: #{primero_kpi.user_name}
-  password: #{primero_kpi.password}
-LOGIN
+require_relative './setup.rb')
 
 # Create test cases
-Child.new_with_user(primero_kpi, {
+Child.new_with_user(TEST_USER, {
     "age"=>100,
     "sex"=>"female",
     "name"=>"Papa John",
