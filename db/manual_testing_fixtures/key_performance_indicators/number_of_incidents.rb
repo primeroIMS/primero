859144954372b5cb927136c2e4adf9e67ad0c0f8
Jasper Lyons <jasper.lyons@gmail.com>
2020-02-25 14:47:31 +0000

    Add testing fixtures for NumberOfIncidents

---
+++ db/manual_testing_fixtures/key_performance_indicators/number_of_incidents.rb
@@ -0,0 +1,16 @@
+# We assume that bundle exec rails db:seed has already been run and that
+# this file is run with bundle exec rails r <filename>
+
+# The number of incidents KPI requires:
+# * Locations
+# * Users with locations
+# * Incidents created by users with location
+
+require_relative './setup.rb'
+
+Incident.new_with_user(TEST_USER, {
+  # TODO: Find out what attributes go in an Incident and use
+  #       some defaults here
+})
+
+puts 'Created test incident for NumberOfCases KPI'
