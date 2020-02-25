938f009f2c1ce8cd5633dcf7d9e99e770a9fe6a1
Jasper Lyons <jasper.lyons@gmail.com>
2020-02-25 15:12:01 +0000

    Add further date ranges to ReportingDelay

--- app/javascript/components/pages/key-performance-indicators/components/reporting-delay/container.jsx
+++ app/javascript/components/pages/key-performance-indicators/components/reporting-delay/container.jsx
@@ -1,17 +1,40 @@
 import * as actions from "../../action-creators";
 import * as selectors from "../../selectors";
 import { withRouter } from "react-router-dom";
-import { TablePercentageBar, DateRangeSelect } from "components/key-performance-indicators";
+import { TablePercentageBar, DateRangeSelect, DateRange} from "components/key-performance-indicators";
 import { OptionsBox, DashboardTable } from "components/dashboard";
-import { connect, batch } from "react-redux";
-import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useI18n } from "components/i18n";
+import { subMonths, addMonths, startOfMonth } from "date-fns";
 
 function ReportingDelay({ fetchReportingDelay, reportingDelay }) {
+  let i18n = useI18n();
+
+  let today = new Date();
+  let dateRanges = [
+    new DateRange(
+      '3-months',
+      i18n.t('key_performance_indicators.time_periods.last_3_months'),
+      startOfMonth(subMonths(today, 2)),
+      startOfMonth(addMonths(today, 1))),
+    new DateRange(
+      '6-months',
+      i18n.t('key_performance_indicators.time_periods.last_6_months'),
+      startOfMonth(subMonths(today, 5)),
+      startOfMonth(addMonths(today, 1))),
+    new DateRange(
+      '1-year',
+      i18n.t('key_performance_indicators.time_periods.last_1_year'),
+      startOfMonth(subMonths(today, 12)),
+      startOfMonth(addMonths(today, 1)))
+  ]
+
+  let [currentDateRange, setCurrentDateRange] = useState(dateRanges[0]);
+
   useEffect(() => {
-    batch(() => {
-      fetchReportingDelay();
-    });
-  }, []);
+    fetchReportingDelay(currentDateRange);
+  }, [currentDateRange]);
 
   let columns = [
     { name: "delay", label: "Delay" },
@@ -30,22 +53,14 @@ function ReportingDelay({ fetchReportingDelay, reportingDelay }) {
   let rows = reportingDelay.get("data")
     .map(row => columns.map(column => row.get(column.name)))
 
-  let threeMonthsAgo = new Date();
-  threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3)
-  let dateRanges = [{
-    value: '3-months',
-    name: 'Last 3 Months',
-    from: threeMonthsAgo,
-    to: new Date()
-  }]
-
   return (
     <OptionsBox
       title="Reporting Delay"
       action={
         <DateRangeSelect
           ranges={dateRanges}
-          selectedRange={dateRanges[0]}
+          selectedRange={currentDateRange}
+          setSelectedRange={setCurrentDateRange}
           withCustomRange
         />
       }
