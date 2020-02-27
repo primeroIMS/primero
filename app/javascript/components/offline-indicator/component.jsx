b03818755439c6d6cef459f380dab1ec7d23037e
Joshua Toliver <jtoliver@quoininc.com>
2020-02-27 17:50:58 -0500

    Restyling offline indicator text and ensuring dashboards doesn't break when translating options

--- app/javascript/components/offline-indicator/component.jsx
+++ app/javascript/components/offline-indicator/component.jsx
@@ -10,10 +10,17 @@ const Component = () => {
   const { online } = useApp();
   const css = makeStyles(styles)();
   const i18n = useI18n();
+  const divider = <div className={css.divider}>* * * * * * *</div>;
 
   if (online) return false;
 
-  return <div className={css.offline}>{i18n.t("offline")}</div>;
+  return (
+    <div className={css.offline}>
+      {divider}
+      <div>{i18n.t("offline")}</div>
+      {divider}
+    </div>
+  );
 };
 
 Component.displayName = "OfflineIndicator";
