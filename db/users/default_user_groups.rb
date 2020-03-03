e0c2aead8c78d26654d128cc4973f7b129c17873
Rael Monge <rmonge@quoininc.com>
2020-03-03 16:40:34 -0600

    PRIM-1827 R2 Create user group gives 409 error

--- db/users/default_user_groups.rb
+++ db/users/default_user_groups.rb
@@ -1,14 +1,11 @@
 def create_or_update_user_group(user_group_hash)
-  user_group_unique_id = "#{UserGroup.name}-#{user_group_hash[:name]}".parameterize.dasherize
-  user_group = UserGroup.find_by(id: user_group_hash[:id])
-
-  user_group_hash[:unique_id] = user_group_unique_id
+  user_group = UserGroup.find_by(unique_id: user_group_hash[:unique_id])
 
   if user_group.nil?
-    puts "Creating user group #{user_group_unique_id}"
+    puts "Creating user group #{user_group_hash[:unique_id]}"
     UserGroup.create! user_group_hash
   else
-    puts "Updating user group #{user_group_unique_id}"
+    puts "Updating user group #{user_group_hash[:unique_id]}"
     user_group.update_attributes user_group_hash
   end
 
@@ -16,19 +13,19 @@ end
 
 
 create_or_update_user_group(
-  id: 1,
+  unique_id: "usergroup-primero-cp",
   name: "Primero CP",
   description: "Default Primero User Group for CP"
 )
 
 create_or_update_user_group(
-  id: 2,
+  unique_id: "usergroup-primero-ftr",
   name: "Primero FTR",
   description: "Default Primero User Group for FTR"
 )
 
 create_or_update_user_group(
-  id: 3,
+  unique_id: "usergroup-primero-gbv",
   name: "Primero GBV",
   description: "Default Primero User Group for GBV"
 )
