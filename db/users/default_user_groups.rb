8d2f08c06c35472b0e844e7d7c6c63af6ba99781
Rael Monge <rmonge@quoininc.com>
2020-03-18 15:01:30 -0600

    PRIM-1827 R2 fixing up the code by rubocop

--- db/users/default_user_groups.rb
+++ db/users/default_user_groups.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 def create_or_update_user_group(user_group_hash)
   user_group = UserGroup.find_by(unique_id: user_group_hash[:unique_id])
 
@@ -8,25 +10,22 @@ def create_or_update_user_group(user_group_hash)
     puts "Updating user group #{user_group_hash[:unique_id]}"
     user_group.update_attributes user_group_hash
   end
-
 end
 
-
 create_or_update_user_group(
-  unique_id: "usergroup-primero-cp",
-  name: "Primero CP",
-  description: "Default Primero User Group for CP"
+  unique_id: 'usergroup-primero-cp',
+  name: 'Primero CP',
+  description: 'Default Primero User Group for CP'
 )
 
 create_or_update_user_group(
-  unique_id: "usergroup-primero-ftr",
-  name: "Primero FTR",
-  description: "Default Primero User Group for FTR"
+  unique_id: 'usergroup-primero-ftr',
+  name: 'Primero FTR',
+  description: 'Default Primero User Group for FTR'
 )
 
 create_or_update_user_group(
-  unique_id: "usergroup-primero-gbv",
-  name: "Primero GBV",
-  description: "Default Primero User Group for GBV"
+  unique_id: 'usergroup-primero-gbv',
+  name: 'Primero GBV',
+  description: 'Default Primero User Group for GBV'
 )
-
