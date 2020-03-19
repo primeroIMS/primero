0ce36aeaf5d6683033bfee9c5e7878e3ab914dd7
Jasper Lyons <jasper.lyons@gmail.com>
2020-03-19 14:38:54 +0000

    Finish implementing AssessmentStatus query

--- app/controllers/api/v2/key_performance_indicators_controller.rb
+++ app/controllers/api/v2/key_performance_indicators_controller.rb
@@ -108,7 +108,7 @@ module Api::V2
         with :status, Record::STATUS_OPEN
         with :created_at, from..to
 
-        facet :completed_survivor_assessment
+        facet :completed_survivor_assessment, only: true
       end
 
       number_of_cases = search.total
