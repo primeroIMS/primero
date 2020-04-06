a2ef0f8223f4a8af5fa9cf3c1168a0193be6d5d7
Jasper Lyons <jasper.lyons@gmail.com>
2020-04-06 16:17:09 +0100

    Implement TimeFromCaseOpenToClose with new KPI HOC

--- app/javascript/components/pages/key-performance-indicators/components/time-from-case-open-to-case-close/container.jsx
+++ app/javascript/components/pages/key-performance-indicators/components/time-from-case-open-to-case-close/container.jsx
@@ -1,42 +1,72 @@
-import React from "react";
-import { fromJS } from "immutable";
-import { DateRangeSelect } from "components/key-performance-indicators";
+import * as actions from "../../action-creators";
+import * as selectors from "../../selectors";
+import { DateRangeSelect } from "components/key-performance-indicators/date-range-select";
 import { OptionsBox, DashboardTable } from "components/dashboard";
+import { connect } from "react-redux";
+import { useI18n } from "components/i18n";
+import React, { useEffect, useState } from "react";
 
-export default function TimeFromCaseOpenToClose({ }) {
-  let columns = ['Time', 'Percent of Cases'];
+function asKeyPerformanceIndicator(identifier, defaultData) {
+  return (Visualizer) => {
+    // TODO: need a better name for this
+    let enhance = connect(
+      (state) => ({ data: selectors.forKPI(identifier, state, defaultData) }),
+      { fetchData: actions.dataFetcherFor(identifier) }
+    );
 
+    return enhance(({ data, fetchData, dateRanges, ...props }) => {
+      let i18n = useI18n();
+
+      let [currentDateRange, setCurrentDateRange] = useState(dateRanges[0]);
+
+      useEffect(() => {
+        fetchData(currentDateRange);
+      }, [currentDateRange]);
+
+      return (
+        <OptionsBox
+          title={i18n.t(`key_performance_indicators.${identifier}.title`)}
+          action={
+            <DateRangeSelect
+              ranges={dateRanges}
+              selectedRange={currentDateRange}
+              setSelectedRange={setCurrentDateRange}
+              withCustomRange
+            />
+          }
+        >
+          <Visualizer data={data} {...props} />
+        </OptionsBox>
+      );
+    });
+  };
+}
+
+function TimeFromCaseOpenToClose({ data, identifer }) {
+  let columns = [{
+    name: 'time',
+    label: i18n.t(`key_performance_indicators.${identifier}.time`)
+  }, {
+    name: 'percent',
+    label: i18n.t(`key_performance_indicators.${identifier}.percent`)
+  }];
+
+  let rows = data.get("data")
+    .map(row => columns.map(column => row.get(column.name)))
+
+  /*
   let rows = [
     ['<1 month', '65%'],
     ['1-3 months', '13%'],
     ['3-6 months', '9%'],
     ['>6 months', '7%']
   ];
-  
-  let currentMonth = new Date();
-  currentMonth.setMonth(currentMonth.getMonth() - 3)
-  let dateRanges = [{
-    value: '1-month',
-    name: 'Current Month',
-    from: currentMonth,
-    to: new Date()
-  }]
+  */
 
-  return (
-    <OptionsBox
-      title="Time From Case Open To Case Close"
-      action={
-        <DateRangeSelect
-          ranges={dateRanges}
-          selectedRange={dateRanges[0]}
-          withCustomRange
-        />
-      }
-    >
-      <DashboardTable
-        columns={columns}
-        data={fromJS(rows)}
-      />
-    </OptionsBox>
-  );
+  return (<DashboardTable columns={columns} rows={rows}/>);
 }
+
+export default asKeyPerformanceIndicator(
+  'time_from_case_open_to_close',
+  { data: [] }
+)(TimeFromCaseOpenToClose)
