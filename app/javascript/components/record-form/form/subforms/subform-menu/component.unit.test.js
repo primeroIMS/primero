c377215a4daf09333fb6fc375a023d7151b2a4bf
Dennis Hernandez <dhernand@quoininc.com>
2020-04-13 16:58:25 -0600

    PRIM-1953 - Referrable services were not showing refer option
    Fix agency selector passing wrong parameter as service

--- app/javascript/components/record-form/form/subforms/subform-menu/component.unit.test.js
+++ app/javascript/components/record-form/form/subforms/subform-menu/component.unit.test.js
@@ -1,17 +1,18 @@
-import { Menu } from "@material-ui/core";
+import { fromJS } from "immutable";
+import { IconButton, Menu, MenuItem } from "@material-ui/core";
 
+import { ACTIONS } from "../../../../../libs/permissions";
 import { setupMountedComponent } from "../../../../../test";
 
 import SubformMenu from "./component";
 
 describe("<SubformMenu />", () => {
   const props = {
-    setReferral: referral => referral,
     values: [
       {
         service_type: "service_1",
-        location: "location_1",
-        transitioned_to: "user_1",
+        service_implementing_agency: "agency_1",
+        service_implementing_agency_individual: "user_1",
         service_record_id: "service_id_1",
         service_status_referred: false
       }
@@ -19,13 +20,75 @@ describe("<SubformMenu />", () => {
     index: 0
   };
 
-  let component;
+  it("renders the subform menu", () => {
+    const { component } = setupMountedComponent(SubformMenu, props, {});
 
-  beforeEach(() => {
-    ({ component } = setupMountedComponent(SubformMenu, props, {}));
+    expect(component.find(Menu)).lengthOf(1);
   });
 
-  it("renders the subform menu", () => {
-    expect(component.find(Menu)).lengthOf(1);
+  it("render the ReferAction if service is referrable", () => {
+    const initialState = fromJS({
+      forms: {
+        options: {
+          lookups: {
+            data: [
+              {
+                id: 1,
+                unique_id: "lookup-service-type",
+                values: [
+                  {
+                    id: "service_1",
+                    display_text: {
+                      en: "Service No. 1"
+                    }
+                  }
+                ]
+              }
+            ]
+          },
+          agencies: [
+            {
+              id: 1,
+              unique_id: "agency_1",
+              agency_code: "a1",
+              name: { en: "Agency 1" },
+              services: ["service_1"]
+            }
+          ]
+        }
+      },
+      records: {
+        transitions: {
+          referral: {
+            users: [
+              {
+                id: 1,
+                user_name: "user_1",
+                services: ["service_1"],
+                agency: "agency_1"
+              }
+            ]
+          }
+        }
+      },
+      user: {
+        permissions: {
+          cases: [ACTIONS.REFERRAL_FROM_SERVICE]
+        }
+      }
+    });
+
+    const { component } = setupMountedComponent(
+      SubformMenu,
+      props,
+      initialState
+    );
+
+    component
+      .find(IconButton)
+      .find("button")
+      .simulate("click");
+
+    expect(component.find(MenuItem)).lengthOf(1);
   });
 });
