253b308d22cc0d3ad8ae2110e4f7f08f92226b7a
Gerald Padgett <gpadgett@quoininc.com>
2020-04-21 14:02:32 -0600

    R2-584 - Old refer-to user options stick around for a second

--- app/javascript/components/record-form/form/subforms/subform-menu/component.jsx
+++ app/javascript/components/record-form/form/subforms/subform-menu/component.jsx
@@ -1,26 +1,39 @@
 import React, { useState } from "react";
 import { batch, useDispatch, useSelector } from "react-redux";
 import PropTypes from "prop-types";
-import { Menu, MenuItem, IconButton } from "@material-ui/core";
+import {
+  Menu,
+  MenuItem,
+  IconButton,
+  CircularProgress
+} from "@material-ui/core";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { makeStyles } from "@material-ui/styles";
 
 import Permission from "../../../../application/permission";
 import { getEnabledAgencies } from "../../../../application/selectors";
 import { RESOURCES, REFER_FROM_SERVICE } from "../../../../../libs/permissions";
-import { getUsersByTransitionType } from "../../../../record-actions/transitions/selectors";
+import {
+  getLoadingTransitionType,
+  getUsersByTransitionType
+} from "../../../../record-actions/transitions/selectors";
 import { REFERRAL_TYPE } from "../../../../record-actions/transitions";
 import { setServiceToRefer } from "../../../action-creators";
 import { getOption } from "../../../selectors";
 import { setDialog } from "../../../../record-actions/action-creators";
 import { useI18n } from "../../../../i18n";
 import { serviceIsReferrable } from "../../utils";
+import { fetchReferralUsers } from "../../../../record-actions/transitions/action-creators";
+import styles from "../styles.css";
+import { RECORD_TYPES } from "../../../../../config";
 
 import { NAME } from "./constants";
 
-const Component = ({ index, values }) => {
+const Component = ({ index, recordType, values }) => {
   const i18n = useI18n();
   const [anchorEl, setAnchorEl] = useState(null);
   const dispatch = useDispatch();
+  const css = makeStyles(styles)();
 
   const services = useSelector(state =>
     getOption(state, "lookup-service-type", i18n.locale)
@@ -30,9 +43,14 @@ const Component = ({ index, values }) => {
     getUsersByTransitionType(state, REFERRAL_TYPE)
   );
 
+  const loading = useSelector(state =>
+    getLoadingTransitionType(state, REFERRAL_TYPE)
+  );
+
   const agencies = useSelector(state => getEnabledAgencies(state));
 
   const handleClick = event => {
+    dispatch(fetchReferralUsers({ record_type: RECORD_TYPES[recordType] }));
     setAnchorEl(event.currentTarget);
   };
 
@@ -90,7 +108,9 @@ const Component = ({ index, values }) => {
           referralUsers
         ) ? (
           <ReferAction />
-        ) : null}
+        ) : (
+          loading && <CircularProgress className={css.loadingIndicator} />
+        )}
       </Menu>
     </>
   );
@@ -100,6 +120,7 @@ Component.displayName = NAME;
 
 Component.propTypes = {
   index: PropTypes.number.isRequired,
+  recordType: PropTypes.string,
   values: PropTypes.array.isRequired
 };
 
