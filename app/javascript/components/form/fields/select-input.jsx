6db767cf5713710c122f4aff24351f894cf853f7
Dennis Hernandez <dhernand@quoininc.com>
2020-04-22 11:08:07 -0600

    Fix warnings in autocomplete

--- app/javascript/components/form/fields/select-input.jsx
+++ app/javascript/components/form/fields/select-input.jsx
@@ -28,7 +28,7 @@ const SelectInput = ({ commonInputProps, metaInputProps, options }) => {
   const optionsUseIntegerIds = Number.isInteger(options?.[0]?.id);
 
   // eslint-disable-next-line no-nested-ternary
-  const defaultValue = multiSelect ? [] : optionsUseIntegerIds ? null : "";
+  const defaultValue = multiSelect ? [] : optionsUseIntegerIds ? null : null;
 
   const handleChange = data => {
     return multiSelect
@@ -39,7 +39,7 @@ const SelectInput = ({ commonInputProps, metaInputProps, options }) => {
   };
 
   const optionEquality = (option, value) =>
-    multiSelect ? option.id === value : option.id === value.id;
+    option.id === value || option.id === value?.id;
 
   const filterOptions = {
     ...(freeSolo && {
