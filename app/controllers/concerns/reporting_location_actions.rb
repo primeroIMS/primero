f5f42a5e4cd326d8b271a5124c4a43c8dab60adc
Ron Elledge <relledge@quoininc.com>
2020-04-23 15:28:26 -0400

    fixing caching issues with secondary reporting location

--- app/controllers/concerns/reporting_location_actions.rb
+++ app/controllers/concerns/reporting_location_actions.rb
@@ -4,10 +4,10 @@ module ReportingLocationActions
   def load_default_settings
     reporting_location_config = current_user.reporting_location_config
     if reporting_location_config.present?
-      @admin_level ||= reporting_location_config.admin_level || ReportingLocation::DEFAULT_ADMIN_LEVEL
-      @reporting_location ||= reporting_location_config.field_key || ReportingLocation::DEFAULT_FIELD_KEY
-      @reporting_location_label ||= reporting_location_config.label_key || ReportingLocation::DEFAULT_LABEL_KEY
-      @reporting_location_hierarchy_filter ||= reporting_location_config.hierarchy_filter || nil
+      @admin_level = reporting_location_config.admin_level || ReportingLocation::DEFAULT_ADMIN_LEVEL
+      @reporting_location = reporting_location_config.field_key || ReportingLocation::DEFAULT_FIELD_KEY
+      @reporting_location_label = reporting_location_config.label_key || ReportingLocation::DEFAULT_LABEL_KEY
+      @reporting_location_hierarchy_filter = reporting_location_config.hierarchy_filter || nil
     else
       @admin_level ||= ReportingLocation::DEFAULT_ADMIN_LEVEL
       @reporting_location ||= ReportingLocation::DEFAULT_FIELD_KEY
