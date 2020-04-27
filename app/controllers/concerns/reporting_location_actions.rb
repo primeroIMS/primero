83a6fdf79b1abd1394ef8891ffcaeeaf2b98f331
Ron Elledge <relledge@quoininc.com>
2020-04-27 14:18:41 -0400

    fixed reporting location caching issue

--- app/controllers/concerns/reporting_location_actions.rb
+++ app/controllers/concerns/reporting_location_actions.rb
@@ -8,10 +8,6 @@ module ReportingLocationActions
       @reporting_location = reporting_location_config.field_key || ReportingLocation::DEFAULT_FIELD_KEY
       @reporting_location_label = reporting_location_config.label_key || ReportingLocation::DEFAULT_LABEL_KEY
       @reporting_location_hierarchy_filter = reporting_location_config.hierarchy_filter || nil
-      Rails.logger.info "**************************************************"
-      Rails.logger.info "**DEBUGGING** User Name #{current_user.user_name}"
-      Rails.logger.info "**DEBUGGING** Admin Level #{reporting_location_config.admin_level}"
-      Rails.logger.info "**************************************************"
     else
       @admin_level ||= ReportingLocation::DEFAULT_ADMIN_LEVEL
       @reporting_location ||= ReportingLocation::DEFAULT_FIELD_KEY
