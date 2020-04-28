7cd25a9802da9c317d2b179bb7a8b7b7cd557d2c
Gerald Padgett <gpadgett@quoininc.com>
2020-04-28 10:16:48 -0600

    R2-614 - Ghana: Error when choosing type of meeting in Case Conference

--- app/javascript/components/record-form/form/subforms/subform-header-lookup/component.jsx
+++ app/javascript/components/record-form/form/subforms/subform-header-lookup/component.jsx
@@ -7,7 +7,7 @@ import { useI18n } from "../../../../i18n";
 import { getOption } from "../../../selectors";
 import { SUBFORM_LOOKUP_HEADER_NAME } from "../constants";
 
-const Component = ({ value, optionsStringSource }) => {
+const Component = ({ value, optionsStringSource, optionsStringText }) => {
   const i18n = useI18n();
   const optionsStrings = useSelector(state =>
     getOption(state, optionsStringSource, i18n)
@@ -15,17 +15,26 @@ const Component = ({ value, optionsStringSource }) => {
 
   if (isEmpty(value)) return <>{value}</>;
 
-  const { display_text: displayText } = optionsStrings.find(
-    o => o.id === value
+  if (!isEmpty(optionsStringSource)) {
+    const { display_text: displayText } = optionsStrings.find(
+      o => o.id === value
+    );
+
+    return <span>{displayText[i18n.locale]}</span>;
+  }
+
+  const { display_text: displayText } = optionsStringText[i18n.locale].find(
+    optionStringText => optionStringText.id === value
   );
 
-  return <span>{displayText[i18n.locale]}</span>;
+  return <span>{displayText}</span>;
 };
 
 Component.displayName = SUBFORM_LOOKUP_HEADER_NAME;
 
 Component.propTypes = {
   optionsStringSource: PropTypes.string,
+  optionsStringText: PropTypes.object,
   value: PropTypes.string
 };
 
