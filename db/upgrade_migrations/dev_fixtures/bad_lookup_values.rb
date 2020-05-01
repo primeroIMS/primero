57e052bb58e26340a9b873dcb54cffe29f3c4b2f
Ron Elledge <relledge@quoininc.com>
2020-05-01 10:34:38 -0400

    PRIM-2057 adding script to identify lookup values that do not migrate

---
+++ db/upgrade_migrations/dev_fixtures/bad_lookup_values.rb
@@ -0,0 +1,128 @@
+#This script will search through records and report on fields that use a lookup but have a value not in that lookup's options
+#Goal: Keep this simple enough to run in the old pre-migrated system (ex. 1.3)
+#Much of this was based on migration_helper.rb in the migrations below
+#TODO determine if we can re-use migration helper.  There are some tweaks here...
+
+
+#Only get the fields that potentially use lookups
+def get_fields(form_section)
+  field_types = ['select_box', 'tick_box', 'radio_button', 'subform']
+  form_section.fields.select{|f| field_types.include?(f.type) || f.is_location? }
+end
+
+def get_option_list(field, locations)
+  #Get the English translated options since old versions weren't i18n yet
+  field.options_list(nil, nil, locations, true, {locale: Primero::Application::LOCALE_ENGLISH})
+end
+
+
+def get_lookup_fields(record_type, locations)
+  lookup_fields = {}
+  record_type = 'case' if record_type == 'child'
+  FormSection.find_by_parent_form(record_type).each do |fs|
+    fields = get_fields(fs)
+    fields.each do |field|
+      if field.subform_section.present?
+        lookup_fields[field.name] = {} if lookup_fields[field.name].blank?
+        sub_fields = get_fields(field.subform_section)
+        sub_fields.each do |sf|
+          sub_options = get_option_list(sf, locations)
+          if sub_options.present?
+            field_hash = {}
+            field_hash[:lookup] = sf.option_strings_source
+            field_hash[:options] = sub_options unless sf.option_strings_source == 'Location'
+            lookup_fields[field.name][sf.name] = field_hash
+          end
+        end
+      else
+        options = get_option_list(field, locations)
+        if options.present?
+          field_hash = {}
+          field_hash[:lookup] = field.option_strings_source
+          field_hash[:options] = options unless field.option_strings_source == 'Location'
+          lookup_fields[field.name] = field_hash
+        end
+      end
+    end
+  end
+  lookup_fields
+end
+
+def get_value(value, options)
+  if value.present? && options.present?
+    if value.is_a?(Array)
+      #look for value iin either the ids or in the display text
+      #If it has already been converted, it should be in one of the id's
+      #If it hasn't yet been converted, it should be in one of the display_text
+      values = value.map{|val| val.to_s.strip}
+      v = options.select{|o| values.include?(o['id']) || values.include?(o['display_text'].strip)}.map{|option| option['id']}.uniq
+    else
+      value = value.gsub(/_[0-9]{5}+$/, "") if value.is_a?(String)
+      #The to_s is necessary to catch cases where the value is true or false
+      v = options.select{|option| option['id'] == value.to_s.strip || option['display_text'].strip == value.to_s.strip || option['display_text'].strip == value.to_s.capitalize.strip}.first.try(:[], 'id')
+      if v == 'true'
+        v = true
+      elsif v == 'false'
+        v = false
+      end
+      v
+    end
+  end
+end
+
+
+#####################
+# Beginning of script
+#####################
+locations = Location.all_names
+
+record_classes = [Child, TracingRequest, Incident]
+record_classes.each do |record_type|
+  puts "------------------------------------------------------------------"
+  puts "Searching #{record_type} records..."
+
+  lookup_fields = get_lookup_fields(record_type.locale_prefix, locations)
+
+  record_type.each_slice do |records|
+    db_records = records.map {|r| record_type.database.get(r.id) }
+    records.each_with_index do |record, rec_index|
+      record.each do |k,v|
+        #use the value from the datbase if v is blank
+        #Sometimes yes/no values can get screwed up by the record model
+        v ||= db_records[rec_index][k]
+        if v.present? && field_options[k].present?
+          if field_options[k].is_a?(Hash)
+            v.each_with_index do |sf, index|
+              field_options[k].each do |sub_key, sub_options|
+                # Get the data from the raw db query...
+                # to handle case where field type changed which could cause new model to step all over old data
+                # EXAMPLE 'Yes' 'No' now handled as a boolean true/false... old 'Yes' 'No' vales seen as nil by the model
+                subform_field_value = db_records[rec_index][k][index][sub_key]
+                new_value = get_value(subform_field_value, sub_options)
+
+                if new_vlaue.blank? && new_value != false
+                  #TODO print log
+                  binding.pry
+                  x=0
+                  puts "#{subform_field_value}"
+                end
+                # record[k][index][sub_key] = new_value if new_value.present? || new_value == false
+              end
+            end
+          elsif field_options[k].is_a?(Array)
+            options = field_options[k]
+            value = get_value(v, options)
+
+            if value.blank? && value != false
+              #TODO print log
+              binding.pry
+              x=0
+            end
+            # record[k] = value if value.present? || value == false
+          end
+        end
+      end
+    end
+  end
+end
+
