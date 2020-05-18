82ebdd8561516e3736d398d7cc9dcb1397b45dfb
Dennis Hernandez <dhernand@quoininc.com>
2020-05-18 12:23:07 -0600

    R2-608 - Refactor to handle different field types

---
+++ app/javascript/components/pages/admin/form-builder/components/field-dialog/forms/text-field.js
@@ -0,0 +1,18 @@
+import { fromJS } from "immutable";
+
+import {
+  validationSchema,
+  generalForm,
+  optionsForm,
+  visibilityForm
+} from "./basic";
+
+/* eslint-disable import/prefer-default-export */
+export const textFieldForm = (fieldName, i18n) => ({
+  forms: fromJS([
+    generalForm(fieldName, i18n),
+    optionsForm(fieldName, i18n),
+    visibilityForm(fieldName, i18n)
+  ]),
+  validationSchema: validationSchema(fieldName, i18n)
+});
