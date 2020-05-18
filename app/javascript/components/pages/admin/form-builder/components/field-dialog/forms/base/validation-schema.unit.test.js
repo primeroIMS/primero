1e21c213f4c811c6493f107426e612bd7c2d25d6
Dennis Hernandez <dhernand@quoininc.com>
2020-05-18 17:06:20 -0600

    R2-608 - Add unit test for validation schema

---
+++ app/javascript/components/pages/admin/form-builder/components/field-dialog/forms/base/validation-schema.unit.test.js
@@ -0,0 +1,30 @@
+import { string } from "yup";
+
+import { validationSchema } from "./validation-schema";
+
+describe("pages/admin/<FormBuilder />/components/<FieldDialog />/forms/basic - validation schema", () => {
+  const i18n = { t: value => value };
+
+  it("should return the default validation schema", () => {
+    const { fields } = validationSchema("test_1", i18n).fields.test_1;
+
+    expect(Object.keys(fields)).to.deep.equal([
+      "display_name",
+      "help_text",
+      "required"
+    ]);
+  });
+
+  it("should return the overrided validation schema", () => {
+    const { fields } = validationSchema("test_1", i18n, {
+      name: string()
+    }).fields.test_1;
+
+    expect(Object.keys(fields)).to.deep.equal([
+      "display_name",
+      "help_text",
+      "required",
+      "name"
+    ]);
+  });
+});
