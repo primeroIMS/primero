19121ea5c279ab2f03a0ff24206f453213f3b85e
Rael Monge <rmonge@quoininc.com>
2020-05-20 10:29:41 -0600

    R2-558 Rename method to logs

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -13,7 +13,7 @@ class AuditLog < ApplicationRecord
     self.timestamp ||= DateTime.now
   end
 
-  def self.for_user(user_name, date_range)
+  def self.logs(user_name, date_range)
     return AuditLog.where(timestamp: date_range) unless user_name.present?
 
     joins(:user).where('users.user_name': user_name, timestamp: date_range)
