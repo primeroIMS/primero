3964a5f734a96f72b25c8b8fb7868cfb1031ce02
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2020-05-20 09:51:24 -0400

    Bulk assigns will now return errors for failed assignments

--- app/controllers/api/v2/assigns_controller.rb
+++ app/controllers/api/v2/assigns_controller.rb
@@ -1,6 +1,8 @@
-module Api::V2
-  class AssignsController < RecordResourceController
+# frozen_string_literal: true
 
+module Api::V2
+  # API endpoints that handle record reassignment
+  class AssignsController < RecordResourceController
     def index
       authorize! :read, @record
       @transitions = @record.assigns
@@ -8,20 +10,37 @@ module Api::V2
     end
 
     def create
-      authorize! :assign, @record
+      authorize_assign!(@record)
       @transition = assign(@record)
       updates_for_record(@record)
       render 'api/v2/transitions/create'
     end
 
     def create_bulk
-      authorize_all!(:assign, @records)
-      @transitions = @records.map { |record| assign(record) }
+      authorize_assign_all!(@records)
+      @transitions =
+        @records.map do |record|
+          assign(record)
+        rescue StandardError => e
+          handle_bulk_error(e, request) && nil
+        end.compact
       updates_for_records(@records)
       render 'api/v2/transitions/create_bulk'
     end
 
     private
+
+    def authorize_assign!(record)
+      can_assign =
+        current_user.can?(Permission::ASSIGN.to_sym, record) ||
+        current_user.can?(Permission::ASSIGN_WITHIN_AGENCY.to_sym, record) ||
+        current_user.can?(Permission::ASSIGN_WITHIN_USER_GROUP.to_sym, record)
+      raise Errors::ForbiddenOperation unless can_assign
+    end
+
+    def authorize_assign_all!(records)
+      records.each { |r| authorize_assign!(r) }
+    end
 
     def assign(record)
       transitioned_to = params[:data][:transitioned_to]
