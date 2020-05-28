7f57bbd39ae2ca11271586e9a21bb8effa20b626
Gerald Padgett <gpadgett@quoininc.com>
2020-05-28 10:13:20 -0600

    R2-651 - UX - Edit field (tick box)
    * Fixing comments

--- app/javascript/components/pages/admin/form-builder/components/field-dialog/forms/tick-box-field.js
+++ app/javascript/components/pages/admin/form-builder/components/field-dialog/forms/tick-box-field.js
@@ -1,6 +1,6 @@
 import { fromJS } from "immutable";
 
-import { TEXT_FIELD } from "../../../../../../form";
+import { FieldRecord, TEXT_FIELD } from "../../../../../../form";
 import { addWithIndex } from "../utils";
 
 import {
@@ -10,13 +10,14 @@ import {
   visibilityForm
 } from "./base";
 
-const labelField = (fieldName, i18n) => ({
-  display_name: i18n.t("fields.tick_box_label"),
-  name: `${fieldName}.tick_box_label.en`,
-  type: TEXT_FIELD,
-  editable: true,
-  help_text: i18n.t("fields.must_be_english")
-});
+const labelField = (fieldName, i18n) =>
+  FieldRecord({
+    display_name: i18n.t("fields.tick_box_label"),
+    name: `${fieldName}.tick_box_label.en`,
+    type: TEXT_FIELD,
+    editable: true,
+    help_text: i18n.t("fields.must_be_english")
+  });
 
 // eslint-disable-next-line import/prefer-default-export
 export const tickboxFieldForm = (fieldName, i18n) => {
