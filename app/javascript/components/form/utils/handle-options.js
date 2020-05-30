c51f6db0441c73732f0e2d7b01b6d1d84099f69e
Dennis Hernandez <dhernand@quoininc.com>
2020-05-30 16:05:23 -0600

    R2-650 - Avoid duplicated option ids

--- app/javascript/components/form/utils/handle-options.js
+++ app/javascript/components/form/utils/handle-options.js
@@ -1,7 +1,9 @@
 export const generateIdForNewOption = () => `new_option_${Date.now()}`;
 
 export const generateIdFromDisplayText = displayText =>
-  displayText.toLowerCase().replace(/\s/g, "_");
+  `${displayText.toLowerCase().replace(/\s/g, "_")}_${Math.floor(
+    Math.random() * 1000000
+  )}`;
 
 export const mergeOptions = (options1, options2) =>
   options1.map((option, index) => {
