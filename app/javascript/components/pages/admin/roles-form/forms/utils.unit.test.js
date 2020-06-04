57c8a035ec0045dc3a80fd252cef6d979d48ef59
Joshua Toliver <jtoliver@quoininc.com>
2020-06-04 14:40:09 -0400

    Adding tooltips to role permissions

--- app/javascript/components/pages/admin/roles-form/forms/utils.unit.test.js
+++ app/javascript/components/pages/admin/roles-form/forms/utils.unit.test.js
@@ -12,20 +12,24 @@ describe("pages/admin/<RolesForm>/forms - utils", () => {
       const expected = [
         {
           id: "action_1",
-          display_text: "permissions.permission.action_1"
+          display_text: "permissions.permission.action_1",
+          tooltip: "permissions.resource.case.actions.action_1.explanation"
         },
         {
           id: "action_2",
-          display_text: "permissions.permission.action_2"
+          display_text: "permissions.permission.action_2",
+          tooltip: "permissions.resource.case.actions.action_2.explanation"
         },
         {
           id: "request_approval_assessment",
-          display_text: "permissions.permission.request_approval_assessment"
+          display_text: "permissions.permission.request_approval_assessment",
+          tooltip:
+            "permissions.resource.case.actions.request_approval_assessment.explanation"
         }
       ];
 
       expect(
-        buildPermissionOptions(actions, i18n, approvalsLabels)
+        buildPermissionOptions(actions, i18n, approvalsLabels, "case")
       ).to.deep.equal(expected);
     });
   });
