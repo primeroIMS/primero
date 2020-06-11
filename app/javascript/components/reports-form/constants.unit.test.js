0a34c8e3fec0b03c06e88a6a2d5dcbbf20b1f0d2
Gerald Padgett <gpadgett@quoininc.com>
2020-06-11 17:57:59 -0600

    R2-675 - UX - Report editor filters
    * fixing tests

--- app/javascript/components/reports-form/constants.unit.test.js
+++ app/javascript/components/reports-form/constants.unit.test.js
@@ -10,7 +10,7 @@ describe("<ReportsForm /> - Constants", () => {
       "CONSTRAINTS",
       "DEFAULT_FILTERS",
       "DESCRIPTION_FIELD",
-      "DATE_CONSTRAINT",
+      "DATE_CONSTRAINTS",
       "DISAGGREGATE_BY_FIELD",
       "FILTERS_FIELD",
       "GROUP_AGES_FIELD",
