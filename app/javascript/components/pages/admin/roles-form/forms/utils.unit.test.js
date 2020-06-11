384aa240bf840972a58619c8c8e4e55cae5df310
Joshua Toliver <jtoliver@quoininc.com>
2020-06-11 17:03:10 -0400

    Fixing broken unit test

--- app/javascript/components/pages/admin/roles-form/forms/utils.unit.test.js
+++ app/javascript/components/pages/admin/roles-form/forms/utils.unit.test.js
@@ -12,22 +12,20 @@ describe("pages/admin/<RolesForm>/forms - utils", () => {
       const expected = [
         {
           id: "action_1",
-          display_text: "permissions.permission.action_1",
-          tooltip: "permissions.resource.case.actions.action_1.explanation",
-          i18nTitle: true
+          display_text: "permissions.resource.case.actions.action_1.label",
+          tooltip: "permissions.resource.case.actions.action_1.explanation"
         },
         {
           id: "action_2",
-          display_text: "permissions.permission.action_2",
-          tooltip: "permissions.resource.case.actions.action_2.explanation",
-          i18nTitle: true
+          display_text: "permissions.resource.case.actions.action_2.label",
+          tooltip: "permissions.resource.case.actions.action_2.explanation"
         },
         {
           id: "request_approval_assessment",
-          display_text: "permissions.permission.request_approval_assessment",
+          display_text:
+            "permissions.resource.case.actions.request_approval_assessment.label",
           tooltip:
-            "permissions.resource.case.actions.request_approval_assessment.explanation",
-          i18nTitle: true
+            "permissions.resource.case.actions.request_approval_assessment.explanation"
         }
       ];
 
