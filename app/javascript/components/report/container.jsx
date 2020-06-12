7277670cd2e8ee4a084b0406874865854464e9a6
Gerald Padgett <gpadgett@quoininc.com>
2020-06-12 11:53:26 -0600

    R2-526 - UX - Delete Report

--- app/javascript/components/report/container.jsx
+++ app/javascript/components/report/container.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import PropTypes from "prop-types";
-import { Paper } from "@material-ui/core";
+import { Paper, Typography } from "@material-ui/core";
 import { push } from "connected-react-router";
 import { useLocation, useParams } from "react-router-dom";
 
@@ -12,12 +12,16 @@ import { useI18n } from "../i18n";
 import { PageContainer, PageContent, PageHeading } from "../page";
 import { FormAction, whichFormMode } from "../form";
 import { usePermissions } from "../user";
-import { WRITE_RECORDS } from "../../libs/permissions";
+import { WRITE_RECORDS, MANAGE } from "../../libs/permissions";
+import ActionDialog from "../action-dialog";
+import { selectDialog, selectDialogPending } from "../record-actions/selectors";
+import { setPending, setDialog } from "../record-actions/action-creators";
 
 import { buildDataForGraph, buildDataForTable } from "./utils";
 import { getReport } from "./selectors";
-import { fetchReport } from "./action-creators";
+import { deleteReport, fetchReport } from "./action-creators";
 import namespace from "./namespace";
+import { NAME, DELETE_MODAL } from "./constants";
 
 const Report = ({ mode }) => {
   const { id } = useParams();
@@ -34,6 +38,16 @@ const Report = ({ mode }) => {
   const loading = useSelector(state => getLoading(state, namespace));
   const report = useSelector(state => getReport(state));
 
+  const deleteModal = useSelector(state => selectDialog(DELETE_MODAL, state));
+  const setDeleteModal = open => {
+    dispatch(setDialog({ dialog: DELETE_MODAL, open }));
+  };
+
+  const dialogPending = useSelector(state => selectDialogPending(state));
+  const setDialogPending = pending => {
+    dispatch(setPending({ pending }));
+  };
+
   const loadingIndicatorProps = {
     overlay: true,
     emptyMessage: i18n.t("report.no_data"),
@@ -46,12 +60,33 @@ const Report = ({ mode }) => {
   const canEditReport =
     usePermissions(namespace, WRITE_RECORDS) && report.get("editable");
 
+  const canDeleteReport =
+    usePermissions(namespace, MANAGE) && report.get("editable");
+
   const handleEdit = () => {
     dispatch(push(`${pathname}/edit`));
   };
 
+  const handleDelete = () => {
+    setDialogPending(true);
+    dispatch(
+      deleteReport({
+        id,
+        message: i18n.t("report.messages.delete_success")
+      })
+    );
+  };
+
   const editButton = formMode.get("isShow") && canEditReport && (
     <FormAction actionHandler={handleEdit} text={i18n.t("buttons.edit")} />
+  );
+
+  const cancelButton = formMode.get("isShow") && canDeleteReport && (
+    <FormAction
+      actionHandler={() => setDeleteModal(true)}
+      cancel
+      text={i18n.t("buttons.delete")}
+    />
   );
 
   return (
@@ -59,6 +94,7 @@ const Report = ({ mode }) => {
       <PageHeading
         title={report.get("name") ? report.get("name").get(i18n.locale) : ""}
       >
+        {cancelButton}
         {editButton}
       </PageHeading>
       <PageContent>
@@ -73,12 +109,26 @@ const Report = ({ mode }) => {
           )}
           <TableValues {...buildDataForTable(report, i18n)} />
         </LoadingIndicator>
+        <ActionDialog
+          open={deleteModal}
+          dialogTitle={i18n.t("reports.delete_report")}
+          successHandler={() => handleDelete()}
+          cancelHandler={() => setDeleteModal(false)}
+          omitCloseAfterSuccess
+          maxSize="xs"
+          pending={dialogPending}
+          confirmButtonLabel={i18n.t("buttons.ok")}
+        >
+          <Typography color="textSecondary">
+            {i18n.t("reports.delete_report_message")}
+          </Typography>
+        </ActionDialog>
       </PageContent>
     </PageContainer>
   );
 };
 
-Report.displayName = "Report";
+Report.displayName = NAME;
 
 Report.propTypes = {
   mode: PropTypes.string.isRequired
