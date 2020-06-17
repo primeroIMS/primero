515c8d3f7e4d824754bf924416027c175245bc1a
Nidhi Kumari <nidhi1691996@outlook.com>
2020-06-18 01:18:21 +0530

    test file updated

--- app/javascript/components/i18n/provider.unit.test.js
+++ app/javascript/components/i18n/provider.unit.test.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { Provider } from "react-redux";
 
-import { createSimpleMount } from "../../test";
-import store from "../../store";
+import { setupMountedComponent } from "../../test";
 
-import I18nProvider from "./provider";
+import { useI18n } from "./provider";
 
 describe("I18nProvider - changeLocale", () => {
   it("should have defaults for window and document", () => {
@@ -20,21 +18,32 @@ describe("I18nProvider - changeLocale", () => {
   it("should handle changeLocale method", () => {
     const newLocale = "fr";
 
-    const component = createSimpleMount(
-      <Provider store={store()}>
-        <I18nProvider>
-          <div>wrapped children</div>
-        </I18nProvider>
-      </Provider>
-    );
+    const TestComponent = () => {
+      const { changeLocale } = useI18n();
 
-    console.log(component.find(I18nProvider).first().instance()); // null
+      return (
+        // eslint-disable-next-line react/button-has-type
+        <button
+          test-id="i18n-test-button"
+          onClick={() => {
+            changeLocale(newLocale);
+          }}
+        >
+          I18n Test Component
+        </button>
+      );
+    };
 
-    component.find(I18nProvider).first().instance().changeLocale(newLocale);
+    const { component } = setupMountedComponent(TestComponent);
+
+    const button = component.find('[test-id="i18n-test-button"]');
+
+    button.simulate("click");
 
     const { locale } = window.I18n;
     const { lang } = document.documentElement;
 
+    expect(button.text()).to.equal("I18n Test Component");
     expect(locale).to.equal(newLocale);
     expect(lang).to.equal(newLocale);
   });
