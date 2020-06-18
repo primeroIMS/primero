5633ba6a8fe2ff617eb9a8c2299823d0a2801fe4
Alberto Espinoza <aespinoz@quoininc.com>
2020-06-18 10:40:53 -0600

    Fixing label test.

--- app/javascript/components/form/fields/label.unit.test.js
+++ app/javascript/components/form/fields/label.unit.test.js
@@ -20,24 +20,4 @@ describe("<Label />", () => {
     expect(component.find(Label)).to.have.lengthOf(1);
     expect(component.find(Label).text()).to.equal("Some text");
   });
-  describe("When recieve metaInputProps", () => {
-    beforeEach(() => {
-      ({ component } = setupMountedComponent(
-        Label,
-        {
-          commonInputProps: { label: "Some text" },
-          metaInputProps: {
-            phraseParts: ["This", "is", "a", "phrase"],
-            boldWords: []
-          }
-        },
-        initialState
-      ));
-    });
-
-    it("renders the Phrase", () => {
-      expect(component.find(Label)).to.have.lengthOf(1);
-      expect(component.find(Label).text()).to.equal("This is a phrase ");
-    });
-  });
 });
