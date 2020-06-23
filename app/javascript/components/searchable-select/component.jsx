8f93a2c5331f560cbb7b0c07cde206991b7c9651
Joshua Toliver <jtoliver@quoininc.com>
2020-06-23 16:12:48 -0400

    Swaping custom searchable select for mui autocomplete

--- app/javascript/components/searchable-select/component.jsx
+++ app/javascript/components/searchable-select/component.jsx
@@ -1,12 +1,84 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import TextField from "@material-ui/core/TextField";
 
 import { NAME } from "./constants";
-import CustomAutoComplete from "./components/custom-auto-complete";
 
-const SearchableSelect = props => {
-  return <CustomAutoComplete {...{ props }} />;
+const SearchableSelect = ({
+  onChange,
+  isLoading,
+  isDisabled,
+  isClearable,
+  helperText,
+  defaultValues,
+  TextFieldProps,
+  options
+}) => {
+  const optionLabel = option => {
+    if (typeof option === "string" && option === "") {
+      return "";
+    }
+
+    const { label } =
+      typeof option === "object"
+        ? option
+        : options?.find(opt => opt.value === option) || "";
+
+    return label || "";
+  };
+
+  const optionEquality = (option, selected) =>
+    option?.value === selected || option?.value === selected?.value;
+
+  const initialValues = Array.isArray(defaultValues)
+    ? defaultValues.map(selected => selected.value || "")[0]
+    : defaultValues || "";
+
+  return (
+    <Autocomplete
+      onChange={(event, value) => onChange(value)}
+      options={options}
+      disabled={isDisabled}
+      getOptionLabel={optionLabel}
+      getOptionSelected={optionEquality}
+      loading={isLoading}
+      disableClearable={!isClearable}
+      filterSelectedOptions
+      value={initialValues}
+      renderInput={params => (
+        <TextField
+          {...params}
+          {...TextFieldProps}
+          helperText={helperText}
+          disabled={isDisabled}
+        />
+      )}
+    />
+  );
 };
 
 SearchableSelect.displayName = NAME;
+
+SearchableSelect.defaultProps = {
+  defaultValues: [],
+  helperText: "",
+  isClearable: true,
+  isDisabled: false,
+  isLoading: false,
+  options: [],
+  TextFieldProps: {}
+};
+
+SearchableSelect.propTypes = {
+  defaultValues: PropTypes.oneOfType([PropTypes.array, PropTypes.string]),
+  helperText: PropTypes.string,
+  isClearable: PropTypes.bool,
+  isDisabled: PropTypes.bool,
+  isLoading: PropTypes.bool,
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.array,
+  TextFieldProps: PropTypes.object
+};
 
 export default SearchableSelect;
--- app/javascript/components/searchable-select/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as NoOptionsMessage } from "./no-options-message";
-export { default as Control } from "./control";
-export { default as Option } from "./option";
-export { default as Placeholder } from "./placeholder";
-export { default as SingleValue } from "./single-value";
-export { default as ValueContainer } from "./value-container";
-export { default as MultiValue } from "./multi-value";
-export { default as Menu } from "./menu";
