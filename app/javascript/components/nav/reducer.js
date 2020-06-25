483a2e09b26f715b6f0a880fdb6ed19398caf886
Joshua Toliver <jtoliver@quoininc.com>
2020-06-25 14:49:26 -0400

    Removing unused actions/action-creators and css fixes

--- app/javascript/components/nav/reducer.js
+++ app/javascript/components/nav/reducer.js
@@ -1,14 +1,12 @@
 import { fromJS, Map } from "immutable";
 
-import { OPEN_DRAWER, FETCH_ALERTS_SUCCESS } from "./actions";
+import { FETCH_ALERTS_SUCCESS } from "./actions";
 import NAMESPACE from "./namespace";
 
 const DEFAULT_STATE = Map({ drawerOpen: true });
 
 const reducer = (state = DEFAULT_STATE, { type, payload }) => {
   switch (type) {
-    case OPEN_DRAWER:
-      return state.set("drawerOpen", fromJS(payload));
     case FETCH_ALERTS_SUCCESS:
       return state.set("alerts", fromJS(payload));
     default:
