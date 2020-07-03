a75f9d3f90e9759a851272093388be5c08c0b3e1
Alberto Espinoza <aespinoz@quoininc.com>
2020-07-03 16:42:06 -0600

    R2-776 - UX - Limit which forms user sees on record show/edit page
    * Adding enable field to verify if record is closed
    * Limiting forms that the user can see(depends of users permitted forms)

--- app/javascript/components/record-form/selectors.unit.test.js
+++ app/javascript/components/record-form/selectors.unit.test.js
@@ -446,20 +446,21 @@ describe("<RecordForm /> - Selectors", () => {
   });
 
   describe("getFormNav", () => {
-    it("should return the forms nav", () => {
-      const expected = OrderedMap({
-        identification_registration: OrderedMap({
-          "62": R.NavRecord({
-            group: "identification_registration",
-            groupName: "Identification / Registration",
-            groupOrder: 30,
-            name: "Basic Identity",
-            order: 10,
-            formId: "basic_identity",
-            is_first_tab: true
-          })
+    const expected = OrderedMap({
+      identification_registration: OrderedMap({
+        "62": R.NavRecord({
+          group: "identification_registration",
+          groupName: "Identification / Registration",
+          groupOrder: 30,
+          name: "Basic Identity",
+          order: 10,
+          formId: "basic_identity",
+          is_first_tab: true
         })
-      });
+      })
+    });
+
+    it("should return the forms nav", () => {
       const record = selectors.getFormNav(stateWithRecords, {
         primeroModule: "primeromodule-cp",
         recordType: "case"
@@ -472,6 +473,16 @@ describe("<RecordForm /> - Selectors", () => {
       const record = selectors.getFormNav(stateWithRecords, {});
 
       expect(record).to.be.equal(OrderedMap({}));
+    });
+
+    it("should return an empty ordered map when there are not any options", () => {
+      const record = selectors.getFormNav(stateWithRecords, {
+        primeroModule: "primeromodule-cp",
+        recordType: "case",
+        formsIds: ["basic_identity"]
+      });
+
+      expect(record).to.be.equal(expected);
     });
   });
 
