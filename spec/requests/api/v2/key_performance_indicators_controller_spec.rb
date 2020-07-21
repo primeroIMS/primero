17e978d1cd97a1414b68bbd56d20c317c3106e1e
Jasper Lyons <jasper.lyons@gmail.com>
2020-07-21 18:12:15 +0100

    Add test for NumberOfCases KPI

--- spec/requests/api/v2/key_performance_indicators_controller_spec.rb
+++ spec/requests/api/v2/key_performance_indicators_controller_spec.rb
@@ -1,16 +1,49 @@
 require 'rails_helper'
 
 describe Api::V2::KeyPerformanceIndicatorsController, type: :request do
-  describe 'GET /api/v2/key_performance_indicators/number_of_cases' do
+  before(:each) do
+    clean_data(Location, Agency, Role, UserGroup, User, Child)
+
+    @uk = Location.create!(location_code: 'GBR', name: 'United Kingdom', placename: 'United Kingdom', type: 'Country', hierarchy_path: 'GBR', admin_level: 0)
+    @england = Location.create!(location_code: '01', name: 'England', placename: 'England', type: 'Region', hierarchy_path: 'GBR.01', admin_level: 1)
+    @london = Location.create!(location_code: '41', name: 'London', placename: 'London', type: 'County', hierarchy_path: 'GBR.01.41', admin_level: 2)
+
+    @unicef = Agency.create!(agency_code: 'UNICEF', name: 'UNICEF')
+    @gbv_manager = Role.create!(name: 'GBV Manager', permissions: [
+      Permission.new()
+    ])
+    @primero_gbv_group = UserGroup.create!(name: 'Primero GBV')
+
+    @primero_kpi = User.new(
+      user_name: 'primero_kpi',
+      agency: @unicef,
+      role: @gbv_manager,
+      user_groups: [@primero_gbv_group],
+      location: @london.location_code
+    )
+    @primero_kpi.save(validate: false)
+
+    Sunspot.commit
+  end
+
+  let(:json) { JSON.parse(response.body, symbolize_names: true) }
+
+  describe 'GET /api/v2/key_performance_indicators/number_of_cases', search: true do
     it 'renders test data appropriately' do
-      login_for_test
+      child = Child.new_with_user(@primero_kpi, {}).save!
+      Sunspot.commit
+
+      sign_in(@primero_kpi)
 
       get '/api/v2/key_performance_indicators/number_of_cases', params: {
-        from: Date.today - 30,
-        to: Date.today
+        from: Date.today,
+        to: Date.today + 1
       }
 
       expect(response).to have_http_status(200)
+      expect(json[:data][0]).to be
+      expect(json[:data][0][:reporting_site]).to eq(@london.placename)
+      expect(json[:data][0][json[:dates].last.to_sym]).to eq(1)
     end
   end
 end
