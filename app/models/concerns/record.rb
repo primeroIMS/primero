b3bba74f4380f6ab6ec87edf8677961906c65e51
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2020-07-22 15:08:48 -0400

    Moved the logic form merging data hashes out into a service

--- app/models/concerns/record.rb
+++ app/models/concerns/record.rb
@@ -39,7 +39,7 @@ module Record
       id = data.delete('id')
       record = new
       record.id = id if id.present?
-      record.data = Utils.merge_data(record.data, data)
+      record.data = RecordMergeDataHashService.merge_data(record.data, data)
       record.creation_fields_for(user)
       record.set_owner_fields_for(user)
       record
@@ -140,7 +140,7 @@ module Record
   end
 
   def update_properties(data, user_name)
-    self.data = Utils.merge_data(self.data, data)
+    self.data = RecordMergeDataHashService.merge_data(self.data, data)
     self.last_updated_by = user_name
   end
 
@@ -176,39 +176,6 @@ module Record
   def unindex_nested_reportables
     nested_reportables_hash.each do |_, reportables|
       Sunspot.remove! reportables if reportables.present?
-    end
-  end
-
-  class Utils
-    def self.merge_data(old_data, new_data)
-      if old_data.is_a?(Hash) && new_data.is_a?(Hash)
-        old_data.merge(new_data) do |_, old_value, new_value|
-          merge_data(old_value, new_value)
-        end
-      elsif is_an_array_of_hashes?(old_data) && is_an_array_of_hashes?(new_data)
-        merged_old_data = old_data.inject([]) do |result, old_nested_record|
-          nested_record_id = old_nested_record['unique_id']
-          if nested_record_id.present?
-            new_nested_record = new_data.find{|r| r['unique_id'] == nested_record_id}
-            if new_nested_record
-              result << merge_data(old_nested_record, new_nested_record)
-            else
-              result << old_nested_record
-            end
-          else
-            result << old_nested_record
-          end
-          result
-        end
-        append = new_data.reject{|new_record| merged_old_data.find{|r| r['unique_id'] == new_record['unique_id']}}
-        (merged_old_data + append).reject{|record| record['_destroy']}
-      else
-        new_data
-      end
-    end
-
-    def self.is_an_array_of_hashes?(value)
-      value.is_a?(Array) && (value.blank? || value.first.is_a?(Hash))
     end
   end
 end
