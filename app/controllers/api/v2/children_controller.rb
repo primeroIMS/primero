a948e54bbcbb6ae2e9b0e9542bfdabf747f81989
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2020-07-23 12:40:58 -0400

    Cleaning up some of the API controllers which reference concerns

--- app/controllers/api/v2/children_controller.rb
+++ app/controllers/api/v2/children_controller.rb
@@ -2,6 +2,6 @@
 
 # Main API controller for Case records
 class Api::V2::ChildrenController < ApplicationApiController
-  include Concerns::Pagination
-  include Concerns::Record
+  include Api::V2::Concerns::Pagination
+  include Api::V2::Concerns::Record
 end
--- app/controllers/api/v2/tracing_requests_controller.rb
+++ app/controllers/api/v2/tracing_requests_controller.rb
@@ -2,6 +2,6 @@
 
 # Main API controller for Tracing Request records
 class Api::V2::TracingRequestsController < ApplicationApiController
-  include Concerns::Pagination
-  include Concerns::Record
+  include Api::V2::Concerns::Pagination
+  include Api::V2::Concerns::Record
 end
