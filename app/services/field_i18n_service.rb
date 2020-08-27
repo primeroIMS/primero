66be4c5be4bcc53ed804b9e52e447914883957f0
Alberto Espinoza <aespinoz@quoininc.com>
2020-08-27 09:04:44 -0600

    R2-782 - API - "Disabled" property for lookup, field options, i18n refactor
    - Fixing merge_i18n_properties, when the second params is empty it keep the original value

--- app/services/field_i18n_service.rb
+++ app/services/field_i18n_service.rb
@@ -32,7 +32,7 @@ class FieldI18nService
     merged_props = {}
     localized_props1.each do |name, value|
       value2 = localized_props2.try(:[], name) || {}
-      merged_props[name] = value.try(:merge, value2) || value2
+      merged_props[name] = value2.present? ? value.try(:merge, value2) || value2 : value
     end
     merged_props
   end
