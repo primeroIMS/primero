29bcf685d73452e9ae511ebe78cd8073ceef5e30
Gerald Padgett <gpadgett@quoininc.com>
2020-09-02 16:44:16 -0600

    R2-866 - UX - Configurations List

--- app/javascript/components/index-table/cell-renderers/DateCell.jsx
+++ app/javascript/components/index-table/cell-renderers/DateCell.jsx
@@ -3,16 +3,17 @@ import PropTypes from "prop-types";
 
 import { useI18n } from "../../i18n";
 
-const DateCell = ({ value }) => {
+const DateCell = ({ value, withTime }) => {
   const i18n = useI18n();
 
-  return <>{i18n.l("date.formats.default", value)}</>;
+  return <>{i18n.l(`date.formats.${ withTime ? "with_time" : "default" }`, value)}</>;
 };
 
 DateCell.displayName = "DateCell";
 
 DateCell.propTypes = {
-  value: PropTypes.string
+  value: PropTypes.string,
+  withTime: PropTypes.bool
 };
 
 export default DateCell;
--- app/javascript/components/pages/admin/configurations-list/utils.js
+++ app/javascript/components/pages/admin/configurations-list/utils.js
@@ -4,6 +4,6 @@ export const getColumns = i18n => [
   { name: "name", label: i18n.t("configurations.attributes.name") },
   { name: "version", label: i18n.t("configurations.attributes.version") },
   { name: "description", label: i18n.t("configurations.attributes.description") },
-  { name: "date_created", label: i18n.t("configurations.attributes.date_created") },
+  { name: "created_on", label: i18n.t("configurations.attributes.date_created") },
   { name: "last_applied_on", label: i18n.t("configurations.attributes.last_applied_on") }
 ];
