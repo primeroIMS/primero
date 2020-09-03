f0e75d20564669b1c97f020158373e9d07264cb2
Gerald Padgett <gpadgett@quoininc.com>
2020-09-03 12:29:04 -0600

    R2-866 - UX - Configurations List

--- app/javascript/components/pages/admin/configurations-list/container.unit.test.js
+++ app/javascript/components/pages/admin/configurations-list/container.unit.test.js
@@ -1,6 +1,6 @@
 import { fromJS } from "immutable";
 
-import { setupMountedComponent, lookups } from "../../../../test";
+import { setupMountedComponent, lookups, useFakeTimers } from "../../../../test";
 import IndexTable from "../../../index-table";
 import { ACTIONS } from "../../../../libs/permissions";
 
@@ -9,13 +9,24 @@ import ConfigurationsList from "./container";
 
 describe("<ConfigurationsList />", () => {
   let component;
+  let clock;
+  const createdOn = "2020-08-26T15:35:13.720Z";
+
+  before(() => {
+    clock = useFakeTimers(new Date(createdOn));
+  });
+
+  after(() => {
+    clock.restore();
+  });
+
   const data = [
     {
       id: "ac3d041c-1592-4f99-8191-b38fbe448735",
       name: "SWIMS - 20200903",
       description: "September 2020 baseline",
       version: "20200826.113513.25bf89a",
-      created_on: "2020-08-26T15:35:13.720Z",
+      created_on: createdOn,
       created_by: "primero_swims_admin",
       applied_on: null,
       applied_by: null
