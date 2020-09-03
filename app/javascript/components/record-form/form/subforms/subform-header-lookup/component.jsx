f85a942505d33c3a16116ee989e1dd29a7feecbc
Gerald Padgett <gpadgett@quoininc.com>
2020-09-03 17:41:22 -0600

    R2-876 - Multi-selects break subform collapsed fields

--- app/javascript/components/record-form/form/subforms/subform-header-lookup/component.jsx
+++ app/javascript/components/record-form/form/subforms/subform-header-lookup/component.jsx
@@ -7,6 +7,8 @@ import { useI18n } from "../../../../i18n";
 import { getOption } from "../../../selectors";
 import { SUBFORM_LOOKUP_HEADER_NAME } from "../constants";
 
+import { getMultiSelectValues } from "./utils";
+
 const Component = ({ value, optionsStringSource, optionsStringText }) => {
   const i18n = useI18n();
   const optionsStrings = useSelector(state => getOption(state, optionsStringSource, i18n.locale));
@@ -16,7 +18,19 @@ const Component = ({ value, optionsStringSource, optionsStringText }) => {
   if (!isEmpty(optionsStringSource)) {
     const { display_text: displayText } = optionsStrings.find(o => o.id === value) || {};
 
-    return <span>{displayText}</span>;
+    if (!Array.isArray(value)) {
+      return <span>{displayText}</span>;
+    }
+
+    const texts = getMultiSelectValues(value, optionsStrings);
+
+    return <span>{texts}</span>;
+  }
+
+  if (Array.isArray(value)) {
+    const texts = getMultiSelectValues(value, optionsStringText, i18n.locale);
+
+    return <span>{texts}</span>;
   }
 
   const { display_text: displayText } = optionsStringText[i18n.locale].find(
