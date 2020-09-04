faabfea90455ce2ffab15e316754beb3d8ae8612
Alberto Espinoza <aespinoz@quoininc.com>
2020-09-04 11:20:29 -0600

    R2-884 -  Not able to update manual dropdown options

--- app/javascript/components/pages/admin/form-builder/components/field-dialog/forms/base/options.unit.test.js
+++ app/javascript/components/pages/admin/form-builder/components/field-dialog/forms/base/options.unit.test.js
@@ -1,8 +1,8 @@
 import { fromJS } from "immutable";
 
-import { FieldRecord, TEXT_FIELD } from "../../../../../../../form";
+import { FieldRecord, TEXT_FIELD, SELECT_FIELD } from "../../../../../../../form";
 
-import { optionsForm } from "./options";
+import { optionsForm, optionsTabs } from "./options";
 
 describe("pages/admin/<FormBuilder />/components/<FieldDialog />/forms/base - options", () => {
   const i18n = { t: value => value };
@@ -48,6 +48,23 @@ describe("pages/admin/<FormBuilder />/components/<FieldDialog />/forms/base - op
 
       expect(form.unique_id).to.equal("field_form_options");
       expect(fieldNames).to.not.equal(["custom_field_1"]);
+    });
+  });
+  describe("optionsTabs", () => {
+    it("should return an array of fields", () => {
+      const field = FieldRecord({
+        display_name: "new_field",
+        name: "new_field",
+        type: SELECT_FIELD,
+        option_strings_text: [{ id: "test1", disabled: false, display_text: { en: "TEST 1" } }]
+      });
+      const tabs = optionsTabs("test", i18n, fromJS({ isShow: false, isEdit: true }), field, fromJS([]));
+
+      expect(tabs).to.have.lengthOf(2);
+      expect(tabs[0].name).to.equal("fields.predifined_lookups");
+      expect(tabs[0].disabled).to.be.true;
+      expect(tabs[1].name).to.equal("fields.create_unique_values");
+      expect(tabs[1].disabled).to.be.false;
     });
   });
 });
