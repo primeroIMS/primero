6205f8f41426ca7963f4781c3da21b28c9a78a3c
Dennis Hernandez <dhernand@quoininc.com>
2020-10-02 20:24:46 -0600

    R2-899 - TH UAT - Help text not appearing on separators or tick boxes

--- app/javascript/components/record-form/form/field-types/seperator.jsx
+++ app/javascript/components/record-form/form/field-types/seperator.jsx
@@ -1,12 +1,20 @@
+/* eslint-disable react/no-multi-comp, react/display-name */
 import React from "react";
 import PropTypes from "prop-types";
+import { FormHelperText } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 import { SEPERATOR_NAME } from "../constants";
+import styles from "../styles.css";
 
-const Seperator = ({ label }) => {
+const Seperator = ({ helperText, label, mode }) => {
+  const css = makeStyles(styles)();
+  const renderHelperText = () => (mode.isEdit ? <FormHelperText>{helperText}</FormHelperText> : null);
+
   return (
     <>
-      <h4>{label}</h4>
+      <h4 className={css.separator}>{label}</h4>
+      {renderHelperText()}
     </>
   );
 };
@@ -14,7 +22,9 @@ const Seperator = ({ label }) => {
 Seperator.displayName = SEPERATOR_NAME;
 
 Seperator.propTypes = {
-  label: PropTypes.string
+  helperText: PropTypes.string,
+  label: PropTypes.string,
+  mode: PropTypes.string
 };
 
 export default Seperator;
