338caecfaa854599376d051f2f438c4202c142d3
Brad Teeman Quoin <bteeman@Brads-MacBook-Pro.local>
2020-10-08 09:53:58 -0400

    location of docker-compose has changed

--- docker/compose.sh
+++ docker/compose.sh
@@ -3,4 +3,4 @@
 : "${PROJECT_NAME:=primero}"
 
 set -euox
-exec docker-compose -p "${PROJECT_NAME}" --project-directory "../" -f "docker-compose.yml" "${@}"
+exec /opt/docker/bin/docker-compose -p "${PROJECT_NAME}" --project-directory "../" -f "docker-compose.yml" "${@}"
