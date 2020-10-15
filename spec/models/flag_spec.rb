52f0802ee2bba591dcc48a3ca6693ff0d9f15ca4
Ron Elledge <relledge@quoininc.com>
2020-10-15 14:55:50 -0400

    R2-508 rubocop cleanup of flag_spec

--- spec/models/flag_spec.rb
+++ spec/models/flag_spec.rb
@@ -29,16 +29,21 @@ describe Flag do
                          user_groups: [@group1], agency_id: @agency2.id)
     @manager1.save(validate: false)
 
-    @case1 = Child.create!(data: { short_id: "abc123", name: "Test1", hidden_name: true, age: 5, sex: 'male', owned_by: 'user1' })
-    @case2 = Child.create!(data: { name: "Test2", age: 7, sex: 'female', owned_by: 'user1' })
-    @case3 = Child.create!(data: { name: "Test3", age: 9, sex: 'female', owned_by: 'user2' })
-    @case4 = Child.create!(data: { name: "Test4", age: 9, sex: 'female', owned_by: 'user2' })
+    @case1 = Child.create!(data: { short_id: 'abc123', name: 'Test1', hidden_name: true, age: 5, sex: 'male',
+                                   owned_by: 'user1' })
+    @case2 = Child.create!(data: { name: 'Test2', age: 7, sex: 'female', owned_by: 'user1' })
+    @case3 = Child.create!(data: { name: 'Test3', age: 9, sex: 'female', owned_by: 'user2' })
+    @case4 = Child.create!(data: { name: 'Test4', age: 9, sex: 'female', owned_by: 'user2' })
 
-    @tracing_request1 = TracingRequest.create!(data: { inquiry_date: Date.new(2019, 3, 1), relation_name: 'Test 1', owned_by: 'user1' })
-    @tracing_request2 = TracingRequest.create!(data: { inquiry_date: Date.new(2018, 3, 1), relation_name: 'Test 2', owned_by: 'user2' })
+    @tracing_request1 = TracingRequest.create!(data: { inquiry_date: Date.new(2019, 3, 1), relation_name: 'Test 1',
+                                                       owned_by: 'user1' })
+    @tracing_request2 = TracingRequest.create!(data: { inquiry_date: Date.new(2018, 3, 1), relation_name: 'Test 2',
+                                                       owned_by: 'user2' })
 
-    @incident1 = Incident.create!(data: { incident_date: Date.new(2019, 3, 1), description: 'Test 1', owned_by: 'user1' })
-    @incident2 = Incident.create!(data: { incident_date: Date.new(2018, 3, 1), description: 'Test 2', owned_by: 'user2' })
+    @incident1 = Incident.create!(data: { incident_date: Date.new(2019, 3, 1), description: 'Test 1',
+                                          owned_by: 'user1' })
+    @incident2 = Incident.create!(data: { incident_date: Date.new(2018, 3, 1), description: 'Test 2',
+                                          owned_by: 'user2' })
 
     @case1.add_flag('This is a flag', Date.today, 'faketest')
     @case1.add_flag('This is a second flag', Date.today, 'faketest')
@@ -53,7 +58,7 @@ describe Flag do
   describe 'by_owner' do
     context 'when the user has query user permissions' do
       before do
-        @query_scope = {user: {'user' => 'user1'}, module: ['primeromodule-cp']}
+        @query_scope = { user: { 'user' => 'user1' }, module: ['primeromodule-cp'] }
       end
 
       context 'when record_types is passed in' do
@@ -66,13 +71,13 @@ describe Flag do
             flags = Flag.by_owner(@query_scope, @record_types)
             f1 = flags.first
             expect(flags.size).to eq(2)
-            expect(f1['record_id']).to eq( @case1.id.to_s)
+            expect(f1['record_id']).to eq(@case1.id.to_s)
             expect(f1['record_type']).to eq('Child')
-            expect(f1['message']).to eq( 'This is a flag')
-            expect(f1['r_short_id']).to eq( 'abc123')
-            expect(f1['r_name']).to eq( 'Test1')
+            expect(f1['message']).to eq('This is a flag')
+            expect(f1['r_short_id']).to eq('abc123')
+            expect(f1['r_name']).to eq('Test1')
             expect(f1['r_hidden_name']).to be
-            expect(f1['r_owned_by']).to eq( 'user1')
+            expect(f1['r_owned_by']).to eq('user1')
 
             # expect(flags.first['removed']).to be_falsey
           end
@@ -89,21 +94,21 @@ describe Flag do
         end
 
         it 'returns case flags owned by or asssociated with the current user' do
-          case_flag = @flags.select{|f| f.record_type == 'Child'}.first
+          case_flag = @flags.select { |f| f.record_type == 'Child' }.first
           expect(case_flag).to be
-          expect(case_flag['record_id']).to eq( @case1.id.to_s)
-          expect(case_flag['message']).to eq( 'This is a flag')
-          expect(case_flag['r_short_id']).to eq( 'abc123')
-          expect(case_flag['r_name']).to eq( 'Test1')
+          expect(case_flag['record_id']).to eq(@case1.id.to_s)
+          expect(case_flag['message']).to eq('This is a flag')
+          expect(case_flag['r_short_id']).to eq('abc123')
+          expect(case_flag['r_name']).to eq('Test1')
           expect(case_flag['r_hidden_name']).to be
-          expect(case_flag['r_owned_by']).to eq( 'user1')
+          expect(case_flag['r_owned_by']).to eq('user1')
         end
 
         it 'returns incident flags owned by or asssociated with the current user' do
-          incident_flag = @flags.select{|f| f.record_type == 'Incident'}.first
+          incident_flag = @flags.select { |f| f.record_type == 'Incident' }.first
           expect(incident_flag).to be
-          expect(incident_flag['record_id']).to eq( @incident1.id.to_s)
-          expect(incident_flag['message']).to eq( 'This is a flag IN')
+          expect(incident_flag['record_id']).to eq(@incident1.id.to_s)
+          expect(incident_flag['message']).to eq('This is a flag IN')
           expect(incident_flag['r_short_id']).to eq(@incident1.short_id)
           expect(incident_flag['r_name']).to be_nil
           expect(incident_flag['r_hidden_name']).to be_nil
@@ -111,10 +116,10 @@ describe Flag do
         end
 
         it 'returns tracing_request flags owned by or asssociated with the current user' do
-          tr_flag = @flags.select{|f| f.record_type == 'TracingRequest'}.first
+          tr_flag = @flags.select { |f| f.record_type == 'TracingRequest' }.first
           expect(tr_flag).to be
-          expect(tr_flag['record_id']).to eq( @tracing_request1.id.to_s)
-          expect(tr_flag['message']).to eq( 'This is a flag TR')
+          expect(tr_flag['record_id']).to eq(@tracing_request1.id.to_s)
+          expect(tr_flag['message']).to eq('This is a flag TR')
           expect(tr_flag['r_short_id']).to eq(@tracing_request1.short_id)
           expect(tr_flag['r_name']).to be_nil
           expect(tr_flag['r_hidden_name']).to be_nil
@@ -125,7 +130,7 @@ describe Flag do
 
     context 'when the user has query group permissions' do
       before do
-        @query_scope = {:user=>{"group"=>[@group1.unique_id]}, :module=>["primeromodule-cp"]}
+        @query_scope = { user: { 'group' => [@group1.unique_id] }, module: ['primeromodule-cp'] }
       end
 
       context 'when record_types is passed in' do
@@ -138,13 +143,13 @@ describe Flag do
             flags = Flag.by_owner(@query_scope, @record_types)
             f1 = flags.first
             expect(flags.size).to eq(3)
-            expect(f1['record_id']).to eq( @case1.id.to_s)
+            expect(f1['record_id']).to eq(@case1.id.to_s)
             expect(f1['record_type']).to eq('Child')
-            expect(f1['message']).to eq( 'This is a flag')
-            expect(f1['r_short_id']).to eq( 'abc123')
-            expect(f1['r_name']).to eq( 'Test1')
+            expect(f1['message']).to eq('This is a flag')
+            expect(f1['r_short_id']).to eq('abc123')
+            expect(f1['r_name']).to eq('Test1')
             expect(f1['r_hidden_name']).to be
-            expect(f1['r_owned_by']).to eq( 'user1')
+            expect(f1['r_owned_by']).to eq('user1')
 
             # expect(flags.first['removed']).to be_falsey
           end
@@ -159,9 +164,9 @@ describe Flag do
             flags = Flag.by_owner(@query_scope, @record_types)
             f1 = flags.first
             expect(flags.size).to eq(4)
-            expect(f1['record_id']).to eq( @incident1.id.to_s)
+            expect(f1['record_id']).to eq(@incident1.id.to_s)
             expect(f1['record_type']).to eq('Incident')
-            expect(f1['message']).to eq( 'This is a flag IN')
+            expect(f1['message']).to eq('This is a flag IN')
             expect(f1['r_short_id']).to eq(@incident1.short_id)
             expect(f1['r_name']).to be_nil
             expect(f1['r_hidden_name']).to be_nil
@@ -180,9 +185,9 @@ describe Flag do
             flags = Flag.by_owner(@query_scope, @record_types)
             f1 = flags.first
             expect(flags.size).to eq(1)
-            expect(f1['record_id']).to eq( @tracing_request1.id.to_s)
+            expect(f1['record_id']).to eq(@tracing_request1.id.to_s)
             expect(f1['record_type']).to eq('TracingRequest')
-            expect(f1['message']).to eq( 'This is a flag TR')
+            expect(f1['message']).to eq('This is a flag TR')
             expect(f1['r_short_id']).to eq(@tracing_request1.short_id)
             expect(f1['r_name']).to be_nil
             expect(f1['r_hidden_name']).to be_nil
