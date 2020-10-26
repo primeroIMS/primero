ee7af9d7e9f9b98efadc0d7d67f67c407da2464d
Joshua Toliver <jtoliver@quoininc.com>
2020-10-26 11:37:24 -0400

    Fixing lint issue

--- app/javascript/components/pdf-exporter/components/table/component.jsx
+++ app/javascript/components/pdf-exporter/components/table/component.jsx
@@ -5,7 +5,6 @@ import isEqual from "lodash/isEqual";
 
 import KeyValueCell from "../key-value-cell";
 import { useI18n } from "../../../i18n";
-import { DATE_FIELD, TICK_FIELD } from "../../../form";
 import { valuesWithDisplayConditions } from "../../../record-form/form/subforms/subform-field-array/utils";
 
 import { EXCLUDED_FIELD_TYPES } from "./constants";
