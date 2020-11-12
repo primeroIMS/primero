ee243e90ea601766ce4eb5608b30422ea10adee1
Dennis Hernandez <dhernand@quoininc.com>
2020-11-12 17:42:37 -0600

    R2-1079 - Merge subforms by id or unique_id to avoid duplicates in indexeddb

---
+++ app/javascript/db/utils.js
@@ -0,0 +1,16 @@
+/* eslint-disable camelcase */
+/* eslint-disable import/prefer-default-export */
+import merge from "deepmerge";
+
+export const subformAwareMerge = (target, source, options) =>
+  source.map(item => {
+    if (options.isMergeableObject(item) && (item?.id || item?.unique_id)) {
+      const targetItem = item?.id
+        ? target.find(t => t?.id === item?.id)
+        : target.find(t => t?.unique_id === item?.unique_id);
+
+      return targetItem ? merge(targetItem, item, options) : item;
+    }
+
+    return item;
+  });
