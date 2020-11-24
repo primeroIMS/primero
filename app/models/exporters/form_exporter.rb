d7b74da3a9b2377e48a64e441180e4f2583191b2
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2020-11-24 11:17:20 -0500

    Forms Excel export should omit subforms when listing out forms

--- app/models/exporters/form_exporter.rb
+++ app/models/exporters/form_exporter.rb
@@ -11,7 +11,7 @@ class Exporters::FormExporter < ValueObject
     opts[:record_type] ||= 'case'
     opts[:module_id] ||= 'primeromodule-cp'
     opts[:visible] = true if opts[:visible].nil?
-    opts[:form_params] = opts.slice(:record_type, :module_id, :visible)&.compact
+    opts[:form_params] = { exclude_subforms: true }.merge(opts.slice(:record_type, :module_id, :visible)&.compact)
     opts[:header] = initialize_header(opts)
     opts[:total] = 0
     opts[:success_total] = 0
