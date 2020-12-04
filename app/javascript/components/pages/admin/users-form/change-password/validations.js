c76445fc5e2cf247657cd89f39f40118d45955c8
Dennis Hernandez <dhernand@quoininc.com>
2020-12-04 11:56:28 -0600

    R2-1107 - Clean up user form email and password validations

--- app/javascript/components/pages/admin/users-form/change-password/validations.js
+++ app/javascript/components/pages/admin/users-form/change-password/validations.js
@@ -2,6 +2,14 @@ import { object, string, ref } from "yup";
 
 export default i18n =>
   object().shape({
-    password: string().required().label(i18n.t("user.password")),
-    password_confirmation: string().oneOf([ref("password"), null], i18n.t("errors.models.user.password_mismatch"))
+    password: string()
+      .nullable()
+      .min(8, i18n.t("errors.models.user.password_length", { min: 8 }))
+      .required(i18n.t("forms.required_field", { field: i18n.t("user.password") }))
+      .label(i18n.t("user.password")),
+    password_confirmation: string()
+      .nullable()
+      .min(8, i18n.t("errors.models.user.password_length", { min: 8 }))
+      .oneOf([ref("password"), null], i18n.t("errors.models.user.password_mismatch"))
+      .required(i18n.t("forms.required_field", { field: i18n.t("user.password_confirmation") }))
   });
