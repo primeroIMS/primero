e2f4180fd9987e3ffae51454f996c19d69caae09
Joshua Toliver <jtoliver@quoininc.com>
2020-12-04 14:54:50 -0500

    Fixing rspec/lint issues

--- app/javascript/components/login/actions.unit.test.js
+++ app/javascript/components/login/actions.unit.test.js
@@ -4,7 +4,7 @@ describe("<Login /> - Actions", () => {
   it("should have known actions", () => {
     const cloneActions = { ...actions };
 
-    ["LOGIN", "LOGIN_SUCCESS", "LOGIN_STARTED", "LOGIN_FAILURE", "LOGIN_FINSIHED"].forEach(property => {
+    ["LOGIN", "LOGIN_SUCCESS", "LOGIN_STARTED", "LOGIN_FAILURE", "LOGIN_FINISHED"].forEach(property => {
       expect(cloneActions).to.have.property(property);
       expect(cloneActions[property]).to.be.a("string");
       delete cloneActions[property];
