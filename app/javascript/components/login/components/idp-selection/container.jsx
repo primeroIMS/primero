3361e327ac6fa7ec5ebce88f89ec58fda5735ee1
Gerald Padgett <gpadgett@quoininc.com>
2020-12-15 10:15:51 -0600

    R2-1168 - IDP - Login styling update

--- app/javascript/components/login/components/idp-selection/container.jsx
+++ app/javascript/components/login/components/idp-selection/container.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Button } from "@material-ui/core";
+import { Button, Link } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 import { PageHeading } from "../../../page";
@@ -12,26 +13,37 @@ import { signIn } from "./auth-provider";
 import { NAME } from "./config";
 import styles from "./styles.css";
 
-const showIdps = (identityProviders, i18n, dispatch) => {
+const showIdps = (identityProviders, i18n, dispatch, css) => {
   const tokenCallback = accessToken => {
     dispatch(attemptLogin(accessToken));
   };
 
-  return identityProviders.map(idp => (
-    <Button
-      className="provider-login"
-      color="primary"
-      type="submit"
-      size="large"
-      fullWidth
-      key={idp.get("name")}
-      onClick={() => signIn(idp, tokenCallback)}
-    >
-      {i18n.t("login.provider_title", {
-        provider: idp.get("name")
-      })}
-    </Button>
-  ));
+  return identityProviders.map(idp => {
+    if (idp.get("unique_id") === "primeroims") {
+      return (
+        <div className={css.linkButtonContainer}>
+          <span>{i18n.t("or_label")}</span>
+          <Link component="a" variant="body2" onClick={() => signIn(idp, tokenCallback)}>
+            {`log in with ${idp.get("name")} username`}
+          </Link>
+        </div>
+      );
+    }
+
+    return (
+      <Button
+        className="provider-login"
+        color="primary"
+        type="submit"
+        size="large"
+        fullWidth
+        key={idp.get("name")}
+        onClick={() => signIn(idp, tokenCallback)}
+      >
+        {idp.get("name")}
+      </Button>
+    );
+  });
 };
 
 const Container = () => {
@@ -43,7 +55,8 @@ const Container = () => {
   return (
     <>
       <PageHeading title={i18n.t("login.title")} whiteHeading />
-      <div className={`${css.loginSelection} loginSelection`}>{showIdps(identityProviders, i18n, dispatch)}</div>
+      <p className={css.selectProvider}>{i18n.t("select_provider")}</p>
+      <div className={`${css.loginSelection} loginSelection`}>{showIdps(identityProviders, i18n, dispatch, css)}</div>
     </>
   );
 };
