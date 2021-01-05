daafadde9460de79cd55bae4388f708dee266a89
Gerald Padgett <gpadgett@quoininc.com>
2021-01-05 12:21:50 -0600

    R2-1204 - UX - Cases by Social Worker Dashboard
    * API integration

--- app/javascript/components/pages/dashboard/components/cases-by-social-worker/component.jsx
+++ app/javascript/components/pages/dashboard/components/cases-by-social-worker/component.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
-import { fromJS } from "immutable";
 
 import { getCasesBySocialWorker } from "../../selectors";
 import { useI18n } from "../../../../i18n";
@@ -16,56 +15,13 @@ import { NAME } from "./constants";
 const Component = ({ loadingIndicator }) => {
   const i18n = useI18n();
 
-  // const data = useSelector(state => getCasesBySocialWorker(state));
-  const data = fromJS({
-    indicators: {
-      total: {
-        primero: {
-          count: 5,
-          query: ["test=0", "user=primero"]
-        },
-        primero_cp: {
-          count: 10,
-          query: ["test=10", "user=primero_cp"]
-        },
-        primero_mgr_cp: {
-          count: 4,
-          query: ["test=4", "user=primero_cp"]
-        }
-      },
-      new_and_updated: {
-        primero: {
-          count: 0,
-          query: ["test=0", "user=primero"]
-        },
-        primero_cp: {
-          count: 1,
-          query: ["test=10", "user=primero_cp"]
-        },
-        primero_mgr_cp: {
-          count: 16,
-          query: ["test=16", "user=primero_cp"]
-        }
-      }
-    }
-  });
-
+  const data = useSelector(state => getCasesBySocialWorker(state));
   const casesBySocialWorkerProps = {
     ...toCasesBySocialWorkerTable(data, i18n)
   };
 
-  console.log("casesBySocialWorkerProps", casesBySocialWorkerProps);
-
-  // const actions = ACTIONS.DASH_CASES_BY_SOCIAL_WORKER;
-  const actions = [
-    ACTIONS.DASH_CASES_BY_TASK_OVERDUE_ASSESSMENT,
-    ACTIONS.DASH_CASES_BY_TASK_OVERDUE_CASE_PLAN,
-    ACTIONS.DASH_CASES_BY_TASK_OVERDUE_SERVICES,
-    ACTIONS.DASH_CASES_BY_TASK_OVERDUE_FOLLOWUPS
-  ];
-
   return (
-    <Permission resources={RESOURCES.dashboards} actions={actions}>
+    <Permission resources={RESOURCES.dashboards} actions={ACTIONS.DASH_CASES_BY_SOCIAL_WORKER}>
       <OptionsBox title={i18n.t("dashboard.cases_by_social_worker")} hasData={Boolean(data.size)} {...loadingIndicator}>
         <DashboardTable
           pathname={ROUTES.cases}
--- app/javascript/components/pages/dashboard/utils/to-cases-by-social-worker-table.js
+++ app/javascript/components/pages/dashboard/utils/to-cases-by-social-worker-table.js
@@ -1,81 +1,15 @@
+import isEmpty from "lodash/isEmpty";
+
 import { CASES_BY_SOCIAL_WORKER_COLUMNS } from "../components/cases-by-social-worker/constants";
 import { dataToJS } from "../../../../libs";
-// SOURCE
-// indicators: {
-// "total": {
-//   "primero": {
-//     "count": 2,
-//     "query": [
-//       "record_state=true",
-//       "status=open",
-//       "owned_by=primero"
-//     ]
-//   },
-//   "primero_cp": {
-//     "count": 0,
-//     "query": [
-//       "record_state=true",
-//       "status=open",
-//       "owned_by=primero_cp"
-//     ]
-//   }
-// },
-// "new_or_updated": {
-//   "primero": {
-//     "count": 1,
-//     "query": [
-//       "record_state=true",
-//       "status=open",
-//       "not_edited_by_owner=true",
-//       "owned_by=primero"
-//     ]
-//   },
-//   "primero_cp": {
-//     "count": 0,
-//     "query": [
-//       "record_state=true",
-//       "status=open",
-//       "not_edited_by_owner=true",
-//       "owned_by=primero_cp"
-//     ]
-//   }
-// }
-// }
-
-// EXPECTED
-// columns: [
-//   {
-//     name: "case_worker",
-//     label: "Case Worker"
-//   },
-//   {
-//     name: "total",
-//     label: "Total"
-//   },
-//   {
-//     name: "new_and_updated",
-//     label: "New & Updated"
-//   }
-// ],
-// data: [
-//   ["primero", 0, 10],
-//   ["primero_cp", 10, 0]
-// ],
-// query: [
-//   {
-//     case_worker: [],
-//     total: ["total=true", "owned_by=primero"],
-//     new_and_updated: ["new_and_updated=true", "owned_by=primero"]
-//   },
-//   {
-//     case_worker: [],
-//     total: ["total=true", "owned_by=primero_cp"],
-//     new_and_updated: ["new_and_updated=true", "owned_by=primero_cp"]
-//   }
-// ]
 
 export default (indicators, i18n) => {
   const newData = dataToJS(indicators);
+
+  if (isEmpty(newData)) {
+    return {};
+  }
+
   const rows = Object.keys(Object.values(newData.indicators)[0]);
   const [, ...columnValues] = CASES_BY_SOCIAL_WORKER_COLUMNS;
   const firstColumn = CASES_BY_SOCIAL_WORKER_COLUMNS[0];
