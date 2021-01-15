828e979fa347ab339e356f1cf672a0242c0a15bd
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2021-01-15 15:09:18 -0500

    Setting user_id cookie in the login controller

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -8,14 +8,24 @@ class LoginController < ApplicationController
   end
 
   def show
-    binding.pry
     provider_type = params[:id]
     @identity_provider = IdentityProvider.find_by(provider_type: provider_type)
     return render_404 unless @identity_provider.present?
+
+    set_user_id_cookie
     render layout: 'identity', template: "login/#{provider_type}"
   end
 
   def render_404
     render file: 'public/404.html', status: 404, layout: false
+  end
+
+  def set_user_id_cookie
+    cookies[:primero_user_id] = {
+      value: @identity_provider.user_from_request(request)&.id,
+      domain: Rails.application.routes.default_url_options[:host],
+      same_site: :strict,
+      secure: (Rails.env == 'production')
+    }
   end
 end
