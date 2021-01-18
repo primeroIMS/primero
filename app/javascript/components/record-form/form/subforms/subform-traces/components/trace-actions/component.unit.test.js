a7b54e6dbbfe4b96f58f443bc3610a9b7568cabc
Dennis Hernandez <dhernand@quoininc.com>
2021-01-18 09:29:45 -0600

    R2-1217 - Add / Fix unit tests

--- app/javascript/components/record-form/form/subforms/subform-traces/components/trace-actions/component.unit.test.js
+++ app/javascript/components/record-form/form/subforms/subform-traces/components/trace-actions/component.unit.test.js
@@ -3,11 +3,12 @@ import { fromJS } from "immutable";
 import { setupMountedComponent } from "../../../../../../../test";
 import { SHOW_FIND_MATCH } from "../../../../../../../libs/permissions";
 import ActionButton from "../../../../../../action-button";
+import { FORMS } from "../../constants";
 
 import TraceActions from "./component";
 
 describe("<RecordForm>/form/subforms/<TraceActions>", () => {
-  const props = { handleBack: () => {}, handleConfirm: () => {} };
+  const props = { handleBack: () => {}, handleConfirm: () => {}, selectedForm: FORMS.trace };
 
   it("should not render find match button if user does not have permission", () => {
     const { component } = setupMountedComponent(TraceActions, props, fromJS([]));
@@ -27,5 +28,13 @@ describe("<RecordForm>/form/subforms/<TraceActions>", () => {
 
     expect(component.find(ActionButton)).to.have.lengthOf(2);
     expect(component.find(ActionButton).last().text()).to.equal("tracing_request.find_match");
+  });
+
+  it("should render comparison actions", () => {
+    const { component } = setupMountedComponent(TraceActions, { ...props, selectedForm: FORMS.comparison }, fromJS({}));
+
+    expect(component.find(ActionButton)).to.have.lengthOf(2);
+    expect(component.find(ActionButton).first().text()).to.equal("tracing_request.back_to_potential_matches");
+    expect(component.find(ActionButton).last().text()).to.equal("tracing_request.match");
   });
 });
