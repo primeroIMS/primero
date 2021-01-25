a8660c743463e6978380bc16becd95379731c020
Gerald Padgett <gpadgett@quoininc.com>
2021-01-25 16:53:14 -0600

    R2-1266 - FTR Cleanup 2021-01-22

--- app/javascript/components/records/reducer.js
+++ app/javascript/components/records/reducer.js
@@ -53,7 +53,8 @@ import {
   FETCH_CASE_MATCHED_TRACES_FINISHED,
   FETCH_CASE_MATCHED_TRACES_STARTED,
   FETCH_CASE_MATCHED_TRACES_SUCCESS,
-  CLEAR_MATCHED_TRACES
+  CLEAR_MATCHED_TRACES,
+  UNMATCH_CASE_FOR_TRACE_SUCCESS
 } from "./actions";
 
 const DEFAULT_STATE = Map({ data: List([]) });
@@ -218,6 +219,17 @@ export default namespace => (state = DEFAULT_STATE, { type, payload }) => {
       return state.set("loading", true);
     }
     case `${namespace}/${SET_MACHED_CASE_FOR_TRACE_SUCCESS}`: {
+      const { tracing_request_id: tracingRequestId, id } = payload.data;
+      const index = state.get("data").findIndex(r => r.get("id") === tracingRequestId);
+      const traceIndex = state
+        .getIn(["data", index, "tracing_request_subform_section"])
+        .findIndex(trace => trace.get("unique_id") === id);
+
+      return state.updateIn(["data", index, "tracing_request_subform_section", traceIndex], u =>
+        mergeRecord(u, fromJS(payload.data))
+      );
+    }
+    case `${namespace}/${UNMATCH_CASE_FOR_TRACE_SUCCESS}`: {
       const { tracing_request_id: tracingRequestId, id } = payload.data;
       const index = state.get("data").findIndex(r => r.get("id") === tracingRequestId);
       const traceIndex = state
