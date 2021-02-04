951f751dbaea194152bc91af8533447f532cfe03
Dennis Hernandez <dhernand@quoininc.com>
2021-02-04 14:52:54 -0600

    R2-1279 - Accepted transitions are still associated with users

--- spec/models/concerns/transitionable_spec.rb
+++ spec/models/concerns/transitionable_spec.rb
@@ -24,6 +24,9 @@ describe Transitionable do
     @group3 = UserGroup.create!(name: 'Group3')
     @user3 = User.new(user_name: 'user3', role: @role, user_groups: [@group3])
     @user3.save(validate: false)
+    @group4 = UserGroup.create!(name: 'Group4')
+    @user4 = User.new(user_name: 'user4', role: @role, user_groups: [@group4])
+    @user4.save(validate: false)
     @case = Child.create(data: {
       name: 'Test', owned_by: 'user1',
       module_id: @module_cp.unique_id,
@@ -32,7 +35,6 @@ describe Transitionable do
   end
 
   describe 'transitions_for_user' do
-
     before :each do
       @assign1 = Assign.create!(transitioned_by: 'user1', transitioned_to: 'user2', record: @case)
       @assign2 = Assign.create!(transitioned_by: 'user2', transitioned_to: 'user1', record: @case)
@@ -58,6 +60,42 @@ describe Transitionable do
       expect(transitions.size).to eq(4)
       expect(transitions.map(&:id)).to include(@assign1.id, @assign2.id, @transfer.id, @referral1.id)
       expect(transitions.map(&:id)).not_to include(@referral2.id)
+    end
+  end
+
+  describe 'transferred_to_users' do
+    before :each do
+      @transfer1 = Transfer.create!(transitioned_by: 'user1', transitioned_to: 'user2', record: @case)
+      @transfer2 = Transfer.create!(transitioned_by: 'user1', transitioned_to: 'user4', record: @case)
+      @transfer3 = Transfer.create!(transitioned_by: 'user1', transitioned_to: 'user3', record: @case)
+      @transfer4 = Transfer.create!(transitioned_by: 'user1', transitioned_to: 'user3', record: @case)
+      @transfer1.status = Transition::STATUS_INPROGRESS
+      @transfer2.status = Transition::STATUS_ACCEPTED
+      @transfer3.status = Transition::STATUS_REJECTED
+      @transfer4.status = Transition::STATUS_DONE
+      [@transfer1, @transfer2, @transfer3, @transfer4].each(&:save!)
+    end
+
+    it 'should return the users with transfers' do
+      expect(@case.transferred_to_users).to match_array(%w[user2 user4])
+    end
+  end
+
+  describe 'referred_users' do
+    before :each do
+      @referral1 = Referral.create!(transitioned_by: 'user1', transitioned_to: 'user2', record: @case)
+      @referral2 = Referral.create!(transitioned_by: 'user1', transitioned_to: 'user3', record: @case)
+      @referral3 = Referral.create!(transitioned_by: 'user1', transitioned_to: 'user4', record: @case)
+      @referral4 = Referral.create!(transitioned_by: 'user1', transitioned_to: 'user4', record: @case)
+      @referral1.status = Transition::STATUS_INPROGRESS
+      @referral2.status = Transition::STATUS_ACCEPTED
+      @referral3.status = Transition::STATUS_REJECTED
+      @referral4.status = Transition::STATUS_DONE
+      [@referral1, @referral2, @referral3, @referral4].each(&:save!)
+    end
+
+    it 'should return the users with transfers' do
+      expect(@case.referred_users).to match_array(%w[user2 user3])
     end
   end
 
