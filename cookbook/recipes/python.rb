ead8679f9d0548f572db4d5a9aa8cda13f7056c5
Chavez, Carlos <carlos.chavez@quoininc.com>
2021-03-09 12:19:21 -0600

    PRIM-2412 Pip setuptools installations issue. Install latest python/pip version.

--- cookbook/recipes/python.rb
+++ cookbook/recipes/python.rb
@@ -2,35 +2,38 @@
 # installed and fails. Python cookbook is a dependency of supervisor cookbook
 # and it can't be avoided.
 
-package 'python3'
-
-# python3-pip install a version that is not compatible with python3.5.
-# https://github.com/pypa/pip/issues/9500
-# https://github.com/pypa/get-pip/pull/87
-cookbook_file '/tmp/get-pip.py' do
-  source 'get-pip.py'
-  owner 'root'
-  group 'root'
-  mode '755'
+# Remove alternatives created with the latest patch to fix the pip issue.
+execute 'remove python3.5 alternatives' do
+  command "update-alternatives --remove python /usr/bin/python3.5"
+  only_if { File.exists?('/usr/bin/python3.5')}
+end
+execute 'remove pip3.5 alternatives' do
+  command "update-alternatives --remove pip /usr/local/bin/pip3"
+  only_if { File.exists?('/usr/local/bin/pip3')}
 end
 
-execute 'pip compatible version' do
-  command "python3 /tmp/get-pip.py"
+execute 'Install python3.9' do
+  command 'apt update -y && apt -y install software-properties-common && add-apt-repository -y ppa:deadsnakes/ppa && apt update -y && apt install -y python3.9 python3.9-venv python3.9-dev'
+end
+
+execute 'Install Pip for python3.9' do
+  command 'wget https://bootstrap.pypa.io/get-pip.py && python3.9 get-pip.py'
+  cwd '/tmp'
 end
 
 # Create symbolic link to make think to the supervisor/python cookbook that
 # python2 is already installed.
 execute 'python alternatives' do
-  command "update-alternatives --install /usr/bin/python python /usr/bin/python3.5 1"
+  command "update-alternatives --install /usr/bin/python python /usr/bin/python3.9 1"
 end
 
 # Create symbolic links to make think to the supervisor cookbook that
 # pip python2 is already installed.
 execute 'pip alternatives option 1' do
-  command "update-alternatives --install /usr/bin/pip pip /usr/local/bin/pip3 1"
-  only_if { !File.exists?('/usr/bin/pip') && File.exists?('/usr/local/bin/pip3') }
+  command "update-alternatives --install /usr/bin/pip pip /usr/local/bin/pip3.9 1"
+  only_if { !File.exists?('/usr/bin/pip') && File.exists?('/usr/local/bin/pip3.9') }
 end
 execute 'pip alternatives option 2' do
-  command "update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1"
-  only_if { !File.exists?('/usr/bin/pip') && File.exists?('/usr/bin/pip3')}
+  command "update-alternatives --install /usr/bin/pip pip /usr/bin/pip3.9 1"
+  only_if { !File.exists?('/usr/bin/pip') && File.exists?('/usr/bin/pip3.9')}
 end
