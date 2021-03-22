5bf425d568fe4ebfce5e3e84bc60aca6d97e1c53
Joshua Toliver <jtoliver@quoininc.com>
2021-03-22 10:13:45 -0400

    Adding back wdyr

--- app/javascript/wdyr.js
+++ app/javascript/wdyr.js
@@ -1,8 +1,11 @@
 /* eslint-disable import/no-namespace */
+import * as React from "react";
+import whyDidYouRender from "@welldone-software/why-did-you-render";
+import * as ReactRedux from "react-redux/lib";
 
 if (process.env.NODE_ENV === "development") {
-  // whyDidYouRender(React, {
-  //   trackAllPureComponents: true,
-  //   trackExtraHooks: [[ReactRedux, "useSelector", "useMemoizedSelector"]]
-  // });
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
+    trackExtraHooks: [[ReactRedux, "useSelector", "useMemoizedSelector"]]
+  });
 }
