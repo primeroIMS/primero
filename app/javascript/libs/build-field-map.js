739022e199b10eae8c5e0614480c170ccb3ab306
Joshua Toliver <jtoliver@quoininc.com>
2021-04-09 16:30:18 -0400

    R2-1466: Always sending owned_by with newly created case incidents

--- app/javascript/libs/build-field-map.js
+++ app/javascript/libs/build-field-map.js
@@ -1,7 +1,18 @@
 import { get } from "../components/form/utils";
 
-const buildFieldMap = (record, fieldMap) => {
-  return fieldMap.reduce((prev, { source, target }) => {
+const OWNED_BY = "owned_by";
+const OWNED_BY_FULL_NAME = "owned_by_full_name";
+const ASSOCIATED_USER_NAMES = "associated_user_names";
+
+const DEFAULT_MAPPINGS = [OWNED_BY, OWNED_BY_FULL_NAME, ASSOCIATED_USER_NAMES].map(field => ({
+  source: field,
+  target: field
+}));
+
+const buildFieldMap = (record, fieldMap = []) => {
+  const fieldMapWithDefaults = [...fieldMap, ...DEFAULT_MAPPINGS];
+
+  return fieldMapWithDefaults.reduce((prev, { source, target }) => {
     return { ...prev, [target]: get(record, source) };
   }, {});
 };
