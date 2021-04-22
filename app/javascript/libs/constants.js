984041be2eac4f5ac35a3b618639bf1678724451
Joshua Toliver <jtoliver@quoininc.com>
2021-04-22 11:07:47 -0400

    Fixing broken unit test

---
+++ app/javascript/libs/constants.js
@@ -0,0 +1,9 @@
+export const QUEUE_ADD = "queue-add";
+export const QUEUE_FINISHED = "queue-finished";
+export const QUEUE_FAILED = "queue-failed";
+export const QUEUE_SKIP = "queue-skip";
+export const QUEUE_SUCCESS = "queue-success";
+
+export const QUEUE_PENDING = "pending";
+export const QUEUE_READY = "ready";
+export const QUEUE_HALTED = "halted";
