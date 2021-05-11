83e051092b2b17bc2f35417ba98ba47db2a07498
Dennis Hernandez <dhernand@quoininc.com>
2021-05-11 10:08:40 -0600

    R2-1475 - Add core forms to seeds

---
+++ db/configuration/forms/case/change_logs.rb
@@ -0,0 +1,13 @@
+FormSection.create_or_update!(
+  unique_id: 'change_logs',
+  parent_form: 'case',
+  visible: true,
+  order_form_group: 0,
+  order: 6,
+  form_group_id: 'record_information',
+  fields: [],
+  name_en: 'Change Log',
+  description_en: 'Change Log',
+  core_form: true,
+  is_first_tab: true
+)
