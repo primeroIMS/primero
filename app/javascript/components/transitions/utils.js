6f5185e401722f413a7ece8d545f75691e4e6caa
Joshua Toliver <jtoliver@quoininc.com>
2021-05-13 11:29:02 -0400

    Redirecting to case list if case transfer rejected

--- app/javascript/components/transitions/utils.js
+++ app/javascript/components/transitions/utils.js
@@ -5,17 +5,20 @@ import { RECORD_PATH, REJECTED } from "../../config";
 export const fetchRecordCallback = ({ recordType, recordId, approvalType = null }) => {
   const callback = fetchRecord(recordType, recordId, true);
 
+  if (approvalType === REJECTED) {
+    return {
+      action: `${recordType}/REDIRECT`,
+      redirectWithIdFromResponse: false,
+      redirect: `/${RECORD_PATH.cases}`
+    };
+  }
+
   return {
     ...callback,
     api: {
       ...callback.api,
       successCallback: [
-        {
-          action: `${recordType}/REDIRECT`,
-          ...(approvalType === REJECTED
-            ? { redirectWithIdFromResponse: false, redirect: `/${RECORD_PATH.cases}` }
-            : { redirectWithIdFromResponse: true, redirect: `/${RECORD_PATH.cases}` })
-        }
+        { action: `${recordType}/REDIRECT`, redirectWithIdFromResponse: true, redirect: `/${RECORD_PATH.cases}` }
       ],
       failureCallback: [
         { action: `${recordType}/REDIRECT`, redirectWithIdFromResponse: false, redirect: `/${RECORD_PATH.cases}` }
