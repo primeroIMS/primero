d801ad5f83e7023e9b57eec08202b150e9ab7b7e
Ralph Paul <ralph.paul@quoininc.com>
2021-07-12 15:22:43 -0400

    Created methods for controller and view for webhooks

--- app/controllers/api/v2/webhook_syncs_controller.rb
+++ app/controllers/api/v2/webhook_syncs_controller.rb
@@ -10,4 +10,26 @@ class Api::V2::WebhookSyncsController < Api::V2::RecordResourceController
     )
     @record.queue_for_webhook(Webhook::POST)
   end
+
+  def update
+    authorize! :update, @record
+    @record.update_properties(record_params)
+    
+    @record.save!
+  end  
+
+  #TO-DO: Finish the post and API_key method
+  def post
+  #  authorize! :
+  end 
+  
+  def api_key
+  #  authorize! :
+  end
+
+  def basic_auth
+    authorize! :auth_secret, @record
+  end
+
+
 end
