45455021b54aaaf2c709eb050b64c0cae1103834
Dennis Hernandez <dhernand@quoininc.com>
2021-07-21 16:23:18 -0600

    R2-1727 - Fix flaky client test, try #1

--- app/javascript/libs/transform-offline-request.unit.test.js
+++ app/javascript/libs/transform-offline-request.unit.test.js
@@ -5,8 +5,8 @@ import { DATE_FIELD, NUMERIC_FIELD } from "../components/record-form/constants";
 import transformOfflineRequest from "./transform-offline-request";
 
 describe("transformOfflineRequest", () => {
-  beforeEach(done => {
-    Promise.all([
+  beforeEach(async () => {
+    await Promise.all([
       DB.add(DB_STORES.FIELDS, {
         name: "first_name"
       }),
@@ -18,14 +18,14 @@ describe("transformOfflineRequest", () => {
         name: "date_of_birth",
         type: DATE_FIELD
       })
-    ]).finally(done);
+    ]);
   });
 
-  afterEach(done => {
-    DB.clearDB().finally(done);
+  afterEach(async () => {
+    await DB.clearDB();
   });
 
-  it("should transform numeric and date fields", done => {
+  it("should transform numeric and date fields", async () => {
     const action = {
       api: {
         body: { data: { first_name: "First Name", age: "10", date_of_birth: "12-10-2010" } },
@@ -40,10 +40,8 @@ describe("transformOfflineRequest", () => {
       }
     };
 
-    transformOfflineRequest(action)
-      .then(result => {
-        expect(result).to.deep.equals(expected);
-      })
-      .finally(done);
+    const result = await transformOfflineRequest(action);
+
+    expect(result).to.deep.equals(expected);
   });
 });
