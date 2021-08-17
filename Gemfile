e1fe36593b98783e4879f25477926d1510fcbc24
Dennis Hernandez <dhernand@quoininc.com>
2021-08-17 15:11:16 -0600

    R2-1766 - Migrate ActiveJob to DelayedJob

--- Gemfile
+++ Gemfile
@@ -6,11 +6,12 @@ ruby '2.6.6'
 gem 'activerecord-nulldb-adapter'      # Running Rake tasks at build time before DB is set up. TODO: Still needed?
 gem 'azure-storage',       '0.15.0.preview',
     require: false                     # Deprecated MS interface for Azure Block Storage, compatible with Rails 5.2
-gem 'backburner',          '~> 1.5'    # Ruby client for the Beanstalkd queue
 gem 'cancancan',           '3.0.1'     # Endpoint user authorization
 gem 'csv-safe',            '>= 1.2.0'  # Safely export data to CSV to avoid formula injection
+gem 'daemons',             '~> 1.4.0'  # Gem to run the delayed jobs
 gem 'deep_merge',          '~> 1.2',   # Recursive merging of Hashes. Used for merging params to existing records.
     require: 'deep_merge/rails_compat'
+gem 'delayed_job_active_record', '~> 4.1.6'
 gem 'devise',              '~> 4.7'    # Authentication framework
 gem 'devise-jwt',          '0.8.0'     # JWT authentication for native Primero users
 gem 'faraday',             '~> 0.17'   # Ruby HTTP client
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_25_000000) do
+ActiveRecord::Schema.define(version: 2021_08_17_000000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
@@ -144,6 +144,21 @@ ActiveRecord::Schema.define(version: 2021_05_25_000000) do
     t.string "position"
     t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+  end
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string "locked_by"
+    t.string "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
   create_table "export_configurations", id: :serial, force: :cascade do |t|
