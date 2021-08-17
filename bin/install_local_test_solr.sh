d580bde0f0b9d02278ee98e31a26c2adef98509d
Joshua Toliver <jtoliver@quoininc.com>
2021-08-17 15:58:44 -0400

    Fixing solr for bitbucket pipelines

--- bin/install_local_test_solr.sh
+++ bin/install_local_test_solr.sh
@@ -44,11 +44,13 @@ chmod 0770 "$SOLR_DIR/data" "$SOLR_DIR/logs"
 cp -a "$SOLR_SRC_DIR/server/solr/solr.xml" "$SOLR_DIR/data/solr.xml"
 cp -a "$SOLR_SRC_DIR/server/solr/zoo.cfg" "$SOLR_DIR/data/zoo.cfg"
 cp -a "$SOLR_SRC_DIR/server/resources/log4j2.xml" "$SOLR_DIR/log4j2.xml"
+cp -a "solr/solr.xml" "$SOLR_SRC_DIR/"
 
 cp -r solr/configsets/primero "$SOLR_DIR/data/$SOLR_CORE"
 cp config/bitbucket/core.properties "$SOLR_DIR/data/$SOLR_CORE/"
 
 chown -R "$SOLR_USER:$SOLR_USER" $SOLR_DIR
+chmod -R 777 "$SOLR_SRC_DIR/bin"
 
 cp docker/solr/root/opt/solr/bin/solr.in.sh.template /opt/solr/bin/solr.in.sh
 
