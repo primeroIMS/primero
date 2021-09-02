64d654a764777e6883c00fde745cd0f1d606c26d
Joshua Toliver <jtoliver@quoininc.com>
2021-09-02 12:46:06 -0400

    Setting up github actions

--- bin/run_server_tests.sh
+++ bin/run_server_tests.sh
@@ -2,37 +2,68 @@
 
 set -ex
 
-# Install JDK
-# TODO: This is for installing OpenJDK-8 which is no longer supported in Debian Buster.
-#       Change after we upgrade Solr
-wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | apt-key add -
-echo "deb https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/ buster main" | tee /etc/apt/sources.list.d/adoptopenjdk.list
-apt update && apt install -y adoptopenjdk-8-hotspot
+BITBUCKET=bitbucket
+GITHUB_ACTIONS=github-actions
 
-# Install Rails pre-requisites
-apt-get update
-apt install -y --no-install-recommends postgresql-11 postgresql-client-11 libsodium-dev
-bundle install --without production
+PIPELINE=${1:-bitbucket}
 
 # Set up test environment
-mkdir -p log
-cp config/bitbucket/database.yml config/
-cp config/bitbucket/sunspot.yml config/
-cp config/bitbucket/mailers.yml config/
-mkdir -p solr/data/test
-mkdir -p solr/cores/test
-cp config/bitbucket/core.properties solr/cores/test/
-export RAILS_ENV=test
-export DEVISE_JWT_SECRET_KEY=DEVISE_JWT_SECRET_KEY
-export DEVISE_SECRET_KEY=DEVISE_SECRET_KEY
+setup_test_env() {
+  mkdir -p log
+  cp "config/$PIPELINE/database.yml" config/
+  cp "config/$PIPELINE/sunspot.yml" config/
+  cp "config/$PIPELINE/mailers.yml" config/
+  mkdir -p solr/data/test
+  mkdir -p solr/cores/test
+  mkdir -p tmp/storage
 
-# Start Solr
-bundle exec rails sunspot:solr:start
+  cp "config/$PIPELINE/core.properties" solr/cores/test/
+  export RAILS_ENV=test
+  export DEVISE_JWT_SECRET_KEY=DEVISE_JWT_SECRET_KEY
+  export DEVISE_SECRET_KEY=DEVISE_SECRET_KEY
+}
 
 # Create the database
-bundle exec rails db:drop
-bundle exec rails db:create
-bundle exec rails db:migrate
+setup_database() {
+  bundle exec rails db:drop
+  bundle exec rails db:create
+  bundle exec rails db:migrate
+}
+
+setup_dependencies() {
+  if [ $PIPELINE == $BITBUCKET ]; then 
+    # Install JDK
+    # TODO: This is for installing OpenJDK-8 which is no longer supported in Debian Buster.
+    #       Change after we upgrade Solr
+    wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
+    echo "deb https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/ buster main" | sudo tee /etc/apt/sources.list.d/adoptopenjdk.list
+    sudo apt update && sudo apt install -y adoptopenjdk-8-hotspot
+  fi
+  
+  if [ $PIPELINE == $GITHUB_ACTIONS ]; then 
+    wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O- | sudo apt-key add -
+    echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt/ focal-pgdg main" | sudo tee /etc/apt/sources.list.d/postgresql.list
+  fi
+
+  # Install Rails pre-requisites
+  sudo apt-get update
+  sudo apt install -y --no-install-recommends postgresql-11 postgresql-client-11 libsodium-dev
+  
+  if [ $PIPELINE == $BITBUCKET ]; then 
+    bundle install --without production
+  fi
+}
+
+setup_dependencies
+setup_test_env
+
+if [ $PIPELINE == $BITBUCKET ]; then 
+  bundle exec rails sunspot:solr:start
+fi
+
+setup_database
 
 # Run tests
 bundle exec rspec spec
+
+
