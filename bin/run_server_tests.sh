41ffaa113ff668e44c87a76a28c1d9d59cf73de5
Joshua Toliver <jtoliver@quoininc.com>
2021-09-03 17:30:01 -0400

    Trigger pipeline

--- bin/run_server_tests.sh
+++ bin/run_server_tests.sh
@@ -8,7 +8,7 @@ TEST_CONFIG_DIR=${1:-bitbucket}
 # TODO: This is for installing OpenJDK-8 which is no longer supported in Debian Buster.
 #       Change after we upgrade Solr
 wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
-echo "deb https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/ buster main" | tee /etc/apt/sources.list.d/adoptopenjdk.list
+echo "deb https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/ buster main" | sudo tee /etc/apt/sources.list.d/adoptopenjdk.list
 sudo apt update && apt install -y adoptopenjdk-8-hotspot
 
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O- | sudo apt-key add -
@@ -18,7 +18,7 @@ echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt/ focal-pgdg main"
 sudo apt-get update
 sudo apt install -y --no-install-recommends postgresql-11 postgresql-client-11 libsodium-dev
 
-bundle install --without production
+# bundle install --without production
 
 # Set up test environment
 mkdir -p log
