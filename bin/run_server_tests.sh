9f9273d80c698a1e3d7be51bf41da1bacfa4a9df
Joshua Toliver <jtoliver@quoininc.com>
2021-09-03 12:42:08 -0400

    Paramertizing run server test script to point to correct configs

--- bin/run_server_tests.sh
+++ bin/run_server_tests.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+TEST_CONFIG_DIR=${1:-bitbucket}
+
 # Install JDK
 # TODO: This is for installing OpenJDK-8 which is no longer supported in Debian Buster.
 #       Change after we upgrade Solr
@@ -16,12 +18,12 @@ bundle install --without production
 
 # Set up test environment
 mkdir -p log
-cp config/bitbucket/database.yml config/
-cp config/bitbucket/sunspot.yml config/
-cp config/bitbucket/mailers.yml config/
+cp "config/$TEST_CONFIG_DIR/database.yml" config/
+cp "config/$TEST_CONFIG_DIR/sunspot.yml" config/
+cp "config/$TEST_CONFIG_DIR/mailers.yml" config/
 mkdir -p solr/data/test
 mkdir -p solr/cores/test
-cp config/bitbucket/core.properties solr/cores/test/
+cp "config/$TEST_CONFIG_DIR/core.properties" solr/cores/test/
 export RAILS_ENV=test
 export DEVISE_JWT_SECRET_KEY=DEVISE_JWT_SECRET_KEY
 export DEVISE_SECRET_KEY=DEVISE_SECRET_KEY
