73288c36bd1e1c51311a8d013a9f7c8497bf1377
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2021-09-03 14:55:25 -0400

    Using bunle exec in the pipline unit test runs to guarantee correct gems. Added back in blank entry for es-GT list of months

--- bin/run_server_tests.sh
+++ bin/run_server_tests.sh
@@ -27,12 +27,12 @@ export DEVISE_JWT_SECRET_KEY=DEVISE_JWT_SECRET_KEY
 export DEVISE_SECRET_KEY=DEVISE_SECRET_KEY
 
 # Start Solr
-rails sunspot:solr:start
+bundle exec rails sunspot:solr:start
 
 # Create the database
-rails db:drop
-rails db:create
-rails db:migrate
+bundle exec rails db:drop
+bundle exec rails db:create
+bundle exec rails db:migrate
 
 # Run tests
-rspec spec
+bundle exec rspec spec
