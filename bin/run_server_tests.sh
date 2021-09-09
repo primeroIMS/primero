3e3f23286dc85c99b4c95d850af40f7e421aae9f
Joshua Toliver <jtoliver@quoininc.com>
2021-09-09 15:08:52 -0400

    Replacing postgres libs for lib pq

--- bin/run_server_tests.sh
+++ bin/run_server_tests.sh
@@ -39,14 +39,14 @@ setup_dependencies() {
     apt update && apt install -y adoptopenjdk-8-hotspot
   fi
   
-  if [ $PIPELINE == $GITHUB_ACTIONS ]; then 
-    wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O- | apt-key add -
-    echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt/ focal-pgdg main" | tee /etc/apt/sources.list.d/postgresql.list
-  fi
+  # if [ $PIPELINE == $GITHUB_ACTIONS ]; then 
+  #   wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O- | apt-key add -
+  #   echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt/ focal-pgdg main" | tee /etc/apt/sources.list.d/postgresql.list
+  # fi
 
   # Install Rails pre-requisites
   apt-get update
-  apt install -y --no-install-recommends postgresql-11 postgresql-client-11 libsodium-dev
+  apt install -y --no-install-recommends libpq-dev libsodium-dev
   
   if [ $PIPELINE == $BITBUCKET ]; then 
     bundle install --without production
