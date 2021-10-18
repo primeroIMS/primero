e29f9a6d4849720fa450d407bbd8ea56c792a319
Dennis Hernandez <dhernand@quoininc.com>
2021-10-18 15:45:30 -0600

    R2-1831 - UAT - Custom exports not respecting display conditions on subforms

--- app/models/exporters/excel_exporter.rb
+++ app/models/exporters/excel_exporter.rb
@@ -49,7 +49,7 @@ class Exporters::ExcelExporter < Exporters::BaseExporter
     worksheet&.write(0, 0, 'ID')
     form.fields.each_with_index do |field, i|
       if field.type == Field::SUBFORM
-        build_worksheet_with_headers(field.subform)
+        build_worksheet_with_headers(constraint_subform_fields(field))
       else
         worksheet&.write(0, i + 1, field.display_name(locale))
       end
@@ -99,7 +99,11 @@ class Exporters::ExcelExporter < Exporters::BaseExporter
     worksheet = worksheets[form.unique_id][:worksheet]
     values, rows_to_write = field_values(data, form, form_name)
     ([id] + values).each_with_index { |value, column| write_value(worksheet, form, value, column, rows_to_write) }
-    form.subform_fields.each { |field| write_record_form(id, data, field.subform, field.name) }
+    form.subform_fields.each do |field|
+      subform = constraint_subform_fields(field)
+      data = apply_subform_display_conditions(data, field)
+      write_record_form(id, data, subform, field.name)
+    end
     rows_to_write
   end
 
@@ -155,5 +159,38 @@ class Exporters::ExcelExporter < Exporters::BaseExporter
     return RecordDataService::CENSORED_VALUE if data['hidden_name'] == true && field.name == 'name'
 
     data[field.name]
+  end
+
+  def constraint_subform_fields(field)
+    field_names = subform_configured_field_names(field)
+
+    return field.subform unless field_names.present?
+
+    subform = field.subform.dup
+    subform.fields = field.subform.fields.to_a.select do |sf_field|
+      field_names[sf_field.name] == true
+    end
+
+    subform
+  end
+
+  def apply_subform_display_conditions(data, field)
+    display_conditions = subform_display_conditions(field)
+
+    return data unless display_conditions.present?
+
+    data[field.name] = data[field.name].select do |subform_values|
+      display_conditions.all? { |key, value| subform_values[key] == value }
+    end
+
+    data
+  end
+
+  def subform_configured_field_names(field)
+    (field.subform_section_configuration&.dig('fields') || []).reduce({}) { |acc, elem| acc.merge(elem => true) }
+  end
+
+  def subform_display_conditions(field)
+    (field.subform_section_configuration&.dig('display_conditions') || []).reduce({}) { |acc, elem| acc.merge(elem) }
   end
 end
