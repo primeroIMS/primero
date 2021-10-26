4f5b0e7ee9ba8d6ab59b5af24626754fa6bb3605
Dennis Hernandez <dhernand@quoininc.com>
2021-10-26 16:11:25 -0600

    R2-1812 - Refactor permissions for the activity log dashboards

--- app/controllers/api/v2/activity_log_controller.rb
+++ app/controllers/api/v2/activity_log_controller.rb
@@ -5,6 +5,7 @@ class Api::V2::ActivityLogController < ApplicationApiController
   include Api::V2::Concerns::Pagination
 
   def index
+    authorize :transfer, ActivityLog
     results = ActivityLog.list(current_user, activity_log_params)
     @total = results.size
     @activity_logs = results.paginate(pagination)
