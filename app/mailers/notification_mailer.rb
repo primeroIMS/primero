a8fe68658e0aa965684b13f0f3ebb3f639482a21
Jasser Gutierrez <jgutierr@quoininc.com>
2021-11-05 17:08:39 -0600

    R2-1855 - LB - Notification emails always send in English

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -11,9 +11,10 @@ class NotificationMailer < ApplicationMailer
     @child = Child.find_by(id: record_id) || (return log_not_found('Case', record_id))
     @user = @child.owner || (return log_not_found('User', @child.owned_by))
     @approval_type = Lookup.display_value('lookup-approval-type', approval_type)
+    @locale_email = @manager.locale
     return log_not_found('Lookup', 'lookup-approval-type') unless @approval_type
 
-    mail(to: @manager.email, subject: t('email_notification.approval_request_subject', id: @child.short_id))
+    mail(to: @manager.email, subject: t('email_notification.approval_request_subject', id: @child.short_id, locale: @locale_email))
   end
 
   def manager_approval_response(record_id, approved, approval_type, manager_user_name)
@@ -24,13 +25,15 @@ class NotificationMailer < ApplicationMailer
     @manager = User.find_by(user_name: manager_user_name)
     lookup_name = @manager.gbv? ? 'lookup-gbv-approval-types' : 'lookup-approval-type'
     @approval_type = Lookup.display_value(lookup_name, approval_type)
-    @approval = approved ? t('approvals.status.approved') : t('approvals.status.rejected')
+    @locale_email = @owner.locale
+    @approval = approved ? t('approvals.status.approved', locale: @locale_email) : t('approvals.status.rejected', locale: @locale_email)
 
-    mail(to: @owner.email, subject: t('email_notification.approval_response_subject', id: @child.short_id))
+    mail(to: @owner.email, subject: t('email_notification.approval_response_subject', id: @child.short_id, locale: @locale_email))
   end
 
   def transition_notify(transition_id)
     @transition = Transition.find_by(id: transition_id)
+    @locale_email = @transition&.transitioned_to_user&.locale
     return log_not_found('Transition', transition_id) unless @transition
     return unless assert_notifications_enabled(@transition&.transitioned_to_user)
 
@@ -39,20 +42,23 @@ class NotificationMailer < ApplicationMailer
       to: @transition&.transitioned_to_user&.email,
       subject: t(
         "email_notification.#{@transition.key}_subject",
-        record_type: t("forms.record_types.#{record.class.parent_form}"),
-        id: record.short_id
+        record_type: t("forms.record_types.#{record.class.parent_form}", locale: @locale_email),
+        id: record.short_id,
+        locale: @locale_email
       )
     )
   end
 
   def transfer_request(transfer_request_id)
     @transition = TransferRequest.find_by(id: transfer_request_id)
+    @locale_email = @transition&.transitioned_to_user&.locale
     return log_not_found('Transfer Request Transition', transfer_request_id) unless @transition
     return unless assert_notifications_enabled(@transition&.transitioned_to_user)
 
     mail(
       to: @transition&.transitioned_to_user&.email,
-      subject: t('email_notification.transfer_request_subject')
+      subject: t('email_notification.transfer_request_subject'),
+      locale: @locale_email
     )
   end
 
