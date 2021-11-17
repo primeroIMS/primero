86d9a2afbfcad37aa020555d9b58577658ecb623
Jasser Gutierrez <jgutierr@quoininc.com>
2021-11-17 16:02:27 -0600

    R2-1855 - LB - Notification emails always send in English - Set default to I18n.locale

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -11,7 +11,7 @@ class NotificationMailer < ApplicationMailer
     @child = Child.find_by(id: record_id) || (return log_not_found('Case', record_id))
     @user = @child.owner || (return log_not_found('User', @child.owned_by))
     @approval_type = Lookup.display_value('lookup-approval-type', approval_type)
-    @locale_email = @manager.locale
+    @locale_email = @manager.locale || I18n.locale
     return log_not_found('Lookup', 'lookup-approval-type') unless @approval_type
 
     mail(to: @manager.email, subject: t('email_notification.approval_request_subject', id: @child.short_id, locale: @locale_email))
@@ -25,7 +25,7 @@ class NotificationMailer < ApplicationMailer
     @manager = User.find_by(user_name: manager_user_name)
     lookup_name = @manager.gbv? ? 'lookup-gbv-approval-types' : 'lookup-approval-type'
     @approval_type = Lookup.display_value(lookup_name, approval_type)
-    @locale_email = @owner.locale
+    @locale_email = @owner.locale || I18n.locale
     @approval = approved ? t('approvals.status.approved', locale: @locale_email) : t('approvals.status.rejected', locale: @locale_email)
 
     mail(to: @owner.email, subject: t('email_notification.approval_response_subject', id: @child.short_id, locale: @locale_email))
@@ -33,7 +33,7 @@ class NotificationMailer < ApplicationMailer
 
   def transition_notify(transition_id)
     @transition = Transition.find_by(id: transition_id)
-    @locale_email = @transition&.transitioned_to_user&.locale
+    @locale_email = @transition&.transitioned_to_user&.locale || I18n.locale
     return log_not_found('Transition', transition_id) unless @transition
     return unless assert_notifications_enabled(@transition&.transitioned_to_user)
 
@@ -51,7 +51,7 @@ class NotificationMailer < ApplicationMailer
 
   def transfer_request(transfer_request_id)
     @transition = TransferRequest.find_by(id: transfer_request_id)
-    @locale_email = @transition&.transitioned_to_user&.locale
+    @locale_email = @transition&.transitioned_to_user&.locale || I18n.locale
     return log_not_found('Transfer Request Transition', transfer_request_id) unless @transition
     return unless assert_notifications_enabled(@transition&.transitioned_to_user)
 
