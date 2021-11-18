2bcd46dea4c7ea437bbeef9614dd13fa221d9c54
Dennis Hernandez <dhernand@quoininc.com>
2021-11-18 11:29:04 -0600

    R2-1818 - Fix error when violence type is not defined

--- app/javascript/components/form/utils/build-linked-incident-options.unit.test.js
+++ app/javascript/components/form/utils/build-linked-incident-options.unit.test.js
@@ -21,5 +21,23 @@ describe("form/utils/build-linked-incident-options", () => {
         buildLinkedIncidentOptions(linkedIncidents, violenceLookupValues, { localizeDate: value => value })
       ).to.deep.equal(expected);
     });
+
+    it("does not fail if the violence lookup value does not exist", () => {
+      const linkedIncidents = fromJS([
+        { unique_id: "001-aaa", short_id: "aaa", incident_date: "2020-05-06", cp_incident_violence_type: "type1" },
+        { unique_id: "001-bbb", short_id: "bbb", incident_date: "2020-05-07", cp_incident_violence_type: "type1" }
+      ]);
+
+      const violenceLookupValues = [];
+
+      const expected = [
+        { id: "001-aaa", display_text: "aaa - 2020-05-06", disabled: false },
+        { id: "001-bbb", display_text: "bbb - 2020-05-07", disabled: false }
+      ];
+
+      expect(
+        buildLinkedIncidentOptions(linkedIncidents, violenceLookupValues, { localizeDate: value => value })
+      ).to.deep.equal(expected);
+    });
   });
 });
