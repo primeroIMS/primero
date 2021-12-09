683edbf9e826f981611173a9dee9cb0206d4a6ca
Dennis Hernandez <dhernand@quoininc.com>
2021-12-09 10:16:23 -0600

    R2-1902 - Add unit tests

---
+++ spec/models/concerns/duplicate_id_alertable_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe DuplicateIdAlertable do
+  before :each do
+    clean_data(Alert, Child)
+
+    SystemSettings.stub(:current).and_return(
+      SystemSettings.new(system_options: { duplicate_field_to_form: { case: { field_id: 'form_1' } } })
+    )
+
+    @child1 = Child.create!(data: { field_id: '0001' })
+  end
+
+  it 'creates a duplicate alert if other record has the same id value' do
+    ActiveJob::Base.queue_adapter = :test
+
+    expect { Child.create!(data: { field_id: '0001' }) }.to enqueue_job(DuplicateFieldAlertJob)
+  end
+end
---
+++ spec/services/duplicated_field_alert_service_spec.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe DuplicatedFieldAlertService do
+  before :each do
+    clean_data(Alert, Child)
+
+    @child1 = Child.create!(data: { id_field: '0001' })
+    @child2 = Child.create!(data: { id_field: '0001' })
+    @child3 = Child.create!(data: { id_field: '0001' })
+    @child4 = Child.create!(data: { id_field: '0002' })
+    @child5 = Child.create!(data: { id_field: '0003' })
+    @child6 = Child.create!(data: { id_field: '0002' })
+  end
+
+  describe 'duplicate_records' do
+    it 'returns the records where the field value is duplicated' do
+      expect(
+        DuplicatedFieldAlertService.duplicate_records(@child1, 'id_field').ids
+      ).to match_array([@child2.id, @child3.id])
+    end
+
+    it 'returns empty if there are no duplicated records' do
+      expect(DuplicatedFieldAlertService.duplicate_records(@child5, 'id_field').ids).to be_empty
+    end
+  end
+
+  describe 'duplicates_field' do
+    it 'returns true if there are duplicated records' do
+      expect(DuplicatedFieldAlertService.duplicates_field?(@child1, 'id_field')).to eq(true)
+    end
+
+    it 'returns false if there are no duplicated records' do
+      expect(DuplicatedFieldAlertService.duplicates_field?(@child5, 'id_field')).to eq(false)
+    end
+  end
+
+  describe 'duplicate_alerts' do
+    before :each do
+      @alert1 = Alert.create!(alert_for: DuplicateIdAlertable::DUPLICATE_FIELD, type: 'id_field', record: @child2)
+    end
+
+    it 'returns alerts for duplicated records' do
+      expect(DuplicatedFieldAlertService.duplicate_alert(@child3, 'id_field').id).to eq(@alert1.id)
+    end
+
+    it 'returns nil if no duplicate field alerts were found' do
+      expect(DuplicatedFieldAlertService.duplicate_alert(@child5, 'id_field')).to be_nil
+    end
+  end
+
+  describe 'generate_alerts!' do
+    it 'generate alerts for the duplicated fields in the record' do
+      DuplicatedFieldAlertService.generate_alerts!(@child3, { 'id_field' => 'form_1' })
+
+      expect(Alert.includes(:record).where(type: 'id_field').map { |alert| alert.record.id }).to match_array([@child3.id])
+    end
+  end
+end
