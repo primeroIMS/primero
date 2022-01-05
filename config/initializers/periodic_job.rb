248023cbb70b2ae818640952fe493c6809950f69
Jasser Gutierrez <jgutierr@quoininc.com>
2022-01-05 09:07:02 -0600

    R2-1932 - Rubocop Cleanup - Rubocop's autocorrections in config

--- config/initializers/periodic_job.rb
+++ config/initializers/periodic_job.rb
@@ -8,7 +8,7 @@ return unless HealthCheckService.database_accessible? && ActiveRecord::Base.conn
 Rails.logger.info('Setting up PeriodicJobs')
 
 %w[ArchiveBulkExports OptimizeSolr RecalculateAge].each do |job_name|
-  next if Delayed::Job.where("handler LIKE :job_class", job_class: "%job_class: #{job_name}%").exists?
+  next if Delayed::Job.where('handler LIKE :job_class', job_class: "%job_class: #{job_name}%").exists?
 
   Rails.logger.info("#{job_name} executed at #{DateTime.now}")
   job_name.constantize.perform_later
