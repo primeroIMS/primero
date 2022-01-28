8c64bfb06c7c3a620987307b206e27a819649a4d
Dennis Hernandez <dhernand@quoininc.com>
2022-01-28 10:35:33 -0600

    R2-1986 - Define the permitted filters in the ManagedReport

--- app/models/managed_report.rb
+++ app/models/managed_report.rb
@@ -2,7 +2,7 @@
 
 # Describes ManagedReport in Primero.
 class ManagedReport < ValueObject
-  attr_accessor :id, :name, :description, :module_id, :subreports, :data, :filter_names
+  attr_accessor :id, :name, :description, :module_id, :subreports, :data, :permitted_filters
 
   REPORTS = {
     Permission::GBV_STATISTICS_REPORT => ManagedReport.new(
@@ -10,15 +10,16 @@ class ManagedReport < ValueObject
       name: 'managed_reports.gbv_statistics.name',
       description: 'managed_reports.gbv_statistics.description',
       subreports: %w[incidents],
-      filter_names: %w[date_of_first_report incident_date],
+      permitted_filters: [date_of_first_report: {}, incident_date: {}],
       module_id: PrimeroModule::GBV
     )
   }.freeze
 
-  def build_report(params = [], subreport_id = nil)
+  def build_report(filters = [], subreport_id = nil)
+    filter_names = permitted_filter_names
     self.data = (filter_subreport(subreport_id)).reduce({}) do |acc, id|
       subreport = "ManagedReports::SubReports::#{id.camelize}".constantize.new
-      subreport.build_report(params.select { |param| filter_names.include?(param.field_name) })
+      subreport.build_report(filters.select { |param| filter_names.include?(param.field_name) })
       acc.merge(subreport.id => subreport.data)
     end
   end
@@ -27,5 +28,9 @@ class ManagedReport < ValueObject
     return subreports if subreport_id.blank?
 
     subreports.select { |subreport| subreport == subreport_id }
+  end
+
+  def permitted_filter_names
+    permitted_filters.map { |filter| filter.is_a?(Hash) ? filter.keys.map(&:to_s) : filter.to_s }.flatten
   end
 end
--- app/models/managed_reports/sql_report_indicator.rb
+++ app/models/managed_reports/sql_report_indicator.rb
@@ -14,28 +14,20 @@ class ManagedReports::SqlReportIndicator < ValueObject
     end
 
     def filter_query(params = [])
-      queries = params.map do |param|
-        "and #{date_range_query(param)}" if param.class == SearchFilters::DateRange
+      params.reduce('') do |acc, param|
+        acc + "and #{date_range_query(param)}" if param.class == SearchFilters::DateRange
       end
-
-      queries.join(' ')
     end
 
-    def date_range_query(param, as_datetime = false)
+    def date_range_query(param)
       ActiveRecord::Base.sanitize_sql_for_conditions(
         [
-          build_date_range_query(as_datetime),
+          "to_timestamp(data ->> ?, 'YYYY-MM-DDTHH\\:\\MI\\:\\SS') between ? and ?",
           param.field_name,
           param.from,
           param.to
         ]
       )
-    end
-
-    def build_date_range_query(as_datetime = false)
-      return "to_date(data ->> ?, 'YYYY-MM-DD') between ? and ?" unless as_datetime
-
-      "to_timestamp(data ->> ?, 'YYYY-MM-DDTHH\\:\\MI\\:\\SS') between ? and ?"
     end
   end
 
