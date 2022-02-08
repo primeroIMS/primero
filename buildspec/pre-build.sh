44253eed76493e753d9b2ca48e7d25cc9eeb81ac
Brad Teeman <bteeman@quoininc.com>
2022-02-08 11:59:38 -0500

    sourcing with .

--- buildspec/pre-build.sh
+++ buildspec/pre-build.sh
@@ -2,6 +2,7 @@
 
 set -eux
 
+touch pre-build-env-vars
 nohup /usr/local/bin/dockerd --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375 --storage-driver=overlay2 &
 timeout 15 sh -c "until docker info; do echo .; sleep 1; done"
 echo Logging in to Amazon ECR...
@@ -9,21 +10,21 @@ aws --version
 aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${CONTAINER_REGISTRY}
 echo "WEBHOOK=${WEBHOOK}"
 if [ "$WEBHOOK" = "true" ]; then BRANCH=$(echo $CODEBUILD_WEBHOOK_HEAD_REF | awk -F/ '{print $NF}'); fi
-echo "export BRANCH=${BRANCH}" >> ~/.bashrc
+echo "export BRANCH=${BRANCH}" >> pre-build-env-vars
 echo "BRANCH=${BRANCH}"
 if [[ "${BRANCH}" =~ "v2." ]]; then
   DEPLOY="false"
-  echo "export DEPLOY=${DEPLOY}" >> ~/.bashrc
+  echo "export DEPLOY=${DEPLOY}" >> pre-build-env-vars
   echo "DEPLOY=${DEPLOY}"
   TAG=${BRANCH}
-  echo "export TAG=${TAG}" >> ~/.bashrc
+  echo "export TAG=${TAG}" >> pre-build-env-vars
   echo "TAG=${TAG}"
 else
   DEPLOY="true"
-  echo "export DEPLOY=${DEPLOY}" >> ~/.bashrc
+  echo "export DEPLOY=${DEPLOY}" >> pre-build-env-vars
   echo "DEPLOY=${DEPLOY}"
   TAG=$(docker/git-to-docker-tag.sh ${BRANCH} ${CODEBUILD_RESOLVED_SOURCE_VERSION})
-  echo "export TAG=${TAG}" >> ~/.bashrc
+  echo "export TAG=${TAG}" >> pre-build-env-vars
   echo "TAG=${TAG}"
   CICD_TAG=$(echo $BRANCH | awk -F. '/release/ { print $1"."$2 } ; $0 !~ /release/ { print $0 }' | tr . -)
   echo "CICD_TAG=${CICD_TAG}"
@@ -31,7 +32,7 @@ else
   echo "RELEASE_SECRET_KEY=${RELEASE_SECRET_KEY}"
   echo "CODEBUILD_SRC_DIR_devops=${CODEBUILD_SRC_DIR_devops}"
   DEPLOY_SERVER_NAME="primero-integration-${CICD_TAG}"
-  echo "export DEPLOY_SERVER_NAME=${DEPLOY_SERVER_NAME}" >> ~/.bashrc
+  echo "export DEPLOY_SERVER_NAME=${DEPLOY_SERVER_NAME}" >> pre-build-env-vars
   echo "DEPLOY_SERVER_NAME=${DEPLOY_SERVER_NAME}"
   DEPLOY_SERVER_INVENTORY_FILE="primero-integration-${CICD_TAG}.vars.yml"
   echo "DEPLOY_SERVER_INVENTORY_FILE=${DEPLOY_SERVER_INVENTORY_FILE}"
