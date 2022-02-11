8b3728db70c24e74158901719a259ee487863345
Alberto Espinoza <aespinoz@quoininc.com>
2022-02-11 16:13:41 -0600

    R2-1973 R2-1991 - Fixing rspec

--- spec/models/managed_reports/indicators/attack_type_spec.rb
+++ spec/models/managed_reports/indicators/attack_type_spec.rb
@@ -8,11 +8,39 @@ describe ManagedReports::Indicators::AttackType do
 
     incident = Incident.create!(data: { incident_date: Date.today, status: 'open' })
 
-    Violation.create!(data: { type: 'killing', attack_type: 'aerial_attack' }, incident_id: incident.id)
-    Violation.create!(data: { type: 'maiming', attack_type: 'aerial_attack' }, incident_id: incident.id)
-    Violation.create!(data: { type: 'killing', attack_type: 'arson' }, incident_id: incident.id)
-    Violation.create!(data: { type: 'killing', attack_type: 'arson' }, incident_id: incident.id)
-    Violation.create!(data: { attack_type: 'other' }, incident_id: incident.id)
+    Violation.create!(
+      data: {
+        type: 'killing',
+        attack_type: 'aerial_attack',
+        violation_tally: { 'boys': 1, 'girls': 1, 'unknown': 1, 'total': 3 }
+      },
+      incident_id: incident.id
+    )
+    Violation.create!(
+      data: {
+        type: 'maiming', attack_type: 'aerial_attack',
+        violation_tally: { 'boys': 3, 'girls': 2, 'unknown': 1, 'total': 6 }
+      },
+      incident_id: incident.id
+    )
+    Violation.create!(
+      data: {
+        type: 'killing', attack_type: 'arson', violation_tally: { 'boys': 1, 'girls': 1, 'unknown': 1, 'total': 3 }
+      },
+      incident_id: incident.id
+    )
+    Violation.create!(
+      data: {
+        type: 'killing', attack_type: 'arson', violation_tally: { 'boys': 1, 'girls': 1, 'unknown': 0, 'total': 2 }
+      },
+      incident_id: incident.id
+    )
+    Violation.create!(
+      data: {
+        attack_type: 'other', violation_tally: { 'boys': 5, 'girls': 10, 'unknown': 5, 'total': 20 }
+      },
+      incident_id: incident.id
+    )
   end
 
   it 'returns data for attack type indicator' do
@@ -23,8 +51,8 @@ describe ManagedReports::Indicators::AttackType do
 
     expect(attack_type_data).to match_array(
       [
-        { 'id' => 'aerial_attack', 'total' => 1 },
-        { 'id' => 'arson', 'total' => 2 }
+        { boys: 1, girls: 1, id: 'aerial_attack', total: 3, unknown: 1 },
+        { boys: 2, girls: 2, id: 'arson', total: 5, unknown: 1 }
       ]
     )
   end
