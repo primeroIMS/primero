01e03ba293834f7a229f8093b3ee5a8d0c1b672c
Ron Elledge <relledge@quoininc.com>
2022-02-24 11:30:39 -0500

    BCAL-55 fixed lint issue in associated-form-sections.js

--- app/javascript/components/pages/admin/roles-form/forms/associated-form-sections.js
+++ app/javascript/components/pages/admin/roles-form/forms/associated-form-sections.js
@@ -65,23 +65,20 @@ const buildFields = (recordType, formsByParentForm, i18n) => {
 };
 
 export default (formSections, i18n) =>
-  [
-    RECORD_TYPES.cases,
-    RECORD_TYPES.tracing_requests,
-    RECORD_TYPES.incidents,
-    RECORD_TYPES.registry_records
-  ].map(recordType => {
-    const uniqueID = `associated_form_sections_${recordType}`;
+  [RECORD_TYPES.cases, RECORD_TYPES.tracing_requests, RECORD_TYPES.incidents, RECORD_TYPES.registry_records].map(
+    recordType => {
+      const uniqueID = `associated_form_sections_${recordType}`;
 
-    return FormSectionRecord({
-      unique_id: uniqueID,
-      name: i18n.t(`permissions.resource.forms.${recordType}.label`),
-      tooltip: i18n.t(`permissions.resource.forms.${recordType}.explanation`),
-      expandable: true,
-      expanded: true,
-      fields: [
-        buildHeader(uniqueID, i18n),
-        ...buildFields(recordType, formSections.get(recordType, fromJS({})).valueSeq(), i18n)
-      ]
-    });
-  });
+      return FormSectionRecord({
+        unique_id: uniqueID,
+        name: i18n.t(`permissions.resource.forms.${recordType}.label`),
+        tooltip: i18n.t(`permissions.resource.forms.${recordType}.explanation`),
+        expandable: true,
+        expanded: true,
+        fields: [
+          buildHeader(uniqueID, i18n),
+          ...buildFields(recordType, formSections.get(recordType, fromJS({})).valueSeq(), i18n)
+        ]
+      });
+    }
+  );
