e581328693fa8d2905272cee0741489fd0c85c43
Ron Elledge <relledge@quoininc.com>
2022-02-24 19:06:52 -0500

    BCAL-53 added shell of csv_record_importer_spec

---
+++ spec/models/importers/csv_record_importer_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+module Importers
+  describe CsvRecordImporter do
+    before do
+      clean_data(RegistryRecord)
+    end
+
+    context 'when input file exists' do
+      #TODO
+    end
+  end
+end
