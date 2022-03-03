0d8dcb500a8b597286e6cfc3de4aa6eaf067ed4b
Joshua Toliver <jtoliver@quoininc.com>
2022-03-03 15:57:51 -0500

    Adding disabled record indicator on case registry details panel

--- app/javascript/components/record-form/form/components/case-registry/components/result-details.js
+++ app/javascript/components/record-form/form/components/case-registry/components/result-details.js
@@ -10,6 +10,7 @@ import css from "../../../subforms/styles.css";
 import { LINK_FIELD, REGISTRY_DETAILS } from "../constants";
 import { fetchRecord, selectRecord } from "../../../../../records";
 import Form, { FORM_MODE_SHOW } from "../../../../../form";
+import DisabledRecordIndicator from "../../disabled-record-indicator";
 
 const ResultDetails = ({
   id,
@@ -61,6 +62,7 @@ const ResultDetails = ({
 
   return (
     <>
+      {!record.get("enabled") && <DisabledRecordIndicator recordType={REGISTRY_RECORD} />}
       <div className={css.subformFieldArrayContainer}>
         <ActionButton type={ACTION_BUTTON_TYPES.default} text={backButtonText} rest={{ onClick: backButtonFunc }} />
         {permissions.writeRegistryRecord && !mode.isShow && (
