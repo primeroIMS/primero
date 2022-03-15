b67718aee4a68cdd74659965ab9c8316d2aa7674
Joshua Toliver <jtoliver@quoininc.com>
2022-03-15 18:15:43 -0400

    Fixing 1 letter var

--- app/javascript/components/record-actions/utils/build-selected-ids.js
+++ app/javascript/components/record-actions/utils/build-selected-ids.js
@@ -2,6 +2,6 @@ export default (selectedRecords, records, currentPage, sendKey = "id") =>
   selectedRecords && records
     ? records
         .toJS()
-        .filter((_r, i) => selectedRecords[currentPage]?.includes(i))
-        .map(r => r[sendKey])
+        .filter((_record, index) => selectedRecords[currentPage]?.includes(index))
+        .map(record => record[sendKey])
     : [];
