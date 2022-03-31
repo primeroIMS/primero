caca584598a46f533af0e0408e7c908c1933d1e9
Joshua Toliver <jtoliver@quoininc.com>
2022-03-31 14:35:13 -0400

    R2-2041: Reverting useapp hook change that broke offline detection

--- app/javascript/components/application/use-app.jsx
+++ app/javascript/components/application/use-app.jsx
@@ -3,15 +3,21 @@ import PropTypes from "prop-types";
 import { isEqual } from "lodash";
 
 import { useMemoizedSelector } from "../../libs";
+import useConnectivityStatus from "../connectivity/use-connectivity-status";
 
 import { getAppData } from "./selectors";
 
 const Context = createContext();
 
 const ApplicationProvider = ({ children }) => {
+  const { online } = useConnectivityStatus();
   const appData = useMemoizedSelector(state => getAppData(state), isEqual);
 
-  return <Context.Provider value={appData}>{children}</Context.Provider>;
+  const value = { ...appData, online };
+
+  console.log('======================')
+
+  return <Context.Provider value={value}>{children}</Context.Provider>;
 };
 
 ApplicationProvider.displayName = "ApplicationProvider";
