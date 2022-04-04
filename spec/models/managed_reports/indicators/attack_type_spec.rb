44abb21f83c51065e358777f9a44624c8a6ef8ad
Alberto Espinoza <aespinoz@quoininc.com>
2022-04-04 16:59:40 -0600

    MRM - Grouping
        - Adding rspec for indicators

--- spec/models/managed_reports/indicators/attack_type_spec.rb
+++ spec/models/managed_reports/indicators/attack_type_spec.rb
@@ -84,15 +84,15 @@ describe ManagedReports::Indicators::AttackType do
       role: all_role
     )
 
-    incident1 = Incident.new_with_user(@self_user, { incident_date: Date.today, status: 'open' })
+    incident1 = Incident.new_with_user(@self_user, { incident_date: Date.new(2020, 8, 8), status: 'open' })
     incident1.save!
-    incident2 = Incident.new_with_user(@group_user, { incident_date: Date.today, status: 'open' })
+    incident2 = Incident.new_with_user(@group_user, { incident_date: Date.new(2021, 8, 8), status: 'open' })
     incident2.save!
-    incident3 = Incident.new_with_user(@agency_user, { incident_date: Date.today, status: 'open' })
+    incident3 = Incident.new_with_user(@agency_user, { incident_date: Date.new(2022, 1, 8), status: 'open' })
     incident3.save!
-    incident4 = Incident.new_with_user(@all_user, { incident_date: Date.today, status: 'open' })
+    incident4 = Incident.new_with_user(@all_user, { incident_date: Date.new(2022, 2, 18), status: 'open' })
     incident4.save!
-    incident5 = Incident.new_with_user(@all_user, { incident_date: Date.today, status: 'open' })
+    incident5 = Incident.new_with_user(@all_user, { incident_date: Date.new(2022, 3, 28), status: 'open' })
     incident5.save!
 
     Violation.create!(
@@ -213,6 +213,117 @@ describe ManagedReports::Indicators::AttackType do
           { boys: 5, girls: 10, id: 'other', unknown: 5, total: 20 }
         ]
       )
+    end
+  end
+
+  describe 'grouped by' do
+    context 'when is year' do
+      it 'should return results grouped by year' do
+        data = ManagedReports::Indicators::AttackType.build(
+          nil,
+          {
+            'grouped_by' => SearchFilters::Value.new(field_name: 'grouped_by', value: 'year'),
+            'incident_date' => SearchFilters::DateRange.new(
+              field_name: 'incident_date',
+              from: '2020-08-01',
+              to: '2022-10-10'
+            ),
+            'type' => SearchFilters::Value.new(field_name: 'type', value: 'killing')
+          }
+        ).data
+
+        expect(data).to match_array(
+          [
+            {
+              group_id: 2020,
+              data: [
+                { id: 'aerial_attack', total: 3, girls: 1, unknown: 1, boys: 1 }
+              ]
+            },
+            {
+              group_id: 2021,
+              data: [
+                { id: 'other', girls: 10, boys: 5, unknown: 5, total: 20 }
+              ]
+            },
+            {
+              group_id: 2022,
+              data: [
+                { id: 'arson', total: 5, boys: 2, girls: 2, unknown: 1 },
+                { id: 'aerial_attack', unknown: 0, boys: 2, girls: 1, total: 3 }
+              ]
+            }
+          ]
+        )
+      end
+    end
+
+    context 'when is month' do
+      it 'should return results grouped by month' do
+        data = ManagedReports::Indicators::AttackType.build(
+          nil,
+          {
+            'grouped_by' => SearchFilters::Value.new(field_name: 'grouped_by', value: 'month'),
+            'incident_date' => SearchFilters::DateRange.new(
+              field_name: 'incident_date',
+              from: '2020-08-01',
+              to: '2022-10-10'
+            ),
+            'type' => SearchFilters::Value.new(field_name: 'type', value: 'killing')
+          }
+        ).data
+
+        expect(data).to match_array(
+          [
+            { group_id: 'august-2020', data: [{ id: 'aerial_attack', total: 3, boys: 1, unknown: 1, girls: 1 }] },
+            { group_id: 'august-2021', data: [{ id: 'other', total: 20, girls: 10, unknown: 5, boys: 5 }] },
+            { group_id: 'january-2022', data: [{ id: 'arson', total: 3, girls: 1, boys: 1, unknown: 1 }] },
+            { group_id: 'february-2022', data: [{ id: 'arson', boys: 1, unknown: 0, total: 2, girls: 1 }] },
+            { group_id: 'march-2022', data: [{ id: 'aerial_attack', unknown: 0, boys: 2, girls: 1, total: 3 }] }
+          ]
+        )
+      end
+    end
+
+    context 'when is quarter' do
+      it 'should return results grouped by quarter' do
+        data = ManagedReports::Indicators::AttackType.build(
+          nil,
+          {
+            'grouped_by' => SearchFilters::Value.new(field_name: 'grouped_by', value: 'quarter'),
+            'incident_date' => SearchFilters::DateRange.new(
+              field_name: 'incident_date',
+              from: '2020-08-01',
+              to: '2022-10-10'
+            ),
+            'type' => SearchFilters::Value.new(field_name: 'type', value: 'killing')
+          }
+        ).data
+
+        expect(data).to match_array(
+          [
+            {
+              group_id: 'q3-2020',
+              data: [
+                { id: 'aerial_attack', total: 3, girls: 1, unknown: 1, boys: 1 }
+              ]
+            },
+            {
+              group_id: 'q3-2021',
+              data: [
+                { id: 'other', girls: 10, total: 20, boys: 5, unknown: 5 }
+              ]
+            },
+            {
+              group_id: 'q1-2022',
+              data: [
+                { id: 'arson', boys: 2, total: 5, girls: 2, unknown: 1 },
+                { id: 'aerial_attack', unknown: 0, boys: 2, total: 3, girls: 1 }
+              ]
+            }
+          ]
+        )
+      end
     end
   end
 end
--- spec/models/managed_reports/indicators/denial_type_spec.rb
+++ spec/models/managed_reports/indicators/denial_type_spec.rb
@@ -6,7 +6,8 @@ describe ManagedReports::Indicators::DenialType do
   before do
     clean_data(Incident, Violation)
 
-    incident = Incident.create!(data: { incident_date: Date.today, status: 'open' })
+    incident = Incident.create!(data: { incident_date: Date.new(2021, 5, 23), status: 'open' })
+    incident1 = Incident.create!(data: { incident_date: Date.new(2022, 4, 4), status: 'open' })
     Violation.create!(
       data: { type: 'denial_humanitarian_access', denial_method: %w[abduction_of_humanitarian_personnel other] },
       incident_id: incident.id
@@ -31,6 +32,19 @@ describe ManagedReports::Indicators::DenialType do
       data: { type: 'maiming', violation_tally: { 'boys': 2, 'girls': 3, 'unknown': 2, 'total': 7 } },
       incident_id: incident.id
     )
+
+    Violation.create!(
+      data: {
+        type: 'denial_humanitarian_access',
+        denial_method: %w[vehicle_hijacking property_damage]
+      },
+      incident_id: incident1.id
+    )
+
+    Violation.create!(
+      data: { type: 'denial_humanitarian_access', denial_method: ['property_damage'] },
+      incident_id: incident1.id
+    )
   end
 
   it 'return data for denial_method indicator' do
@@ -45,8 +59,124 @@ describe ManagedReports::Indicators::DenialType do
         { id: 'besiegement', total: 2 },
         { id: 'abduction_of_humanitarian_personnel', total: 1 },
         { id: 'restrictions_of_beneficiaries_access', total: 1 },
+        { id: 'property_damage', total: 2 },
+        { id: 'vehicle_hijacking', total: 1 },
         { id: 'other', total: 1 }
       ]
     )
+  end
+
+  describe 'grouped by' do
+    context 'when is year' do
+      it 'should return results grouped by year' do
+        data = ManagedReports::Indicators::DenialType.build(
+          nil,
+          {
+            'grouped_by' => SearchFilters::Value.new(field_name: 'grouped_by', value: 'year'),
+            'incident_date' => SearchFilters::DateRange.new(
+              field_name: 'incident_date',
+              from: '2021-04-01',
+              to: '2022-05-01'
+            ),
+            'type' => SearchFilters::Value.new(field_name: 'type', value: 'denial_humanitarian_access')
+          }
+        ).data
+
+        expect(data).to match_array(
+          [
+            {
+              group_id: 2021,
+              data: [
+                { id: 'other', total: 1 },
+                { id: 'besiegement', total: 2 },
+                { id: 'abduction_of_humanitarian_personnel', total: 1 },
+                { id: 'restrictions_of_beneficiaries_access', total: 1 },
+                { id: 'theft', total: 1 }
+              ]
+            },
+            {
+              group_id: 2022,
+              data: [
+                { id: 'vehicle_hijacking', total: 1 },
+                { id: 'property_damage', total: 2 }
+              ]
+            }
+          ]
+        )
+      end
+    end
+
+    context 'when is month' do
+      it 'should return results grouped by month' do
+        data = ManagedReports::Indicators::DenialType.build(
+          nil,
+          {
+            'grouped_by' => SearchFilters::Value.new(field_name: 'grouped_by', value: 'month'),
+            'incident_date' => SearchFilters::DateRange.new(
+              field_name: 'incident_date',
+              from: '2020-08-01',
+              to: '2022-10-10'
+            ),
+            'type' => SearchFilters::Value.new(field_name: 'type', value: 'denial_humanitarian_access')
+          }
+        ).data
+
+        expect(data).to match_array(
+          [
+            {
+              group_id: 'may-2021',
+              data: [
+                { id: 'besiegement', total: 2 },
+                { id: 'restrictions_of_beneficiaries_access', total: 1 },
+                { id: 'theft', total: 1 },
+                { id: 'other', total: 1 },
+                { id: 'abduction_of_humanitarian_personnel', total: 1 }
+              ]
+            },
+            {
+              group_id: 'april-2022',
+              data: [
+                { id: 'property_damage', total: 2 },
+                { id: 'vehicle_hijacking', total: 1 }
+              ]
+            }
+          ]
+        )
+      end
+    end
+
+    context 'when is quarter' do
+      it 'should return results grouped by quarter' do
+        data = ManagedReports::Indicators::DenialType.build(
+          nil,
+          {
+            'grouped_by' => SearchFilters::Value.new(field_name: 'grouped_by', value: 'quarter'),
+            'incident_date' => SearchFilters::DateRange.new(
+              field_name: 'incident_date',
+              from: '2020-08-01',
+              to: '2022-10-10'
+            ),
+            'type' => SearchFilters::Value.new(field_name: 'type', value: 'denial_humanitarian_access')
+          }
+        ).data
+
+        expect(data).to match_array(
+          [
+            {
+              group_id: 'q2-2021',
+              data: [{ id: 'restrictions_of_beneficiaries_access', total: 1 },
+                     { id: 'besiegement', total: 2 },
+                     { id: 'theft', total: 1 },
+                     { id: 'other', total: 1 },
+                     { id: 'abduction_of_humanitarian_personnel', total: 1 }]
+            },
+            {
+              group_id: 'q2-2022',
+              data: [{ id: 'property_damage', total: 2 }, { id: 'vehicle_hijacking', total: 1 }]
+            }
+          ]
+        )
+      end
+    end
   end
 end
