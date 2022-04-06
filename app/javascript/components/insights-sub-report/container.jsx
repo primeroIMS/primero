d46b554e87e2a3d1fa5ddaece9ac5043da1f3bc6
Joshua Toliver <jtoliver@quoininc.com>
2022-04-06 18:53:22 -0400

    R2-2050: Moving insights filters to the parent container

--- app/javascript/components/insights-sub-report/container.jsx
+++ app/javascript/components/insights-sub-report/container.jsx
@@ -15,7 +15,6 @@ import TableValues from "../charts/table-values";
 import BarChartGraphic from "../charts/bar-chart";
 import useOptions from "../form/use-options";
 import { CHART_COLORS } from "../../config/constants";
-import InsightsFilters from "../insights-filters";
 
 import { getInsight } from "./selectors";
 import namespace from "./namespace";
@@ -23,7 +22,7 @@ import { NAME } from "./constants";
 import css from "./styles.css";
 
 const Component = () => {
-  const { id, subReport, moduleID } = useParams();
+  const { id, subReport } = useParams();
   const i18n = useI18n();
   const dispatch = useDispatch();
 
@@ -130,7 +129,6 @@ const Component = () => {
 
   return (
     <>
-      <InsightsFilters moduleID={moduleID} id={id} subReport={subReport} />
       <LoadingIndicator {...loadingIndicatorProps}>
         <div className={css.subReportContent}>
           <div className={css.subReportTables}>
