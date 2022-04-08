8fed3cacc200ab439bd30a2989aba70e3c94610f
Alberto Espinoza <aespinoz@quoininc.com>
2022-04-08 12:58:29 -0600

    R2-2056 Verification Status field in MRM UI
    - ctfmr_verified field should be used for the Verification Status in List View filters, Insights, Reports, and on the Incident collapsed subform display.

--- app/models/managed_reports/indicators/perpetrators_denials.rb
+++ app/models/managed_reports/indicators/perpetrators_denials.rb
@@ -26,7 +26,6 @@ class ManagedReports::Indicators::PerpetratorsDenials < ManagedReports::SqlRepor
           #{date_range_query(params['date_of_first_report'], 'incidents')&.prepend('and ')}
           #{date_range_query(params['ctfmr_verified_date'], 'violations')&.prepend('and ')}
           #{equal_value_query(params['ctfmr_verified'], 'violations')&.prepend('and ')}
-          #{equal_value_query(params['verified_ctfmr_technical'], 'violations')&.prepend('and ')}
           #{equal_value_query(params['type'], 'violations')&.prepend('and ')}
         group by p."data"->>'armed_force_group_party_name'
       }
