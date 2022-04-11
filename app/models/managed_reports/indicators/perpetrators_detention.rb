130aa1f24c917fd694d7660e8b76755e286f77f7
Alberto Espinoza <aespinoz@quoininc.com>
2022-04-11 12:14:32 -0600

    R2-1993 - Fixing sql where

--- app/models/managed_reports/indicators/perpetrators_detention.rb
+++ app/models/managed_reports/indicators/perpetrators_detention.rb
@@ -22,7 +22,7 @@ class ManagedReports::Indicators::PerpetratorsDetention < ManagedReports::SqlRep
           on incidents.id = violations.incident_id
           #{user_scope_query(current_user, 'incidents')&.prepend('and ')}
         WHERE p.data->>'armed_force_group_party_name' is not null
-        and (iv.data->>'victim_deprived_liberty_security_reasons')::boolean
+        and (iv.data->>'victim_deprived_liberty_security_reasons') = 'true'
         #{date_range_query(params['incident_date'], 'incidents')&.prepend('and ')}
         #{date_range_query(params['date_of_first_report'], 'incidents')&.prepend('and ')}
         #{date_range_query(params['ctfmr_verified_date'], 'incidents')&.prepend('and ')}
