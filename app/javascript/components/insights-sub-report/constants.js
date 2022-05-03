d96a1e0e01408bf872ce5082b88b3f6b2440ab0e
Alberto Espinoza <aespinoz@quoininc.com>
2022-05-03 17:17:33 -0600

    R2-2083 - adding support for MRM export

--- app/javascript/components/insights-sub-report/constants.js
+++ app/javascript/components/insights-sub-report/constants.js
@@ -1,14 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export const NAME = "InsightsSubReport";
 export const COMBINED_INDICATORS = {
-  incidents: ["total", "gbv_sexual_violence", "gbv_previous_incidents"],
-  killing: ["violation"],
-  maiming: ["violation"],
-  recruitment: ["violation"],
-  sexual_violence: ["violation"],
-  abduction: ["violation"],
-  military_use: ["violation"],
-  denial_humanitarian_access: ["violation"],
-  detention: ["violation"]
+  incidents: ["total", "gbv_sexual_violence", "gbv_previous_incidents"]
 };
 export const GROUPED_BY_FILTER = "grouped_by";
