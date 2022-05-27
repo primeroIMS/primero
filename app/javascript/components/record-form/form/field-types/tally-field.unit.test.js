f4820aa4cafda4b17d71463d2cefb25e57028549
Alberto Espinoza <aespinoz@quoininc.com>
2022-05-27 15:22:50 -0600

    R2-2134 - MRM - Tally field total column should not be editable

--- app/javascript/components/record-form/form/field-types/tally-field.unit.test.js
+++ app/javascript/components/record-form/form/field-types/tally-field.unit.test.js
@@ -48,4 +48,31 @@ describe("<FormSectionField />", () => {
   it("render a TextFields", () => {
     expect(component.find(TextField)).lengthOf(2);
   });
+
+  context("when autosum_total is true", () => {
+    const newProps = {
+      ...props,
+      field: {
+        type: TALLY_FIELD_NAME,
+        display_name: { en: "Test2" },
+        helperText: "This is a help text 2",
+        disabled: false,
+        autosum_total: true,
+        tally: [
+          { id: "test1", display_text: { en: "Test 1" } },
+          { id: "test2", display_text: { en: "Test 2" } }
+        ]
+      }
+    };
+    const tallyFieldComponent = setupMountedComponent(TallyField, newProps, {}, [], formProps).component;
+
+    it("render 3 TextFields", () => {
+      expect(tallyFieldComponent.find(TextField)).lengthOf(3);
+    });
+
+    it("render Total as a disabled TextField", () => {
+      expect(tallyFieldComponent.find(TextField).last().find("label").text()).to.be.equal("fields.total");
+      expect(tallyFieldComponent.find(TextField).last().props().disabled).to.be.true;
+    });
+  });
 });
