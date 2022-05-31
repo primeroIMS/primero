51f5f51c57a8d9bffb2458bbcf5c15ec46848cc8
Alberto Espinoza <aespinoz@quoininc.com>
2022-05-31 17:05:08 -0600

    R2-2133 - MRM - Make display text for reporting location values the lowest-level name

---
+++ app/javascript/components/insights-sub-report/utils/get-lookup-value.unit.test.js
@@ -0,0 +1,69 @@
+import { fromJS } from "immutable";
+
+import getLookupValue from "./get-lookup-value";
+
+describe("<InsightsSubReport />/utils/getLookupValue", () => {
+  const translateId = value => `return.sample.${value}`;
+
+  const lookups = {
+    reporting_location: [
+      {
+        id: "MC",
+        display_text: "MyCountry"
+      },
+      {
+        id: "MCMP1",
+        display_text: "MyProvince1"
+      },
+      {
+        id: "MCMP2",
+        display_text: "MyProvince2"
+      },
+      {
+        id: "MCMP1MD1",
+        display_text: "MyDistrict1"
+      },
+      {
+        id: "MCMP2MD2",
+        display_text: "MyDistrict2"
+      }
+    ],
+    lookup_sample: [
+      {
+        id: "lookup_sample_1",
+        display_text: "Lookup Sample 1"
+      },
+      {
+        id: "lookup_sample_2",
+        display_text: "Lookup Sample 2"
+      }
+    ]
+  };
+
+  context("when is lookup", () => {
+    it("returns value translated", () => {
+      const value = fromJS({ id: "lookup_sample_1", boys: 1, total: 2, unknown: 1 });
+      const result = getLookupValue(lookups, translateId, "lookup_sample", value);
+
+      expect(result).to.equal("Lookup Sample 1");
+    });
+  });
+
+  context("when is reporting_location", () => {
+    it("returns Location translated", () => {
+      const value = fromJS({ id: "MCMP1MD1", boys: 1, total: 2, unknown: 1 });
+      const columns = getLookupValue(lookups, translateId, "reporting_location", value);
+
+      expect(columns).to.equal("MyDistrict1");
+    });
+  });
+
+  context("when there is NO lookup", () => {
+    it("returns value translated", () => {
+      const value = fromJS({ id: "test", total: 2 });
+      const result = getLookupValue({}, translateId, "random_lookup", value);
+
+      expect(result).to.equal("return.sample.test");
+    });
+  });
+});
