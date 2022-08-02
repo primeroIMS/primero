77371178d6eab520b09ca3c932b2ae09cd5eaa1c
Dennis Hernandez <dhernand@quoininc.com>
2022-08-02 15:09:48 -0600

    Back button was not working for date parameters
    When date parameters were passed to the url they were
    not converted correctly.

--- app/javascript/components/record-list/utils/filters-to-query-string.unit.test.js
+++ app/javascript/components/record-list/utils/filters-to-query-string.unit.test.js
@@ -12,4 +12,10 @@ describe("<RecordList />/utils - filtersToQueryString", () => {
   it("returns a query string with a value for each element in the array of the filter", () => {
     expect(filtersToQueryString(fromJS({ status: ["open", "closed"] }))).to.equals("status[0]=open&status[1]=closed");
   });
+
+  it("returns a query string with a value for each element in the map of the filter", () => {
+    expect(filtersToQueryString(fromJS({ date: { from: "2010-01-05", to: "2010-01-08" } }))).to.equals(
+      "date[from]=2010-01-05&date[to]=2010-01-08"
+    );
+  });
 });
