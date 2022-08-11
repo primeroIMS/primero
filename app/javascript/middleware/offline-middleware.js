6d2510456c259fead4222f9da203edf80e9c97b0
Joshua Toliver <jtoliver@quoininc.com>
2022-08-11 16:52:19 -0400

    Fixing broken unit test

--- app/javascript/middleware/offline-middleware.js
+++ app/javascript/middleware/offline-middleware.js
@@ -1,14 +1,14 @@
 import { getServerStatus } from "../components/connectivity/action-creators";
 import { METHODS } from "../config";
 
-import { isOnline, isServerOnline, retrieveData, queueData, queueFetch, userToggleOffline } from "./utils";
+import { isOnline, isServerOnline, userToggleOffline, retrieveData, queueData, queueFetch } from "./utils";
 
 const offlineMiddleware = store => next => action => {
   const online = isOnline(store);
   const serverOnline = isServerOnline(store);
   const userToggledOffline = userToggleOffline(store);
 
-  if (!action?.api?.path || (online && serverOnline) || !userToggledOffline) {
+  if (!action?.api?.path || (online && serverOnline && !userToggledOffline)) {
     return next(action);
   }
 
