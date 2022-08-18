93d4f5fa6e3cf12fd74b1bf27bc6574460bf1da1
Dennis Hernandez <dhernand@quoininc.com>
2022-08-18 09:53:04 -0600

    BCAL-173 - Refactor: Create component for sortable columns

--- app/javascript/components/record-list/components/sort-container/component.jsx
+++ app/javascript/components/record-list/components/sort-container/component.jsx
@@ -1,19 +1,19 @@
-import { Drawer, FormControlLabel, Radio, RadioGroup } from "@material-ui/core";
+import { useCallback } from "react";
+import { Drawer } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 import { fromJS } from "immutable";
 import PropTypes from "prop-types";
-import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
-import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 
 import { useMemoizedSelector } from "../../../../libs";
 import { useI18n } from "../../../i18n";
 import { getFilters } from "../../../index-table";
 
+import SortableColumns from "./sortable-columns";
 import css from "./styles.css";
 
 const EXCLUDED_COLUMNS = Object.freeze(["case_id_display"]);
 
-const SortContainer = ({ columns, open, onClose, recordType, applyFilters }) => {
+function SortContainer({ columns, open, onClose, recordType, applyFilters }) {
   const filters = useMemoizedSelector(state => getFilters(state, recordType));
   const dispatch = useDispatch();
   const i18n = useI18n();
@@ -28,57 +28,27 @@ const SortContainer = ({ columns, open, onClose, recordType, applyFilters }) => 
         !EXCLUDED_COLUMNS.includes(column.name)
     ) || fromJS([]);
 
-  const onChange = data => {
-    const { value } = data.target;
-    const orderBy = value.replace("_asc", "").replace("_desc", "");
-    const order = value.replace(`${orderBy}_`, "");
+  const onChange = useCallback(
+    data => {
+      const { value } = data.target;
+      const orderBy = value.replace("_asc", "").replace("_desc", "");
+      const order = value.replace(`${orderBy}_`, "");
 
-    onClose();
-    dispatch(applyFilters({ recordType, data: filters.merge(fromJS({ order, order_by: orderBy })) }));
-  };
+      onClose();
+      dispatch(applyFilters({ recordType, data: filters.merge(fromJS({ order, order_by: orderBy })) }));
+    },
+    [open, recordType, filters]
+  );
 
   return (
     <Drawer anchor="right" open={open} onClose={onClose} onChange={onChange}>
       <div className={css.sortContainer}>
-        <h3 className={css.sortByTitle}>Sort by</h3>
-        <RadioGroup
-          defaultValue={`${filters.get("order_by")}_${filters.get("order")}`}
-          aria-label="gender"
-          name="order_by"
-        >
-          {sortableColumns.map(column => (
-            <>
-              <div className={css.sortableColumn}>
-                <div className={css.sortField}>
-                  <FormControlLabel
-                    value={`${column.name}_asc`}
-                    control={<Radio />}
-                    label={column.label?.trim() || i18n.t(`${recordType}.${column.name}`)}
-                  />
-                </div>
-                <div className={css.sortDir}>
-                  <ArrowUpwardIcon />
-                </div>
-              </div>
-              <div className={css.sortableColumn}>
-                <div className={css.sortField}>
-                  <FormControlLabel
-                    value={`${column.name}_desc`}
-                    control={<Radio />}
-                    label={column.label?.trim() || i18n.t(`${recordType}.${column.name}`)}
-                  />
-                </div>
-                <div className={css.sortDir}>
-                  <ArrowDownwardIcon />
-                </div>
-              </div>
-            </>
-          ))}
-        </RadioGroup>
+        <h3 className={css.sortByTitle}>{i18n.t(`${recordType}.sort_by`)}</h3>
+        <SortableColumns filters={filters} sortableColumns={sortableColumns} recordType={recordType} />
       </div>
     </Drawer>
   );
-};
+}
 
 SortContainer.displayName = "SortContainer";
 
