7493de791a7e38bc6e1e057a7bf2927559965100
Dennis Hernandez <dhernand@quoininc.com>
2022-09-12 16:03:25 -0600

    R2-2301 - Refactor to allow not_null conditions

--- app/javascript/components/pages/admin/form-builder/utils/conditions-to-field-array.js
+++ app/javascript/components/pages/admin/form-builder/utils/conditions-to-field-array.js
@@ -1,5 +1,20 @@
 import first from "lodash/first";
 
+import { COMPARISON_OPERATORS, LOGICAL_OPERATORS } from "../../../../../libs/expressions/constants";
+import { NOT_NULL } from "../../../../reports-form/constants";
+
+const isNotNullCondition = condition => {
+  const [constraint, current] = first(Object.entries(condition));
+  const [operator, data] = first(Object.entries(current));
+  const [, value] = first(Object.entries(data));
+
+  if (constraint === LOGICAL_OPERATORS.NOT && operator === COMPARISON_OPERATORS.EQ && value === "") {
+    return true;
+  }
+
+  return false;
+};
+
 export default conditions => {
   if (!conditions || !Object.keys(conditions).length) {
     return [];
@@ -9,6 +24,12 @@ export default conditions => {
     return conditions.and.map(condition => {
       const [constraint, current] = first(Object.entries(condition));
       const [attribute, value] = first(Object.entries(current));
+
+      if (isNotNullCondition(condition)) {
+        const [key] = first(Object.entries(value));
+
+        return { constraint: NOT_NULL, attribute: key };
+      }
 
       return { constraint, attribute, value };
     });
--- app/javascript/components/pages/admin/form-builder/utils/field-array-to-conditions.js
+++ app/javascript/components/pages/admin/form-builder/utils/field-array-to-conditions.js
@@ -1,6 +1,33 @@
+import { COMPARISON_OPERATORS } from "../../../../../libs/expressions/constants";
+import { isOperator } from "../../../../../libs/expressions/utils";
+import { isNotNullConstraint } from "../components/condition-dialog/utils";
+
+const getConstraint = condition => {
+  const { attribute, constraint, value } = condition;
+
+  if (isNotNullConstraint(constraint)) {
+    return { not: { eq: { [attribute]: "" } } };
+  }
+
+  if (Array.isArray(value) && value.length > 1) {
+    return COMPARISON_OPERATORS.IN;
+  }
+
+  return COMPARISON_OPERATORS.EQ;
+};
+
 export default conditionArray =>
   conditionArray.reduce(
-    (acc, elem) => ({ and: [...acc.and, { [elem.constraint]: { [elem.attribute]: elem.value } }] }),
+    (acc, elem) => {
+      const constraint = getConstraint(elem);
+
+      return {
+        and: [
+          ...acc.and,
+          isOperator(constraint) ? { [getConstraint(elem)]: { [elem.attribute]: elem.value } } : constraint
+        ]
+      };
+    },
     {
       and: []
     }
