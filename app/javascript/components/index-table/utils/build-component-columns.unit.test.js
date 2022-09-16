fe4f130d4e8834ad43b79ffc350e76e16c5f92e9
Dennis Hernandez <dhernand@quoininc.com>
2022-09-16 10:46:46 -0600

    R2-2217 - Fix lookup sort still broken
    The utils file was removed and splitted into several
    files under the utils directory. The change was applied
    in the new file: build-component-columns.js

---
+++ app/javascript/components/index-table/utils/build-component-columns.unit.test.js
@@ -0,0 +1,45 @@
+import { fromJS, List } from "immutable";
+
+import buildComponentColumns from "./build-component-columns";
+
+describe("buildComponentColumns", () => {
+  it("generates a sorted column with all its options for List", () => {
+    const customBodyRender = () => {};
+
+    const columns = buildComponentColumns(
+      List([
+        { label: "Column 1", name: "column1" },
+        { label: "Column 2", name: "column2" },
+        { label: "Column 3", name: "column3", options: { customBodyRender } }
+      ]),
+      "asc",
+      "column3"
+    );
+
+    expect(columns.map(column => column.name)).to.deep.equals(fromJS(["column1", "column2", "column3"]));
+    expect(columns.find(column => column.name === "column3").options).to.deep.equals({
+      customBodyRender,
+      sortOrder: "asc"
+    });
+  });
+
+  it("generates a sorted column with all its options for array", () => {
+    const customBodyRender = () => {};
+
+    const columns = buildComponentColumns(
+      [
+        { label: "Column 1", name: "column1" },
+        { label: "Column 2", name: "column2" },
+        { label: "Column 3", name: "column3", options: { customBodyRender } }
+      ],
+      "asc",
+      "column3"
+    );
+
+    expect(columns.map(column => column.name)).to.deep.equals(fromJS(["column1", "column2", "column3"]));
+    expect(columns.find(column => column.name === "column3").options).to.deep.equals({
+      customBodyRender,
+      sortOrder: "asc"
+    });
+  });
+});
