c8569bf1c7be256946ba1626948861d6fac2c0d3
Dennis Hernandez <dhernand@quoininc.com>
2022-10-24 08:54:15 -0600

    BCAL-177 - Add baseline registry and registry_from_case forms to the IA and CP configurations

---
+++ db/configuration/forms/case/registry_from_case.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+FormSection.create_or_update!(
+  unique_id: 'registry_from_case',
+  parent_form: 'case',
+  visible: false,
+  order_form_group: 1,
+  order: 3,
+  form_group_id: 'identification_registration',
+  is_first_tab: true,
+  fields: [],
+  name_en: 'Farmer Details',
+  description_en: 'Registry from case',
+  core_form: true
+)
