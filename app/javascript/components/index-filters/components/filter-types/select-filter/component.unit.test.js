d74679e35318ca889858fa3d7b1057f6a82fbef2
Alberto Espinoza <aespinoz@quoininc.com>
2022-10-27 13:15:27 -0600

    R2-2345 W - UAT - Multiple of each location appearing in reporting location filter

--- app/javascript/components/index-filters/components/filter-types/select-filter/component.unit.test.js
+++ app/javascript/components/index-filters/components/filter-types/select-filter/component.unit.test.js
@@ -1,3 +1,4 @@
+import { fromJS } from "immutable";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 
 import { setupMockFormComponent, spy } from "../../../../../test";
@@ -58,6 +59,110 @@ describe("<SelectFilter>", () => {
     });
 
     expect(clone).to.be.empty;
+  });
+
+  it("should render reporting location filter", () => {
+    const initialState = fromJS({
+      application: {
+        agencies: [
+          {
+            unique_id: "agency-test-1",
+            name: {
+              en: "Agency Test 1"
+            },
+            agency_code: "test1",
+            disabled: false,
+            services: ["service_test_1"]
+          },
+          {
+            unique_id: "agency-test-2",
+            name: {
+              en: "Agency Test 2"
+            },
+            agency_code: "test2",
+            disabled: false,
+            services: ["service_test_1", "service_test_2"]
+          }
+        ],
+        reportingLocationConfig: {
+          field_key: "owned_by_location",
+          admin_level: 2,
+          admin_level_map: {
+            1: ["province"],
+            2: ["district"]
+          },
+          hierarchy_filter: [],
+          label_keys: ["district"]
+        }
+      },
+      forms: {
+        options: {
+          locations: [
+            { id: 1, code: "MC", admin_level: 0, disabled: false, name: { en: "MyCountry" } },
+            { id: 2, code: "MCMP1", admin_level: 1, disabled: false, name: { en: "MyCountry:MyProvince1" } },
+            { id: 3, code: "MCMP2", admin_level: 1, disabled: false, name: { en: "MyCountry:MyProvince2" } },
+            {
+              id: 4,
+              code: "MCMP1MD1",
+              admin_level: 2,
+              disabled: false,
+              name: { en: "MyCountry:MyProvince1:MyDistrict1" }
+            },
+            {
+              id: 5,
+              code: "MCMP2MD2",
+              admin_level: 2,
+              disabled: false,
+              name: { en: "MyCountry:MyProvince2:MyDistrict2" }
+            },
+            {
+              id: 6,
+              code: "MCMP2MD2MC1",
+              admin_level: 3,
+              disabled: false,
+              name: { en: "MyCountry:MyProvince2:MyDistrict2:MyCity1" }
+            },
+            {
+              id: 7,
+              code: "MCMP2MD2MC2",
+              admin_level: 3,
+              disabled: false,
+              name: { en: "MyCountry:MyProvince2:MyDistrict2:MyCity2" }
+            }
+          ]
+        }
+      }
+    });
+    const newProps = {
+      addFilterToList: () => {},
+      filter: {
+        field_name: "filter",
+        name: "Filter 1",
+        option_strings_source: "ReportingLocation"
+      },
+      mode: {
+        secondary: false
+      },
+      isDateFieldSelectable: true,
+      moreSectionFilters: {},
+      reset: false,
+      setMoreSectionFilters: spy(),
+      setReset: () => {}
+    };
+
+    const { component } = setupMockFormComponent(SelectFilter, {
+      props: newProps,
+      includeFormProvider: true,
+      state: initialState
+    });
+
+    const selectFilter = component.find(Autocomplete);
+
+    const expected = ["MCMP1MD1", "MCMP2MD2"];
+    const result = selectFilter.props().options.map(opt => opt.id);
+
+    expect(selectFilter).to.have.lengthOf(1);
+    expect(result).to.deep.equal(expected);
   });
 
   it("should have not call setMoreSectionFilters if mode.secondary is false when changing value", () => {
