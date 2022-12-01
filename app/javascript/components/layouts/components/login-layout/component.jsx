de064a246c60a40b9d99ea7f61cfc6ce8f25c1ab
Dennis Hernandez <dhernand@quoininc.com>
2022-12-01 08:29:47 -0600

    R2-2364 - UAT: Agency logo not visible with identity enabled

--- app/javascript/components/layouts/components/login-layout/component.jsx
+++ app/javascript/components/layouts/components/login-layout/component.jsx
@@ -8,22 +8,19 @@ import TranslationsToggle from "../../../translations-toggle";
 import Notifier from "../../../notifier";
 import DemoIndicator from "../../../demo-indicator";
 import { useApp } from "../../../application";
-import { useMemoizedSelector } from "../../../../libs";
-import { getUseIdentityProvider } from "../../../login/selectors";
 
 import { NAME } from "./constants";
 import css from "./styles.css";
 
 const Component = ({ children }) => {
   const { demo } = useApp();
-  const useIdentity = useMemoizedSelector(state => getUseIdentityProvider(state));
 
   // TODO: Module hardcoded till we figure out when to switch modules
   const primeroModule = "cp";
   const moduleClass = `${primeroModule}${demo ? "-demo" : ""}`;
   const classes = clsx({ [css.primeroBackground]: true, [css[moduleClass]]: true, [css.demo]: demo });
-  const classesLoginLogo = clsx({ [css.loginLogo]: !useIdentity });
-  const classesAuthDiv = clsx({ [css.auth]: true, [css.useIdentityWidth]: useIdentity });
+  const classesLoginLogo = clsx({ [css.loginLogo]: true });
+  const classesAuthDiv = clsx({ [css.auth]: true });
 
   return (
     <>
