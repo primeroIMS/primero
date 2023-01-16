15beaaf200951b3e1aaba3ecab6caae34fea2f38
Dennis Hernandez <dhernand@quoininc.com>
2023-01-16 11:25:56 -0600

    R2-2179 - TSFV UAT - Cannot click on part of the cases to assign dashboard

--- app/javascript/components/pages/dashboard/utils/table-cell-greater-than-zero.js
+++ app/javascript/components/pages/dashboard/utils/table-cell-greater-than-zero.js
@@ -1,6 +1,8 @@
-const tableCellGreaterThanZero = condition => ({
+import getCellValue from "./get-cell-value";
+
+export default condition => ({
   setCellProps: currValue => {
-    const value = parseInt(currValue, 10);
+    const value = getCellValue(currValue);
     const style = typeof value === "number" && value > 0 && condition ? { cursor: "pointer" } : { cursor: "auto" };
 
     return {
@@ -8,5 +10,3 @@ const tableCellGreaterThanZero = condition => ({
     };
   }
 });
-
-export default tableCellGreaterThanZero;
