0d64d3a349f7d3fb40c320e02ef4df697de1350d
Stefan Silviu <silviu.stefan@atta.systems>
2023-02-06 15:26:49 +0200

    Add comment to validation change

--- app/javascript/components/pages/admin/users-form/validations.js
+++ app/javascript/components/pages/admin/users-form/validations.js
@@ -11,10 +11,16 @@ export default (formMode, i18n, useIdentityProviders, providers, isMyAccountPage
   const isIdpProvider = function (inputRef, message) {
     // eslint-disable-next-line func-names, consistent-return
     return this.test("isIdpProvider", message, function (value) {
+      if (formMode.get("isEdit")) {
+        // The username field is disabled anyway in edit mode
+        // Skipping this validation allows editing users with invalid usernames from the UI
+        return true;
+      }
+
       const providerId = this.resolve(inputRef);
       const provider = providers.find(currentProvider => currentProvider.get("unique_id") === providerId);
 
-      if (provider && !formMode.get("isEdit")) {
+      if (provider) {
         const regexMatch = new RegExp(`@${provider.get("user_domain")}$`);
 
         return value.match(regexMatch)
@@ -26,8 +32,6 @@ export default (formMode, i18n, useIdentityProviders, providers, isMyAccountPage
               path: this.path
             });
       }
-
-      return true;
     });
   };
 
