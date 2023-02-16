ae51dac091c049da67f8f2d11d4dd1315019d49a
Dennis Hernandez <dhernand@quoininc.com>
2023-02-16 15:20:29 -0600

    R2-2381 - Columns don't dissappear when empty rows are not displayed

--- app/javascript/components/report/utils/get-column-objects.js
+++ app/javascript/components/report/utils/get-column-objects.js
@@ -1,10 +1,11 @@
 import omit from "lodash/omit";
+import merge from "deepmerge";
 
-export default (object, countRows) => {
+export default (object, qtyRows) => {
   let columnObjects = {};
   // eslint-disable-next-line consistent-return
   const getColumnsObj = (obj, level = 0) => {
-    if (level >= countRows) {
+    if (level >= qtyRows) {
       return obj;
     }
 
@@ -16,7 +17,7 @@ export default (object, countRows) => {
     for (let i = 0; i < keys.length; i += 1) {
       const columnObj = getColumnsObj(obj[keys[i]], level);
 
-      columnObjects = columnObj ? { ...columnObjects, ...columnObj } : columnObjects;
+      columnObjects = columnObj ? merge(columnObjects, columnObj) : columnObjects;
     }
 
     return columnObjects;
