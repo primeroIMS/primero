1a2e6bcb5aed8e41dc75658db3c127e37bc25b0e
Dennis Hernandez <dhernand@quoininc.com>
2023-03-01 16:45:12 -0600

    R2-2381 - Fix Age ranges are displayed in order

--- app/javascript/components/report/utils/format-columns.js
+++ app/javascript/components/report/utils/format-columns.js
@@ -2,6 +2,8 @@ import max from "lodash/max";
 import uniq from "lodash/uniq";
 import isNil from "lodash/isNil";
 
+import { sortWithSortedArray } from "../../insights-sub-report/utils";
+
 import formattedDate from "./formatted-date";
 import sortByDate from "./sort-by-date";
 import translateColumn from "./translate-column";
@@ -15,13 +17,19 @@ const columnsHeading = (keys, column, index) =>
     return acc;
   }, []);
 
-export default (keys, columns, i18n) => {
+export default (keys, columns, ageRanges, groupAges, i18n) => {
   const items = columns.map((column, index) => {
-    const uniqueItems = sortByDate(uniq(columnsHeading(keys, column, index).concat(i18n.t("report.total")))).map(
-      columnHeading => formattedDate(columnHeading, i18n)
-    );
+    let uniqueItems = uniq(columnsHeading(keys, column, index));
 
-    return { items: uniqueItems };
+    if (column.name.startsWith("age") && groupAges) {
+      uniqueItems = sortWithSortedArray(uniqueItems, ageRanges);
+    } else {
+      uniqueItems = sortByDate(uniqueItems);
+    }
+
+    return {
+      items: uniqueItems.map(columnHeading => formattedDate(columnHeading, i18n)).concat(i18n.t("report.total"))
+    };
   });
 
   const colspan = max(items.map((item, index) => (index === 1 ? item.items.length : 0)));
