0e509b4b60fcdc49d929071acb6ac912c295f9c3
Dennis Hernandez <dhernand@quoininc.com>
2023-03-29 12:29:54 -0600

    R2-2461 - Remove records using filters

--- app/services/data_removal_service.rb
+++ app/services/data_removal_service.rb
@@ -2,35 +2,92 @@
 
 # Service that allow users to remove data and handling referential integrity
 class DataRemovalService
-  RECORD_MODELS = [Child, Incident, TracingRequest, Trace, Flag].freeze
-  DATA_CONFIG = [Alert, Attachment, AuditLog, BulkExport, RecordHistory, SavedSearch, Transition].freeze
+  RECORD_MODELS = [Child, Incident, TracingRequest].freeze
+  DATA_MODELS = [
+    Trace, Flag, Alert, Attachment, AuditLog, BulkExport, RecordHistory, SavedSearch, Transition, Violation
+  ].freeze
   METADATA_MODELS = [
     Agency, ContactInformation, Field, FormSection, Location, Lookup, PrimeroModule, PrimeroProgram, Report, Role,
     SystemSettings, UserGroup, ExportConfiguration, PrimeroConfiguration, Webhook, IdentityProvider
   ].freeze
+  DATE_TIME_FORMAT = 'YYYY-MM-DDTHH:MI:SS'
 
   class << self
     def remove_records(args = {})
       if args.present?
-        if args[:model_class].present?
-        end
-
-        if args[:filters].present?
+        record_models = args[:record_models].present? ? record_models_to_delete(args[:record_models]) : RECORD_MODELS
+        if record_models.present?
+          record_models.each do |record_model|
+            query = apply_filters(record_model, args[:filters])
+            ModelDeletionService.new(model_class: record_model).delete_records!(query)
+          end
+        else
+          puts 'No valid record model was entered. Nothing was deleted.'
         end
       else
-        # TODO: Delete everything
+        remove_all_records
       end
     end
 
+    def remove_all_records
+      (RECORD_MODELS + DATA_MODELS).each { |model| ModelDeletionService.new(model_class: model).delete_all! }
+      Sunspot.remove_all(RECORD_MODELS)
+    end
+
     def remove_config(args = {})
-      # TODO: Include users?
-      if args[:metadata].present?
-        args[:metadata].map { |m| Kernel.const_get(m) }.each do |model|
-          ModelDeletionService.new(model_class: model).delete!
-        end
-      else
-        METADATA_MODELS.each { |model| ModelDeletionService.new(model_class: model).delete_all! }
+      metadata_models = metadata_models_to_delete(args[:metadata])
+      metadata_models << User if args[:include_users] == true
+      metadata_models.each { |model| ModelDeletionService.new(model_class: model).delete_all! }
+    end
+
+    private
+
+    def record_models_to_delete(record_models)
+      record_model_classes = model_classes(record_models)
+      RECORD_MODELS.select { |record_model| record_model_classes.include?(record_model) }
+    end
+
+    def metadata_models_to_delete(metadata_models)
+      metadata_model_classes = model_classes(metadata_models)
+      METADATA_MODELS.select { |metadata_model| metadata_model_classes.include?(metadata_model) }
+    end
+
+    def apply_filters(model, filters)
+      query = model
+      filters.each do |filter|
+        # TODO: We only allow the created_at filter for now but once the
+        # SearchService is migrated we'll use it here instead
+        next unless filter[:created_at].present? && filter.is_a?(Hash)
+
+        query = from_query(query, filter[:created_at])
+        query = to_query(query, filter[:created_at])
       end
+
+      query
+    end
+
+    def from_query(query, filter)
+      return query unless filter[:from].present?
+
+      query.where(
+        'to_timestamp(data->> :field, :format) > to_timestamp(:from, :format)',
+        field: 'created_at', format: DATE_TIME_FORMAT, from: filter[:from]
+      )
+    end
+
+    def to_query(query, filter)
+      return query unless filter[:to].present?
+
+      query.where(
+        'to_timestamp(data->> :field, :format) < to_timestamp(:to, :format)',
+        field: 'created_at', format: DATE_TIME_FORMAT, to: filter[:to]
+      )
+    end
+
+    def model_classes(models)
+      return [] unless models.present?
+
+      models.map { |model| Kernel.const_get(model) }
     end
   end
 end
--- app/services/model_deletion_service.rb
+++ app/services/model_deletion_service.rb
@@ -2,21 +2,31 @@
 
 # Service to handle deletion of data for a specific model and its associations
 class ModelDeletionService < ValueObject
+  UUID_REFERENCED_MODELS = [Alert, Attachment, Trace, Violation].freeze
   attr_accessor :model_class
 
-  def delete!
+  def delete_records!(query)
     puts "Removing data for model: #{model_class.name}..."
-    delete_join_tables
-    delete_referenced_tables
-    model_class.delete_all
+
+    ActiveRecord::Base.transaction do
+      delete_data_references(query)
+      nullify_record_references(query)
+      delete_join_references(query)
+      Sunspot.remove_by_id(model_class, query.pluck(:id))
+      query.delete_all
+    end
   end
 
   def delete_all!
     puts "Removing all data for model: #{model_class.name} and its associations..."
-    delete_join_tables
-    delete_referenced_tables
-    model_class.delete_all
+    ActiveRecord::Base.transaction do
+      delete_join_tables
+      nullify_references
+      model_class.delete_all
+    end
   end
+
+  private
 
   def delete_join_tables
     model_join_reflections.each do |reflection|
@@ -28,39 +38,77 @@ class ModelDeletionService < ValueObject
     end
   end
 
-  def delete_referenced_tables
+  def nullify_references
     model_reflections.each do |reflection|
       table_name = ActiveRecord::Base.connection.quote_table_name(reflection.table_name)
       column_name = ActiveRecord::Base.connection.quote_column_name(reflection.foreign_key)
-      puts "Removing data in referenced table: #{table_name}..."
-      ActiveRecord::Base.connection.execute(
-        ActiveRecord::Base.sanitize_sql_for_conditions(["DELETE FROM #{table_name} where #{column_name} IS NOT NULL"])
-      )
-    end
-  end
-
-  def nullify_references(record)
-    model_reflections(record.class).each do |reflection|
-      table_name = ActiveRecord::Base.connection.quote_table_name(reflection.table_name)
-      column_name = ActiveRecord::Base.connection.quote_column_name(reflection.foreign_key)
+      puts "Removing references in table: #{table_name}..."
       ActiveRecord::Base.connection.execute(
         ActiveRecord::Base.sanitize_sql_for_conditions(
-          ["UPDATE #{table_name} SET #{column_name} = NULL WHERE #{column_name} = ?", record.id]
+          ["UPDATE #{table_name} SET #{column_name} = NULL WHERE #{column_name} IS NOT NULL"]
         )
       )
     end
   end
 
-  def remove_row_reference(record)
-    model_join_reflections(record.class).each do |reflection|
+  def delete_referenced_tables
+    model_reflections.each do |reflection|
+      table_name = ActiveRecord::Base.connection.quote_table_name(reflection.table_name)
+      puts "Removing data in referenced table: #{table_name}..."
+      ActiveRecord::Base.connection.execute(
+        ActiveRecord::Base.sanitize_sql_for_conditions(["DELETE FROM #{table_name}"])
+      )
+    end
+  end
+
+  def nullify_record_references(query)
+    record_model_reflections.each do |reflection|
+      table_name = ActiveRecord::Base.connection.quote_table_name(reflection.table_name)
+      column_name = ActiveRecord::Base.connection.quote_column_name(reflection.foreign_key)
+      ActiveRecord::Base.connection.execute(
+        ActiveRecord::Base.sanitize_sql_for_conditions(
+          ["UPDATE #{table_name} SET #{column_name} = NULL WHERE #{column_name} IN (#{query.select(:id).to_sql})"]
+        )
+      )
+    end
+  end
+
+  def delete_data_references(query)
+    data_model_reflections.each do |reflection|
+      table_name = ActiveRecord::Base.connection.quote_table_name(reflection.table_name)
+      column_name = ActiveRecord::Base.connection.quote_column_name(reflection.foreign_key)
+      ActiveRecord::Base.connection.execute(
+        ActiveRecord::Base.sanitize_sql_for_conditions(
+          ["DELETE FROM #{table_name} WHERE #{column_name} IN (#{data_query(reflection.klass, query).to_sql})"]
+        )
+      )
+    end
+  end
+
+  def data_query(data_model, query)
+    return query.select('id') if UUID_REFERENCED_MODELS.include?(data_model)
+
+    query.select('CAST (id AS VARCHAR) as id')
+  end
+
+  def delete_join_references(query)
+    model_join_reflections.each do |reflection|
       join_table = ActiveRecord::Base.connection.quote_column_name(reflection.join_table)
       column_name = ActiveRecord::Base.connection.quote_column_name(reflection.foreign_key)
       ActiveRecord::Base.connection.execute(
         ActiveRecord::Base.sanitize_sql_for_conditions(
-          ["DELETE FROM #{join_table} WHERE #{column_name} = ?", record.id]
+          ["DELETE FROM #{join_table} WHERE #{column_name} IN (#{query.select(:id).to_sql})"]
         )
       )
     end
+  end
+
+  def record_model_reflections
+    model_reflections.select { |reflection| DataRemovalService::RECORD_MODELS.include?(reflection.klass) }
+  end
+
+  def data_model_reflections
+    model_reflections.select { |reflection| DataRemovalService::RECORD_MODELS.exclude?(reflection.klass) }
   end
 
   def model_reflections
