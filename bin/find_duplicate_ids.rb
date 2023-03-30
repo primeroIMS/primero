51d0001043298878f2cb53659b21bc1ea851770e
Dennis Hernandez <dhernand@quoininc.com>
2023-03-30 16:27:37 -0600

    Adds a new param to fix records with duplicated ids

--- bin/find_duplicate_ids.rb
+++ bin/find_duplicate_ids.rb
@@ -6,10 +6,10 @@ def print_log(message)
 end
 
 show_records = ARGV[0] || false
+fix_records = ARGV[1] || false
 
-[Child, Incident, TracingRequest].each do |model|
-  id_field = "#{model.parent_form}_id"
-  results = ActiveRecord::Base.connection.execute(
+def find_duplicates(model, id_field)
+  ActiveRecord::Base.connection.execute(
     model.select(
       ActiveRecord::Base.sanitize_sql_for_conditions(
         ['data->>? as record_id, count(*) as total_duplicates', id_field]
@@ -18,6 +18,29 @@ show_records = ARGV[0] || false
      .group(ActiveRecord::Base.sanitize_sql_for_conditions(['data->>?', id_field]))
      .to_sql
   ).to_a
+end
+
+def show_record_details(results, id_field, model)
+  results.each do |result|
+    print_log("---#{id_field} = #{result['record_id']} has #{result['total_duplicates']} duplicate(s).---")
+    ids = Child.where('data->>? = ?', id_field, result['record_id']).pluck(:id)
+    print_log("Duplicated #{model.name} ids: #{ids}")
+  end
+end
+
+def fix_duplicated_records(results, id_field, model)
+  results.each do |result|
+    Child.where('data->>? = ?', id_field, result['record_id']).each_with_index do |record, index|
+      print_log("Fixing #{model.name}.id = #{record.id}...")
+      record.case_id = "#{record.case_id}#{index}"
+      record.save!
+    end
+  end
+end
+
+[Child, Incident, TracingRequest].each do |model|
+  id_field = "#{model.parent_form}_id"
+  results = find_duplicates(model, id_field)
 
   if results.blank?
     print_log("No duplicates found for #{model.name}")
@@ -30,11 +53,11 @@ show_records = ARGV[0] || false
 
   next unless show_records
 
-  results.each do |result|
-    print_log("---#{id_field} = #{result['record_id']} has #{result['total_duplicates']} duplicate(s).---")
-    ids = Child.where('data->>? = ?', id_field, result['record_id']).pluck(:id)
-    print_log("Duplicated #{model.name} ids: #{ids}")
-  end
+  show_record_details(results, id_field, model)
+
+  next unless fix_records
+
+  fix_duplicated_records(results, id_field, model)
 end
 
 print_log('---Done.---')
