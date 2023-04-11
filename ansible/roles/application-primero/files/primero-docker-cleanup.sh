1c6754ccfa5284ecee837b2880cd74d9ac6d04d6
Joshua Toliver <jtoliver@quoininc.com>
2023-04-11 04:53:12 -1000

    Setting cron user to root

--- ansible/roles/application-primero/files/primero-docker-cleanup.sh
+++ ansible/roles/application-primero/files/primero-docker-cleanup.sh
@@ -3,6 +3,6 @@
 version=$(</srv/primero/docker-app-version.txt)
 
 if [[ $version ]]; then
-  sudo docker container ls -a -f status=exited | grep -vE "$version" | awk 'NR>1 {print $1}' | xargs sudo docker rm; 
-  sudo docker images -a | grep -vE "$version" | grep "<none>" | awk '{print $3}' | xargs sudo docker rmi
+  docker container ls -a -f status=exited | grep -vE "$version" | awk 'NR>1 {print $1}' | xargs docker rm; 
+  docker images -a | grep -vE "$version" | grep "<none>" | awk '{print $3}' | xargs docker rmi
 fi
