78990f9a9d728b206a9bffc3d6d582a9177ebf21
Joshua Toliver <jtoliver@quoininc.com>
2023-04-11 04:02:00 -1000

    Adding cron to cleanup ansible primero images/containers

--- ansible/roles/application-primero/files/primero-docker-cleanup.sh
+++ ansible/roles/application-primero/files/primero-docker-cleanup.sh
@@ -4,5 +4,5 @@ version=$(</srv/primero/docker-app-version.txt)
 
 if [[ $version ]]; then
   sudo docker container ls -a -f status=exited | grep -vE "$version" | awk 'NR>1 {print $1}' | xargs sudo docker rm; 
-  sudo docker rmi '$(sudo docker images -f dangling=true -q)' > /dev/null 2>&1
+  sudo docker images -a | grep -vE "$version" | grep "<none>" | awk '{print $3}' | xargs sudo docker rmi
 fi
