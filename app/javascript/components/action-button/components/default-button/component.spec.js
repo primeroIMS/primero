bc8f36786e19cdd7ee7f88a5302e7050a1829da1
Divyanshu Bhardwaj <divyanshubhardwaj4@gmail.com>
2023-05-04 11:51:59 +0530

    [Update] copy new test cases in old file and rename those files as component.spec.js

--- app/javascript/components/action-button/components/default-button/component.spec.js
+++ app/javascript/components/action-button/components/default-button/component.spec.js
@@ -2,55 +2,55 @@ import { mountedComponent, screen } from "test-utils";
 import DefaultButton from "./component";
 
 describe("<DefaultButton /> components/action-button/components", () => {
-    const props = {
-      icon: <></>,
-      isTransparent: false,
-      cancel: true,
-      pending: false,
-      text: "Test",
-      rest: {}
+  const props = {
+    icon: <></>,
+    isTransparent: false,
+    cancel: true,
+    pending: false,
+    text: "Test",
+    rest: {}
+  };
+  it("renders DefaultButton with text", () => {
+    mountedComponent(<DefaultButton {...props} />);
+    expect(screen.getByRole("button")).toHaveTextContent("Test");
+  });
+
+  it("renders a <Button /> and a <CircularProgress /> component", () => {
+    const newProps = {
+      ...props,
+      pending: true
     };
-    it("renders DefaultButton with text", () => {
-        mountedComponent(<DefaultButton {...props} />);
-        expect(screen.getByRole("button")).toHaveTextContent("Test");   
-    });
+    mountedComponent(<DefaultButton {...newProps} />);
+    expect(screen.getAllByRole('progressbar')).toHaveLength(1);
+    expect(screen.getByRole('button', { disabled: true })).toBeInTheDocument();
+  });
 
-    it("renders a <Button /> and a <CircularProgress /> component", () => {
-        const newProps = {
-          ...props,
-          pending: true
-        };    
-        mountedComponent(<DefaultButton {...newProps} />);
-        expect(screen.getAllByRole('progressbar')).toHaveLength(1);
-        expect(screen.getByRole('button', { disabled: true })).toBeInTheDocument();
-    }); 
-    
-    it("should contain .isTransparent class", () => {
-        const newProps = {
-          ...props,
-          isTransparent: true
-        }; 
-        mountedComponent(<DefaultButton {...newProps} />);
-        expect(screen.getAllByRole('button', { className: 'isTransparent' })).toHaveLength(1); 
-    });
+  it("should contain .isTransparent class", () => {
+    const newProps = {
+      ...props,
+      isTransparent: true
+    };
+    mountedComponent(<DefaultButton {...newProps} />);
+    expect(screen.getAllByRole('button', { className: 'isTransparent' })).toHaveLength(1);
+  });
 
-    it("renders an default icon button class", () => {
-        const newProps = {
-          ...props,
-          className: "MuiSvgIcon-root"
-        };    
-        mountedComponent(<DefaultButton {...newProps} />);
-        expect(screen.getByRole("button")).toHaveClass('MuiSvgIcon-root');
-    });
+  it("renders an default icon button class", () => {
+    const newProps = {
+      ...props,
+      className: "MuiSvgIcon-root"
+    };
+    mountedComponent(<DefaultButton {...newProps} />);
+    expect(screen.getByRole("button")).toHaveClass('MuiSvgIcon-root');
+  });
 
-    it("should contain .cancel class", () => {
-        const newProps = {
-          ...props,
-          cancel:true
-        };    
-        mountedComponent(<DefaultButton {...newProps} />);
-        expect(screen.getAllByRole('button', { className: 'cancel' })).toHaveLength(1);     
-    });
+  it("should contain .cancel class", () => {
+    const newProps = {
+      ...props,
+      cancel: true
+    };
+    mountedComponent(<DefaultButton {...newProps} />);
+    expect(screen.getAllByRole('button', { className: 'cancel' })).toHaveLength(1);
+  });
 });
 
 
--- app/javascript/components/action-button/components/link/component.spec.js
+++ app/javascript/components/action-button/components/link/component.spec.js
@@ -2,19 +2,19 @@ import { mountedComponent, screen } from "test-utils";
 import Link from "./component";
 
 describe("<Link /> components/link-button/components", () => {
-    const props = {
-      icon: <></>,
-      isTransparent: false,
-      rest: {},
-      text:"Test Link",
-      id:"link"
-    };  
-    it("renders a <Link /> component", () => {
-        const newProps = {
-            ...props,
-            className: "MuiLink-root"
-          }; 
-        mountedComponent(<Link {...newProps} />);        
-        expect(screen.getByText("Test Link").closest('a')).toHaveClass('MuiLink-root');    
-    });
+  const props = {
+    icon: <></>,
+    isTransparent: false,
+    rest: {},
+    text: "Test Link",
+    id: "link"
+  };
+  it("renders a <Link /> component", () => {
+    const newProps = {
+      ...props,
+      className: "MuiLink-root"
+    };
+    mountedComponent(<Link {...newProps} />);
+    expect(screen.getByText("Test Link").closest('a')).toHaveClass('MuiLink-root');
+  });
 });
