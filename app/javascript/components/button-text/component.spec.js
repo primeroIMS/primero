226573616f07a175b60b1e9a5dfa97cc4d84ffd2
Divyanshu Bhardwaj <divyanshubhardwaj4@gmail.com>
2023-05-06 17:42:14 +0530

    [New] Converted test cases for button-text components from enzyme to rtl

---
+++ app/javascript/components/button-text/component.spec.js
@@ -0,0 +1,21 @@
+import { mountedComponent, screen } from "test-utils";
+import ButtonText from "./component";
+
+describe("<ButtonText />", () => {
+  const props = {
+    text: "Test Title"
+  };
+
+  beforeEach(() => {
+    mountedComponent(<ButtonText {...props} />);
+  });
+
+  it("should render text", () => {
+    expect(screen.getByText("Test Title")).toBeInTheDocument();
+  });
+});
+
+
+
+
+
