8d8f01daed2a32dd4da8cc72ea192445e482f54d
Bharanikumar-Gandla <bharanikumar.gandla@trigyn.com>
2023-05-12 18:57:23 +0530

    app/javascript/components/agency-logo/component.spec.js

---
+++ app/javascript/components/agency-logo/component.spec.js
@@ -0,0 +1,29 @@
+import { fromJS } from "immutable";
+import { mountedComponent, screen } from "test-utils";
+import AgencyLogo from "./component";
+
+describe("<AgencyLogo />", () => {
+  it("renders agency logo from state", () => {
+
+    const state = fromJS({
+      application: {
+        primero: {
+          sandbox_ui: false,
+          agencies: [
+            {
+              unique_id: "1",
+              name: "Test logo",
+              logo_icon: "logo-icon.png",
+              logo_full: "logo-full.png"
+            }
+          ]
+        }
+      }
+    });
+
+    mountedComponent(<AgencyLogo />, state);
+    expect(screen.getByTestId('background')).toHaveStyle(`background-image: url(${"logo-icon.png"})`)
+
+  });
+});
+
