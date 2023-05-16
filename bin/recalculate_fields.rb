508165bf00178599cadb2e26b2f7c3ad10ac5e31
Dennis Hernandez <dhernand@quoininc.com>
2023-05-16 12:16:49 -0600

    BCAL-220 - Do not query records if the fields to recalculate are not specified.

--- bin/recalculate_fields.rb
+++ bin/recalculate_fields.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# Example of usage:
+# rails r bin/recalculate_fields true field1
+# rails r bin/recalculate_fields true field1,field2
+
 def print_log(message)
   message = "#{DateTime.now.strftime('%m/%d/%Y %H:%M')}|| #{message}"
   puts message
@@ -8,17 +12,21 @@ end
 save_records = ARGV[0] == 'true'
 fields_to_recalculate = (ARGV[1] || '').split(',')
 
-Child.find_in_batches(batch_size: 1000) do |records|
-  records.each do |record|
-    fields_to_recalculate.each do |field|
-      field_value = record.send("calculate_#{field}".to_sym)
+if fields_to_recalculate.present?
+  Child.find_in_batches(batch_size: 1000) do |records|
+    records.each do |record|
+      fields_to_recalculate.each do |field|
+        field_value = record.send("calculate_#{field}".to_sym)
 
-      if save_records
-        record.update_column('data', record.data.merge(field => field_value))
-        print_log("Child Id:#{record.id} updated with #{field} = #{field_value}")
-      else
-        print_log("Child Id:#{record.id} will be updated with #{field} = #{field_value}")
+        if save_records
+          record.update_column('data', record.data.merge(field => field_value))
+          print_log("Child Id:#{record.id} updated with #{field} = #{field_value}")
+        else
+          print_log("Child Id:#{record.id} will be updated with #{field} = #{field_value}")
+        end
       end
     end
   end
+else
+  print_log('No fields to recalculate were specified.')
 end
