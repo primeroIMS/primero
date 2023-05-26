749e9cdc76439856a740b4696daa65ad24406535
Divyanshu Bhardwaj <divyanshubhardwaj4@gmail.com>
2023-05-26 11:25:32 +0530

    [Update] Updated test cases for form components from enzyme to rtl

--- app/javascript/components/form/components/form-section-tabs/component.spec.js
+++ app/javascript/components/form/components/form-section-tabs/component.spec.js
@@ -1,22 +1,21 @@
-import { mountedComponent, screen, setupMockFormComponent } from "test-utils";
+import { mountedComponent, screen } from "test-utils";
 
 import FormSectionTabs from "./component";
 
 describe("<FormSectionTabs />", () => {
-    let component;
-    const props = {
-        tabs: [
-            { name: "tab 1", disabled: true, fields: [] },
-            { name: "tab2", disabled: false, fields: [] }
-        ],
-        handleTabChange: () => { }
-    };
+  const props = {
+    tabs: [
+      { name: "tab 1", disabled: true, fields: [] },
+      { name: "tab2", disabled: false, fields: [] }
+    ],
+    handleTabChange: () => {}
+  };
 
-    beforeEach(() => {
-        mountedComponent(<FormSectionTabs {...props} />);
-    });
+  beforeEach(() => {
+    mountedComponent(<FormSectionTabs {...props} />);
+  });
 
-    it("should render the FormSectionTabs component", () => {
-        expect(screen.getByText("tab 1")).toBeInTheDocument();
-    });
+  it("should render the FormSectionTabs component", () => {
+    expect(screen.getByText("tab 1")).toBeInTheDocument();
+  });
 });
