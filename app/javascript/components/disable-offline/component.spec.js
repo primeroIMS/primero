384165643120d12f6a5c08b4d9361e781451c179
Divyanshu Bhardwaj <divyanshubhardwaj4@gmail.com>
2023-06-12 15:10:33 +0530

    [Update] Updated test cases for disable offline components from enzyme to rtl

--- app/javascript/components/disable-offline/component.spec.js
+++ app/javascript/components/disable-offline/component.spec.js
@@ -1,27 +1,26 @@
 import { mountedComponent, screen } from "test-utils";
 import { fromJS } from "immutable";
+
 import DisableOffline from "./component";
+
 describe("<DisableOffline /> - Component", () => {
   const props = {
     overrideCondition: true,
-    text: "offline",
+    text: "offline"
   };
 
   it("renders element in offline state if offline", () => {
-    mountedComponent(<DisableOffline {...props}>
-      <div>element</div>
-    </DisableOffline>
-      , fromJS({
+    mountedComponent(
+      <DisableOffline {...props}>
+        <div>element</div>
+      </DisableOffline>,
+      fromJS({
         connectivity: {
           online: false,
           serverOnline: true
         }
-      }));
-    expect(screen.getByText('element')).toBeInTheDocument();
+      })
+    );
+    expect(screen.getByText("element")).toBeInTheDocument();
   });
 });
-
-
-
-
-
