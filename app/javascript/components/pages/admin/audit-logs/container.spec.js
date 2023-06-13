771a639cb07968332563e3a90cc96f6cc1ca7f4d
Divyanshu Bhardwaj <divyanshubhardwaj4@gmail.com>
2023-06-13 18:16:24 +0530

    [Update] Updated test cases for pages-admin-audit-logs component from enzyme to rtl

--- app/javascript/components/pages/admin/audit-logs/container.spec.js
+++ app/javascript/components/pages/admin/audit-logs/container.spec.js
@@ -1,40 +1,31 @@
-
 import { mountedComponent, screen } from "test-utils";
-
 import { fromJS } from "immutable";
 
-
-import { PageHeading } from "../../../page";
 import { ACTIONS } from "../../../permissions";
-import IndexTable from "../../../index-table";
-import { FiltersForm } from "../../../form-filters/components";
 
 import AuditLogs from "./container";
 
 describe("<AuditLogs />", () => {
-    let component;
-    const state = fromJS({
-        user: {
-            permissions: {
-                audit_logs: [ACTIONS.MANAGE]
-            }
-        }
-    });
+  const state = fromJS({
+    user: {
+      permissions: {
+        audit_logs: [ACTIONS.MANAGE]
+      }
+    }
+  });
 
-    beforeEach(() => {
-        mountedComponent(<AuditLogs />, state, ["/admin/audit_logs"]);
+  beforeEach(() => {
+    mountedComponent(<AuditLogs />, state, ["/admin/audit_logs"]);
+  });
+  it("renders <PageHeading /> component", () => {
+    expect(screen.getByTestId("page-heading")).toBeInTheDocument();
+  });
 
-    });
-    it("renders <PageHeading /> component", () => {
-        expect(screen.getByTestId('page-heading')).toBeInTheDocument();
-    });
+  it("renders <IndexTable /> component", () => {
+    expect(screen.getByRole("grid")).toBeInTheDocument();
+  });
 
-    it("renders <IndexTable /> component", () => {
-        expect(screen.getByRole('grid')).toBeInTheDocument();
-    });
-
-    it("renders <FiltersForm /> component", () => {
-        expect(screen.getByTestId('form-filter')).toBeInTheDocument();
-    });
-
+  it("renders <FiltersForm /> component", () => {
+    expect(screen.getByTestId("form-filter")).toBeInTheDocument();
+  });
 });
