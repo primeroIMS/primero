d5068dfa50cf513a91f21a967e97aff13627cb0a
Divyanshu Bhardwaj <divyanshubhardwaj4@gmail.com>
2023-06-19 19:15:02 +0530

    [New] Created test cases for pages-admin-form-builder-custom-field-selector-dialog component from enzyme to rtl

---
+++ app/javascript/components/pages/admin/form-builder/components/custom-field-selector-dialog/component.spec.js
@@ -0,0 +1,27 @@
+import { fromJS } from "immutable";
+import { mountedComponent, screen } from "test-utils";
+
+import CustomFieldSelectorDialog from "./component";
+
+describe("<CustomFieldSelectorDialog />", () => {
+  const initialState = fromJS({
+    ui: { dialogs: { dialog: "custom_field_selector_dialog", open: true } }
+  });
+
+  beforeEach(() => {
+    mountedComponent(<CustomFieldSelectorDialog />, initialState);
+  });
+
+  it("should render the CustomFieldSelectorDialog component", () => {
+    expect(screen.getByRole("dialog")).toBeInTheDocument();
+  });
+
+  it("should render list of fields types", () => {
+    expect(screen.getByText("forms.type_label")).toBeInTheDocument();
+    expect(screen.getByText("forms.select_label")).toBeInTheDocument();
+    const elements = screen.getAllByTestId("field");
+    const totalCount = elements.length;
+
+    expect(totalCount).toBe(12);
+  });
+});
