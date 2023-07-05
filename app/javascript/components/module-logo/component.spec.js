d33572a2f822a3cf7630470893351d50caab8471
srinivasulu chimaladinne <srinivasulu.chimaladinne@trigyn.com>
2023-07-05 15:43:04 +0530

    [New] added test cases for module logo

---
+++ app/javascript/components/module-logo/component.spec.js
@@ -0,0 +1,47 @@
+import { fromJS } from "immutable";
+
+import { mountedComponent, screen } from "../../test-utils";
+import { PrimeroModuleRecord } from "../application/records";
+import { MODULES } from "../../config";
+
+import ModuleLogo from "./component";
+
+describe("<ModuleLogo />", () => {
+  it("renders a default primero module logo", () => {
+    const state = fromJS({
+      application: {
+        modules: [
+          {
+            unique_id: "test-1"
+          }
+        ]
+      },
+      user: {
+        modules: ["test-1"]
+      }
+    });
+
+    mountedComponent(<ModuleLogo />, state);
+
+    expect(screen.getByText((content, element) => element.tagName.toLowerCase() === "img")).toBeInTheDocument();
+  });
+
+  it("renders a primero module logo from props", () => {
+    const state = fromJS({
+      application: {
+        modules: [
+          PrimeroModuleRecord({
+            unique_id: MODULES.MRM
+          })
+        ]
+      },
+      user: {
+        modules: [MODULES.MRM]
+      }
+    });
+
+    mountedComponent(<ModuleLogo />, state);
+
+    expect(screen.getByText((content, element) => element.tagName.toLowerCase() === "img")).toBeInTheDocument();
+  });
+});
