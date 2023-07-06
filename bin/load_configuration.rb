5974c6c51a36b604710893b9f261bbfc4491655c
Dennis Hernandez <dhernand@quoininc.com>
2023-07-06 16:02:31 -0600

    R2-2515 - Close database connections and return a message instead of error

--- bin/load_configuration.rb
+++ bin/load_configuration.rb
@@ -2,8 +2,6 @@
 
 require_relative('../lib/primero_database.rb')
 
-# Close the db connection
-
 def check_not_seeded
   return true if ENV['PRIMERO_CONFIGURATION_RUN_SCRIPTS'] == 'true'
 
@@ -19,15 +17,17 @@ def check_configuration_file_version
 end
 
 def not_loading_already_seeded
+  PrimeroDatabase.instance.connection.close
   puts('Not loading: PRIMERO_CONFIGURATION_RUN_SCRIPTS != true; Primero is already seeded')
 end
 
 def not_loading_pinned_version
+  PrimeroDatabase.instance.connection.close
   puts("Not loading: Primero configuration is pinned to #{ENV['PRIMERO_CONFIGURATION_FILE_VERSION']}")
 end
 
 def usage
-  puts 'Usage: load_configuration <path/to/configuration/script.rb'
+  puts 'Usage: load_configuration path/to/configuration/script.rb'
 end
 
 return usage unless ARGV.length.positive? && File.exist?(ARGV[0])
