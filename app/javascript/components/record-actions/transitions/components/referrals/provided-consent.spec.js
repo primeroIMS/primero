1c26d328cc221227687d417d4993c32e49b44cc4
srinivasulu chimaladinne <srinivasulu.chimaladinne@trigyn.com>
2023-07-14 16:40:05 +0530

    [New] Added test cases for reord actions transitions components referrals

---
+++ app/javascript/components/record-actions/transitions/components/referrals/provided-consent.spec.js
@@ -0,0 +1,67 @@
+/* eslint-disable no-unused-expressions */
+import { mountedComponent, screen } from "../../../../../test-utils";
+import { RECORD_TYPES } from "../../../../../config";
+
+import ProvidedConsent from "./provided-consent";
+
+describe("<ProvidedConsent /> - referrals", () => {
+  const formProps = {
+    initialValues: {
+      transfer: false
+    }
+  };
+
+  it("should not render anything when child has provided consent and user can't consent override", () => {
+    const props = {
+      canConsentOverride: false,
+      providedConsent: true
+    };
+
+    mountedComponent(<ProvidedConsent {...props} />, {}, [], [], [], formProps);
+
+    expect(screen.queryByText(/referral.provided_consent_label/i)).toBeNull();
+  });
+
+  it("should not render anything when child has provided consent and user can consent override", () => {
+    const props = {
+      canConsentOverride: true,
+      providedConsent: true
+    };
+
+    mountedComponent(<ProvidedConsent {...props} />, {}, [], [], [], formProps);
+
+    expect(screen.queryByText(/referral.provided_consent_label/i)).toBeNull();
+  });
+
+  it("should render <ProvidedForm> when child has not provided consent and user can consent override", () => {
+    const props = {
+      canConsentOverride: true,
+      providedConsent: false
+    };
+
+    mountedComponent(<ProvidedConsent {...props} />, {}, [], [], formProps);
+    expect(screen.getByText(/referral.provided_consent_label/i)).toBeInTheDocument();
+  });
+
+  it("should render <ProvidedForm> when child has not provided consent and user can't consent override", () => {
+    const props = {
+      canConsentOverride: false,
+      providedConsent: false
+    };
+
+    mountedComponent(<ProvidedConsent {...props} />, {}, [], [], formProps);
+    expect(screen.getByText(/referral.provided_consent_label/i)).toBeInTheDocument();
+  });
+
+  it("should render <ProvidedForm> with valid props", () => {
+    const props = {
+      canConsentOverride: true,
+      providedConsent: false,
+      setDisabled: () => {},
+      recordType: RECORD_TYPES.cases
+    };
+
+    mountedComponent(<ProvidedConsent {...props} />, {}, [], [], formProps);
+    expect(screen.queryAllByTestId("form-control")).toHaveLength(1);
+  });
+});
