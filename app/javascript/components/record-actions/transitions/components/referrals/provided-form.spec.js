d328010fc94ed3f5facacae09ba6a0e283f4673a
srinivasulu chimaladinne <srinivasulu.chimaladinne@trigyn.com>
2023-07-18 17:54:00 +0530

    Resolved PR comments

--- app/javascript/components/record-actions/transitions/components/referrals/provided-form.spec.js
+++ app/javascript/components/record-actions/transitions/components/referrals/provided-form.spec.js
@@ -13,25 +13,26 @@ describe("<ProvidedForm /> - referrals", () => {
   };
 
   it("should render properly when user can override consent", () => {
-    const props = {
+    const overrideProps = {
       canConsentOverride: true
     };
 
-    mountedComponent(<ProvidedForm {...props} />, {}, [], [], formProps);
+    mountedComponent(<ProvidedForm {...overrideProps} />, {}, [], [], formProps);
     expect(screen.queryAllByTestId("grid")).toHaveLength(3);
     expect(screen.getByTestId("form-control")).toBeInTheDocument();
     expect(screen.getByText(/referral.refer_anyway_label/i)).toBeInTheDocument();
   });
 
   it("should render some components when user can not override consent", () => {
-    const props = {
+    const canNotOverrideProps = {
       canConsentOverride: false
     };
 
-    mountedComponent(<ProvidedForm {...props} />, {}, [], [], formProps);
-
+    mountedComponent(<ProvidedForm {...canNotOverrideProps} />, {}, [], [], formProps);
     expect(screen.queryAllByTestId("grid")).toHaveLength(3);
     expect(screen.queryByTestId("form-control")).toBeNull();
+    expect(screen.queryAllByRole("checkbox")).toHaveLength(0);
     expect(screen.getByText(/referral.provided_consent_label/i)).toBeInTheDocument();
+
   });
 });
