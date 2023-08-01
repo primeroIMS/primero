e05b1f8a6d28d66df46ee9cc67f68d202fe7930d
srinivasulu chimaladinne <srinivasulu.chimaladinne@trigyn.com>
2023-08-01 17:20:44 +0530

    [New] Added test cases for transitions

---
+++ app/javascript/components/transitions/TransitionPanel.spec.js
@@ -0,0 +1,15 @@
+import { mountedComponent, screen } from "../../test-utils";
+
+import TransitionPanel from "./TransitionPanel";
+
+describe("<TransitionPanel />", () => {
+  const props = {
+    children: <p>This is a children</p>
+  };
+
+  it("renders a <p> as children of  TransitionPanel", () => {
+    mountedComponent(<TransitionPanel {...props} />);
+
+    expect(screen.getByText(/This is a children/i)).toBeInTheDocument();
+  });
+});
