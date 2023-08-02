00b17c203dba515e4f6b4a7b4d35ce99f9328ec6
srinivasulu chimaladinne <srinivasulu.chimaladinne@trigyn.com>
2023-08-02 17:10:29 +0530

    [New] added test cases for libs

--- jest.config.js
+++ jest.config.js
@@ -153,7 +153,9 @@ module.exports = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ["<rootDir>/app/javascript/components/**/*.spec.js"],
+  testMatch: ["<rootDir>/app/javascript/components/**/*.spec.js",
+  "<rootDir>/app/javascript/libs/**/*.spec.js"
+],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: ["/node_modules/"],
