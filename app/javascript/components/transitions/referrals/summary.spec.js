6b5d80d00769480d37c2534adcd1c5dc4dc68de9
srinivasulu chimaladinne <srinivasulu.chimaladinne@trigyn.com>
2023-08-03 17:12:30 +0530

    [New] Added test cases for transitions referrals

--- app/javascript/components/transitions/referrals/summary.spec.js
+++ app/javascript/components/transitions/referrals/summary.spec.js
@@ -1,12 +1,15 @@
+<<<<<<< HEAD:app/javascript/components/transitions/referrals/summary.unit.test.js
 // Copyright (c) 2014 - 2023 UNICEF. All rights reserved.
 
 import { setupMountedComponent } from "../../../test";
 import DateTransitionsSummary from "../components/date-transitions-summary";
+=======
+import { mountedComponent, screen } from "../../../test-utils";
+>>>>>>> e5ec5b3b64 ([New] Added test cases for transitions referrals):app/javascript/components/transitions/referrals/summary.spec.js
 
 import ReferralSummary from "./summary";
 
 describe("<ReferralSummary />", () => {
-  let component;
   const props = {
     transition: {
       id: "4142488e-ccd9-4ac5-a3c1-c3c0fd063fc8",
@@ -28,13 +31,10 @@ describe("<ReferralSummary />", () => {
     }
   };
 
-  beforeEach(() => {
-    ({ component } = setupMountedComponent(ReferralSummary, props));
-  });
-
   it("renders divs with its corresponding class", () => {
-    expect(component.find("div.wrapperStyle")).to.have.lengthOf(1);
-    expect(component.find("div.titleHeaderStyle")).to.have.lengthOf(1);
-    expect(component.find(DateTransitionsSummary)).to.have.lengthOf(1);
+    mountedComponent(<ReferralSummary {...props} />);
+    expect(screen.getByText(/transition.status.done/i)).toBeInTheDocument();
+    expect(screen.getByText(/transition.type.referral/i)).toBeInTheDocument();
+    expect(screen.getByTestId("date")).toBeInTheDocument();
   });
 });
