b3d1c74a19d439533ed88239a8651c877fbbd51b
srinivasulu chimaladinne <srinivasulu.chimaladinne@trigyn.com>
2023-08-10 17:04:53 +0530

    [New] added test cases for attachments

---
+++ app/javascript/components/record-form/form/field-types/attachments/render-preview.spec.js
@@ -0,0 +1,34 @@
+import { screen, simpleMountedComponent } from "../../../../../test-utils";
+
+import AttachmentPreview from "./attachment-preview";
+import { ATTACHMENT_TYPES } from "./constants";
+import renderPreview from "./render-preview";
+
+describe("renderPreview", () => {
+  const css = {};
+  const deleteButton = null;
+
+  it("should return false if there is no data", () => {
+    expect(renderPreview(ATTACHMENT_TYPES.photo, {}, css, deleteButton)).toBeFalsy()
+  });
+
+  it("should return a span with the file name if attachment it's document", () => {
+    const file = {
+      data: {},
+      fileName: "test.txt"
+    };
+
+    simpleMountedComponent(renderPreview(ATTACHMENT_TYPES.document, file, css, deleteButton));(renderPreview(ATTACHMENT_TYPES.document, file, css, deleteButton));
+    expect(screen.getByText(/test.txt/i)).toBeInTheDocument();
+  });
+
+  it("should return an AttachmentPreview", () => {
+    const file = {
+      data: "abcd",
+      fileName: "test.png"
+    };
+
+    simpleMountedComponent(renderPreview(ATTACHMENT_TYPES.photo, file, css, deleteButton));
+    expect(screen.getByTestId('attachment')).toBeInTheDocument();
+  });
+});
