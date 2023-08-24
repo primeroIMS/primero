62f76b0f4d9512298af3d2a21e1fc0e43d263ad2
Divyanshu Bhardwaj <divyanshubhardwaj4@gmail.com>
2023-08-24 17:01:45 +0530

    Created New Test for admin/forms-list/components/form-filters

---
+++ app/javascript/components/pages/admin/forms-list/components/form-filters/component.spec.js
@@ -0,0 +1,35 @@
+import { mountedComponent, screen } from "test-utils";
+import { List } from "immutable";
+
+import FormExporter from "./component";
+
+describe("<FormsList />/components/<FormExporter />", () => {
+  const props = {
+    modules: List([
+      {
+        name: "Module 1",
+        unique_id: "module-1",
+        associated_record_types: ["record-type-1"]
+      },
+      {
+        name: "Module 2",
+        unique_id: "module-2",
+        associated_record_types: ["record-type-2"]
+      }
+    ]),
+    handleClearValue: () => {},
+    handleSetFilterValue: () => {}
+  };
+
+  beforeEach(() => {
+    mountedComponent(<FormExporter {...props} />);
+  });
+
+  it("renders clear button", () => {
+    expect(screen.getByText("clear")).toBeInTheDocument();
+  });
+
+  it("renders <FiltersExpansionPanel />", () => {
+    expect(screen.getAllByTestId("test")).toHaveLength(2);
+  });
+});
