cd7d713e79d27062c10cf524680a8f29a0e6dd19
Divyanshu Bhardwaj <divyanshubhardwaj4@gmail.com>
2023-09-20 17:25:19 +0530

    Created new test cases for pages-admin-form-builder-translations-note component

---
+++ app/javascript/components/pages/admin/form-builder/components/translations-note/component.spec.js
@@ -0,0 +1,32 @@
+import { fromJS } from "immutable";
+import { mountedFormComponent, screen } from "test-utils";
+
+import TranslationsNote from "./component";
+
+describe("<SettingsTab />", () => {
+  const props = {
+    moduleId: "primeromodule-my-module",
+    parentForm: "parent"
+  };
+
+  const state = fromJS({
+    forms: {
+      options: { lookups: [{ id: 1, unique_id: "lookup-form-group-my-module-parent" }] }
+    }
+  });
+
+  beforeEach(() => {
+    mountedFormComponent(<TranslationsNote {...props} />, state);
+  });
+
+  it("should render <SettingsTab />", () => {
+    expect(screen.getByText("forms.translations.note")).toBeInTheDocument();
+  });
+
+  it("should render a link to the lookup", () => {
+    const linkElement = screen.getByText("forms.translations.edit_form_group");
+    const hrefAttributeValue = linkElement.getAttribute("href");
+
+    expect(hrefAttributeValue).toBe("/admin/lookups/undefined/edit");
+  });
+});
