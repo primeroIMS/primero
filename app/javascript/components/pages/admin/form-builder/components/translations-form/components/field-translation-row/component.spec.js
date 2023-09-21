3229db2d119666e2abf0c5c627aaad97f737ea52
srinivasulu chimaladinne <srinivasulu.chimaladinne@trigyn.com>
2023-09-21 20:04:38 +0530

    [New] Added test cases for pages admin form builder components translations form components field translation

--- app/javascript/components/pages/admin/form-builder/components/translations-form/components/field-translation-row/component.spec.js
+++ app/javascript/components/pages/admin/form-builder/components/translations-form/components/field-translation-row/component.spec.js
@@ -2,13 +2,11 @@
 
 import { fromJS } from "immutable";
 
-import { FormSectionField } from "../../../../../../../form";
-import { setupMockFormComponent } from "../../../../../../../../test";
+import { mountedFormComponent, screen } from "../../../../../../../../test-utils";
 
 import FieldTranslationRow from "./component";
 
 describe("<FieldTranslationRow />", () => {
-  let component;
   const field1 = {
     name: "field_1",
     display_name: { en: "Field 1" }
@@ -30,26 +28,16 @@ describe("<FieldTranslationRow />", () => {
     }
   });
 
-  beforeEach(() => {
-    ({ component } = setupMockFormComponent(FieldTranslationRow, {
-      props: { field: fromJS(field1), selectedLocaleId: "fr" },
-      state
-    }));
-  });
 
+  const props = { field: fromJS(field1), selectedLocaleId: "fr" }
   it("should render <FieldTranslationRow />", () => {
-    expect(component.find(FieldTranslationRow)).to.have.lengthOf(1);
+    mountedFormComponent(<FieldTranslationRow {...props} />, state)
+    expect(screen.getByText('forms.manage')).toBeInTheDocument();
   });
 
   it("should render the <FormSectionField /> for the available languages", () => {
-    const expectedNames = [
-      "fields.field_1.display_name.en",
-      "fields.field_1.display_name.fr",
-      "fields.field_1.display_name.ar"
-    ];
 
-    const fields = component.find(FormSectionField).map(f => f.props().field.get("name"));
-
-    expect(fields).to.deep.equal(expectedNames);
+    mountedFormComponent(<FieldTranslationRow {...props} />, state)
+    expect(screen.getAllByRole('textbox')).toHaveLength(1);
   });
 });
