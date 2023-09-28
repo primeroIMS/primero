1fa8703c92d92f90409a12eeadf369a633791737
Joshua Toliver <jtoliver@quoininc.com>
2023-09-28 16:59:01 -0400

    Fixing push notifications keepalive timer running when not subscribed, running when logged out, not running on initial login when subscribed

--- app/javascript/components/push-notifications-toggle/use-push-notifications.js
+++ app/javascript/components/push-notifications-toggle/use-push-notifications.js
@@ -1,8 +1,9 @@
 import { useCallback, useEffect, useRef } from "react";
 import { workerTimers } from "react-idle-timer";
 import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
 
-import { POST_MESSAGES, PUSH_NOTIFICATION_SUBSCRIPTION_REFRESH_INTERVAL } from "../../config/constants";
+import { POST_MESSAGES, PUSH_NOTIFICATION_SUBSCRIPTION_REFRESH_INTERVAL, ROUTES } from "../../config/constants";
 import useMemoizedSelector from "../../libs/use-memoized-selector";
 import { getNotificationSubscription, getUserProperty } from "../user/selectors";
 import { toServerDateFormat } from "../../libs";
@@ -13,42 +14,73 @@ let refreshTimer;
 
 function usePushNotifications() {
   const dispatch = useDispatch();
+  const location = useLocation();
   const endpoint = useRef();
   const notificationEndpoint = useMemoizedSelector(state => getNotificationSubscription(state));
-  const receiveWebpush = useMemoizedSelector(state => getUserProperty(state, "receive_webpush"));
+  const receiveWebpush = useMemoizedSelector(state => getUserProperty(state, "receiveWebpush"));
   const userLoaded = useMemoizedSelector(state => getUserProperty(state, "loaded"));
+  const isAuthenticated = useMemoizedSelector(state => getUserProperty(state, "isAuthenticated"));
+
+  const locationNotLogout = location.pathname !== ROUTES.logout;
+  const shouldPingSubscription = notificationEndpoint && userLoaded && receiveWebpush && locationNotLogout;
 
   useEffect(() => {
     endpoint.current = notificationEndpoint;
   }, [notificationEndpoint]);
 
+  const pingSubscription = () => {
+    dispatch(
+      refreshNotificationSubscription({
+        data: {
+          updated_at: toServerDateFormat(Date.now(), { includeTime: true }),
+          notification_url: endpoint.current
+        }
+      })
+    );
+  };
+
   const startRefreshNotificationTimer = useCallback(() => {
-    refreshTimer = workerTimers.setInterval(() => {
-      dispatch(
-        refreshNotificationSubscription({
-          data: {
-            updated_at: toServerDateFormat(Date.now(), { includeTime: true }),
-            notification_url: endpoint.current
-          }
-        })
-      );
-    }, PUSH_NOTIFICATION_SUBSCRIPTION_REFRESH_INTERVAL);
-  }, [notificationEndpoint]);
+    if (shouldPingSubscription) {
+      refreshTimer = workerTimers.setInterval(() => {
+        pingSubscription();
+      }, PUSH_NOTIFICATION_SUBSCRIPTION_REFRESH_INTERVAL);
+    }
+  }, [notificationEndpoint, userLoaded, receiveWebpush, locationNotLogout]);
 
   const stopRefreshNotificationTimer = useCallback(() => {
     if (refreshTimer) {
       workerTimers.clearInterval(refreshTimer);
+      refreshTimer = undefined;
     }
   });
 
   useEffect(() => {
+    if (shouldPingSubscription) {
+      pingSubscription();
+    }
+  }, [userLoaded]);
+
+  useEffect(() => {
+    startRefreshNotificationTimer();
+  }, [notificationEndpoint, userLoaded, receiveWebpush]);
+
+  useEffect(() => {
     if (!receiveWebpush && userLoaded) {
       stopRefreshNotificationTimer();
-      postMessage({
-        type: POST_MESSAGES.UNSUBSCRIBE_NOTIFICATIONS
-      });
+      postMessage(
+        {
+          type: POST_MESSAGES.UNSUBSCRIBE_NOTIFICATIONS
+        },
+        window.origin
+      );
     }
   }, [receiveWebpush]);
+
+  useEffect(() => {
+    return () => {
+      stopRefreshNotificationTimer();
+    };
+  }, [notificationEndpoint]);
 
   return {
     startRefreshNotificationTimer,
