a27c468daabe2b2713f2ce7cb9f886d606a8d7bc
Awen Saunders <awen@jozian.com>
2023-10-23 15:05:30 +0100

    Spin dismiss button into separate component

---
+++ app/javascript/components/internal-alert/components/dismiss-button/component.jsx
@@ -0,0 +1,19 @@
+import { IconButton } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+import PropTypes from "prop-types";
+
+import css from "./styles.css";
+
+const Component = ({ handler }) => {
+  return (
+    <IconButton className={css.dismissButton} onClick={handler}>
+      <CloseIcon />
+    </IconButton>
+  );
+};
+
+Component.displayName = "InternalAlertDismissButton";
+Component.propTypes = {
+  handler: PropTypes.func.isRequired
+};
+export default Component;
--- app/javascript/components/internal-alert/components/item/component.jsx
+++ app/javascript/components/internal-alert/components/item/component.jsx
@@ -1,22 +1,14 @@
 import PropTypes from "prop-types";
-import { IconButton } from "@material-ui/core";
-import CloseIcon from "@material-ui/icons/Close";
+
+import InternalAlertDismissButton from "../dismiss-button";
 
 import css from "./styles.css";
 
 const Component = ({ item }) => {
-  const renderDismissButton = handler => {
-    return (
-      <IconButton className={css.dismissButton} onClick={handler}>
-        <CloseIcon />
-      </IconButton>
-    );
-  };
-
   return (
     <div className={css.alertItemElement}>
       <span>{item.get("message")}</span>
-      {item.get("onDismiss") && renderDismissButton(item.get("onDismiss"))}
+      {item.get("onDismiss") && InternalAlertDismissButton({ handler: item.get("onDismiss") })}
     </div>
   );
 };
