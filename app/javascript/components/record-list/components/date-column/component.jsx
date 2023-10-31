c220033a9150c051adf8496401e242dc235f783a
Joshua Toliver <jtoliver@quoininc.com>
2023-10-31 09:51:15 -0400

    R2-2669: Fixing date formating on record index list

--- app/javascript/components/record-list/components/date-column/component.jsx
+++ app/javascript/components/record-list/components/date-column/component.jsx
@@ -3,7 +3,7 @@
 import PropTypes from "prop-types";
 
 import { ConditionalWrapper } from "../../../../libs";
-import { DATE_TIME_FORMAT, LOCALE_KEYS } from "../../../../config";
+import { DATE_FORMAT, DATE_TIME_FORMAT, LOCALE_KEYS } from "../../../../config";
 import { useI18n } from "../../../i18n";
 import NepaliCalendar from "../../../nepali-calendar-input";
 
@@ -11,7 +11,9 @@ import css from "./styles.css";
 
 const Component = ({ rowAvailable, wrapper, value, valueWithTime }) => {
   const i18n = useI18n();
-  const parsedValue = valueWithTime ? i18n.localizeDate(value, DATE_TIME_FORMAT) : i18n.localizeDate(value);
+  const parsedValue = valueWithTime
+    ? i18n.localizeDate(value, DATE_TIME_FORMAT)
+    : i18n.localizeDate(value, DATE_FORMAT);
 
   const children =
     i18n.locale === LOCALE_KEYS.ne ? (
