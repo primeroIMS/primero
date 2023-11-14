72955c96954d0d844e58801292ef4bbaf8f40dc2
Joshua Toliver <jtoliver@quoininc.com>
2023-11-14 11:09:39 -0500

    Fixing csp

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,7 +4,7 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-return unless Rails.env.production?
+# return unless Rails.env.production?
 
 self_sources = %i[self https]
 
@@ -18,8 +18,9 @@ storage_sources =
 
 media_sources = storage_sources + %i[data blob]
 font_and_image_sources = self_sources + %i[data blob]
-style_sources = self_sources
+style_sources = self_sources + %i[strict_dynamic]
 child_sources = self_sources + %i[blob]
+script_sources = self_sources + %i[strict_dynamic]
 
 Rails.application.config.content_security_policy do |policy|
   policy.default_src(*self_sources)
@@ -27,10 +28,11 @@ Rails.application.config.content_security_policy do |policy|
   policy.img_src(*font_and_image_sources)
   policy.media_src(*media_sources)
   policy.object_src(:none)
-  policy.script_src(*self_sources)
+  policy.script_src(*script_sources)
   policy.style_src(*style_sources)
   policy.child_src(*child_sources)
   policy.frame_src(:none)
+  policy.base_uri(:self)
 
   # Specify URI for violation reports
   # policy.report_uri "/csp-violation-report-endpoint"
