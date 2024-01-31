a9bcd931c1e55234f35475ac69ab35e0e46ee640
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2024-01-31 11:00:37 -0500

    Draft language service for diacritics, arabic vowels, etc.

---
+++ app/services/language_service.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2014 - 2023 UNICEF. All rights reserved.
+
+# A service to strip diacritic marks from various languages
+class LanguageService
+  # rubocop:disable Layout/HashAlignment
+  # rubocop:disable Layout/LineLength
+  LATIN_GREEK = {
+    from: 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖØÙÚÛÜÝàáâãäåæçèéêëìíîïñòóôõöøùúûüýÿΆΈΊΌΐάέήίΰϊϋόύώỏἀἁἂἃἄἅἆἈἉἊἌἍἎἐἑἒἓἔἕἘἙἜἝἠἡἢἣἤἥἦἧἨἩἫἬἭἮἯἰἱἲἳἴἵἶἷἸἹἼἽἾὀὁὂὃὄὅὈὉὊὋὌὍὐὑὓὔὕὖὗὙὝὠὡὢὣὤὥὦὧὨὩὫὬὭὮὯὰὲὴὶὸὺὼᾐᾑᾓᾔᾕᾖᾗᾠᾤᾦᾧᾰᾱᾳᾴᾶᾷᾸᾹῂῃῄῆῇῐῑῒῖῗῘῙῠῡῢῥῦῨῩῬῳῴῶῷῸ',
+    to:   'AAAAAAÆCEEEEIIIINOOOOOOUUUUYaaaaaaæceeeeiiiinoooooouuuuyyΑΕΙΟιαεηιυιυουωoαααααααΑΑΑΑΑΑεεεεεεΕΕΕΕηηηηηηηηΗΗΗΗΗΗΗιιιιιιιιΙΙΙΙΙοοοοοοΟΟΟΟΟΟυυυυυυυΥΥωωωωωωωωΩΩΩΩΩΩΩαεηιουωηηηηηηηωωωωααααααΑΑηηηηηιιιιιΙΙυυυρυΥΥΡωωωωΟ'
+  }.freeze
+
+  CYRILLIC = {
+    from: 'ҐЁЇЙґёїй',
+    to:   'ГЕІИгеіи'
+  }.freeze
+  # rubocop:enable Layout/HashAlignment
+  # rubocop:enable Layout/LineLength
+
+  DIACRITICS = {
+    from: LATIN_GREEK[:from] + CYRILLIC[:from],
+    to: LATIN_GREEK[:to] + CYRILLIC[:to]
+  }.freeze
+
+  def self.strip_diacritics(value)
+    value.unicode_normalize(:nfc).tr(DIACRITICS[:from], DIACRITICS[:to])
+  end
+
+  def self.strip_arabic_vowels(value)
+    # TODO: implement
+  end
+
+  def self.latin?(value)
+    # TODO: implement
+  end
+end
