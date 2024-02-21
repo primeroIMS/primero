355914e76e26f31c55b56e496352b7e9d01d9676
Dennis Hernandez <dhernand@quoininc.com>
2024-02-21 15:12:39 -0600

    R2-2758 - has_incidents is now stored in the record data

--- app/models/child.rb
+++ app/models/child.rb
@@ -68,7 +68,7 @@ class Child < ApplicationRecord
     :registry_id_display, :registry_name, :registry_no, :registry_location_current,
     :urgent_protection_concern, :child_preferences_section, :family_details_section, :care_arrangements_section,
     :duplicate, :cp_case_plan_subform_case_plan_interventions, :has_case_plan,
-    :family_member_id, :family_id_display, :family_number
+    :family_member_id, :family_id_display, :family_number, :has_incidents
   )
   # rubocop:enable Naming/VariableNumber
 
@@ -152,7 +152,7 @@ class Child < ApplicationRecord
     date(:assessment_due_dates, multiple: true) { Tasks::AssessmentTask.from_case(self).map(&:due_date) }
     date(:case_plan_due_dates, multiple: true) { Tasks::CasePlanTask.from_case(self).map(&:due_date) }
     date(:followup_due_dates, multiple: true) { Tasks::FollowUpTask.from_case(self).map(&:due_date) }
-    boolean(:has_incidents) { incidents.size.positive? }
+    boolean :has_incidents
   end
 
   validate :validate_date_of_birth
@@ -160,6 +160,7 @@ class Child < ApplicationRecord
   before_save :sync_protection_concerns
   before_save :stamp_registry_fields
   before_save :calculate_has_case_plan
+  before_save :calculate_has_incidents
   before_create :hide_name
   after_save :save_incidents
 
@@ -300,6 +301,10 @@ class Child < ApplicationRecord
     end
 
     has_case_plan
+  end
+
+  def calculate_has_incidents
+    self.has_incidents = incidents.size.positive?
   end
 
   def sync_protection_concerns
