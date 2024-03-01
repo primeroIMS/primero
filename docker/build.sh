38d52f6d3588af5bcdfda398ab1287b6f6a1323c
akshaypawartw <akshay.pawar@thoughtworks.com>
2024-03-01 11:28:02 +0530

    pom 49 | cleanup migration image references

--- docker/build.sh
+++ docker/build.sh
@@ -49,7 +49,7 @@ build_registry=${b:-""}
 BUILD_NGINX="docker build -f nginx/Dockerfile . -t primero/nginx:${tag} -t ${repository}/primero/nginx:${tag} --build-arg NGINX_UID=${NGINX_UID} --build-arg NGINX_GID=${NGINX_GID} --build-arg BUILD_REGISTRY=${build_registry}"
 BUILD_SOLR="docker build -f solr/Dockerfile ../ -t primero/solr:${tag} -t ${repository}/primero/solr:${tag} --build-arg BUILD_REGISTRY=${build_registry}"
 BUILD_APP="docker build -f application/Dockerfile ../ -t bahmni-msf/primero:${tag} -t ${repository}/bahmni-msf/primero:${tag} --build-arg APP_ROOT=${APP_ROOT} --build-arg RAILS_LOG_PATH=${RAILS_LOG_PATH} --build-arg APP_UID=${APP_UID} --build-arg APP_GID=${APP_GID} --build-arg BUILD_REGISTRY=${build_registry}"
-BUILD_MIGRATION="docker build -f migration/Dockerfile ../ -t primero/migration:${tag} -t ${repository}/primero/migration:${tag} --build-arg BUILD_REGISTRY=${build_registry} --build-arg PRIMERO_VERSION=${tag}"
+#BUILD_MIGRATION="docker build -f migration/Dockerfile ../ -t primero/migration:${tag} -t ${repository}/primero/migration:${tag} --build-arg BUILD_REGISTRY=${build_registry} --build-arg PRIMERO_VERSION=${tag}"
 
 apply_tags_and_push () {
   local image=${1}
@@ -84,13 +84,13 @@ case ${image} in
   application)
     eval "${BUILD_APP}" && apply_tags_and_push application
     ;;
-  migration)
-    eval "${BUILD_APP}" && apply_tags_and_push application
-    eval "${BUILD_MIGRATION}" && apply_tags_and_push migration
-    ;;
+  # migration)
+  #   eval "${BUILD_APP}" && apply_tags_and_push application
+  #   eval "${BUILD_MIGRATION}" && apply_tags_and_push migration
+  #   ;;
   all)
     eval "${BUILD_APP}" && apply_tags_and_push application
-    eval "${BUILD_MIGRATION}" && apply_tags_and_push migration
+    # eval "${BUILD_MIGRATION}" && apply_tags_and_push migration
     eval "${BUILD_SOLR}" && apply_tags_and_push solr
     eval "${BUILD_NGINX}" && apply_tags_and_push nginx
     ;;
