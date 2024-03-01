01d6c4ef76e9622a47ecd80f6be348aeaee02a80
Alberto Espinoza <aespinoz@quoininc.com>
2024-03-01 16:31:28 -0600

    R2-2787 Fix ElapsedReportingTimeRape indicator, missing order

--- app/models/managed_reports/indicators/elapsed_reporting_time_rape.rb
+++ app/models/managed_reports/indicators/elapsed_reporting_time_rape.rb
@@ -26,6 +26,7 @@ class ManagedReports::Indicators::ElapsedReportingTimeRape < ManagedReports::Sql
         #{user_scope_query(current_user)&.prepend('and ')}
         group by data ->> 'elapsed_reporting_time'
         #{grouped_date_query(params['grouped_by'], date_param)&.prepend(', ')}
+        order by id
       }
     end
     # rubocop:enable Metrics/MethodLength
