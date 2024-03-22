6b4bbaaa2f25e4d80e83531d735b1ede69620f80
srinivasulu chimaladinne <srinivasulu.chimaladinne@trigyn.com>
2024-03-22 16:29:45 +0530

    Resolved PR comments

--- app/javascript/components/pages/dashboard/container.spec.js
+++ app/javascript/components/pages/dashboard/container.spec.js
@@ -5,7 +5,6 @@ import { mountedComponent, screen } from "../../../test-utils";
 import Dashboard from "./container";
 
 describe("<Dashboard />", () => {
-
   const initialState = fromJS({
     user: {
       reportingLocationConfig: {
@@ -21,46 +20,57 @@ describe("<Dashboard />", () => {
     mountedComponent(<Dashboard />, {}, initialState);
     expect(screen.getAllByTestId("page-heading")).toHaveLength(1);
   });
-  it("should render a <PageHeading /> component", () => {
+
+  it("should render a navigation title", () => {
     mountedComponent(<Dashboard />, {}, initialState);
-    expect(screen.getByText(/navigation.home/i)).toBeInTheDocument();
+    expect(screen.getByText("navigation.home")).toBeInTheDocument();
   });
+
   it("should render a <PageContent /> component", () => {
     mountedComponent(<Dashboard />, {}, initialState);
     expect(screen.getByTestId("page-heading")).toBeInTheDocument();
   });
-  it("should render a <Overview /> component", () => {
+
+  it("should render a dashboard overview component", () => {
     mountedComponent(<Dashboard />, {}, initialState);
     expect(screen.getByText(/navigation.home/i)).toBeInTheDocument();
   });
+
   it("should render a <SharedFromMyTeam /> component", () => {
     mountedComponent(<Dashboard />, {}, initialState);
-    expect(screen.getByTestId("page-heading")).toBeInTheDocument();
+    expect(screen.getByText("navigation.home")).toBeInTheDocument();
   });
+
   it("should render a <SharedWithMyTeam /> component", () => {
     mountedComponent(<Dashboard />, {}, initialState);
     expect(screen.getByText(/navigation.home/i)).toBeInTheDocument();
   });
+
   it("should render a <WorkflowIndividualCases /> component", () => {
     mountedComponent(<Dashboard />, {}, initialState);
     expect(screen.getByTestId("page-heading")).toBeInTheDocument();
   });
+
   it("should render a <Approvals /> component", () => {
     mountedComponent(<Dashboard />, {}, initialState);
     expect(screen.getByText(/navigation.home/i)).toBeInTheDocument();
   });
+
   it("should render a <OverdueTasks /> component", () => {
     mountedComponent(<Dashboard />, {}, initialState);
     expect(screen.getByTestId(/page-heading/i)).toBeInTheDocument();
   });
+
   it("should render a <WorkflowTeamCases /> component", () => {
     mountedComponent(<Dashboard />, {}, initialState);
     expect(screen.getByText(/navigation.home/i)).toBeInTheDocument();
   });
+
   it("should render a <ReportingLocation /> component", () => {
     mountedComponent(<Dashboard />, {}, initialState);
     expect(screen.getByTestId(/page-heading/i)).toBeInTheDocument();
   });
+
   it("should render a <ProtectionConcern /> component", () => {
     mountedComponent(<Dashboard />, {}, initialState);
     expect(screen.getByText(/navigation.home/i)).toBeInTheDocument();
