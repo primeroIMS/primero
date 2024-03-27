e963dc1e8f6b0e19d69385ec730cb9ebcec50656
srinivasulu chimaladinne <srinivasulu.chimaladinne@trigyn.com>
2024-03-27 17:58:07 +0530

    Resolved PR comments

--- app/javascript/components/pages/admin/forms-list/component.spec.js
+++ app/javascript/components/pages/admin/forms-list/component.spec.js
@@ -117,7 +117,7 @@ describe("<FormsList />", () => {
 
     it("renders <FormFilters/>", () => {
         mountedComponent(<FormsList  />, stateWithoutRecords)
-        expect(screen.queryAllByTestId('form-group')).toHaveLength(0);
+        expect(screen.getByTestId('form-list')).toBeInTheDocument()
     
     });
     it("does not renders form sections", () => {
@@ -136,7 +136,7 @@ describe("<FormsList />", () => {
 
     it("disable the <FormFilters/>", () => {
         mountedComponent(<FormsList  />, stateReorderEnabled)
-        expect(screen.getAllByTestId('form-group')).toHaveLength(2);
+        expect(screen.getByTestId('forms-list')).toHaveClass('disabledFilters');
     });
   });
 });
