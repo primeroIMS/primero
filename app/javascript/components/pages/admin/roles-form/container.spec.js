a6c7c7d0e08284875ff4327946a17f94420f113c
Divyanshu Bhardwaj <divyanshubhardwaj4@gmail.com>
2024-04-05 11:33:10 +0530

    Updated test cases as per comment

--- app/javascript/components/pages/admin/roles-form/container.spec.js
+++ app/javascript/components/pages/admin/roles-form/container.spec.js
@@ -3,6 +3,7 @@ import { mountedComponent, screen } from "test-utils";
 
 import { ACTIONS } from "../../../permissions";
 import { ROUTES } from "../../../../config/constants";
+import { FormSectionRecord } from "../../../form/records";
 
 import RolesForm from "./container";
 
@@ -50,6 +51,92 @@ describe("<RolesForm />", () => {
 
     it("will not render actions menu", () => {
       expect(document.querySelector("#form-record-actions")).not.toBeInTheDocument();
+    });
+  });
+  describe("Show", () => {
+    beforeEach(() => {
+      const state = fromJS({
+        records: {
+          admin: {
+            roles: {
+              selectedRole: {
+                id: 10,
+                name: "Test Role",
+                module_unique_ids: ["primeromodule-cp"]
+              },
+              loading: false,
+              errors: false,
+              serverErrors: []
+            }
+          }
+        },
+        forms: {
+          formSections: {
+            1: FormSectionRecord({
+              id: 1,
+              unique_id: "form_1",
+              fields: [
+                {
+                  name: "field_1"
+                }
+              ],
+              visible: true,
+              is_nested: false,
+              parent_form: "case",
+              module_ids: ["primeromodule-cp"]
+            }),
+            2: FormSectionRecord({
+              id: 2,
+              unique_id: "core_form_1",
+              fields: [],
+              visible: true,
+              is_nested: false,
+              core_form: true,
+              parent_form: "case",
+              module_ids: ["primeromodule-cp"]
+            })
+          }
+        },
+        user: {
+          permissions: {
+            roles: [ACTIONS.MANAGE]
+          }
+        },
+        application: {
+          modules: [
+            {
+              unique_id: "primeromodule-cp"
+            }
+          ]
+        }
+      });
+
+      mountedComponent(<RolesForm mode="show" />, state, ["/admin/roles/10"]);
+    });
+
+    xit("renders role form sections", () => {
+      const formSections = screen.getAllByTestId("form-section");
+
+      expect(formSections).toHaveLength(8);
+    });
+
+    it("renders core forms disabled and empty", () => {
+      const disabledInputs = screen.getAllByRole("textbox", { class: "Mui-disabled" });
+
+      expect(disabledInputs.length).toBeGreaterThan(0);
+    });
+
+    xit("renders the selected modules", () => {
+      const autocomplete = screen.getAllByTestId("autocomplete");
+      const selectedModuleExists = autocomplete
+        .map(element => element.props())
+        .some(props => props.name === "module_unique_ids" && props.value.includes("primeromodule-cp"));
+
+      expect(selectedModuleExists).toBeTruthy();
+    });
+
+    it("renders the roles-actions component", () => {
+      expect(document.querySelector("#long-menu")).toBeInTheDocument();
     });
   });
 });
