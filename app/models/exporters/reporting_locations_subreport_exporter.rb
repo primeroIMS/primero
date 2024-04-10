4e9aa6f6008e724430c5178be00eadb8cb8724d2
Joshua Toliver <jtoliver@quoininc.com>
2024-04-10 19:27:37 -0400

    Updating copyright date

--- app/models/exporters/reporting_locations_subreport_exporter.rb
+++ app/models/exporters/reporting_locations_subreport_exporter.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Copyright (c) 2014 - 2023 UNICEF. All rights reserved.
+# Copyright (c) 2014 - 2024 UNICEF. All rights reserved.
 
 # Concern for Reporting Locations Subreport Exporter
 class Exporters::ReportingLocationsSubreportExporter < Exporters::FieldSubreportExporter
