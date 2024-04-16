6f8571fb552a0f3a6df0b32f959e79ba981c2927
srinivasulu chimaladinne <srinivasulu.chimaladinne@trigyn.com>
2024-04-16 13:21:16 +0530

    Reverted deleted file

---
+++ app/javascript/components/pages/dashboard/components/overdue-tasks/compoent.uinit.test.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2014 - 2023 UNICEF. All rights reserved.
+
+import * as constants from "./constants";
+
+describe("<OverdueTasks> - pages/dashboard/components/overdue-tasks/constants", () => {
+  const clone = { ...constants };
+
+  it("should have known properties", () => {
+    expect(clone).to.be.an("object");
+    ["NAME"].forEach(property => {
+      expect(clone).to.have.property(property);
+      delete clone[property];
+    });
+
+    expect(clone).to.be.empty;
+  });
+});
