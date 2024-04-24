10929d16bcb6ebbc2f57756018b4db444cc3db77
srinivasulu chimaladinne <srinivasulu.chimaladinne@trigyn.com>
2024-04-24 12:31:53 +0530

    Fixed lint issue

--- app/javascript/components/session-timeout-dialog/component.spec.js
+++ app/javascript/components/session-timeout-dialog/component.spec.js
@@ -7,35 +7,34 @@ import { mountedComponent, screen } from "../../test-utils";
 import SessionTimeoutDialog from "./component";
 
 describe("<SessionTimeoutDialog />", () => {
-  let component;
   let clock;
 
   clock = sinon.useFakeTimers();
   createMocks();
   it("should idle after 15 minutes", () => {
-    mountedComponent(<SessionTimeoutDialog />,  fromJS({
-        application: {
-          userIdle: false
-        }
-      }))
- 
+    mountedComponent(<SessionTimeoutDialog />, fromJS({
+      application: {
+        userIdle: false
+      }
+    }))
+
     expect(screen.queryByRole('dialog')).toBeNull()
 
   });
 
   describe("when user is offline", () => {
     it("should not idle after 15 minutes", () => {
-        mountedComponent(<SessionTimeoutDialog />,  fromJS({
-            application: {
-              userIdle: false
-            },
-            connectivity: {
-                online: false
-              }
-          }))
-  
-          expect(screen.queryByRole('dialog')).toBeNull()
-     
+      mountedComponent(<SessionTimeoutDialog />, fromJS({
+        application: {
+          userIdle: false
+        },
+        connectivity: {
+          online: false
+        }
+      }))
+
+      expect(screen.queryByRole('dialog')).toBeNull()
+
     });
   });
 });
