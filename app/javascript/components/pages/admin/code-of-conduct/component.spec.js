3822f7639b48afff4c3f08c17afb90ffd00c98d8
Joshua Toliver <jtoliver@quoininc.com>
2024-05-17 12:36:23 -0400

    Refactoring code of conduct spec

--- app/javascript/components/pages/admin/code-of-conduct/component.spec.js
+++ app/javascript/components/pages/admin/code-of-conduct/component.spec.js
@@ -1,5 +1,4 @@
 import { mountedComponent, screen } from "test-utils";
-import { fromJS } from "immutable";
 
 import { MODES } from "../../../../config";
 import { ACTIONS } from "../../../permissions";
@@ -7,7 +6,24 @@ import { ACTIONS } from "../../../permissions";
 import CodeOfConductForm from "./component";
 
 describe("pages/admin/<CodeOfConductForm />", () => {
-  const initialState = fromJS({
+  beforeAll(() => {
+    // eslint-disable-next-line no-extend-native
+    Date.prototype.getTimezoneOffset = jest.fn(() => 0);
+  });
+
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+
+  const userPermissions = {
+    user: {
+      permissions: {
+        codes_of_conduct: [ACTIONS.MANAGE]
+      }
+    }
+  };
+
+  const initialState = {
     records: {
       codeOfConduct: {
         data: {
@@ -19,12 +35,8 @@ describe("pages/admin/<CodeOfConductForm />", () => {
         }
       }
     },
-    user: {
-      permissions: {
-        codes_of_conduct: [ACTIONS.MANAGE]
-      }
-    }
-  });
+    ...userPermissions
+  };
 
   describe("when isShow", () => {
     const props = {
@@ -35,8 +47,17 @@ describe("pages/admin/<CodeOfConductForm />", () => {
       mountedComponent(<CodeOfConductForm {...props} />, initialState);
       expect(screen.getByLabelText("code_of_conduct.field.created_by")).toHaveValue("test_user");
       expect(screen.getByLabelText("code_of_conduct.field.title *")).toHaveValue("Some Title");
-      expect(screen.getByText("Some Content")).toBeInTheDocument();
-      expect(screen.getByLabelText("code_of_conduct.field.created_on")).toHaveValue("14-t-2021 19:22");
+      expect(screen.getByLabelText("code_of_conduct.field.content *")).toHaveValue("Some Content");
+      // Month is translated to t, due to i18n mocks in jest
+      expect(screen.getByLabelText("code_of_conduct.field.created_on")).toHaveValue("14-t-2021 13:52");
+    });
+
+    it("should render an empty form if code of conduct is not present", () => {
+      mountedComponent(<CodeOfConductForm {...props} />, { ...userPermissions });
+      expect(screen.getByLabelText("code_of_conduct.field.created_by")).toHaveValue("");
+      expect(screen.getByLabelText("code_of_conduct.field.title *")).toHaveValue("");
+      expect(screen.getByLabelText("code_of_conduct.field.content *")).toHaveValue("");
+      expect(screen.getByLabelText("code_of_conduct.field.created_on")).toHaveValue("");
     });
   });
 });
