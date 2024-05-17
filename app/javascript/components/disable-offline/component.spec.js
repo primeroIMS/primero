099f59c84d747edfdccee4975b094c44dfcca3c4
Joshua Toliver <jtoliver@quoininc.com>
2024-05-17 11:18:46 -0400

    Checking if tooltip renders in spec

--- app/javascript/components/disable-offline/component.spec.js
+++ app/javascript/components/disable-offline/component.spec.js
@@ -1,17 +1,48 @@
-import { mountedComponent, screen } from "test-utils";
+import { mountedComponent, screen, fireEvent, waitFor, expectNever } from "test-utils";
 import { fromJS } from "immutable";
 
 import DisableOffline from "./component";
 
 describe("<DisableOffline /> - Component", () => {
-  const props = {
-    overrideCondition: true,
-    text: "offline"
-  };
-
-  it("renders element in offline state if offline", () => {
+  it("renders element with tooltip if offline", async () => {
     mountedComponent(
-      <DisableOffline {...props}>
+      <DisableOffline>
+        <div>element</div>
+      </DisableOffline>,
+      {
+        connectivity: {
+          online: false,
+          serverOnline: true
+        }
+      }
+    );
+
+    fireEvent.mouseEnter(screen.getByText("element"));
+    await waitFor(() => expect(screen.getByRole("tooltip")).toBeInTheDocument());
+  });
+
+  it("renders element with no tooltip if online", async () => {
+    mountedComponent(
+      <DisableOffline>
+        <div>element</div>
+      </DisableOffline>,
+      fromJS({
+        connectivity: {
+          online: true,
+          serverOnline: true
+        }
+      })
+    );
+
+    fireEvent.mouseEnter(screen.getByText("element"));
+    await expectNever(() => {
+      expect(screen.getByRole("tooltip")).toBeInTheDocument();
+    });
+  });
+
+  it("renders element and tooltip if overrideCondition", async () => {
+    mountedComponent(
+      <DisableOffline overrideCondition>
         <div>element</div>
       </DisableOffline>,
       fromJS({
@@ -21,6 +52,8 @@ describe("<DisableOffline /> - Component", () => {
         }
       })
     );
-    expect(screen.getByText("element")).toBeInTheDocument();
+
+    fireEvent.mouseEnter(screen.getByText("element"));
+    await waitFor(() => expect(screen.getByRole("tooltip")).toBeInTheDocument());
   });
 });
