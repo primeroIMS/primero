119a849f9dda769b1393f9a109cc6cb00a8f44f9
Dennis Hernandez <dhernand@quoininc.com>
2024-05-20 16:40:56 -0600

    Fix lint

--- app/javascript/components/pages/admin/user-groups-list/container.spec.js
+++ app/javascript/components/pages/admin/user-groups-list/container.spec.js
@@ -1,6 +1,6 @@
 import { fromJS } from "immutable";
 
-import { mountedComponent, screen, stub, userEvent } from "../../../../test-utils";
+import { mountedComponent, screen, userEvent } from "../../../../test-utils";
 import { listHeaders, lookups } from "../../../../test";
 import { ACTIONS } from "../../../permissions";
 
@@ -8,7 +8,6 @@ import NAMESPACE from "./namespace";
 import UserGroupsList from "./container";
 
 describe("<UserGroupsList />", () => {
-  let stubI18n = null;
   const dataLength = 30;
   const data = Array.from({ length: dataLength }, (_, i) => ({
     id: i + 1,
@@ -17,52 +16,53 @@ describe("<UserGroupsList />", () => {
     description: `Test description ${i + 1}`
   }));
 
-    stubI18n = stub(window.I18n, "t").withArgs("messages.record_list.of").returns("of");
-    const initialState = fromJS({
-      records: {
-        user_groups: {
-          data,
-          metadata: { total: dataLength, per: 20, page: 1 },
-          loading: false,
-          errors: false
-        }
-      },
-      user: {
-        permissions: {
-          users: [ACTIONS.MANAGE]
-        },
-        listHeaders: {
-          user_groups: listHeaders(NAMESPACE)
-        }
-      },
-      forms: {
-        options: {
-          lookups: lookups()
-        }
+  const initialState = fromJS({
+    records: {
+      user_groups: {
+        data,
+        metadata: { total: dataLength, per: 20, page: 1 },
+        loading: false,
+        errors: false
       }
-    });
+    },
+    user: {
+      permissions: {
+        users: [ACTIONS.MANAGE]
+      },
+      listHeaders: {
+        user_groups: listHeaders(NAMESPACE)
+      }
+    },
+    forms: {
+      options: {
+        lookups: lookups()
+      }
+    }
+  });
 
   it("should render record list table", () => {
-    mountedComponent(<UserGroupsList />, initialState, [`/admin/${NAMESPACE}`])
-    expect(screen.getByRole('grid')).toBeInTheDocument();
+    mountedComponent(<UserGroupsList />, initialState, [`/admin/${NAMESPACE}`]);
+    expect(screen.getByRole("grid")).toBeInTheDocument();
   });
 
   it("should trigger a sort action when a header is clicked", () => {
-    mountedComponent(<UserGroupsList />, initialState, [`/admin/${NAMESPACE}`])
-    expect(screen.getByTestId('headcol-0')).toBeInTheDocument();
+    mountedComponent(<UserGroupsList />, initialState, [`/admin/${NAMESPACE}`]);
+    expect(screen.getByTestId("headcol-0")).toBeInTheDocument();
   });
 
   it("should trigger a valid action with next page when clicking next page", async () => {
     const user = userEvent.setup();
+
     mountedComponent(<UserGroupsList />, initialState, [`/admin/${NAMESPACE}`]);
-    await user.click(screen.getByTestId('pagination-next'));
+    await user.click(screen.getByTestId("pagination-next"));
     expect(screen.getByText(/Test description 30/i)).toBeInTheDocument();
   });
 
   it("should set the filters when apply is clicked", async () => {
     const user = userEvent.setup();
+
     mountedComponent(<UserGroupsList />, initialState, [`/admin/${NAMESPACE}`]);
-    await user.click(screen.getByTestId('pagination-next'));
+    await user.click(screen.getByTestId("pagination-next"));
     expect(screen.getByText(/Test description 30/i)).toBeInTheDocument();
-  })
+  });
 });
