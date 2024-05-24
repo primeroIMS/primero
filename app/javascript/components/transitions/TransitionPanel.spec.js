111134ccbf273ef76d53573a2063f9996bb35668
Alberto Espinoza <aespinoz@quoininc.com>
2024-05-24 11:19:31 -0600

    Adding test for Accordion component

--- app/javascript/components/transitions/TransitionPanel.spec.js
+++ app/javascript/components/transitions/TransitionPanel.spec.js
@@ -7,9 +7,15 @@ describe("<TransitionPanel />", () => {
     children: <p>This is a children</p>
   };
 
-  it("renders a <p> as children of  TransitionPanel", () => {
+  it("renders a <p> as children of TransitionPanel", () => {
     mountedComponent(<TransitionPanel {...props} />);
 
     expect(screen.getByText(/This is a children/i)).toBeInTheDocument();
+  });
+
+  it("renders an Accordion component", () => {
+    mountedComponent(<TransitionPanel {...props} />);
+
+    expect(screen.getByTestId("accordion")).toBeInTheDocument();
   });
 });
