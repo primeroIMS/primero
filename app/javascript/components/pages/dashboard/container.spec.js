3beea9dff2853197eb34fa9872bb55a7bc82b5c2
Joshua Toliver <jtoliver@quoininc.com>
2024-05-24 12:23:37 -0400

    Refactoring spec

--- app/javascript/components/pages/dashboard/container.spec.js
+++ app/javascript/components/pages/dashboard/container.spec.js
@@ -1,9 +1,9 @@
 import { fromJS } from "immutable";
 
 import { mountedComponent, screen } from "../../../test-utils";
+import { ACTIONS } from "../../permissions";
 
 import Dashboard from "./container";
-import { ACTIONS } from "../../permissions";
 
 describe("<Dashboard />", () => {
   const initialState = fromJS({
@@ -15,78 +15,73 @@ describe("<Dashboard />", () => {
         label_keys: ["district"]
       },
       permissions: {
-        dashboards: [ACTIONS.DASH_FLAGS, ACTIONS.DASH_CASE_RISK, ACTIONS.DASH_SHARED_FROM_MY_TEAM, 
+        dashboards: [
+          ACTIONS.DASH_FLAGS,
+          ACTIONS.DASH_CASE_RISK,
+          ACTIONS.DASH_SHARED_FROM_MY_TEAM,
           ACTIONS.DASH_CASES_BY_TASK_OVERDUE_ASSESSMENT,
-        ACTIONS.DASH_CASES_BY_TASK_OVERDUE_CASE_PLAN,
-        ACTIONS.DASH_CASES_BY_TASK_OVERDUE_SERVICES,
-        ACTIONS.DASH_CASES_BY_TASK_OVERDUE_FOLLOWUPS,
-          
+          ACTIONS.DASH_CASES_BY_TASK_OVERDUE_CASE_PLAN,
+          ACTIONS.DASH_CASES_BY_TASK_OVERDUE_SERVICES,
+          ACTIONS.DASH_CASES_BY_TASK_OVERDUE_FOLLOWUPS,
           ACTIONS.DASH_APPROVALS_ASSESSMENT,
           ACTIONS.DASH_WORKFLOW_TEAM,
-        ACTIONS.DASH_WORKFLOW,
-        ACTIONS.DASH_REPORTING_LOCATION,
-        ACTIONS.DASH_PROTECTION_CONCERNS]
+          ACTIONS.DASH_WORKFLOW,
+          ACTIONS.DASH_REPORTING_LOCATION,
+          ACTIONS.DASH_PROTECTION_CONCERNS
+        ]
       }
-    },
+    }
+  });
+
+  beforeEach(() => {
+    mountedComponent(<Dashboard />, initialState);
   });
 
   it("should render a <PageContainer /> component", () => {
-    mountedComponent(<Dashboard />, {}, initialState);
     expect(screen.getAllByTestId("page-heading")).toHaveLength(1);
   });
 
   it("should render a navigation title", () => {
-    mountedComponent(<Dashboard />, {}, initialState);
     expect(screen.getByText("navigation.home")).toBeInTheDocument();
   });
 
   it("should render a <PageContent /> component", () => {
-    mountedComponent(<Dashboard />, {}, initialState);
     expect(screen.getByTestId("page-heading")).toBeInTheDocument();
   });
 
   it("should render a dashboard overview component", () => {
-    mountedComponent(<Dashboard />, initialState);
     expect(screen.getByText(/dashboard.overview/i)).toBeInTheDocument();
   });
 
   it("should render a <SharedFromMyTeam /> component", () => {
-    mountedComponent(<Dashboard />, initialState);
     expect(screen.queryAllByText(/dashboard.dash_shared_from_my_team/i)).toHaveLength(3);
   });
 
   it("should render a <SharedWithMyTeam /> component", () => {
-    mountedComponent(<Dashboard />, initialState);
     expect(screen.queryAllByText(/dashboard.dash_shared_with_my_team/i)).toHaveLength(3);
   });
 
   it("should render a <WorkflowIndividualCases /> component", () => {
-    mountedComponent(<Dashboard />, initialState);
     expect(screen.queryAllByText(/dashboard.workflow/i)).toHaveLength(4);
   });
 
   it("should render a <Approvals /> component", () => {
-    mountedComponent(<Dashboard />, initialState);
     expect(screen.getByText(/dashboard.approvals/i)).toBeInTheDocument();
   });
 
   it("should render a <OverdueTasks /> component", () => {
-    mountedComponent(<Dashboard />, initialState);
-    expect(screen.getByText(/dashboard.approvals/i)).toBeInTheDocument();
+    expect(screen.getByText(/dashboard.cases_by_task_overdue/i, { selector: "h4" })).toBeInTheDocument();
   });
 
   it("should render a <WorkflowTeamCases /> component", () => {
-    mountedComponent(<Dashboard />, initialState);
     expect(screen.queryAllByText(/dashboard.workflow_team/i)).toHaveLength(3);
   });
 
   it("should render a <ReportingLocation /> component", () => {
-    mountedComponent(<Dashboard /> ,initialState);
     expect(screen.getByText(/dashboard.overview/i)).toBeInTheDocument();
   });
 
   it("should render a <ProtectionConcern /> component", () => {
-    mountedComponent(<Dashboard />, initialState);
     expect(screen.queryAllByText(/dashboard.protection_concerns/i)).toHaveLength(3);
   });
 });
