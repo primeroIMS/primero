d18472cd80955ef28e9bc95ceecaa55b4eec2f77
Joshua Toliver <jtoliver@quoininc.com>
2024-05-29 10:02:28 -0400

    Refactoring spec

--- app/javascript/components/pages/admin/form-builder/components/custom-field-dialog/component.spec.js
+++ app/javascript/components/pages/admin/form-builder/components/custom-field-dialog/component.spec.js
@@ -26,41 +26,4 @@ describe("<CustomFieldDialog />", () => {
   it("should render the CustomFieldDialog component", () => {
     expect(screen.getByText("fields.add_existing_field")).toBeInTheDocument();
   });
-
-  describe("When is a called from a subform", () => {
-    const initialStateSubform = {
-      ui: {
-        dialogs: {
-          dialog: "custom_field_dialog",
-          open: true
-        }
-      },
-      records: {
-        admin: {
-          forms: {
-            selectedSubformField: {
-              id: 20,
-              description: {
-                en: "Family Details Subform"
-              },
-              unique_id: "family_details_section",
-              type: "subform",
-              fields: []
-            }
-          }
-        }
-      }
-    };
-
-    beforeEach(() => {
-      mountedComponent(<CustomFieldDialog />, initialStateSubform);
-    });
-
-    it("should render only 3 ActionButton", () => {
-      expect(screen.getAllByText("fields.add_field")).toBeTruthy();
-      expect(screen.getAllByText("fields.add_new_field")).toBeTruthy();
-      expect(screen.getAllByText("fields.add_existing_field")).toBeTruthy();
-      expect(screen.getAllByText("buttons.cancel")).toBeTruthy();
-    });
-  });
 });
