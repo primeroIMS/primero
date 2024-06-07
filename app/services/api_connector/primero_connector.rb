03c0dd8db9bc03d22e9b6e23325d6ab6248faaca
Alberto Espinoza <aespinoz@quoininc.com>
2024-06-07 10:38:05 -0600

    R2-2901 - Promote Configuration to multiple target servers
    - Adding support to multiple target host

--- app/services/api_connector/primero_connector.rb
+++ app/services/api_connector/primero_connector.rb
@@ -7,18 +7,22 @@
 # TODO: This is an abstract connector, but it could be made pretty generic and functional.
 # TODO: Consider constraining outbound sends by a Role, like the webhook connector
 class ApiConnector::PrimeroConnector < ApiConnector::AbstractConnector
+  RETRY_LIMIT = 3
+  RETRY_DELAY = 5 # Retry every 5 seconds
   def create(record)
-    # TODO: Retry logic
-    status, response = connection.post(record.class.api_path, params(record))
-    # TODO: Should we log on the record that it was sent?
-    { status:, response: }
+    with_retry(RETRY_LIMIT) do
+      status, response = connection.post(record.class.api_path, params(record))
+      # TODO: Should we log on the record that it was sent?
+      { status:, response: }
+    end
   end
 
   def update(record)
-    # TODO: Retry logic
-    status, response = connection.patch(record.api_path, params(record))
-    # TODO: Should we log on the record that it was sent?
-    { status:, response: }
+    with_retry(RETRY_LIMIT) do
+      status, response = connection.patch(record.api_path, params(record))
+      # TODO: Should we log on the record that it was sent?
+      { status:, response: }
+    end
   end
 
   def syncable?(_record)
@@ -36,5 +40,17 @@ class ApiConnector::PrimeroConnector < ApiConnector::AbstractConnector
 
   def params(_record)
     raise NotImplementedError
+  end
+
+  def with_retry(retry_limit)
+    retry_limit.times do |attempt|
+      Rails.logger.info("Attempt connection. number #{attempt + 1}")
+      return yield
+    rescue StandardError => e
+      raise e if attempt == retry_limit - 1
+
+      sleep(RETRY_DELAY)
+      Rails.logger.info('Conenction Failed, Retrying.')
+    end
   end
 end
--- app/services/primero_configuration_sync_service.rb
+++ app/services/primero_configuration_sync_service.rb
@@ -9,7 +9,7 @@
 class PrimeroConfigurationSyncService
   ENV_PREFIX = 'PRIMERO_PROMOTE_CONFIG_PROD_'
 
-  attr_accessor :connector
+  attr_accessor :connector, :connectors
 
   class << self
     def instance
@@ -18,7 +18,7 @@ class PrimeroConfigurationSyncService
 
     def build
       new.tap do |s|
-        s.connector = ApiConnector::PrimeroConfigurationConnector.build_from_env(prefix: ENV_PREFIX)
+        s.connectors = ApiConnector::PrimeroConfigurationConnector.build_connectors(prefix: ENV_PREFIX)
       end
     end
 
@@ -28,6 +28,14 @@ class PrimeroConfigurationSyncService
   end
 
   def sync!(record)
-    connector.sync(record)
+    connectors.each do |connector|
+      Rails.logger.info("[sync][config]['host'] #{connector.connection.options['host']}")
+      sync_response = connector.sync(record)
+
+      Rails.logger.info("[sync][response][status] #{sync_response[:status]}")
+    rescue StandardError => e
+      puts e
+      next
+    end
   end
 end
