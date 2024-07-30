9cbac16fd6ba65b512e5a308c6473bea79f94a5c
Dennis Hernandez <dhernand@quoininc.com>
2024-07-30 15:31:20 -0600

    R2-2855 - Rearrange Flagging Dashboard

--- app/javascript/components/pages/dashboard/components/referrals/component.jsx
+++ app/javascript/components/pages/dashboard/components/referrals/component.jsx
@@ -2,26 +2,31 @@
 
 import PropTypes from "prop-types";
 
-import Permission, { RESOURCES, ACTIONS } from "../../../../permissions";
+import Permission, { usePermissions, RESOURCES, ACTIONS } from "../../../../permissions";
 import { OptionsBox } from "../../../../dashboard";
 import { DASHBOARD_TYPES } from "../../constants";
 import { useI18n } from "../../../../i18n";
-import { permittedSharedWithMe, dashboardType, filterIndicatorsByKey } from "../../utils";
+import { permittedSharedWithMe, filterIndicatorsByKey } from "../../utils";
 import { getSharedWithMe, getSharedWithOthers } from "../../selectors";
 import { useMemoizedSelector } from "../../../../../libs";
-import css from "../styles.css";
+import DashboardList from "../overview/dashboard-list";
 
 const sharedWithMeIndicators = ["shared_with_me_new_referrals", "shared_with_me_total_referrals"];
 const sharedWithOthersIndicators = ["shared_with_others_referrals"];
 
 function Component({ loadingIndicator, userPermissions }) {
   const i18n = useI18n();
+  const canSeeSharedWithMeIndicators = usePermissions(RESOURCES.cases, [ACTIONS.RECEIVE_REFERRAL, ACTIONS.MANAGE]);
+  const canSeeSharedWithOthers = usePermissions(RESOURCES.dashboards, [ACTIONS.DASH_SHARED_WITH_OTHERS]);
   const sharedWithMe = useMemoizedSelector(state => getSharedWithMe(state));
   const sharedWithOthers = useMemoizedSelector(state => getSharedWithOthers(state));
+
   const referralsDashHasData = Boolean(sharedWithMe.size || sharedWithOthers.size);
 
-  const dashboards = [
-    {
+  const dashboards = [];
+
+  if (canSeeSharedWithMeIndicators) {
+    dashboards.push({
       type: DASHBOARD_TYPES.OVERVIEW_BOX,
       actions: ACTIONS.DASH_SHARED_WITH_ME,
       options: {
@@ -29,8 +34,11 @@ function Component({ loadingIndicator, userPermissions }) {
         sumTitle: i18n.t("dashboard.dash_shared_with_me"),
         withTotal: false
       }
-    },
-    {
+    });
+  }
+
+  if (canSeeSharedWithOthers) {
+    dashboards.push({
       type: DASHBOARD_TYPES.OVERVIEW_BOX,
       actions: ACTIONS.DASH_SHARED_WITH_OTHERS,
       options: {
@@ -38,26 +46,8 @@ function Component({ loadingIndicator, userPermissions }) {
         sumTitle: i18n.t("dashboard.dash_shared_with_others"),
         withTotal: false
       }
-    }
-  ];
-
-  const renderDashboards = () => {
-    return dashboards.map((dashboard, index) => {
-      const { type, actions, options } = dashboard;
-      const Dashboard = dashboardType(type);
-
-      return (
-        <Permission key={actions} resources={RESOURCES.dashboards} actions={actions}>
-          <div className={css.optionsBox}>
-            <OptionsBox flat>
-              <Dashboard {...options} />
-            </OptionsBox>
-          </div>
-          {index === dashboards.length - 1 || <div className={css.divider} />}
-        </Permission>
-      );
     });
-  };
+  }
 
   return (
     <Permission resources={RESOURCES.dashboards} actions={dashboards.map(dashboard => dashboard.actions).flat()}>
@@ -66,7 +56,7 @@ function Component({ loadingIndicator, userPermissions }) {
         hasData={referralsDashHasData || false}
         {...loadingIndicator}
       >
-        <div className={css.container}>{renderDashboards()}</div>
+        <DashboardList dashboards={dashboards} />
       </OptionsBox>
     </Permission>
   );
--- app/javascript/components/pages/dashboard/components/transfers/component.jsx
+++ app/javascript/components/pages/dashboard/components/transfers/component.jsx
@@ -2,46 +2,60 @@
 
 import PropTypes from "prop-types";
 
-import Permission, { RESOURCES, ACTIONS } from "../../../../permissions";
+import Permission, { usePermissions, RESOURCES, ACTIONS } from "../../../../permissions";
 import { OptionsBox } from "../../../../dashboard";
 import { DASHBOARD_TYPES } from "../../constants";
 import { useI18n } from "../../../../i18n";
-import { permittedSharedWithMe, dashboardType, filterIndicatorsByKey } from "../../utils";
+import { permittedSharedWithMe, filterIndicatorsByKey } from "../../utils";
 import { getSharedWithMe, getSharedWithMyTeamOverview, getSharedWithOthers } from "../../selectors";
 import { useMemoizedSelector } from "../../../../../libs";
-import css from "../styles.css";
+import DashboardList from "../overview/dashboard-list";
 
 const sharedWithMeIndicators = ["shared_with_me_transfers_awaiting_acceptance"];
 const sharedWithOthersIndicators = ["shared_with_others_pending_transfers", "shared_with_others_rejected_transfers"];
 
 function Component({ loadingIndicator, userPermissions }) {
   const i18n = useI18n();
-
   const sharedWithMe = useMemoizedSelector(state => getSharedWithMe(state));
   const sharedWithOthers = useMemoizedSelector(state => getSharedWithOthers(state));
   const sharedWithMyTeamOverview = useMemoizedSelector(state => getSharedWithMyTeamOverview(state));
+  const canSeeSharedWithMeTransfers = usePermissions(RESOURCES.cases, [ACTIONS.RECEIVE_TRANSFER, ACTIONS.MANAGE]);
+  const canSeeSharedWithMyTeamDashboard = usePermissions(RESOURCES.dashboards, [
+    ACTIONS.DASH_SHARED_WITH_MY_TEAM_OVERVIEW
+  ]);
+  const canSeeSharedWithOthers = usePermissions(RESOURCES.dashboards, [ACTIONS.DASH_SHARED_WITH_OTHERS]);
 
   const transfersDashHasData = Boolean(sharedWithMe.size || sharedWithOthers.size || sharedWithMyTeamOverview.size);
 
-  const dashboards = [
-    {
+  const dashboards = [];
+
+  const sharedWithMyTeamDashboard = {
+    type: DASHBOARD_TYPES.OVERVIEW_BOX,
+    actions: ACTIONS.DASH_SHARED_WITH_MY_TEAM_OVERVIEW,
+    options: {
+      items: sharedWithMyTeamOverview,
+      sumTitle: i18n.t("dashboard.dash_shared_with_my_team_overview"),
+      withTotal: false
+    }
+  };
+
+  if (canSeeSharedWithMeTransfers) {
+    dashboards.push({
       type: DASHBOARD_TYPES.OVERVIEW_BOX,
       actions: ACTIONS.DASH_SHARED_WITH_ME,
       options: {
         items: filterIndicatorsByKey(permittedSharedWithMe(sharedWithMe, userPermissions), sharedWithMeIndicators),
         sumTitle: i18n.t("dashboard.dash_shared_with_me"),
         withTotal: false,
-        subColumns: [
-          {
-            actions: ACTIONS.DASH_SHARED_WITH_MY_TEAM_OVERVIEW,
-            items: sharedWithMyTeamOverview,
-            sumTitle: i18n.t("dashboard.dash_shared_with_my_team_overview"),
-            withTotal: false
-          }
-        ]
+        subColumns: [{ actions: sharedWithMyTeamDashboard.actions, ...sharedWithMyTeamDashboard.options }]
       }
-    },
-    {
+    });
+  } else if (canSeeSharedWithMyTeamDashboard) {
+    dashboards.push(sharedWithMyTeamDashboard);
+  }
+
+  if (canSeeSharedWithOthers) {
+    dashboards.push({
       type: DASHBOARD_TYPES.OVERVIEW_BOX,
       actions: ACTIONS.DASH_SHARED_WITH_OTHERS,
       options: {
@@ -49,26 +63,8 @@ function Component({ loadingIndicator, userPermissions }) {
         sumTitle: i18n.t("dashboard.dash_shared_with_others"),
         withTotal: false
       }
-    }
-  ];
-
-  const renderDashboards = () => {
-    return dashboards.map((dashboard, index) => {
-      const { type, actions, options } = dashboard;
-      const Dashboard = dashboardType(type);
-
-      return (
-        <Permission key={actions} resources={RESOURCES.dashboards} actions={actions}>
-          <div className={css.optionsBox}>
-            <OptionsBox flat>
-              <Dashboard {...options} />
-            </OptionsBox>
-          </div>
-          {index === dashboards.length - 1 || <div className={css.divider} />}
-        </Permission>
-      );
     });
-  };
+  }
 
   return (
     <Permission resources={RESOURCES.dashboards} actions={dashboards.map(dashboard => dashboard.actions).flat()}>
@@ -77,7 +73,7 @@ function Component({ loadingIndicator, userPermissions }) {
         hasData={transfersDashHasData || false}
         {...loadingIndicator}
       >
-        <div className={css.container}>{renderDashboards()}</div>
+        <DashboardList dashboards={dashboards} />
       </OptionsBox>
     </Permission>
   );
