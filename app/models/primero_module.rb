24dca34c8fe823fad64d8af2e6b95710ee2f64ab
Joshua Toliver <jtoliver@quoininc.com>
2024-08-21 12:56:56 -0400

    Validating primero modules in permitted field service from db ids

--- app/models/primero_module.rb
+++ app/models/primero_module.rb
@@ -76,6 +76,10 @@ class PrimeroModule < ApplicationRecord
     find_by(unique_id: MRM)
   end
 
+  def self.permitted_modules
+    all.map(&:unique_id)
+  end
+
   def form_section_unique_ids
     form_sections.pluck(:unique_id)
   end
