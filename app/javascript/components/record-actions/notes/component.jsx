36fc40700b55e9293c424ef9f9591341e69cb66d
Dennis Hernandez <dhernand@quoininc.com>
2024-10-03 18:47:20 -0600

    Refactor: Move logic to custom hook

--- app/javascript/components/record-actions/notes/component.jsx
+++ app/javascript/components/record-actions/notes/component.jsx
@@ -4,9 +4,7 @@ import { useMemo } from "react";
 import PropTypes from "prop-types";
 import { List } from "immutable";
 import { batch, useDispatch } from "react-redux";
-import { object } from "yup";
 
-import { useApp } from "../../application";
 import { useI18n } from "../../i18n";
 import ActionDialog from "../../action-dialog";
 import Form, { FORM_MODE_DIALOG } from "../../form";
@@ -15,65 +13,16 @@ import { ACTIONS } from "../../permissions";
 import { fetchAlerts } from "../../nav/action-creators";
 import { NOTES_DIALOG } from "../constants";
 import { useMemoizedSelector } from "../../../libs";
-import { getSubFormForFieldName } from "../../record-form/selectors";
-import { RECORD_TYPES } from "../../../config";
-import { fieldValidations } from "../../record-form/form/validations";
-import displayConditionsEnabled from "../../record-form/form/utils/display-conditions-enabled";
-import getDisplayConditions from "../../record-form/form/utils/get-display-conditions";
-import { parseExpression } from "../../../libs/expressions";
 
 import { NAME, FORM_ID } from "./constants";
-
-const HIDDEN_FIELD_NAMES = ["note_date", "note_created_by"];
+import useNotesForm from "./use-notes-form";
 
 function Component({ close, open, pending, record, recordType, setPending, primeroModule }) {
   const i18n = useI18n();
   const dispatch = useDispatch();
-  const { online } = useApp();
-
-  const notesSubform = useMemoizedSelector(state =>
-    getSubFormForFieldName(state, {
-      recordType: RECORD_TYPES[recordType],
-      primeroModule,
-      fieldName: "notes_section",
-      checkVisible: false
-    })
-  );
-
   const recordAlerts = useMemoizedSelector(state => getRecordAlerts(state, recordType));
-
-  const formSection = useMemo(() => {
-    const fields = notesSubform?.fields?.map(field => {
-      if (HIDDEN_FIELD_NAMES.includes(field.name)) {
-        return field.merge({ visible: false });
-      }
-
-      if (displayConditionsEnabled(field.display_conditions_subform)) {
-        return field.merge({
-          watchedInputs: notesSubform.fields
-            .filter(subformField => !HIDDEN_FIELD_NAMES.includes(subformField.name))
-            .map(watchedField => watchedField.name),
-          showIf: values => parseExpression(getDisplayConditions(field.display_conditions_subform)).evaluate(values)
-        });
-      }
-
-      return field;
-    });
-
-    return notesSubform?.set("fields", fields);
-  }, [notesSubform]);
-
+  const { validationSchema, formSection } = useNotesForm({ recordType, primeroModule });
   const formSections = useMemo(() => (formSection ? List([formSection]) : List([])), [formSection]);
-
-  const validationSchema = useMemo(
-    () =>
-      object().shape(
-        formSection?.fields
-          ?.filter(field => !field.disabled)
-          ?.reduce((acc, field) => ({ ...acc, ...fieldValidations(field, { i18n, online }) }), {})
-      ),
-    [formSection]
-  );
 
   const handleSubmit = data => {
     setPending(true);
