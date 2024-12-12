f22844e69eeff0f81bb58a282dc915b0433df42f
Dennis Hernandez <dhernand@quoininc.com>
2024-12-12 16:29:57 -0600

    R2-3107 - Add more tests

--- spec/models/concerns/normalizeable_spec.rb
+++ spec/models/concerns/normalizeable_spec.rb
@@ -61,6 +61,15 @@ describe Normalizeable do
       expect(numerics.size).to eq(1)
       expect(numerics.first.value).to eq(5)
     end
+
+    it 'updates a searchable numeric for age' do
+      child.age = 8
+      child.save!
+
+      numerics = SearchableNumeric.where(record_id: child.id, record_type: Child.name, field_name: 'age')
+      expect(numerics.size).to eq(1)
+      expect(numerics.first.value).to eq(8)
+    end
   end
 
   describe 'searchable_datetimes' do
@@ -113,6 +122,37 @@ describe Normalizeable do
       values = SearchableDatetime.where(record_id: child.id, record_type: Child.name, field_name: 'service_due_dates')
       expect(values.size).to eq(1)
       expect(values.map(&:value)).to match_array([Time.zone.parse('2022-01-15T07:20:05.000Z')])
+    end
+
+    it 'does not create duplicates' do
+      child.services_section = [
+        {
+          'unique_id' => 'de9eba6a-b7dc-11ef-a44d-18c04db5c362',
+          'service_response_day_time' => Time.zone.parse('2012-12-01T08:48:01.000Z'),
+          'service_response_timeframe' => '1_day'
+        },
+        {
+          'unique_id' => 'f23b85ee-b7dc-11ef-8870-18c04db5c362',
+          'service_response_day_time' => Time.zone.parse('2022-01-10T07:20:05.000Z'),
+          'service_response_timeframe' => '5_day'
+        },
+        {
+          'unique_id' => '95a4c030-b8d5-11ef-8249-18c04db5c362',
+          'service_response_day_time' => Time.zone.parse('2023-12-05T15:12:02.000Z'),
+          'service_response_timeframe' => '2_day'
+        }
+      ]
+      child.save!
+
+      values = SearchableDatetime.where(record_id: child.id, record_type: Child.name, field_name: 'service_due_dates')
+      expect(values.size).to eq(3)
+      expect(values.map(&:value)).to match_array(
+        [
+          Time.zone.parse('2012-12-02T08:48:01.000Z'),
+          Time.zone.parse('2022-01-15T07:20:05.000Z'),
+          Time.zone.parse('2023-12-07T15:12:02.000Z')
+        ]
+      )
     end
   end
 end
