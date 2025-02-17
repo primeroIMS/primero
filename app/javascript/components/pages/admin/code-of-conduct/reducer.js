c7f7b966b897c62ca40672579278bfbd96caa139
Dennis Hernandez <dhernand@quoininc.com>
2025-02-17 14:04:14 -0600

    R2-3192 - UAT - Admins can create code of conduct if none exists

--- app/javascript/components/pages/admin/code-of-conduct/reducer.js
+++ app/javascript/components/pages/admin/code-of-conduct/reducer.js
@@ -10,10 +10,22 @@ export default (state = DEFAULT_STATE, { type, payload }) => {
   switch (type) {
     case actions.FETCH_CODE_OF_CONDUCT_STARTED:
     case actions.SAVE_CODE_OF_CONDUCT_STARTED:
-      return state.set("loading", true);
+      return state.set("loading", true).set("errors", false).set("fetchErrors", fromJS([]));
     case actions.FETCH_CODE_OF_CONDUCT_SUCCESS:
     case actions.SAVE_CODE_OF_CONDUCT_SUCCESS:
       return state.set("data", fromJS(payload.data));
+    case actions.SAVE_CODE_OF_CONDUCT_FAILURE: {
+      return state.set("loading", false).set("errors", true);
+    }
+    case actions.FETCH_CODE_OF_CONDUCT_FAILURE: {
+      const failureState = state.set("loading", false).set("errors", true);
+
+      if (payload.errors) {
+        return failureState.set("fetchErrors", fromJS(payload.errors));
+      }
+
+      return failureState;
+    }
     case actions.FETCH_CODE_OF_CONDUCT_FINISHED:
     case actions.SAVE_CODE_OF_CONDUCT_FINISHED:
       return state.set("loading", false);
--- app/javascript/components/pages/admin/code-of-conduct/selectors.js
+++ app/javascript/components/pages/admin/code-of-conduct/selectors.js
@@ -4,4 +4,7 @@ import { fromJS } from "immutable";
 
 export const getCodeOfConduct = state => state.getIn(["records", "codeOfConduct", "data"], fromJS({}));
 
+export const getFetchErrorsCodeOfConduct = state =>
+  state.getIn(["records", "codeOfConduct", "fetchErrors"], fromJS([]));
+
 export const getLoadingCodeOfConduct = state => state.getIn(["records", "codeOfConduct", "loading"], false);
