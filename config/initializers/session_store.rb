4fa811614e01b6aa3786e6aa0f6bd38e24fe06ca
Dennis Hernandez <dhernand@quoininc.com>
2025-02-25 15:23:17 -0600

    R2-3182 - Make session cookie strict

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,5 +2,6 @@
 
 # Copyright (c) 2014 - 2023 UNICEF. All rights reserved.
 
-Rails.application.config.session_store :active_record_store, key: '_app_session', expire_after: 1.hour
+Rails.application.config.session_store :active_record_store, key: '_app_session', expire_after: 1.hour,
+                                                             same_site: 'Strict'
 ActiveRecord::SessionStore::Session.serializer = :null
