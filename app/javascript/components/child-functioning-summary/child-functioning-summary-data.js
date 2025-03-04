9a3554b19d3c08abc0ddfc029587ef4fb8467c49
afrah <afrah.kariyadan@trigyn.com>
2025-03-04 14:42:32 +0530

    translation added

--- app/javascript/components/child-functioning-summary/child-functioning-summary-data.js
+++ app/javascript/components/child-functioning-summary/child-functioning-summary-data.js
@@ -11,7 +11,7 @@ const childFunctioningSummaryData = [
     key: latestValue => `cfm_${latestValue?.cfm_age}_vision_difficulty_with_glasses`
   },
   {
-    label: "cases.continue_hearing_section",
+    label: "cases.child_functioning.continue_hearing_section",
     key: latestValue => `cfm_${latestValue?.cfm_age}_hearing_display`
   },
   {
@@ -23,11 +23,11 @@ const childFunctioningSummaryData = [
     key: latestValue => `cfm_${latestValue?.cfm_age}_hearing_difficulty_with_hearing_aid`
   },
   {
-    label: "cases.continue_mobility_section",
+    label: "cases.child_functioning.continue_mobility_section",
     key: latestValue => `cfm_${latestValue?.cfm_age}_mobility_display`
   },
   {
-    label: "cases.child_functioning.child_uses_equipment_or_receive_assistance_for_walking",
+    label: "cases.child_functioning.child_functioning.child_uses_equipment_or_receive_assistance_for_walking",
     key: latestValue => `cfm_${latestValue?.cfm_age}_mobility_uses_equipment`
   },
   {
@@ -35,11 +35,11 @@ const childFunctioningSummaryData = [
     key: latestValue => `cfm_${latestValue?.cfm_age}_mobility_difficulty_without_equipment`
   },
   {
-    label: "cases.mobility_difficulty_without_equipment_100m",
+    label: "cases.child_functioning.mobility_difficulty_without_equipment_100m",
     key: latestValue => `cfm_${latestValue?.cfm_age}_mobility_difficulty_without_equipment_100m`
   },
   {
-    label: "cases.mobility_difficulty_without_equipment_500m",
+    label: "cases.child_functioning.mobility_difficulty_without_equipment_500m",
     key: latestValue => `cfm_${latestValue?.cfm_age}_mobility_difficulty_without_equipment_500m`
   },
   {
@@ -47,15 +47,15 @@ const childFunctioningSummaryData = [
     key: latestValue => `cfm_${latestValue?.cfm_age}_mobility_difficulty_with_equipment`
   },
   {
-    label: "cases.mobility_difficulty_with_equipment_100m",
+    label: "cases.child_functioning.mobility_difficulty_with_equipment_100m",
     key: latestValue => `cfm_${latestValue?.cfm_age}_mobility_difficulty_with_equipment_100m`
   },
   {
-    label: "cases.mobility_difficulty_with_equipment_500m",
+    label: "cases.child_functioning.mobility_difficulty_with_equipment_500m",
     key: latestValue => `cfm_${latestValue?.cfm_age}_mobility_difficulty_with_equipment_500m`
   },
   {
-    label: "cases.continue_dexterity_display_section",
+    label: "cases.child_functioning.continue_dexterity_display_section",
     key: latestValue => `cfm_${latestValue?.cfm_age}_dexterity_display`
   },
   {
@@ -63,15 +63,15 @@ const childFunctioningSummaryData = [
     key: latestValue => `cfm_${latestValue?.cfm_age}_dexterity_difficulty`
   },
   {
-    label: "cases.self_care_display",
+    label: "cases.child_functioning.self_care_display",
     key: latestValue => `cfm_${latestValue?.cfm_age}_self_care_display`
   },
   {
-    label: "cases.self_care_difficulty",
+    label: "cases.child_functioning.self_care_difficulty",
     key: latestValue => `cfm_${latestValue?.cfm_age}_self_care_difficulty`
   },
   {
-    label: "cases.communication_display_section",
+    label: "cases.child_functioning.communication_display_section",
     key: latestValue => `cfm_${latestValue?.cfm_age}_communication_display`
   },
   {
@@ -79,11 +79,11 @@ const childFunctioningSummaryData = [
     key: latestValue => `cfm_${latestValue?.cfm_age}_communication_difficulty_understanding_you`
   },
   {
-    label: "cases.child_have_difficulty_to_understanding_child_household",
+    label: "cases.child_functioning.child_have_difficulty_to_understanding_child_household",
     key: latestValue => `cfm_${latestValue?.cfm_age}_communication_difficulty_understanding_child_household`
   },
   {
-    label: "cases.difficulty_to_understanding_child_others",
+    label: "cases.child_functioning.difficulty_to_understanding_child_others",
     key: latestValue => `cfm_${latestValue?.cfm_age}_communication_difficulty_understanding_child_others`
   },
   {
@@ -91,7 +91,7 @@ const childFunctioningSummaryData = [
     key: latestValue => `cfm_${latestValue?.cfm_age}_communication_difficulty_understanding_child`
   },
   {
-    label: "cases.learning_display_section",
+    label: "cases.child_functioning.learning_display_section",
     key: latestValue => `cfm_${latestValue?.cfm_age}_learning_display`
   },
   {
@@ -99,39 +99,39 @@ const childFunctioningSummaryData = [
     key: latestValue => `cfm_${latestValue?.cfm_age}_learning_difficulty`
   },
   {
-    label: "cases.playing_display_section",
+    label: "cases.child_functioning.playing_display_section",
     key: latestValue => `cfm_${latestValue?.cfm_age}_playing_display`
   },
   {
-    label: "cases.child_functioning.child_have_difficulty_for_playing",
+    label: "cases.child_functioning.child_functioning.child_have_difficulty_for_playing",
     key: latestValue => `cfm_${latestValue?.cfm_age}_playing_difficulty`
   },
   {
-    label: "cases.remembering_display",
+    label: "cases.child_functioning.remembering_display",
     key: latestValue => `cfm_${latestValue?.cfm_age}_remembering_display`
   },
   {
-    label: "cases.remembering_difficulty",
+    label: "cases.child_functioning.remembering_difficulty",
     key: latestValue => `cfm_${latestValue?.cfm_age}_remembering_difficulty`
   },
   {
-    label: "cases.concentrating_display",
+    label: "cases.child_functioning.concentrating_display",
     key: latestValue => `cfm_${latestValue?.cfm_age}_concentrating_display`
   },
   {
-    label: "cases.concentrating_difficulty",
+    label: "cases.child_functioning.concentrating_difficulty",
     key: latestValue => `cfm_${latestValue?.cfm_age}_concentrating_difficulty`
   },
   {
-    label: "cases.accepting_change_display",
+    label: "cases.child_functioning.accepting_change_display",
     key: latestValue => `cfm_${latestValue?.cfm_age}_accepting_change_display`
   },
   {
-    label: "cases.accepting_change_difficulty",
+    label: "cases.child_functioning.accepting_change_difficulty",
     key: latestValue => `cfm_${latestValue?.cfm_age}_accepting_change_difficulty`
   },
   {
-    label: "cases.controlling_behavior_display_section",
+    label: "cases.child_functioning.controlling_behavior_display_section",
     key: latestValue => `cfm_${latestValue?.cfm_age}_controlling_behavior_display`
   },
   {
@@ -139,27 +139,27 @@ const childFunctioningSummaryData = [
     key: latestValue => `cfm_${latestValue?.cfm_age}_controlling_behavior_difficulty`
   },
   {
-    label: "cases.making_friends_display_section",
+    label: "cases.child_functioning.making_friends_display_section",
     key: latestValue => `cfm_${latestValue?.cfm_age}_making_friends_display`
   },
   {
-    label: "cases.making_friends_difficulty",
+    label: "cases.child_functioning.making_friends_difficulty",
     key: latestValue => `cfm_${latestValue?.cfm_age}_making_friends_difficulty`
   },
   {
-    label: "cases.anxiety_display_section",
+    label: "cases.child_functioning.anxiety_display_section",
     key: latestValue => `cfm_${latestValue?.cfm_age}_anxiety_display`
   },
   {
-    label: "cases.anxiety_how_often",
+    label: "cases.child_functioning.anxiety_how_often",
     key: latestValue => `cfm_${latestValue?.cfm_age}_anxiety_how_often`
   },
   {
-    label: "cases.depression_display_section",
+    label: "cases.child_functioning.depression_display_section",
     key: latestValue => `cfm_${latestValue?.cfm_age}_depression_display`
   },
   {
-    label: "cases.depression_how_often",
+    label: "cases.child_functioning.depression_how_often",
     key: latestValue => `cfm_${latestValue?.cfm_age}_depression_how_often`
   }
 ];
