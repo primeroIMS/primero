e823ea9feb16a9375d7427c9afdaa83f463ad109
Joshua Toliver <jtoliver@quoininc.com>
2025-03-21 13:31:48 -0400

    Fixing merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2024_12_19_000000) do
-=======
 ActiveRecord::Schema.define(version: 2025_02_21_221207) do
->>>>>>> 07f80df2eb182305306ee3ec1f4c685daa674f08
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
