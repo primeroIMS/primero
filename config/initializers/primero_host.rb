da44a378c63d5026cdc65a270ade7d5440717aca
Joshua Toliver <jtoliver@quoininc.com>
2025-03-25 13:22:21 -0400

    Fixing conflict

--- config/initializers/primero_host.rb
+++ config/initializers/primero_host.rb
@@ -3,12 +3,8 @@
 # Copyright (c) 2014 - 2023 UNICEF. All rights reserved.
 
 Rails.application.config.before_initialize do
-<<<<<<< Updated upstream
-  primero_host = ENV['PRIMERO_HOST'] || ENV['LETS_ENCRYPT_DOMAIN'] || 'localhost'
-=======
   primero_host = ENV['PRIMERO_HOST'] || ENV['LETS_ENCRYPT_DOMAIN'] || 'localhost:3000'
   Rails.application.config.force_ssl ? 'https://' : 'http://'
 
->>>>>>> Stashed changes
   Rails.application.routes.default_url_options[:host] = primero_host
 end
