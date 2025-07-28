6498e4bbd558d9a9a8cf130c2f1281d9425efbbd
Alberto Espinoza <aespinoz@quoininc.com>
2025-07-28 16:35:30 -0600

    R2-3419 - Fix broken jest test

--- app/javascript/components/form-filters/components/access-log/utils.unit.test.js
+++ app/javascript/components/form-filters/components/access-log/utils.unit.test.js
@@ -5,7 +5,9 @@ import { getFilters } from "./utils";
 describe("<RecordForms /> - utils", () => {
   describe("getFilters", () => {
     it("returns filters for access_logs form", () => {
-      expect(getFilters(() => {}).map(filter => filter.name)).toEqual([
+      const i18n = { t: t => t };
+
+      expect(getFilters(i18n).map(filter => filter.name)).toEqual([
         "access_log.filters.actions",
         "access_log.filters.timestamp"
       ]);
