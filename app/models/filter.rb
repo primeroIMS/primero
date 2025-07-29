958f6a8b0eae38fece3cec307bc06988540dacdc
Awen Saunders <awen@jozian.com>
2025-07-29 21:18:54 +0100

    Fix incorrect formatting

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -756,7 +756,8 @@ class Filter < ValueObject
     self.options = I18n.available_locales.map do |locale|
       locale_options = [registration_date_options(locale), assessment_requested_on_options(locale),
                         date_case_plan_options(locale), date_closure_options(locale), followup_date_options(locale),
-                        date_reunification_options(locale), tracing_date_options(locale), service_date_options(locale), reassigned_transferred_on_date_options(locale)]
+                        date_reunification_options(locale), tracing_date_options(locale), service_date_options(locale),
+                        reassigned_transferred_on_date_options(locale)]
       date_label = opts[:user].gbv? ? 'created_at' : 'date_of_creation'
       locale_options << created_at_options(locale, date_label)
       { locale => locale_options }
