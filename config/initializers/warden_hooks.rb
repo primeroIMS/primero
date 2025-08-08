6a5d2c79e137b412ba7bbd10db1110b808241a13
Alberto Espinoza <aespinoz@quoininc.com>
2025-08-08 13:05:27 -0600

    R2-3422 - Debug config prom

--- config/initializers/warden_hooks.rb
+++ config/initializers/warden_hooks.rb
@@ -8,8 +8,20 @@ Warden::Manager.before_failure do |env, _opts|
   user_name = request.params.dig('user', 'user_name')
   user = User.find_by(user_name:)
 
+  debug_hash = {
+    params: request.params,
+    headers: request.headers.env.select { |k, _| k.start_with?('HTTP_') },
+    cookies: request.cookies,
+    fullpath: request.fullpath,
+    method: request.request_method,
+    ip: request.remote_ip
+  }
+
+  Rails.logger.info('[AUDIT] Login failed: request=')
+  Rails.logger.info(debug_hash.to_json)
+
   AuditLogJob.perform_later(
-    record_type: User.class.name,
+    record_type: User.name,
     record_id: user&.id,
     action: 'failed_login',
     user_id: user&.id,
