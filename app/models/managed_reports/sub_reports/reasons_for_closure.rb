197d4323fc7b32922f5a0520606b67df37ba4bb2
Dennis Hernandez <dhernand@quoininc.com>
2025-11-14 09:44:32 -0600

    Do not use the field closure_reason from the GBV Module.

--- app/models/managed_reports/sub_reports/reasons_for_closure.rb
+++ app/models/managed_reports/sub_reports/reasons_for_closure.rb
@@ -23,11 +23,11 @@ class ManagedReports::SubReports::ReasonsForClosure < ManagedReports::SubReport
   private
 
   def closure_reasons_options
-    field = Field.joins(form_section: :primero_modules).find_by(
+    field = Field.joins(form_section: :primero_modules).where.not(
+      form_sections: { primero_modules: { unique_id: PrimeroModule::GBV } }
+    ).find_by(
       name: 'closure_reason',
-      form_sections: {
-        parent_form: PrimeroModelService.to_name(Child.name), primero_modules: { unique_id: PrimeroModule::CP }
-      }
+      form_sections: { parent_form: PrimeroModelService.to_name(Child.name) }
     )
     return [] unless field.present? && field.option_strings_text_i18n.present?
 
