e579b6c30d1f196a97cdef7b8a42f55c7a6620b9
Dennis Hernandez <dhernand@quoininc.com>
2025-12-18 14:56:38 -0600

    Fix copyright

--- db/data_migration/v2.14.1/generate_unused_fields_report.rb
+++ db/data_migration/v2.14.1/generate_unused_fields_report.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Copyright (c) 2014 - 2023 UNICEF. All rights reserved.
+# Copyright (c) 2014 - 2025 UNICEF. All rights reserved.
 
 # Example of usage:
 # rails r generate_unused_fields_report.rb true
