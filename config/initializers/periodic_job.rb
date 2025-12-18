05f3ef7bf44241269d431057fd05c387f0c41ad7
Dennis Hernandez <dhernand@quoininc.com>
2025-12-17 18:32:13 -0600

    Fix periodic_job initializer for Unused Fields Report

--- config/initializers/periodic_job.rb
+++ config/initializers/periodic_job.rb
@@ -9,7 +9,7 @@ return unless HealthCheckService.database_accessible? && ActiveRecord::Base.conn
 
 Rails.logger.info('Setting up PeriodicJobs')
 
-jobs = %w[ArchiveBulkExports RecalculateAge UnusedFieldsReport]
+jobs = %w[ArchiveBulkExports RecalculateAge GenerateUnusedFieldsReport]
 jobs << 'OptimizeSolr' if Rails.configuration.solr_enabled
 jobs << 'SessionCleanup' unless Rails.configuration.x.idp.use_identity_provider
 
