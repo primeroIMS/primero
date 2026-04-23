563d0cc9cb65fc8f49950daaf8e7bd15e04dc81f
Dennis Hernandez <dhernand@quoininc.com>
2026-04-23 08:31:30 -0600

    Remove copyright header

--- db/migrate/20260331000000_add_identifier_type_to_searchable_identifiers.rb
+++ db/migrate/20260331000000_add_identifier_type_to_searchable_identifiers.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# Copyright (c) 2014 - 2026 UNICEF. All rights reserved.
 class AddIdentifierTypeToSearchableIdentifiers < ActiveRecord::Migration[8.1]
   def change
     add_column :searchable_identifiers, :identifier_type, :string
