7ce856d639baa9f83a04003dd6974b44d14688d1
Joshua Toliver <jtoliver@quoininc.com>
2026-05-01 14:19:54 -0400

    Fixing broken unit test

--- app/javascript/test-utils/setup.js
+++ app/javascript/test-utils/setup.js
@@ -3,6 +3,7 @@
 import "./globals";
 import "@testing-library/jest-dom";
 import { MessageChannel } from "worker_threads";
+import { TextEncoder, TextDecoder } from "util";
 
 import { createMocks } from "react-idle-timer";
 import { cleanup } from "@testing-library/react";
@@ -10,6 +11,8 @@ import { cleanup } from "@testing-library/react";
 import db from "../db";
 
 global.IS_REACT_ACT_ENVIRONMENT = true;
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 
 beforeAll(() => {
   // eslint-disable-next-line no-console
