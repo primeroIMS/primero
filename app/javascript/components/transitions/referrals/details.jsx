a24e4030967ae6dad2df6124e517d0c677e7aa69
Dennis Hernandez <dhernand@quoininc.com>
2026-09-11 10:54:41 -0600

    R2-3936 - Makes sure the referral subform gets updated after a revoke

--- app/javascript/components/transitions/referrals/details.jsx
+++ app/javascript/components/transitions/referrals/details.jsx
@@ -43,7 +43,7 @@ function Details({ transition, classes }) {
     : null;
 
   const successfulReferral =
-    transition.status === TRANSITION_STATUS.done &&
+    [TRANSITION_STATUS.done, TRANSITION_STATUS.revoked].includes(transition.status) &&
     transition.data?.success_status === REFERRAL_SUCCESS_STATUS.successful;
 
   const agencyName = referralAgencyName(transition, agencies);
