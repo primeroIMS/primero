94c668e5a786bb7d748349d3dc55bf17ea8f7d80
Joshua Toliver <jtoliver@quoininc.com>
2026-09-16 10:53:58 -0400

    Reverting back to pinned postgres docker image versions

--- docker/build.sh
+++ docker/build.sh
@@ -49,8 +49,8 @@ BUILD_SOLR="docker build -f solr/Dockerfile ../ -t primero/solr:${tag} -t ${repo
 BUILD_APP="docker build -f application/Dockerfile ../ -t primero/application:${tag} -t ${repository}/primero/application:${tag} --build-arg APP_ROOT=${APP_ROOT} --build-arg RAILS_LOG_PATH=${RAILS_LOG_PATH} --build-arg APP_UID=${APP_UID} --build-arg APP_GID=${APP_GID} --build-arg BUILD_REGISTRY=${build_registry}"
 BUILD_WORKER="docker build -f application/Dockerfile ../ -t primero/worker:${tag} -t ${repository}/primero/worker:${tag} --build-arg APP_ROOT=${APP_ROOT} --build-arg RAILS_LOG_PATH=${RAILS_LOG_PATH} --build-arg APP_UID=${APP_UID} --build-arg APP_GID=${APP_GID} --build-arg BUILD_REGISTRY=${build_registry} --build-arg IS_WORKER=true"
 BUILD_MIGRATION="docker build -f migration/Dockerfile ../ -t primero/migration:${tag} -t ${repository}/primero/migration:${tag} --build-arg BUILD_REGISTRY=${build_registry} --build-arg PRIMERO_VERSION=${tag}"
-BUILD_POSTGRES14="docker build -f postgres/Dockerfile . -t primero/postgres:${tag}-pg14 -t ${repository}/primero/postgres:${tag}-pg14 --build-arg BUILD_REGISTRY=${build_registry} --build-arg POSTGRES_VERSION=14.24 --build-arg ALPINE_VERSION=3.24"
-BUILD_POSTGRES15="docker build -f postgres/Dockerfile . -t primero/postgres:${tag}-pg15 -t ${repository}/primero/postgres:${tag}-pg15 --build-arg BUILD_REGISTRY=${build_registry} --build-arg POSTGRES_VERSION=15.19 --build-arg ALPINE_VERSION=3.24"
+BUILD_POSTGRES14="docker build -f postgres/Dockerfile . -t primero/postgres:${tag}-pg14 -t ${repository}/primero/postgres:${tag}-pg14 --build-arg BUILD_REGISTRY=${build_registry} --build-arg POSTGRES_VERSION=14.11 --build-arg ALPINE_VERSION=3.19"
+BUILD_POSTGRES15="docker build -f postgres/Dockerfile . -t primero/postgres:${tag}-pg15 -t ${repository}/primero/postgres:${tag}-pg15 --build-arg BUILD_REGISTRY=${build_registry} --build-arg POSTGRES_VERSION=15.6 --build-arg ALPINE_VERSION=3.19"
 
 apply_tags () {
   local image=${1}
